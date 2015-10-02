be5ecdbff0415b7803979faf204ba8daee5c4843
caneballante <jonbridgman@gmail.com>
2015-10-01 19:00:38 -0700

    NAV BAR
    big change!
    I made the nav bar persistent blue. I commented out the on scroll
    jquery function and deleted the .top-nav-collapse class cause that a
    stupid name.

--- js/custom.js
+++ js/custom.js
@@ -1,35 +1,32 @@
 // JavaScript Document
 
 // jQuery to collapse the navbar on scroll
-$(window).scroll(function() {
-    if ($(".navbar").offset().top > 50) {
-        $(".navbar-fixed-top").addClass("top-nav-collapse");
+//$(window).scroll(function() {
+//    if ($(".navbar").offset().top > 50) {
+//        $(".navbar-fixed-top").addClass("top-nav-collapse");
+//
+//    } else {
+ //       $(".navbar-fixed-top").removeClass("top-nav-collapse");
+//    }
+//});
 
-    } else {
-        $(".navbar-fixed-top").removeClass("top-nav-collapse");
-    }
-});
 // jQuery to change the menu button on window resize
 $(window).resize(function() {
 	 if ( $(window).width() < 739) {     
 	   $(".menu_button").hide();
 	   $(".menu_button_small").show();
-	   console.log("i'm hiding");
 	}
 	else {
 	   $(".menu_button").show();
 	   $(".menu_button_small").hide();
-	   console.log("i'm showing");
 	}
 });
 // jQuery to detect window size on first load
  if ( $(window).width() < 739) {     
 	   $(".menu_button").hide();
 	   $(".menu_button_small").show();
-	   console.log("i'm hiding");
 	}
 	else {
 	   $(".menu_button").show();
 	   $(".menu_button_small").hide();
-	   console.log("i'm showing");
 	}
