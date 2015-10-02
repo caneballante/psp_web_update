b5cea037a6c3f11c2cbe1591daebf2de9408eeee
caneballante <jonbridgman@gmail.com>
2015-10-01 17:51:00 -0700

    NAV BAR
    Added javascipt to change the nav smaller on smaller screen sizes -
    still need to fix that it pops to the left

--- js/custom.js
+++ js/custom.js
@@ -4,9 +4,32 @@
 $(window).scroll(function() {
     if ($(".navbar").offset().top > 50) {
         $(".navbar-fixed-top").addClass("top-nav-collapse");
-		console.log("i pooped");
+
     } else {
         $(".navbar-fixed-top").removeClass("top-nav-collapse");
-		console.log("i peed");
     }
 });
+// jQuery to change the menu button on window resize
+$(window).resize(function() {
+	 if ( $(window).width() < 739) {     
+	   $(".menu_button").hide();
+	   $(".menu_button_small").show();
+	   console.log("i'm hiding");
+	}
+	else {
+	   $(".menu_button").show();
+	   $(".menu_button_small").hide();
+	   console.log("i'm showing");
+	}
+});
+// jQuery to detect window size on first load
+ if ( $(window).width() < 739) {     
+	   $(".menu_button").hide();
+	   $(".menu_button_small").show();
+	   console.log("i'm hiding");
+	}
+	else {
+	   $(".menu_button").show();
+	   $(".menu_button_small").hide();
+	   console.log("i'm showing");
+	}
