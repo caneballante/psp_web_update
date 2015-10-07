360ac2c034209676cf0b4fd555971f1752f735bf
caneballante <jonbridgman@gmail.com>
2015-10-07 11:58:55 -0700

    DELETE and MODAL INCLUDE
    I deleted a whole bunch of files so I could send erika something a
    little cleaner
    
    I set up MAMP and took the modal out as an include

---
+++ Untitled-17.php
@@ -0,0 +1,144 @@
+<!DOCTYPE html>
+<html lang="en"><!-- InstanceBegin template="/Templates/page-4-template.dwt" codeOutsideHTMLIsLocked="false" -->
+<head>
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<!-- InstanceBeginEditable name="doctitle" -->
+<title>Puget Sound Partnership - Draft Home Page</title>
+<!-- InstanceEndEditable -->
+<!-- Bootstrap -->
+<link href="css/bootstrap.css" rel="stylesheet">
+<link href="css/custom.css" rel="stylesheet" type="text/css">
+<!-- loads the wf-loading class right away to minimize FOUT -->
+<script>document.documentElement.className += ' wf-loading';</script>
+<!-- Font PRENTON TYPEKIT -->
+<script src="https://use.typekit.net/srt5jze.js"></script>
+<script>try{Typekit.load({ async: true });}catch(e){}</script>
+<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+<!--[if lt IE 9]>
+	<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+	<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+<!-- InstanceBeginEditable name="head" -->
+<!-- InstanceEndEditable -->
+<!-- InstanceParam name="OptionalRegion1" type="boolean" value="true" -->
+<!-- InstanceParam name="OptionalRegion2" type="boolean" value="false" -->
+<!-- InstanceParam name="OptionalRegion3" type="boolean" value="true" -->
+<!-- InstanceParam name="OptionalRegion4" type="boolean" value="false" -->
+<!-- InstanceParam name="OptionalRegion5" type="boolean" value="true" -->
+<!-- InstanceParam name="OptionalRegion6" type="boolean" value="true" -->
+<!-- InstanceParam name="OptionalRegion7" type="boolean" value="false" -->
+</head>
+<body>
+<?php include 'includes/modal-inc.html';?>
+<!-- START IMAGE HEADER --> 
+ <!-- InstanceBeginEditable name="overviewphoto" -->
+<header class="overview-page-image-style overview-page-image1"> </header>
+<!-- InstanceEndEditable -->  
+<div class="container page-content">
+	<div class="row">
+		<div class="col-md-3 padding-20-top"></div>
+		<div class="col-md-6 padding-20-top"> <!-- InstanceBeginEditable name="6col_header" -->
+			<h1>SCIENCE BASIS FOR RECOVERY</h1>
+			<!-- InstanceEndEditable --></div>
+		<div class="col-md-3 padding-20-top"></div>
+	</div>
+	
+	<div class="row"> 
+		<div class="col-md-3 padding-20-top">
+			<ul class="nav nav-stacked nav-pills nav-leftside-custom">
+			<!-- InstanceBeginEditable name="left_nav" -->
+				<li class="active" role="presentation"> <a href="#">SCIENCE BASIS OF RECOVERY</a></li>
+				<li role="presentation"> <a href="#">STRATEGIC SCIENCE PLAN</a></li>
+				<li role="presentation"> <a href="#">SCIENCE WORKPLAN</a></li>
+				<li role="presentation"> <a href="#">ADAPTIVE MANAGEMENT WORKPLAN</a></li>
+				<li role="presentation"> <a href="#">PUGET SOUND PRESSURE ASSESSMENT</a></li>
+				<li role="presentation"> <a href="#">PUGET SOUND SCIENCE UPDATES</a></li>
+		
+			<!-- InstanceEndEditable --> 
+			</ul>
+			
+		</div>
+	
+		<div class="col-md-6 padding-20-top">
+		<!-- InstanceBeginEditable name="6col_content" -->
+			<p>The Puget Sound Partnership is developing a strategic science program to support the science-based recovery of the Puget Sound ecosystem by 2020. </p>
+			<h2>STATE OF THE SOUND</h2>
+			<p>The Partnership is required to produce a State of the Sound report every two years. The statutory reporting requirements are to document the current status of the ecosystem, as well as status of implementation and funding. This information can be used to inform decisions about changes to funding, programs, or policies that might accelerate the regional progress towards ecosystem recovery, including more efficient use of resources. LEARN MORE</p>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur felis tellus, condimentum non leo eget, sodales euismod dolor. Integer porta mauris odio, a pulvinar nisl tristique nec. Sed mollis lorem nec sem condimentum vestibulum. Donec pellentesque faucibus mauris, sed dignissim augue lobortis at. Suspendisse fringilla malesuada neque at sagittis. Ut bibendum eros eu egestas tempor. Nulla bibendum ligula id placerat elementum.</p>
+				<p>Mauris eu mi sed nibh imperdiet accumsan. In tincidunt ligula erat. Pellentesque blandit porta purus, nec suscipit justo auctor in. Mauris a nisl bibendum, volutpat sem eu, semper ante. Sed tristique metus in nisi pretium mattis. Aliquam placerat felis eu erat feugiat, non viverra tellus scelerisque. Aenean feugiat, metus eget gravida faucibus, metus lorem ullamcorper diam, in feugiat erat purus eget tellus. Pellentesque vitae hendrerit tellus. In hac habitasse platea dictumst.</p>
+				<p>Nullam et imperdiet nunc. Nam eu tempus nulla, quis condimentum tortor. Donec euismod ut nulla non tristique. Fusce eu eleifend nibh, at porttitor ipsum. Mauris neque nisi, sollicitudin eget nisi a, faucibus scelerisque turpis. Duis facilisis est vitae ultrices ornare. Cras tincidunt iaculis tortor, et eleifend dolor mollis iaculis. Quisque consequat porta lacus.</p>
+				<p>Suspendisse et tortor ut neque hendrerit tincidunt ac non eros. Vivamus leo metus, finibus vel dolor tincidunt, rhoncus imperdiet justo. Sed porta lectus nec metus porta feugiat. Morbi vel lacus libero. Fusce porta, erat faucibus feugiat porta, urna erat hendrerit nisi, vel rhoncus ligula risus quis mi. Duis laoreet hendrerit sem, a tempor velit lobortis eget. Nam eget justo vel ex pulvinar finibus.</p>
+				<p>Pellentesque vestibulum sagittis mollis. Aliquam erat volutpat. Mauris consectetur varius mauris, mattis porttitor purus euismod sed. Cras nec dapibus est. Pellentesque mattis, tortor ut malesuada commodo, libero diam consequat nisl, vel sollicitudin ipsum nibh hendrerit metus. Aenean ornare arcu et auctor tincidunt. Nulla vitae tristique urna. Mauris auctor mi arcu. Proin ullamcorper venenatis leo eget rhoncus. Suspendisse fringilla commodo sem non interdum. Mauris eleifend est eu enim iaculis, vitae tristique mi bibendum. Vestibulum vulputate elit eu mi pellentesque commodo. Fusce placerat faucibus tortor et fermentum.</p>
+			<!-- InstanceEndEditable -->
+		</div>
+	
+		<div class="col-md-3 padding-20-top"> 
+			<!-- InstanceBeginRepeat name="right_nav_repeat" --><!-- InstanceBeginRepeatEntry --> 
+				<!-- InstanceBeginEditable name="right_nav_title" -->
+					<h3 class="margin-0-top">SEE ALSO</h3>
+				<!-- InstanceEndEditable -->
+					<ul class="nav-rightside-custom">
+						<!-- InstanceBeginEditable name="right_nav_links" -->
+					<li class="active" role="presentation"> <a href="#">SCIENCE BASIS OF RECOVERY</a></li>
+					<li role="presentation"> <a href="#">STRATEGIC SCIENCE PLAN</a></li>
+					<li role="presentation"> <a href="#">SCIENCE WORKPLAN</a></li>
+					<li role="presentation"> <a href="#">ADAPTIVE MANAGEMENT WORKPLAN</a></li>
+					<li role="presentation"> <a href="#">PUGET SOUND PRESSURE ASSESSMENT</a></li>
+					<li role="presentation"> <a href="#">PUGET SOUND SCIENCE UPDATES</a></li>
+						<!-- InstanceEndEditable -->
+					</ul>
+			<!-- InstanceEndRepeatEntry --><!-- InstanceEndRepeat -->
+		</div>
+	</div>
+	<!--END OF ROW -->
+</div>
+<!--END OF CONTENT CONTAINER -->
+
+<!--FOOTER START -->
+<div class="container-fluid blue-fill padding-20-top padding-20-bottom">
+	<div class="container">
+		<div class="row white">
+			<div class="col-md-3 padding-20-top">Sitemap links<br>
+				<br>
+			</div>
+			<div class="col-md-3 padding-20-top">Sitemap links<br>
+				<br>
+			</div>
+			<div class="col-md-3 padding-20-top">Sitemap links<br>
+				<br>
+			</div>
+			<div class="col-md-3 padding-20-top">Sitemap links<br>
+				<br>
+		</div>
+		</div>
+		<div class="align-right white">  <a href="https://app.smartsheet.com/b/form?EQBCT=54600eddd8ea4d5c82ed0b94f1d4a259" target="_blank">report a problem or make a suggestion</a></div>
+		<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+	<br><br><br>
+	</div>
+</div>
+
+<!-- END FOOTER -->
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) --> 
+<script src="js/jquery-1.11.2.min.js"></script> 
+
+<!-- Include all compiled plugins (below), or include individual files as needed --> 
+<script src="js/bootstrap.js"></script> 
+<!-- custom js --> 
+<script src="js/custom.js"></script>
+</body>
+<!-- InstanceEnd --></html>
