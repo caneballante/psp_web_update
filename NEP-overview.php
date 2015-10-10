1800bf4b5d2794f23438b81372406df34ddd87ed
caneballante <jonbridgman@gmail.com>
2015-10-10 13:09:34 -0700

    NEP
    created the NEP starter page

---
+++ NEP-overview.php
@@ -0,0 +1,126 @@
+<!DOCTYPE html>
+<html lang="en"><!-- InstanceBegin template="/Templates/page-4-template.dwt" codeOutsideHTMLIsLocked="false" -->
+<head>
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<!-- InstanceBeginEditable name="doctitle" -->
+<title>Puget Sound Partnership - Draft Home Page</title>
+<!-- InstanceEndEditable -->
+<!-- Bootstrap -->
+<link href="css/bootstrap.css" rel="stylesheet">
+<link href="css/custom.css" rel="stylesheet" type="text/css">
+<!-- loads the wf-loading class right away to minimize FOUT -->
+<script>document.documentElement.className += ' wf-loading';</script>
+<!-- Font PRENTON TYPEKIT -->
+<script src="https://use.typekit.net/srt5jze.js"></script>
+<script>try{Typekit.load({ async: true });}catch(e){}</script>
+<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+<!--[if lt IE 9]>
+	<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+	<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+<!-- InstanceBeginEditable name="head" -->
+<!-- InstanceEndEditable -->
+<!-- InstanceParam name="OptionalRegion1" type="boolean" value="true" -->
+<!-- InstanceParam name="OptionalRegion2" type="boolean" value="false" -->
+<!-- InstanceParam name="OptionalRegion3" type="boolean" value="true" -->
+<!-- InstanceParam name="OptionalRegion4" type="boolean" value="false" -->
+<!-- InstanceParam name="OptionalRegion5" type="boolean" value="true" -->
+<!-- InstanceParam name="OptionalRegion6" type="boolean" value="true" -->
+<!-- InstanceParam name="OptionalRegion7" type="boolean" value="false" -->
+</head>
+<body>
+<?php include 'includes/modal-inc.html';?>
+<!-- START IMAGE HEADER --> 
+ <!-- InstanceBeginEditable name="overviewphoto" -->
+<header class="overview-page-image-style overview-page-image4"> </header>
+<!-- InstanceEndEditable -->  
+<div class="container page-content">
+	<div class="row">
+		<div class="col-md-3 padding-20-top"></div>
+		<div class="col-md-7 padding-20-top"> <!-- InstanceBeginEditable name="6col_header" -->
+			<h1>PUGET SOUND<br>
+				National Estuary program (NEP)<br>
+			</h1>
+			<!-- InstanceEndEditable --></div>
+		<div class="col-md-2 padding-20-top"></div>
+	</div>
+	
+	<div class="row"> 
+		<div class="col-md-3">
+			<ul class="nav nav-stacked nav-pills nav-leftside-custom padding-left-0 margin-10-top margin-left-20">
+			<!-- InstanceBeginEditable name="left_nav" -->
+				<li class="active" role="presentation"> <a href="partnership-boards-overview.php">PARTNERSHIP BOARDS OVERVIEW</a></li>
+				<li role="presentation"> <a href="LC_about.php">ABOUT THE LEADERSHIP COUNCIL</a></li>
+				<li role="presentation"> <a href="LC_meetings.php">LEADERSHIP COUNCIL MEETINGS AND AGENDAS</a></li>
+				<li role="presentation"> <a href="EB_about.php">ABOUT THE ECOSYSTEM COORDINATION BOARD</a></li>
+				<li role="presentation"> <a href="EB_meetings.php">ECOSYSTEM COORDINATION BOARD MEETINGS AND AGENDAS</a></li>
+				<li role="presentation"> <a href="SP_about.php">ABOUT THE SCIENCE PANEL</a></li>
+				<li role="presentation"> <a href="SP_meetings.php">SCIENCE PANEL MEETINGS AND AGENDAS</a></li>
+				<li role="presentation"> <a href="SR_about.php">ABOUT THE SALMON RECOVERY COUNCIL</a></li>
+				<li role="presentation"> <a href="SR_meetings.php">THE SALMON RECOVERY COUNCIL MEETINGS AND AGENDAS</a></li>
+			<!-- InstanceEndEditable --> 
+			</ul>
+		
+		</div>
+			
+	
+		<div class="col-md-7 padding-20-top">
+		<!-- InstanceBeginEditable name="6col_content" -->
+			<p>I love cheese, especially feta fondue. Manchego squirty cheese who moved my cheese cheese slices paneer blue castello say cheese boursin. Lancashire monterey jack stilton cottage cheese emmental jarlsberg manchego cheesy feet. Dolcelatte cheese and biscuits melted cheese stinking bishop port-salut when the cheese comes out everybody's happy roquefort red leicester. Gouda cheese slices cauliflower cheese cottage cheese.</p>
+			<p>Manchego cheese and biscuits emmental. Cottage cheese the big cheese cow pecorino parmesan cheese on toast edam airedale. Bocconcini bocconcini cheese slices cheesy feet macaroni cheese gouda pecorino parmesan. Jarlsberg.</p>
+			<h2>THE LEADERSHIP COUNCIL</h2>
+			
+			<p>Blue castello cheddar goat. Boursin gouda cheese on toast feta caerphilly emmental monterey jack mascarpone. Stinking bishop jarlsberg croque monsieur gouda cheddar fondue parmesan edam. Parmesan cheeseburger roquefort hard cheese st. agur blue cheese st. agur blue cheese halloumi cheeseburger. Pecorino cheese strings pepper jack.</p>
+			<h2>ECOSYSTEM COORDINATION BOARD</h2>
+			<p>Blue castello cheddar goat. Boursin gouda cheese on toast feta caerphilly emmental monterey jack mascarpone. Stinking bishop jarlsberg croque monsieur gouda cheddar fondue parmesan edam. Parmesan cheeseburger roquefort hard cheese st. agur blue cheese st. agur blue cheese halloumi cheeseburger. Pecorino cheese strings pepper jack.</p>
+		<h2>SCIENCE PANEL</h2>
+			<p>Rubber cheese port-salut dolcelatte. Cheeseburger gouda mozzarella cheese slices emmental pecorino emmental cut the cheese. Cow mozzarella cottage cheese macaroni cheese macaroni cheese stilton pecorino cheeseburger. Mozzarella paneer cheese strings smelly cheese jarlsberg.</p>
+				<h2>SALMON RECOVERY COUNCIL</h2>
+				<p>Cheese ricotta edam. Lancashire squirty cheese taleggio taleggio stilton cheeseburger say cheese fromage frais. Cheese on toast boursin squirty cheese cheddar cheese and biscuits pecorino say cheese queso. Roquefort paneer.<br>
+					<br>
+					<br>
+					<br>
+					<br>
+					<br>
+					<br>
+					<br>
+				</p>
+			<!-- InstanceEndEditable -->
+		</div>
+	
+		<div class="col-md-2 padding-20-top"> 
+			<!-- InstanceBeginRepeat name="right_nav_repeat" --><!-- InstanceBeginRepeatEntry --> 
+				<!-- InstanceBeginEditable name="right_nav_title" -->
+					<div class="right-nav-title">SEE ALSO</div>
+				<!-- InstanceEndEditable -->
+					<ul class="nav-rightside-custom">
+						<!-- InstanceBeginEditable name="right_nav_links" -->
+					<li role="presentation"> <a href="#">LINKS</a></li>
+					<li role="presentation"> <a href="#">LINKS</a></li>
+				
+						<!-- InstanceEndEditable -->
+					</ul>
+			<!-- InstanceEndRepeatEntry --><!-- InstanceEndRepeat -->
+		</div>
+	</div>
+	<!--END OF ROW -->
+</div>
+<!--END OF CONTENT CONTAINER -->
+
+
+<?php include 'includes/footer-inc.html';?>
+
+
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) --> 
+<script src="js/jquery-1.11.2.min.js"></script> 
+
+<!-- Include all compiled plugins (below), or include individual files as needed --> 
+<script src="js/bootstrap.js"></script> 
+<!-- custom js --> 
+<script src="js/custom.js"></script>
+</body>
+<!-- InstanceEnd --></html>
