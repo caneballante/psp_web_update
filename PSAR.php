8cc6c1427cd17b8997bc0789df5df9591c641350
caneballante <jonbridgman@gmail.com>
2015-10-13 10:00:36 -0700

    PSAR
    made the psar page

---
+++ PSAR.php
@@ -0,0 +1,142 @@
+<!DOCTYPE html>
+<html lang="en"><!-- InstanceBegin template="/Templates/page-4-template.dwt" codeOutsideHTMLIsLocked="false" -->
+<head>
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<!-- InstanceBeginEditable name="doctitle" -->
+<title>Puget Sound Partnership - Draft Home Page</title>
+<!-- InstanceEndEditable -->
+<!-- Bootstrap -->
+<link href="css/bootstrap.css" rel="stylesheet">
+<link href="css/custom.css" rel="stylesheet" type="text/css">
+<link href="css/custom-erika.css" rel="stylesheet" type="text/css">
+<!-- loads the wf-loading class right away to minimize FOUT -->
+<script>document.documentElement.className += ' wf-loading';</script>
+<!-- Font PRENTON TYPEKIT -->
+<script src="https://use.typekit.net/srt5jze.js"></script>
+<script>try{Typekit.load({ async: true });}catch(e){}</script>
+<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+<!--[if lt IE 9]>
+	<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+	<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+<!-- InstanceBeginEditable name="head" -->
+<!-- InstanceEndEditable -->
+<!-- InstanceParam name="OptionalRegion1" type="boolean" value="true" -->
+<!-- InstanceParam name="OptionalRegion2" type="boolean" value="false" -->
+<!-- InstanceParam name="OptionalRegion3" type="boolean" value="true" -->
+<!-- InstanceParam name="OptionalRegion4" type="boolean" value="false" -->
+<!-- InstanceParam name="OptionalRegion5" type="boolean" value="true" -->
+<!-- InstanceParam name="OptionalRegion6" type="boolean" value="true" -->
+<!-- InstanceParam name="OptionalRegion7" type="boolean" value="false" -->
+</head>
+<body>
+<?php include 'includes/modal-inc.html';?>
+<!-- START IMAGE HEADER --> 
+ <!-- InstanceBeginEditable name="overviewphoto" -->
+<header class="overview-page-image-style overview-page-image7">
+
+<div class="overview-quote pull-right"><h3>Every $1 million invested in PSAR projects means at least 17 jobs and over $3 million in economic activity. For the 2013-15 biennium, PSAR projects produced 845 jobs.PSAR projects completed in the 2013-15 biennium restored and protected: <br>
+1,289 estuary and nearshore acres, 770 floodplain acres, and 14 river and stream miles.</h3></div>
+
+ </header>
+<!-- InstanceEndEditable -->  
+<div class="container page-content">
+	<div class="row">
+		<div class="col-md-3 padding-20-top"></div>
+		<div class="col-md-7 padding-20-top"> <!-- InstanceBeginEditable name="6col_header" -->
+			<h1>PSAR FUND</h1>
+			<!-- InstanceEndEditable --></div>
+		<div class="col-md-2 padding-20-top"></div>
+	</div>
+	
+	<div class="row"> 
+		<div class="col-md-3">
+			<ul class="nav nav-stacked nav-pills nav-leftside-custom padding-left-0 margin-10-top margin-left-20">
+			<!-- InstanceBeginEditable name="left_nav" -->
+				<li class="active" role="presentation"> <a href="PSAR.php">PSAR FUND</a></li>
+				<li role="presentation"> <a href="PUGET-SOUND-FUNDING-PROGRAMS.php">PUGET SOUND FUNDING PROGRAMS</a></li>
+		
+			<!-- InstanceEndEditable --> 
+			</ul>
+		
+		</div>
+			
+	
+		<div class="col-md-7 padding-20-top content-column">
+		<!-- InstanceBeginEditable name="6col_content" -->
+			
+		   <p>Investing in habitat restoration to bring back Puget Sound’s dynamic natural systems – for salmon, people, and community</p>
+     <h2>What is PSAR?</h2>
+     <p>The Puget Sound Acquisition and Restoration program was created in 2007 to help implement the most important habitat protection and restoration priorities for Puget Sound. Funding is appropriated by the Legislature through the Salmon Recovery Funding Board. The Puget Sound Partnership works with local entities to identify and prioritize projects. This funding is critical to advancing the most effective projects throughout our region.</p>
+<h2>Why is PSAR an effective investment?</h2>
+<ul>
+		<li><strong>PSAR-funded projects restore critical habitat and salmon populations to Puget Sound. </strong></li>
+</ul>
+<p>Because the projects are thoroughly reviewed, their benefit to improved salmon habitat in Puget Sound is significant. Since 2007, when PSAR fund was initiated, <strong>more than 300</strong> critical habitat projects have been completed. PSAR projects are developed with the input and review of scientists and environmental planners, who use <strong>monitoring data</strong> collected from key habitat indicators to inform project selection and completion.</p>
+<ul>
+		<li><strong>PSAR-funded projects benefit local communities. The projects:</strong></li>
+		<ul>
+				<li>Are <strong>locally vetted</strong> and locally supported</li>
+				<li>Are <strong>endorsed</strong> by citizens, cities, counties, tribes, non-profit organizations, and government agencies</li>
+				<li>Evolve from <strong>hours of volunteer effort</strong> in planning and labor</li>
+				<li>Produce <strong>significant community pride</strong>—and investment—in habitat restoration</li>
+				<li>Generate <strong>local jobs</strong></li>
+				<li>Improve <strong>public access</strong> to Puget Sound</li>
+				<li><strong>Reduce the risk of floods</strong> and damage to private property</li>
+		</ul>
+</ul>
+<ul>
+		<li><strong>PSAR projects make efficient use of public money. </strong></li>
+</ul>
+<p>To be eligible for funding, proposed projects must first advance through a series of <strong>rigorous, science-driven reviews</strong>. The reviews occur at the local and regional level and ensure that all PSAR projects align with the <strong>Puget Sound Salmon Recovery Plan</strong>, as well as with the Puget Sound Partnership&rsquo;s ecosystem recovery targets, listed in the <strong>Action Agenda</strong>.<br />
+</p>
+<p><strong><em>Staff with the Puget Sound Partnership manage the PSAR grant fund program; PSAR funds are administered through the Washington State Recreation and Conservation Office. For more information about the funding process, or to apply for a PSAR grant, go to <a href="http://www.rco.wa.gov/grants/salmon.shtml">http://www.rco.wa.gov/grants/salmon.shtml</a></em></strong><br />
+</p>
+<p><img src="images/RCO.jpg" width="200" height="52" alt=""/></p>
+		<!-- InstanceEndEditable -->
+		</div>
+	
+		<div class="col-md-2 padding-20-top"> 
+			<!-- InstanceBeginRepeat name="right_nav_repeat" --><!-- InstanceBeginRepeatEntry --> 
+				<!-- InstanceBeginEditable name="right_nav_title" -->
+					<div class="right-nav-title">SEE ALSO</div>
+				<!-- InstanceEndEditable -->
+					<ul class="nav-rightside-custom">
+						<!-- InstanceBeginEditable name="right_nav_links" -->
+			          <li role="presentation"><a href="https://app.box.com/s/5h7adnq2hd7u3srmiqqd">PSAR FUND 2015-17 Ranked List and Map</a></li>
+				
+				
+					 <li role="presentation"><a href="https://app.box.com/s/ii5hkscxa6zk98juwz51">PSAR 2015-17 Project Snapshots Factsheets Individual</a> </li>
+					 <li role="presentation"><a href=" https://app.box.com/s/fct26xzwhsh3co3kuefl">PSAR 2015-17 Project Snapshots Factsheets Combined</a> </li>
+				 <li role="presentation"><a href="https://app.box.com/s/3pmck57jdlx5qho7ikwk">PSAR FUND Fact Sheet</a> </li>
+				
+				 <li role="presentation"><a href="https://app.box.com/s/31rg8lftqjecrrc6erv3">PSAR FUND Fact Sheet 11"x17" printable</a> </li>
+					 <li role="presentation"><a href="https://app.box.com/s/6bec8ax6ruaytrk4d5ms">PSAR 2013-15 Project Map</a> </li>
+				
+				
+						<!-- InstanceEndEditable -->
+					</ul>
+			<!-- InstanceEndRepeatEntry --><!-- InstanceEndRepeat -->
+		</div>
+	</div>
+	<!--END OF ROW -->
+</div>
+<!--END OF CONTENT CONTAINER -->
+
+
+<?php include 'includes/footer-inc.html';?>
+
+
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) --> 
+<script src="js/jquery-1.11.2.min.js"></script> 
+
+<!-- Include all compiled plugins (below), or include individual files as needed --> 
+<script src="js/bootstrap.js"></script> 
+<!-- custom js --> 
+<script src="js/custom.js"></script>
+</body>
+<!-- InstanceEnd --></html>
