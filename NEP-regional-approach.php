3c63664ce5bb60a3716b841e831b58a3c2e10160
caneballante <jonbridgman@gmail.com>
2015-10-14 12:44:53 -0700

    Lightbox
    implementing a bootstrap modal for a lightbox. I was over-writing all
    modals with a fix to center the separators on the main menu modal. I
    stopped doing that and created a new class “custom-modal-center-fix”
    
    I have a first pass at the bootstrap lightbox on
    salmon-recovery-plans.php but it’s taking up the entire viewport. I am
    going to try and make it take up less.

--- NEP-regional-approach.php
+++ NEP-regional-approach.php
@@ -12,7 +12,7 @@
 <link href="css/bootstrap.css" rel="stylesheet">
 <link href="css/custom.css" rel="stylesheet" type="text/css">
 <link href="css/custom-erika.css" rel="stylesheet" type="text/css">
-<link href="css/bootstrap-lightbox.css" rel="stylesheet" type="text/css">
+
 
 <!-- loads the wf-loading class right away to minimize FOUT -->
 <script>document.documentElement.className += ' wf-loading';</script>
@@ -126,8 +126,7 @@
 <script src="js/bootstrap.js"></script> 
 <!-- custom js --> 
 <script src="js/custom.js"></script>
-<!--lightbox js --> 
-<script src="js/bootstrap-lightbox.js"></script>
+
 
 </body>
 <!-- InstanceEnd --></html>
--- salmon-watershed-recovery-plans.php
+++ salmon-watershed-recovery-plans.php
@@ -1,10 +1,10 @@
 <!DOCTYPE html>
-<html lang="en"><!-- InstanceBegin template="/Templates/page-4-template.dwt" codeOutsideHTMLIsLocked="false" -->
+<html lang="en">
+<!-- InstanceBegin template="/Templates/page-4-template.dwt" codeOutsideHTMLIsLocked="false" -->
 <head>
 <meta charset="UTF-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-
 <!-- InstanceBeginEditable name="doctitle" -->
 <title>Puget Sound Partnership - Draft Home Page</title>
 <!-- InstanceEndEditable -->
@@ -12,7 +12,6 @@
 <link href="css/bootstrap.css" rel="stylesheet">
 <link href="css/custom.css" rel="stylesheet" type="text/css">
 <link href="css/custom-erika.css" rel="stylesheet" type="text/css">
-<link href="css/bootstrap-lightbox.css" rel="stylesheet" type="text/css">
 
 <!-- loads the wf-loading class right away to minimize FOUT -->
 <script>document.documentElement.className += ' wf-loading';</script>
@@ -38,13 +37,13 @@
 <body>
 <?php include 'includes/modal-inc.html';?>
 <!-- START IMAGE HEADER --> 
- <!-- InstanceBeginEditable name="overviewphoto" -->
+<!-- InstanceBeginEditable name="overviewphoto" -->
 <header class="overview-page-image-style overview-page-image8">
-
-<div class="overview-quote pull-right"><h3>The Puget Sound NEP brings together regional leaders, experts and community members to protect, restore and maintain the ecological integrity of Puget Sound.</h3></div>
-
- </header>
-<!-- InstanceEndEditable -->  
+	<div class="overview-quote pull-right">
+		<h3>The Puget Sound NEP brings together regional leaders, experts and community members to protect, restore and maintain the ecological integrity of Puget Sound.</h3>
+	</div>
+</header>
+<!-- InstanceEndEditable -->
 <div class="container page-content">
 	<div class="row">
 		<div class="col-md-3 padding-20-top"></div>
@@ -53,124 +52,110 @@
 			<!-- InstanceEndEditable --></div>
 		<div class="col-md-2 padding-20-top"></div>
 	</div>
-	
-	<div class="row"> 
+	<div class="row">
 		<div class="col-md-3">
 			<ul class="nav nav-stacked nav-pills nav-leftside-custom padding-left-0 margin-10-top margin-left-20">
-			<!-- InstanceBeginEditable name="left_nav" -->
+				<!-- InstanceBeginEditable name="left_nav" -->
 				<li role="presentation"> <a href="salmon-recovery-overview.php">SALMON RECOVERY OVERVIEW</a></li>
 				<li role="presentation"> <a href="salmon-recovery-status.php">SALMON RECOVERY STATUS</a></li>
 				<li class="active" role="presentation"> <a href="salmon-watershed-recovery-plans.php">WATERSHED RECOVERY PLANS</a></li>
 				<li role="presentation"> <a href="salmon-three-year-work-plans.php">THREE YEAR WORKPLANS</a></li>
 				<li role="presentation"> <a href="salmon-lead-entities.php">LEAD ENTITIES</a></li>
 				<li role="presentation"> <a href="salmon-resources.php">SALMON RECOVERY RESOURCES</a></li>
-			<!-- InstanceEndEditable --> 
-			</ul>
-		
-		</div>
-			
-	
-		<div class="col-md-7 padding-20-top content-column">
-		<!-- InstanceBeginEditable name="6col_content" -->
-			
-				<h3> What does the Puget Sound Salmon Recovery Plan include?</h3>
-		<p>
-				The Recovery Plan is organized with a Regional Chapter (Volume I) and Fourteen Watershed-Specific Chapters and a Nearshore Chapter (Volume II). The Plan includes strategies and actions associated with marine and freshwater habitat protection and restoration, hatchery management, and harvest management.</p>
-		<p><a href="downloads/SALMON_RECOVERY/VOL_I/16_VOLUME_I_PUGETS_SOUND_SALMON_RECOVERY_PLAN.zip">Download Puget Sound Salmon Recovery Plan (Volume I)</a></p>
-		<h3>
-				What are the Watersheds? </h3>
-		<p><a href="images/PS_salmonrecovery.jpg"><img src="images/PS_salmonrecovery.jpg" alt="Puget Sound Watersheds" width="500" height="489" /></a><br />
-		</p>
-		<table class="table">
-				<tr>
-						<td class="tableheader">Watershed</td>
-						<td class="tableheader">Watershed Recovery Plan Chapters</td>
-						</tr>
-				<tr>
-						<td>San Juan</td>
-						<td><a href="downloads/SALMON_RECOVERY/VOL_2/SAN_JUAN.zip">chapter download zip</a> </td>
-			</tr>
-				<tr>
-						<td>Nooksack</td>
-						<td><a href="downloads/SALMON_RECOVERY/VOL_2/NOOKSACK.zip">chapter download zip</a></td>
-						</tr>
-				<tr>
-						<td>Skagit</td>
-						<td><a href="downloads/SALMON_RECOVERY/VOL_2/SKAGIT.zip"> chapter download zip</a></td>
-						</tr>
-				<tr>
-						<td>Stillaguamish</td>
-						<td><a href="downloads/SALMON_RECOVERY/VOL_2/STILLAGUAMISH.zip"> chapter download zip</a></td>
-						</tr>
-				<tr>
-						<td>Snohomish</td>
-						<td><a href="downloads/SALMON_RECOVERY/VOL_2/SNOHOMISH.zip">chapter download zip</a></td>
-						</tr>
-				<tr>
-						<td>Island </td>
-						<td><a href="downloads/SALMON_RECOVERY/VOL_2/ISLAND.zip">chapter download zip</a></td>
-						</tr>
-				<tr>
-						<td>Lake Washington/Cedar/Sammamish</td>
-						<td><a href="downloads/SALMON_RECOVERY/VOL_2/LAKE_WASHINGTON_CEDAR_SAMMAMMISH.zip">chapter download zip</a></td>
-						</tr>
-				<tr>
-						<td>Green/Duwamish</td>
-						<td><a href="downloads/SALMON_RECOVERY/VOL_2/GREEN_DUWAMISH.zip">chapter download zip</a></td>
-						</tr>
-				<tr>
-						<td>Puyallup-White/Chambers-Clover</td>
-						<td><a href="downloads/SALMON_RECOVERY/VOL_2/PUYALLUP_WHITE_AND_CHAMBERS_CLOVER.zip">chapter download zip</a></td>
-						</tr>
-				<tr>
-						<td>Nisqually</td>
-						<td><a href="downloads/SALMON_RECOVERY/VOL_2/NISQUALLY.zip">chapter download zip</a></td>
-						</tr>
-				<tr>
-						<td>South Sound </td>
-						<td><a href="downloads/SALMON_RECOVERY/VOL_2/SOUTHSOUND.zip">chapter download zip</a></td>
-						</tr>
-				<tr>
-						<td>West Sound/East Kitsap </td>
-						<td><a href="downloads/SALMON_RECOVERY/VOL_2/EAST_KITSAP_WEST_SOUND.zip">chapter download zip</a></td>
-						</tr>
-				<tr>
-						<td>Hood Canal</td>
-						<td><a href="downloads/SALMON_RECOVERY/VOL_2/MID_HOOD_CANAL.zip">chapter download zip</a></td>
-						</tr>
-				<tr>
-						<td>Elwha/Dungeness/Straits</td>
-						<td><p><a href="downloads/SALMON_RECOVERY/VOL_2/ELWHA.zip"> chapter download zip</a><a href="ttp://www.sharedsalmonstrategy.org/watersheds/watershed-elwah.htm"> (Elwha)</a></p>
-						<p><a href="downloads/SALMON_RECOVERY/VOL_2/DUNGENESS_SALMONOID.zip">chapter download zip</a><a href="ttp://www.sharedsalmonstrategy.org/watersheds/watershed-elwah.htm"> (Dungeness)</a></p></td>
-						</tr>
-		</table>
-		
-
-		<!-- InstanceEndEditable -->
-		</div>
-	
-		<div class="col-md-2 padding-20-top"> 
-			<!-- InstanceBeginRepeat name="right_nav_repeat" --><!-- InstanceBeginRepeatEntry --> 
-				<!-- InstanceBeginEditable name="right_nav_title" -->
-					<div class="right-nav-title">SEE ALSO</div>
 				<!-- InstanceEndEditable -->
-					<ul class="nav-rightside-custom">
-						<!-- InstanceBeginEditable name="right_nav_links" -->
-					<li role="presentation"> <a href="#">LINKS</a></li>
-					<li role="presentation"> <a href="#">LINKS</a></li>
-				
-						<!-- InstanceEndEditable -->
-					</ul>
-			<!-- InstanceEndRepeatEntry --><!-- InstanceEndRepeat -->
+			</ul>
 		</div>
+		<div class="col-md-7 padding-20-top content-column"> <!-- InstanceBeginEditable name="6col_content" -->
+			<h3> What does the Puget Sound Salmon Recovery Plan include?</h3>
+			<p> The Recovery Plan is organized with a Regional Chapter (Volume I) and Fourteen Watershed-Specific Chapters and a Nearshore Chapter (Volume II). The Plan includes strategies and actions associated with marine and freshwater habitat protection and restoration, hatchery management, and harvest management.</p>
+			<p><a href="downloads/SALMON_RECOVERY/VOL_I/16_VOLUME_I_PUGETS_SOUND_SALMON_RECOVERY_PLAN.zip">Download Puget Sound Salmon Recovery Plan (Volume I)</a></p>
+			<h3>What are the Watersheds? </h3>
+			<a href="javascript:void(0);" title="Puget Sound Map - enlarge" data-toggle="modal" data-target="#PSmap"> <img src="images/PS_salmonrecovery.jpg" alt="Map of Puget Sound Watersheds" width="500" height="489"/> </a>
+			<div class="modal fade" id="PSmap" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+				<div class="modal-dialog" role="document">
+					<div class="modal-content"> <img src="images/PS_salmonrecovery.jpg" class="img-responsive"  alt=""/> </div>
+				</div>
+			</div>
+			<table class="table">
+				<tr>
+					<td class="tableheader">Watershed</td>
+					<td class="tableheader">Watershed Recovery Plan Chapters</td>
+				</tr>
+				<tr>
+					<td>San Juan</td>
+					<td><a href="downloads/SALMON_RECOVERY/VOL_2/SAN_JUAN.zip">chapter download zip</a></td>
+				</tr>
+				<tr>
+					<td>Nooksack</td>
+					<td><a href="downloads/SALMON_RECOVERY/VOL_2/NOOKSACK.zip">chapter download zip</a></td>
+				</tr>
+				<tr>
+					<td>Skagit</td>
+					<td><a href="downloads/SALMON_RECOVERY/VOL_2/SKAGIT.zip"> chapter download zip</a></td>
+				</tr>
+				<tr>
+					<td>Stillaguamish</td>
+					<td><a href="downloads/SALMON_RECOVERY/VOL_2/STILLAGUAMISH.zip"> chapter download zip</a></td>
+				</tr>
+				<tr>
+					<td>Snohomish</td>
+					<td><a href="downloads/SALMON_RECOVERY/VOL_2/SNOHOMISH.zip">chapter download zip</a></td>
+				</tr>
+				<tr>
+					<td>Island </td>
+					<td><a href="downloads/SALMON_RECOVERY/VOL_2/ISLAND.zip">chapter download zip</a></td>
+				</tr>
+				<tr>
+					<td>Lake Washington/Cedar/Sammamish</td>
+					<td><a href="downloads/SALMON_RECOVERY/VOL_2/LAKE_WASHINGTON_CEDAR_SAMMAMMISH.zip">chapter download zip</a></td>
+				</tr>
+				<tr>
+					<td>Green/Duwamish</td>
+					<td><a href="downloads/SALMON_RECOVERY/VOL_2/GREEN_DUWAMISH.zip">chapter download zip</a></td>
+				</tr>
+				<tr>
+					<td>Puyallup-White/Chambers-Clover</td>
+					<td><a href="downloads/SALMON_RECOVERY/VOL_2/PUYALLUP_WHITE_AND_CHAMBERS_CLOVER.zip">chapter download zip</a></td>
+				</tr>
+				<tr>
+					<td>Nisqually</td>
+					<td><a href="downloads/SALMON_RECOVERY/VOL_2/NISQUALLY.zip">chapter download zip</a></td>
+				</tr>
+				<tr>
+					<td>South Sound </td>
+					<td><a href="downloads/SALMON_RECOVERY/VOL_2/SOUTHSOUND.zip">chapter download zip</a></td>
+				</tr>
+				<tr>
+					<td>West Sound/East Kitsap </td>
+					<td><a href="downloads/SALMON_RECOVERY/VOL_2/EAST_KITSAP_WEST_SOUND.zip">chapter download zip</a></td>
+				</tr>
+				<tr>
+					<td>Hood Canal</td>
+					<td><a href="downloads/SALMON_RECOVERY/VOL_2/MID_HOOD_CANAL.zip">chapter download zip</a></td>
+				</tr>
+				<tr>
+					<td>Elwha/Dungeness/Straits</td>
+					<td><p><a href="downloads/SALMON_RECOVERY/VOL_2/ELWHA.zip"> chapter download zip</a><a href="ttp://www.sharedsalmonstrategy.org/watersheds/watershed-elwah.htm"> (Elwha)</a></p>
+						<p><a href="downloads/SALMON_RECOVERY/VOL_2/DUNGENESS_SALMONOID.zip">chapter download zip</a><a href="ttp://www.sharedsalmonstrategy.org/watersheds/watershed-elwah.htm"> (Dungeness)</a></p></td>
+				</tr>
+			</table>
+			<!-- InstanceEndEditable --> </div>
+		<div class="col-md-2 padding-20-top"> <!-- InstanceBeginRepeat name="right_nav_repeat" --><!-- InstanceBeginRepeatEntry --> <!-- InstanceBeginEditable name="right_nav_title" -->
+			<div class="right-nav-title">SEE ALSO</div>
+			<!-- InstanceEndEditable -->
+			<ul class="nav-rightside-custom">
+				<!-- InstanceBeginEditable name="right_nav_links" -->
+				<li role="presentation"> <a href="#">LINKS</a></li>
+				<li role="presentation"> <a href="#">LINKS</a></li>
+				<!-- InstanceEndEditable -->
+			</ul>
+			<!-- InstanceEndRepeatEntry --><!-- InstanceEndRepeat --> </div>
 	</div>
-	<!--END OF ROW -->
+	<!--END OF ROW --> 
 </div>
 <!--END OF CONTENT CONTAINER -->
 
-
 <?php include 'includes/footer-inc.html';?>
-
 
 <!-- jQuery (necessary for Bootstrap's JavaScript plugins) --> 
 <script src="js/jquery-1.11.2.min.js"></script> 
@@ -179,8 +164,6 @@
 <script src="js/bootstrap.js"></script> 
 <!-- custom js --> 
 <script src="js/custom.js"></script>
-<!--lightbox js --> 
-<script src="js/bootstrap-lightbox.js"></script>
-
 </body>
-<!-- InstanceEnd --></html>
+<!-- InstanceEnd -->
+</html>
