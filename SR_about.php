c428744fc442fc68805f277329e51a472b2361c3
caneballante <jonbridgman@gmail.com>
2015-10-27 11:10:32 -0700

    bug

--- SR_about.php
+++ SR_about.php
@@ -1,10 +1,10 @@
 <!DOCTYPE html>
-<html lang="en"><!-- InstanceBegin template="/Templates/page-4-template.dwt" codeOutsideHTMLIsLocked="false" -->
+<html lang="en">
+<!-- InstanceBegin template="/Templates/page-4-template.dwt" codeOutsideHTMLIsLocked="false" -->
 <head>
 <meta charset="UTF-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-
 <!-- InstanceBeginEditable name="doctitle" -->
 <title>Puget Sound Partnership - Salmon Recovery Council Members</title>
 <!-- InstanceEndEditable -->
@@ -12,7 +12,6 @@
 <link href="css/bootstrap.css" rel="stylesheet">
 <link href="css/custom.css" rel="stylesheet" type="text/css">
 <link href="css/custom-erika.css" rel="stylesheet" type="text/css">
-
 
 <!-- loads the wf-loading class right away to minimize FOUT -->
 <script>document.documentElement.className += ' wf-loading';</script>
@@ -29,7 +28,7 @@
 <script>
 /*this variable is used to set the proper nav to active. It should to the order the nav item is in the list*/
   	navSelected = 8;
-</script> 
+</script>
 <!-- InstanceEndEditable -->
 <!-- InstanceParam name="OptionalRegion1" type="boolean" value="true" -->
 <!-- InstanceParam name="OptionalRegion2" type="boolean" value="false" -->
@@ -42,9 +41,9 @@
 <body>
 <?php include 'includes/modal-inc.html';?>
 <!-- START IMAGE HEADER --> 
- <!-- InstanceBeginEditable name="overviewphoto" -->
+<!-- InstanceBeginEditable name="overviewphoto" -->
 <header class="overview-page-image-style overview-page-image4"> </header>
-<!-- InstanceEndEditable -->  
+<!-- InstanceEndEditable -->
 <div class="container page-content padding-50-bottom">
 	<div class="row">
 		<div class="col-md-3 padding-20-top"></div>
@@ -54,20 +53,15 @@
 			<!-- InstanceEndEditable --></div>
 		<div class="col-md-2 padding-20-top"></div>
 	</div>
-	
-	<div class="row"> 
+	<div class="row">
 		<div class="col-md-3">
 			<ul class="nav nav-stacked nav-pills nav-leftside-custom padding-left-0 margin-10-top margin-left-20">
-			<!-- InstanceBeginEditable name="left_nav" -->
-				<?php include 'includes/ln-boards.html';?>	
-				<!-- InstanceEndEditable --> 
+				<!-- InstanceBeginEditable name="left_nav" -->
+				<?php include 'includes/ln-boards.html';?>
+				<!-- InstanceEndEditable -->
 			</ul>
-		
 		</div>
-			
-	
-		<div class="col-md-7 padding-20-top content-column">
-		<!-- InstanceBeginEditable name="6col_content" -->
+		<div class="col-md-7 padding-20-top content-column"> <!-- InstanceBeginEditable name="6col_content" -->
 			<p>The Puget Sound Salmon Recovery Council is a regional body of policy decision‐makers for salmon recovery. The role of the Council is to advise the Puget Sound Partnership&rsquo;s Leadership Council on decisions relating to salmon recovery and the implementation of the Puget Sound Chinook Recovery Plan. Recovery Council decisions accomplish the following:</p>
 			<ul>
 				<li>Set policy direction for implementation of the Puget Sound Chinook Recovery Plan;</li>
@@ -89,6 +83,20 @@
 					<td valign="top"><p><strong>&nbsp;</strong></p></td>
 					<td valign="top"><p>&nbsp;</p></td>
 					<td valign="top"><p>&nbsp;</p></td>
+				</tr>
+				<tr>
+					<td valign="top"><p><strong>Cities</strong></p></td>
+					<td valign="top"><p>Association of Washington Cities</p></td>
+					<td valign="top"><p><strong>Vacant</strong></p></td>
+					<td valign="top"><p>&nbsp;</p></td>
+					<td valign="top"></td>
+				</tr>
+				<tr>
+					<td valign="top"><p><strong>Counties</strong></p></td>
+					<td valign="top"><p>Washington Association of Counties</p></td>
+					<td valign="top"><p><strong>Vacant</strong></p></td>
+					<td valign="top"><p>&nbsp;</p></td>
+					<td valign="top"></td>
 				</tr>
 				<tr>
 					<td valign="top"><p><strong>Hood Canal Coordinating Council</strong></p></td>
@@ -426,32 +434,25 @@
 					<td valign="top"></td>
 				</tr>
 			</table>
-		<!-- InstanceEndEditable -->
-		</div>
-	
-		<div class="col-md-2 padding-20-top"> 
-			<!-- InstanceBeginRepeat name="right_nav_repeat" --><!-- InstanceBeginRepeatEntry --> 
-				<!-- InstanceBeginEditable name="right_nav_title" -->
+			<!-- InstanceEndEditable --> </div>
+		<div class="col-md-2 padding-20-top"> <!-- InstanceBeginRepeat name="right_nav_repeat" --><!-- InstanceBeginRepeatEntry --> <!-- InstanceBeginEditable name="right_nav_title" -->
 			<div class="right-nav-title margin-0-top ">BOARD CONTACT</div>
 			<!-- InstanceEndEditable -->
-					<ul class="nav-rightside-custom">
-						<!-- InstanceBeginEditable name="right_nav_links" -->
+			<ul class="nav-rightside-custom">
+				<!-- InstanceBeginEditable name="right_nav_links" -->
 				<p class="right-nav-contact">For questions about the Partnership's Councils, Boards and Panels contact :<br />
 					<a href="mailto:dominique.hampton@psp.wa.gov"><strong>Dominique Hampton</strong></a>, <br>
 					Special Assistant,<br>
 					360.628.2423</p>
 				<!-- InstanceEndEditable -->
-					</ul>
-			<!-- InstanceEndRepeatEntry --><!-- InstanceEndRepeat -->
-		</div>
+			</ul>
+			<!-- InstanceEndRepeatEntry --><!-- InstanceEndRepeat --> </div>
 	</div>
-	<!--END OF ROW -->
+	<!--END OF ROW --> 
 </div>
 <!--END OF CONTENT CONTAINER -->
 
-
 <?php include 'includes/footer-inc.html';?>
-
 
 <!-- jQuery (necessary for Bootstrap's JavaScript plugins) --> 
 <script src="js/jquery-1.11.2.min.js"></script> 
@@ -459,8 +460,8 @@
 <!-- Include all compiled plugins (below), or include individual files as needed --> 
 <script src="js/bootstrap.js"></script> 
 <!-- custom js --> 
-<script src="js/custom.js"></script>
-<!-- Google Tracking  -->
+<script src="js/custom.js"></script> 
+<!-- Google Tracking  --> 
 <script>
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
@@ -471,6 +472,6 @@
   ga('send', 'pageview');
 
 </script>
-
 </body>
-<!-- InstanceEnd --></html>
+<!-- InstanceEnd -->
+</html>
