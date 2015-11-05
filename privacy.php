b8edcdc227ec7c5f6f406259861d95635b82a36f
caneballante <jonbridgman@gmail.com>
2015-11-05 15:48:00 -0800

    privacy
    created new page

---
+++ privacy.php
@@ -0,0 +1,227 @@
+<!DOCTYPE html>
+<html lang="en"><!-- InstanceBegin template="/Templates/page-4-template.dwt" codeOutsideHTMLIsLocked="false" -->
+<head>
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<!-- InstanceBeginEditable name="doctitle" -->
+<title>Puget Sound Partnership - Draft Home Page</title>
+<!-- InstanceEndEditable -->
+<!-- Bootstrap -->
+<link href="css/bootstrap.css" rel="stylesheet">
+<link href="css/custom.css" rel="stylesheet" type="text/css">
+<link href="css/custom-erika.css" rel="stylesheet" type="text/css">
+<!-- loads the wf-loading class right away to minimize FOUT -->
+<script>document.documentElement.className += ' wf-loading';</script>
+<!-- Font PRENTON TYPEKIT -->
+<script src="https://use.typekit.net/srt5jze.js"></script>
+<script>try{Typekit.load({ async: true });}catch(e){}</script>
+<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+<!--[if lt IE 9]>
+	<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+	<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+<!-- InstanceBeginEditable name="head" -->
+<!-- InstanceEndEditable -->
+<!-- InstanceParam name="OptionalRegion1" type="boolean" value="true" -->
+<!-- InstanceParam name="OptionalRegion2" type="boolean" value="false" -->
+<!-- InstanceParam name="OptionalRegion3" type="boolean" value="false" -->
+<!-- InstanceParam name="OptionalRegion4" type="boolean" value="true" -->
+<!-- InstanceParam name="OptionalRegion5" type="boolean" value="false" -->
+<!-- InstanceParam name="OptionalRegion6" type="boolean" value="false" -->
+<!-- InstanceParam name="OptionalRegion7" type="boolean" value="true" -->
+<!-- InstanceParam name="OptionalRegion8" type="boolean" value="false" -->
+</head>
+<body>
+<?php include 'includes/modal-inc.html';?>
+<!-- START IMAGE HEADER --> 
+ <!-- InstanceBeginEditable name="overviewphoto" -->
+<header class="overview-page-image-style overview-page-image1">
+	<div class="overview-quote pull-right">The Puget Sound National Estuary Program brings together regional leaders, experts and community members to protect, restore and maintain the ecological integrity of Puget Sound.</div>
+</header>
+<!-- InstanceEndEditable -->  
+<div class="container page-content padding-50-bottom">
+	<div class="row">
+		<div class="col-sm-10 padding-20-top padding-0-left"> <!-- InstanceBeginEditable name="9col_header" -->
+			<h1>PRIVACY POLICY</h1>
+			<!-- InstanceEndEditable --></div>
+		<div class="col-sm-2 padding-20-top"></div>
+	</div>
+	
+	<div class="row">  
+		<div class="col-sm-10 padding-20-top padding-0-left content-column mobile-padding-20-left"> <!-- InstanceBeginEditable name="9col_content" -->
+				<p class="text">Thank you for visiting Puget Sound Partnership ( the Partnerhship) and reviewing our Privacy Notice. This notice addresses the collection, use and security of and access to information that may be obtained through use of this Web site. This notice covers the following topics:</p>
+        		<ul type="disc">
+        				<ul type="circle">
+        						<li class="text" 
+style="mso-margin-top-alt: auto; mso-margin-bottom-alt: auto; mso-list: l2 level2 lfo3; tab-stops: list 1.0in"><a 
+href="#_Information_Collected_and">Information collected and how it is used</a>
+        								<O:P></O:P>
+							</li>
+            		<li class="text" 
+style="mso-margin-top-alt: auto; mso-margin-bottom-alt: auto; mso-list: l2 level2 lfo3; tab-stops: list 1.0in"><a 
+href="#_Personal_Information_and">Personal information and choice</a> </li>
+            		<li class="text" 
+style="mso-margin-top-alt: auto; mso-margin-bottom-alt: auto; mso-list: l2 level2 lfo3; tab-stops: list 1.0in"><a href="#_Public_Disclosure">Public disclosure</a> </li>
+            		<li class="text" 
+style="mso-margin-top-alt: auto; mso-margin-bottom-alt: auto; mso-list: l2 level2 lfo3; tab-stops: list 1.0in"><a 
+href="#_Access_and_Correction">Access and correction of personal information</a> </li>
+            		<li class="text" 
+style="mso-margin-top-alt: auto; mso-margin-bottom-alt: auto; mso-list: l2 level2 lfo3; tab-stops: list 1.0in"><a href="#_Security">Security</a> </li>
+            		<li class="text" 
+style="mso-margin-top-alt: auto; mso-margin-bottom-alt: auto; mso-list: l2 level2 lfo3; tab-stops: list 1.0in"><a href="#_Disclaimer">Disclaimer</a> </li>
+            		<li class="text" 
+style="mso-margin-top-alt: auto; mso-margin-bottom-alt: auto; mso-list: l2 level2 lfo3; tab-stops: list 1.0in"><a 
+href="#_PSWQAT_Contact_Information">PSP contact iInformation</a>
+            				<O:P></O:P>
+            				</li>
+          		</ul>
+        		</ul>
+        		<h4><font face="Arial, Helvetica, sans-serif" size="2"><a 
+name="_Information_Collected_and" id="_Information_Collected_and"></a></font><span class="text_headline">Information collected and how it is used</span></h4>
+        		<p class="text">If you do nothing during your visit to the Partnership Web site but browse or download information, the Web site automatically collects and stores the following information about your visit:</p>
+        		<ol type="1">
+        				<li class="text" 
+style="mso-margin-top-alt: auto; mso-margin-bottom-alt: auto; mso-list: l1 level1 lfo6; tab-stops: list .5in">The type of browser and operating system you used.</li>
+          		<li class="text" 
+style="mso-margin-top-alt: auto; mso-margin-bottom-alt: auto; mso-list: l1 level1 lfo6; tab-stops: list .5in">The date and time you visited this site.</li>
+          		<li class="text" 
+style="mso-margin-top-alt: auto; mso-margin-bottom-alt: auto; mso-list: l1 level1 lfo6; tab-stops: list .5in">The web pages or services you accessed at this site.</li>
+        		</ol>
+        		<p class="text">The information automatically collected or stored is used to improve the content of our web services and to help us understand how people are using our services. The Partnership Web team analyzes the Web site logs to continually improve the value of the materials available on this site. We cannot obtain personal information about you from these Web site logs. </p>
+        		<p class="text">If during your visit to the Partnership's Web site you participate in a survey or send an email, the following additional information will be collected:</p>
+        		<ol type="1">
+        				<li class="text" 
+style="mso-margin-top-alt: auto; mso-margin-bottom-alt: auto; mso-list: l0 level1 lfo9; tab-stops: list .5in">The email address and contents of the email.</li>
+          		<li class="text" 
+style="mso-margin-top-alt: auto; mso-margin-bottom-alt: auto; mso-list: l0 level1 lfo9; tab-stops: list .5in">Information volunteered in response to a survey. </li>
+        		</ol>
+        		<p class="text">The information collected is not limited to text characters and may include audio, video and graphic information formats you send us. The information is retained in accordance with <a href="http://apps.leg.wa.gov/RCW/default.aspx?cite=40.14" target="_blank">Chapter 40.14 RCW</a>, Preservation and Destruction of Public Records.</p>
+        		<p class="text">The Partnership will use your email address to respond appropriately. This may be to respond to you, to address issues you identify, to further improve our Web site, or to forward the email to another agency for appropriate action. Survey information is used for the purpose designated.
+        				<O:P>
+        				<!--delete-->
+			</p>
+        		<p align="left" class="text">
+        				<O:P>
+        				<a href="#top">^ Top</a>
+        				<!--delete-->
+			</p>
+        		<h4 class="text"><a 
+name="_Personal_Information_and" id="_Personal_Information_and"></a><span class="text_headline">Personal information and choice </span></h4>
+        		<p class="text"><strong>You may choose whether to provide personal information online.</strong><br />
+        				&quot;Personal information&quot; is information about a natural person that is readily identifiable to that specific individual. Personal information includes such things as an individual's name, address, and phone number. A domain name or Internet Protocol address is not considered personal information. </p>
+        		<p class="text">We collect no personal information about you unless you voluntarily participate in an activity that asks for information (i.e. sending an email or participating in a survey). If you choose not to participate in these activities, your choice will in no way effect your ability to use any other feature of the Partnership's Web site. </p>
+        		<p class="text">If personal information is requested on the Web site or volunteered by the user, state law and the federal Privacy Act of 1974 may protect it. However, this information is a public record once you provide it, and may be subject to public inspection and copying if not protected by federal or state law.</p>
+        		<p class="text">Users are cautioned that the collection of personal information requested from or volunteered by children on-line or by email will be treated the same as information given by an adult, and may be subject to public access.
+        				<O:P>
+        				<!--delete-->
+			</p>
+        		<p class="text">
+        				<O:P>
+        				<a href="#top">^ Top</a>
+        				<!--delete-->
+			</p>
+        		<h4 class="text"><a 
+name="_Public_Disclosure" id="_Public_Disclosure"></a><span class="text_headline">Public disclosure</span></h4>
+        		<p class="text">In the State of Washington, laws exist to ensure that government is open and that the public has a right to access appropriate records and information possessed by state government. At the same time, there are exceptions to the public's right to access public records that serve various needs including the privacy of individuals. Exceptions are provided by both state and federal laws. </p>
+        		<p class="text">All information collected at this site becomes public records that may be subject to inspection and copying by the public, unless an exemption in law exists. <a 
+href="http://search.leg.wa.gov/wslrcw/RCW%20%2042%20%20TITLE/RCW%20%2042%20.%2017%20%20CHAPTER/RCW%20%2042%20.%2017%20.260.htm">RCW 42.17. 260(1)</a> states that: </p>
+        		<p class="text style1" style="MARGIN-LEFT: 1in">Each agency, in accordance with published rules, shall make available for public inspection and copying all public records, unless the record falls within the specific exemptions of subsection (6) of this section <i>[RCW 42.17.260(6)]</i>, RCW 42.17.310, 42.17.315, or other statute which exempts or prohibits disclosure of specific information or records. To the extent required to prevent an unreasonable invasion of personal privacy interests protected by RCW 42.17.310 and 42.17.315, an agency shall delete identifying details in a manner consistent with RCW 42.17.310 and 42.17.315 when it makes available or publishes any public record; however, in each case, the justification for the deletion shall be explained fully in writing.</p>
+        		<p class="text">In the event of a conflict between this Privacy Notice and the Public Records Act or other law governing the disclosure of records, the Public Records Act or other applicable law will control.
+        				<O:P>
+        				<!--delete-->
+			</p>
+        		<p class="text">
+        				<O:P>
+        				<a href="#top">^ Top</a>
+        				<!--delete-->
+			</p>
+        		<h4 class="text"><a 
+name="_Access_and_Correction" id="_Access_and_Correction"></a><span class="text_headline">Access and correction of personal information</span></h4>
+        		<p class="text">You may review any personal information the Partnership collects about you. You may recommend changes to your personal information you believe in error by submitting a written request that credibly shows the error. If you believe that your personal information is being used for a purpose other than what was intended when submitted, you may contact the Partnership. In all cases, we will take reasonable steps to verify your identity before granting access or making corrections. See Contact Information section.
+        				<O:P>
+        				<!--delete-->
+			</p>
+        		<p class="text">
+        				<O:P>
+        				<a href="#top">^ Top</a>
+        				<!--delete-->
+			</p>
+        		<h4 class="text"><a 
+name="_Security" id="_Security"></a><span class="text_headline">Security</span></h4>
+        		<p class="text">The Puget Sound Partnership is the repository for the Partnership Web site. The Partnership has taken several steps to safeguard the integrity of its telecommunications and computing infrastructure, including but not limited to authentication, monitoring, auditing, and 
+        				encryption. Security measures have been integrated into the design, implementation and day-to-day practices of the entire Puget Sound Partnership Web site's operating environment as part of its continuing commitment to risk management.</p>
+        		<p>This information should not be construed in any way as giving business, legal, or other advice, or warranting as fail proof, the security of information provided via Web sites supported by the Puget Sound Partnership.
+        				<O:P>
+        				<!--delete-->
+			</p>
+        		<p class="text">
+        				<O:P>
+        				<a href="#top">^ Top</a>
+        				<!--delete-->
+			</p>
+        		<h4 class="text"><a 
+name="_Disclaimer" id="_Disclaimer"></a><span class="text_headline">Disclaimer</span></h4>
+        		<p class="text">The Partnership Web site contains  links to other Web sites. These include links to Web sites operated by other government agencies, nonprofit organizations and private businesses. When you link to another site, you are no longer on the Partnership's site and this Privacy Notice will not apply. When you link to another Web site, you are subject to the privacy policy of that new site.</p>
+        		<p class="text">Neither the State of Washington, nor any agency, officer, or employee of the State of Washington warrants the accuracy, reliability or timeliness of any information published by this system, nor endorses any content, viewpoints, products, or services linked from this system, and shall not be held liable for any losses caused by reliance on the accuracy, reliability or timeliness of such information. Portions of such information may be incorrect or not current. Any person or entity that relies on any information obtained from this system does so at his or her own risk.
+        				<O:P>
+        				<!--delete-->
+			</p>
+        		<p class="text">
+        				<O:P>
+        				<a href="#top">^ Top</a>
+        				<!--delete-->
+			</p>
+        		<h4 class="text"><a 
+name="_PSWQAT_Contact_Information" id="_PSWQAT_Contact_Information"></a><span class="text_headline">Puget Sound Partnership contact Information</span></h4>
+        		<p class="text">To access your personally identifiable information we collect, if any, or request correction of factual errors in your personally identifiable information, or to offer comments about the Partnesrhip, contact the Puget Sound Partnership at P.O. Box 40900, Olympia, WA 98504-0900  or email <a href="mailto:info@psp.wa.gov"><span 
+style="mso-bidi-font-size: 10.0pt; mso-bidi-font-family: 'Times New Roman'">info</span></a><a 
+href="mailto:bstewart@psp.wa.gov"><span 
+style="mso-bidi-font-size: 10.0pt; mso-bidi-font-family: 'Times New Roman'">@psp.wa.gov</span></a>.</p>
+        		<p class="text"><a href="#top">^ Top</a></p>
+      
+      		<p>&nbsp;</p>
+			<!-- InstanceEndEditable --> </div>
+		
+		<div class="col-sm-2 padding-20-top padding-0-right"> <!-- InstanceBeginRepeat name="right_nav_repeat" --><!-- InstanceBeginRepeatEntry --> <!-- InstanceBeginEditable name="right_nav_title" -->
+			<div class="right-nav-title margin-0-top ">SEE ALSO</div>
+			<!-- InstanceEndEditable -->
+			<ul class="nav-rightside-custom">
+				<!-- InstanceBeginEditable name="right_nav_links" -->
+				<li class="active" role="presentation"> <a href="#">SCIENCE BASIS OF RECOVERY</a></li>
+				<li role="presentation"> <a href="#">STRATEGIC SCIENCE PLAN</a></li>
+				<li role="presentation"> <a href="#">SCIENCE WORKPLAN</a></li>
+				<li role="presentation"> <a href="#">ADAPTIVE MANAGEMENT WORKPLAN</a></li>
+				<li role="presentation"> <a href="#">PUGET SOUND PRESSURE ASSESSMENT</a></li>
+				<li role="presentation"> <a href="#">PUGET SOUND SCIENCE UPDATES</a></li>
+				<!-- InstanceEndEditable -->
+			</ul>
+			<!-- InstanceEndRepeatEntry --><!-- InstanceEndRepeat --> </div>
+	</div>
+	<!--END OF ROW --> 
+</div>
+<!--END OF CONTENT CONTAINER -->
+
+<?php include 'includes/footer-inc.html';?>
+
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) --> 
+<script src="js/jquery-1.11.2.min.js"></script> 
+
+<!-- Include all compiled plugins (below), or include individual files as needed --> 
+<script src="js/bootstrap.js"></script> 
+<!-- custom js --> 
+<script src="js/custom.js"></script> 
+<!-- Google Tracking  --> 
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-69373425-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
+</body>
+<!-- InstanceEnd --></html>
