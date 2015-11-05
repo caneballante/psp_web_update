7c71bdbd9a2d517efbd1247bbd1b4e0f8dfa8931
caneballante <jonbridgman@gmail.com>
2015-11-05 10:39:00 -0800

    2016 AA update
    removed incorrect timeline graphic and color banner optional

--- 2016_AA_update.php
+++ 2016_AA_update.php
@@ -5,7 +5,7 @@
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <!-- InstanceBeginEditable name="doctitle" -->
-<title>Puget Sound Partnership - 2016 Action Agenda Update </title>
+<title>Puget Sound Partnership - 2016 Action Agenda Update</title>
 <!-- InstanceEndEditable -->
 <!-- Bootstrap -->
 <link href="css/bootstrap.css" rel="stylesheet">
@@ -26,7 +26,7 @@
 <script>
 /*this variable is used to set the proper nav to active. It should to the order the nav item is in the list*/
   	navSelected = 1;
-</script> 
+</script>
 <!-- InstanceEndEditable -->
 <!-- InstanceParam name="OptionalRegion1" type="boolean" value="true" -->
 <!-- InstanceParam name="OptionalRegion2" type="boolean" value="false" -->
@@ -35,7 +35,7 @@
 <!-- InstanceParam name="OptionalRegion5" type="boolean" value="true" -->
 <!-- InstanceParam name="OptionalRegion6" type="boolean" value="true" -->
 <!-- InstanceParam name="OptionalRegion7" type="boolean" value="false" -->
-<!-- InstanceParam name="OptionalRegion8" type="boolean" value="true" -->
+<!-- InstanceParam name="OptionalRegion8" type="boolean" value="false" -->
 </head>
 <body>
 <?php include 'includes/modal-inc.html';?>
@@ -46,11 +46,7 @@
 		<h3>&nbsp;</h3>
 	</div>
 </header>
-<!-- InstanceEndEditable -->  <!-- InstanceBeginEditable name="EditRegion11" -->
-<div class="row">
-	<div class="col-sm-12 orange"><img src="file:///Macintosh HD/Users/jonbridgman/Desktop/files/sites/psp/2015-SOS-banner-logo.png" width="433" height="89" alt=""/></div>
-</div>
-<!-- InstanceEndEditable -->
+<!-- InstanceEndEditable -->  
 <div class="container page-content padding-50-bottom">
 	<div class="row">
 		<div class="col-sm-3 padding-20-top"></div>
@@ -66,15 +62,15 @@
 		<div class="col-sm-3">
 			<ul class="nav nav-stacked nav-pills nav-leftside-custom padding-left-0 margin-10-top">
 				<!-- InstanceBeginEditable name="left_nav" -->
-				<?php include 'includes/ln-2016-AA.html';?>	
+				<?php include 'includes/ln-2016-AA.html';?>
 				<!-- InstanceEndEditable -->
 			</ul>
 		</div>
 		 
 		<div class="col-sm-7 padding-20-top content-column"> <!-- InstanceBeginEditable name="6col_content" -->
 			<div class="highlightbox">
-			<h2 class="margin-0-top"><span class="new-red">NEW!</span> 9/21/15 Near Term Action Proposals</h2>
-			<p>Are you interested in proposing an activity to be included in the Puget Sound Action Agenda? Proposals are due 10 a.m., December 31, 2015. <a href="2016_AA_NTA.php">Find out more.</a></p>
+				<h2 class="margin-0-top"><span class="new-red">NEW!</span> 9/21/15 Near Term Action Proposals</h2>
+				<p>Are you interested in proposing an activity to be included in the Puget Sound Action Agenda? Proposals are due 10 a.m., December 31, 2015. <a href="2016_AA_NTA.php">Find out more.</a></p>
 			</div>
 			<h2>What is in the Action Agenda?</h2>
 			<p> Several years of <strong>research and effort</strong> have shown three areas of focus that deliver the most progress toward recovery for dollars invested. Termed &ldquo;Strategic Initiatives&rdquo; these <strong>three priority areas</strong> form the basis of the Action Agenda for 2016.</p>
@@ -84,8 +80,7 @@
 			<p>Two types of actions are included in the Action Agenda: Near Term Actions, which can be begun or completed in 2 years; and longer term actions. Local Integrating Organizations, regional organizations and groups, and state agencies may sponsor Near Term Actions (NTAs) and propose them for inclusion in the Action Agenda. If the proposed actions align with one of the three Strategic Initiatives, they may be considered for ranking in the Action Agenda. Once an action becomes designated as an NTA, it becomes eligible for state and federal funding.</p>
 			<h2>What is the process for updating the Action Agenda?</h2>
 			<p>The process for updating Near Term Actions for the 2016 Action Agenda began in spring 2015 with the formation of three Strategic Initiative Update Workgroups. Each workgroup reviewed and discussed the scope for addressing the issues associated with their assigned Strategic Initiative. In June 2015, the workgroups summarized their findings in a white paper submitted to the Science Panel for consideration and to partners and the public for a 30-day comment period. After the Leadership Council reviews and approves the scope for the Strategic Initiatives, the process of selecting Near Term Actions for the 2016 Action Agenda can begin. The figure below shows the process for developing the 2016 Action Agenda.</p>
-			<p><img class="img-responsive" src="images/AA_2015_historical_timeline.jpg" width="600" height="456" alt=""/></p>
-			<ul type="disc">
+			<ul>
 				<li>Local Integrating Organizations work within their communities to      develop local plans of action for recovering Puget Sound, preferably      aligning with the Action Agenda.</li>
 				<li>Partners propose Near Term Actions associated with the Strategic      Initiatives.</li>
 				<li>Interdisciplinary teams of experts evaluate and rank the Near Term      Actions based in part on the importance of the NTAs in addressing the      Strategic Initiatives and for Sound-wide recovery Sound-wide.</li>
@@ -109,7 +104,8 @@
 			<ul class="nav-rightside-custom">
 				<!-- InstanceBeginEditable name="right_nav_links" -->
 				<li role="presentation"><a href="action_agenda_center.php">Action Agenda Overview</a></li>
-				<li role="presentation"><a href="2014_action_agenda_download.php">2014-15 Action Agenda</a></li><li role="presentation"><a href="2016_AA_implementation_strategies.php">Implementation Strategies</a></li>
+				<li role="presentation"><a href="2014_action_agenda_download.php">2014-15 Action Agenda</a></li>
+				<li role="presentation"><a href="2016_AA_implementation_strategies.php">Implementation Strategies</a></li>
 				<li role="presentation"><a href="http://wa-geoservices.maps.arcgis.com/apps/Viewer/index.html?appid=4a40b5acc8884b849dd3c8dd65c2d985">Action Area Maps</a></li>
 				<!-- InstanceEndEditable -->
 			</ul>
