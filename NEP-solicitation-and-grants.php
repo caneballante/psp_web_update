f21bd67d29d3537796504cb009b229a06321caed
caneballante <jonbridgman@gmail.com>
2015-12-04 12:22:18 -0800

    454 NEP
    removed the solicitation notice

--- NEP-solicitation-and-grants.php
+++ NEP-solicitation-and-grants.php
@@ -26,7 +26,7 @@
 <script>
 /*this variable is used to set the proper nav to active. It should to the order the nav item is in the list*/
   	navSelected = 7;
-</script> 
+</script>
 <!-- InstanceEndEditable -->
 <!-- InstanceParam name="OptionalRegion1" type="boolean" value="true" -->
 <!-- InstanceParam name="OptionalRegion2" type="boolean" value="false" -->
@@ -35,22 +35,18 @@
 <!-- InstanceParam name="OptionalRegion5" type="boolean" value="true" -->
 <!-- InstanceParam name="OptionalRegion6" type="boolean" value="true" -->
 <!-- InstanceParam name="OptionalRegion7" type="boolean" value="false" -->
-<!-- InstanceParam name="OptionalRegion8" type="boolean" value="true" -->
+<!-- InstanceParam name="OptionalRegion8" type="boolean" value="false" -->
 </head>
 <body>
 <?php include 'includes/modal-inc.html';?>
 <!-- START IMAGE HEADER --> 
  <!-- InstanceBeginEditable name="overviewphoto" -->
 <header class="overview-page-image-style overview-page-image6">
-
-<div class="overview-quote pull-right"><h3>The Puget Sound NEP brings together regional leaders, experts and community members to protect, restore and maintain the ecological integrity of Puget Sound.</h3></div>
-
- </header>
-<!-- InstanceEndEditable -->  <!-- InstanceBeginEditable name="EditRegion11" -->
-<div class="row">
-	<div class="col-sm-12 orange"><img src="file:///Macintosh HD/Users/jonbridgman/Desktop/files/sites/psp/2015-SOS-banner-logo.png" width="433" height="89" alt=""/></div>
-</div>
-<!-- InstanceEndEditable -->
+	<div class="overview-quote pull-right">
+		<h3>The Puget Sound NEP brings together regional leaders, experts and community members to protect, restore and maintain the ecological integrity of Puget Sound.</h3>
+	</div>
+</header>
+<!-- InstanceEndEditable -->  
 <div class="container page-content padding-50-bottom">
 	<div class="row">
 		<div class="col-sm-3 padding-20-top"></div>
@@ -65,91 +61,80 @@
 		<div class="col-sm-3">
 			<ul class="nav nav-stacked nav-pills nav-leftside-custom padding-left-0 margin-10-top">
 				<!-- InstanceBeginEditable name="left_nav" -->
-			<?php include 'includes/ln-NEP.html';?>	
-			<!-- InstanceEndEditable -->
+				<?php include 'includes/ln-NEP.html';?>
+				<!-- InstanceEndEditable -->
 			</ul>
 		</div>
 		 
 		<div class="col-sm-7 padding-20-top content-column"> <!-- InstanceBeginEditable name="6col_content" -->
-			
-			<div class="highlightbox">
-				<p><strong>EPA releases RFP for Strategic Initiative implementation leads for FY2016
-					</strong><br>
-			On September 22, EPA released a Request for Proposals titled “Puget Sound Action Agenda – Implementation Strategies and Actions for Vital Sign Recovery Assistance Program.” These funds will support the three Strategic Initiative Teams and their work with the EPA Puget Sound National Estuary Program and Puget Sound Partnership’s 2016 Action Agenda. Grant recipients will provide technical advice and manage subawards for implementing priority Action Agenda strategies.<strong> Proposals are due November 30, 2015</strong>.  For more details on the RFP, visit EPA’s <a href="http://www2.epa.gov/puget-sound/funding-and-grants-puget-sound">Funding and Grants for Puget Sound website</a>.</p></div><br /><br />
-
-		<p>Under the National Estuary Program, the U.S. Environmental Protection Agency (EPA) provides funding to Washington state agencies as Lead Organizations and to the Northwest Indian Fisheries Commission to implement the Puget Sound Action Agenda. Lead Organizations develop and implement six-year strategies for four categories of ecosystem protection and restoration. EPA funding to the Lead Organizations and the Fisheries Commission totals about $115 million since 2010. EPA can provide additional funding for the next two yearsof the Lead Organization six year strategies, depending on congressional appropriations. Lead Organizations   investments implement the 2012 Puget Sound Action Agenda and the priorities set through the strategic initiative process. The three strategic initiatives for 2012/2013 include:</p>
-<ul>
-		<li>Prevent pollution from urban stormwater runoff</li>
-		  <li>Protect and restore habitat</li>
-		  <li>Recover shellfish beds<br />
-	      </li>
-  </ul>
-<p>Lead Organizations use competitive processes, and in some cases direct awards, to fund work identified in their six-year strategies. The <a href="http://blogs.nwifc.org/psp/about/">Northwest Indian Fisheries Commission </a> awards grants to the 19 federally recognized tribes in Puget Sound and any authorized consortium of these tribes to implement high priority projects identified in the Action Agenda.</p>
-<p>The Puget Sound Partnership also has cooperative agreements with EPA focused on regional engagement, stewardship and managing the implementation of the Action Agenda.  </p>
-<p>EPA funding is a small part of the overall funding needed to  protect and restore Puget Sound. The 2013 State of the Sound report estimates a $390 million shortfall over a three year period to implement near-term actions identified in the 2012 Action Agenda. </p>
-<p>For more information on other grant and loan funding for protection and restoration projects in Puget Sound visit:</p>
-<ul>
-		<li><a href="http://www.rco.wa.gov/grants/index.shtml">Washington state Recreation and Conservation Office website</a></li>
-  <li><a href="http://www.ecy.wa.gov/puget_sound/index.html">Washington state Department of Ecology website</a><br />
-    <br />
-  </li>
-</ul>
-<h2><a name="1" id="1"></a><strong>Funding Opportunities</strong></h2>
-		<p>Click here to receive email notification of funding opportunities through the Lead Organizations. </p>
-
-		<table width="100%" border="0" cellpadding="0" bordercolor="#89B7D5">
-          <tr>
-            <td> <a href="http://psp.us1.list-manage.com/subscribe?u=1e527a1c4505cbd9d6662d091&id=a58a377414"><strong>Sign up to receive RFP notification by email</strong></a></td>
-          </tr>
-        </table>
-		
-		<p>&nbsp;</p>
-		<p>Specific information about funding opportunities, including how to apply is available at the links below:</p>
-		<p><strong><a href="http://wdfw.wa.gov/grants/ps_marine_nearshore/">Marine and nearshore protection and restoration</a></strong> Lead Organization focuses on: Effective regulation and stewardship, strategic capital investments, programs that address high priority threats to the marine and nearshore ecosystem, and adaptive management. The state departments of Fish and Wildlife and Natural Resources are the Lead Organizations for this category.</p>
-	    <ul>
-	      <li><a href="https://pspwa.box.com/LOWorkPlanMarineNearshore">Six-Year Investment Strategy to Protect and Restore Marine and Nearshore Habitat FFY2014-15</a></li>
-		  <li><a href="https://pspwa.box.com/LOWorkPlanMarineNearshoreTable">Six-Year Investment Strategy to Protect and Restore Marine and Nearshore Habitat Table FFY2014-15</a></li>
-		  
-		  
-
-      </ul>
-<p>For more information, please contact:<br />
-        Patricia Jatczak, WDFW: <a href="mailto:Patricia.Jatczak@dfw.wa.gov">Patricia.Jatczak@dfw.wa.gov</a>, 360.902.2597</p>
-	    <p> </p>
-	    <p><a href="http://www.ecy.wa.gov/puget_sound/grants_fed_watershed.html"><strong>Watershed protection and restoration</strong></a> focuses on: Watershed characterization, land use and working lands, strategies to manage stormwater and strategies to protect and restore watershed ecosystems. The state departments of Ecology and Commerce are the Lead Organizations for this work.</p>
-	    <ul>
-	      <li><a href="https://pspwa.box.com/LOWorkPlanWatershed">Six Year Investment Strategy to Protect and Restore Watersheds FFY2014-15</a></li>
-      </ul>
-<p>For more information, please contact:</p>
-<p> <br />
-		Colin Hume, ECY: <a href="mailto:chum461@ECY.WA.GOV">chum461@ECY.WA.GOV</a>, 425.649.7139<br />
-		<br />
-		Doug Peters, Department of Commerce: <a href="mailto:doug.peters@commerce.wa.gov">doug.peters@commerce.wa.gov</a>, 360.725.3046</p>
-	    <p> </p>
-	    <p><a href="http://www.ecy.wa.gov/puget_sound/grants_fed_toxics.html"><strong>Toxics and nutrients prevention, reduction and control</strong></a> Lead Organization focuses on:  Reducing toxic chemicals in products, preventing toxic chemicals in stormwater, determining the how human sources of nutrients affect Puget Sound, and reducing nutrient loadings in a prioritized fashion. The state Department of Ecology is the Lead Organization for this category.</p>
-	    <ul>
-	      <li><a href="https://pspwa.box.com/LOWorkPlanPathogens">Six year Investment Strategy to Prevent, Reduce and Control Toxics and Nutrient Pollution FFY2014-15</a></li>
-      </ul>
-      <p>For more information, please contact:<br />
-        Blake Nelson, Department of Ecology: <a href="mailto:bnel461@ECY.WA.GOV">bnel461@ECY.WA.GOV</a>, 360.407.6940</p>
-	    <p> </p>
-	    <p><a href="http://www.doh.wa.gov/CommunityandEnvironment/Shellfish/EPAGrants/PathogensGrant.aspx"><strong>Pathogen prevention, reduction and control </strong></a>Lead Organization focuses on:  Implementing local on-site sewage management plans, establishing sustainable local nonpoint pollution identification and correction programs, and reducing pathogen loadings through improved manure management. The state departments of Health and Ecology are the Lead Organizations.</p>
-	    <ul>
-   		  <li><a href="https://pspwa.box.com/LOWorkPlanPathogens">Six-Year Investment Strategy to Prevent, Reduce and Control Pathogen Pollution FFY2014-15</a></li>
-      </ul>
-	    <p>For more information, please contact:<br />
-	      Mary Knackstedt, Department of Health: <a href="mailto:Mary.Knackstedt@DOH.WA.GOV">Mary.Knackstedt@DOH.WA.GOV</a>, 360.236.3319</p>
-	    <p><br />
-	    		</p>
-	    <p><strong>Projects Funded by Lead Organizations</strong><br />
-          <a href="https://pspwa.box.com/s/jv7zp8dsaam8i31zko6vhn52yaffejb4">Click here</a> to find a list of projects that Lead Organizations have funded to implement the Puget Sound Action Agenda. <br />
-        </p>
-	    <table width="100%" border="0" cellpadding="0" bordercolor="#89B7D5">
-          <tr>
-            <td><a href="http://psp.wa.gov/gis/NEPAtlas/NEPActivities"><strong>EPA Puget Sound Funding Map</strong></a></td>
-          </tr>
-        </table>
-		<!-- InstanceEndEditable --> </div>
+			<p>Under the National Estuary Program, the U.S. Environmental Protection Agency (EPA) provides funding to Washington state agencies as Lead Organizations and to the Northwest Indian Fisheries Commission to implement the Puget Sound Action Agenda. Lead Organizations develop and implement six-year strategies for four categories of ecosystem protection and restoration. EPA funding to the Lead Organizations and the Fisheries Commission totals about $115 million since 2010. EPA can provide additional funding for the next two yearsof the Lead Organization six year strategies, depending on congressional appropriations. Lead Organizations   investments implement the 2012 Puget Sound Action Agenda and the priorities set through the strategic initiative process. The three strategic initiatives for 2012/2013 include:</p>
+			<ul>
+				<li>Prevent pollution from urban stormwater runoff</li>
+				<li>Protect and restore habitat</li>
+				<li>Recover shellfish beds<br />
+				</li>
+			</ul>
+			<p>Lead Organizations use competitive processes, and in some cases direct awards, to fund work identified in their six-year strategies. The <a href="http://blogs.nwifc.org/psp/about/">Northwest Indian Fisheries Commission </a> awards grants to the 19 federally recognized tribes in Puget Sound and any authorized consortium of these tribes to implement high priority projects identified in the Action Agenda.</p>
+			<p>The Puget Sound Partnership also has cooperative agreements with EPA focused on regional engagement, stewardship and managing the implementation of the Action Agenda. </p>
+			<p>EPA funding is a small part of the overall funding needed to  protect and restore Puget Sound. The 2013 State of the Sound report estimates a $390 million shortfall over a three year period to implement near-term actions identified in the 2012 Action Agenda. </p>
+			<p>For more information on other grant and loan funding for protection and restoration projects in Puget Sound visit:</p>
+			<ul>
+				<li><a href="http://www.rco.wa.gov/grants/index.shtml">Washington state Recreation and Conservation Office website</a></li>
+				<li><a href="http://www.ecy.wa.gov/puget_sound/index.html">Washington state Department of Ecology website</a><br />
+					<br />
+				</li>
+			</ul>
+			<h2><a name="1" id="1"></a><strong>Funding Opportunities</strong></h2>
+			<p>Click here to receive email notification of funding opportunities through the Lead Organizations. </p>
+			<table width="100%" border="0" cellpadding="0" bordercolor="#89B7D5">
+				<tr>
+					<td><a href="http://psp.us1.list-manage.com/subscribe?u=1e527a1c4505cbd9d6662d091&id=a58a377414"><strong>Sign up to receive RFP notification by email</strong></a></td>
+				</tr>
+			</table>
+			<p>&nbsp;</p>
+			<p>Specific information about funding opportunities, including how to apply is available at the links below:</p>
+			<p><strong><a href="http://wdfw.wa.gov/grants/ps_marine_nearshore/">Marine and nearshore protection and restoration</a></strong> Lead Organization focuses on: Effective regulation and stewardship, strategic capital investments, programs that address high priority threats to the marine and nearshore ecosystem, and adaptive management. The state departments of Fish and Wildlife and Natural Resources are the Lead Organizations for this category.</p>
+			<ul>
+				<li><a href="https://pspwa.box.com/LOWorkPlanMarineNearshore">Six-Year Investment Strategy to Protect and Restore Marine and Nearshore Habitat FFY2014-15</a></li>
+				<li><a href="https://pspwa.box.com/LOWorkPlanMarineNearshoreTable">Six-Year Investment Strategy to Protect and Restore Marine and Nearshore Habitat Table FFY2014-15</a></li>
+			</ul>
+			<p>For more information, please contact:<br />
+				Patricia Jatczak, WDFW: <a href="mailto:Patricia.Jatczak@dfw.wa.gov">Patricia.Jatczak@dfw.wa.gov</a>, 360.902.2597</p>
+			<p> </p>
+			<p><a href="http://www.ecy.wa.gov/puget_sound/grants_fed_watershed.html"><strong>Watershed protection and restoration</strong></a> focuses on: Watershed characterization, land use and working lands, strategies to manage stormwater and strategies to protect and restore watershed ecosystems. The state departments of Ecology and Commerce are the Lead Organizations for this work.</p>
+			<ul>
+				<li><a href="https://pspwa.box.com/LOWorkPlanWatershed">Six Year Investment Strategy to Protect and Restore Watersheds FFY2014-15</a></li>
+			</ul>
+			<p>For more information, please contact:</p>
+			<p> <br />
+				Colin Hume, ECY: <a href="mailto:chum461@ECY.WA.GOV">chum461@ECY.WA.GOV</a>, 425.649.7139<br />
+				<br />
+				Doug Peters, Department of Commerce: <a href="mailto:doug.peters@commerce.wa.gov">doug.peters@commerce.wa.gov</a>, 360.725.3046</p>
+			<p> </p>
+			<p><a href="http://www.ecy.wa.gov/puget_sound/grants_fed_toxics.html"><strong>Toxics and nutrients prevention, reduction and control</strong></a> Lead Organization focuses on:  Reducing toxic chemicals in products, preventing toxic chemicals in stormwater, determining the how human sources of nutrients affect Puget Sound, and reducing nutrient loadings in a prioritized fashion. The state Department of Ecology is the Lead Organization for this category.</p>
+			<ul>
+				<li><a href="https://pspwa.box.com/LOWorkPlanPathogens">Six year Investment Strategy to Prevent, Reduce and Control Toxics and Nutrient Pollution FFY2014-15</a></li>
+			</ul>
+			<p>For more information, please contact:<br />
+				Blake Nelson, Department of Ecology: <a href="mailto:bnel461@ECY.WA.GOV">bnel461@ECY.WA.GOV</a>, 360.407.6940</p>
+			<p> </p>
+			<p><a href="http://www.doh.wa.gov/CommunityandEnvironment/Shellfish/EPAGrants/PathogensGrant.aspx"><strong>Pathogen prevention, reduction and control </strong></a>Lead Organization focuses on:  Implementing local on-site sewage management plans, establishing sustainable local nonpoint pollution identification and correction programs, and reducing pathogen loadings through improved manure management. The state departments of Health and Ecology are the Lead Organizations.</p>
+			<ul>
+				<li><a href="https://pspwa.box.com/LOWorkPlanPathogens">Six-Year Investment Strategy to Prevent, Reduce and Control Pathogen Pollution FFY2014-15</a></li>
+			</ul>
+			<p>For more information, please contact:<br />
+				Mary Knackstedt, Department of Health: <a href="mailto:Mary.Knackstedt@DOH.WA.GOV">Mary.Knackstedt@DOH.WA.GOV</a>, 360.236.3319</p>
+			<p><br />
+			</p>
+			<p><strong>Projects Funded by Lead Organizations</strong><br />
+				<a href="https://pspwa.box.com/s/jv7zp8dsaam8i31zko6vhn52yaffejb4">Click here</a> to find a list of projects that Lead Organizations have funded to implement the Puget Sound Action Agenda. <br />
+			</p>
+			<table width="100%" border="0" cellpadding="0" bordercolor="#89B7D5">
+				<tr>
+					<td><a href="http://psp.wa.gov/gis/NEPAtlas/NEPActivities"><strong>EPA Puget Sound Funding Map</strong></a></td>
+				</tr>
+			</table>
+			<!-- InstanceEndEditable --> </div>
 		
 		<div class="col-sm-2 padding-20-top padding-0-right"> <!-- InstanceBeginRepeat name="right_nav_repeat" --><!-- InstanceEndRepeat --> </div>
 	</div>
