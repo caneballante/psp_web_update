8fad7057336e0f18f5544db46b669ccd89b51983
caneballante <jonbridgman@gmail.com>
2016-01-08 15:31:44 -0800

    LEG UPDATES
    +archived 2015 links and added 2016 headers

--- legislative_updates.php
+++ legislative_updates.php
@@ -67,45 +67,20 @@
 		</div>
 		 
 		<div class="col-sm-7 padding-20-top content-column"> <!-- InstanceBeginEditable name="6col_content" -->
-			<h2 class="margin-0-top">2015 Legislative Updates</h2>
+			<h2 class="margin-0-top">2016 Legislative Updates</h2>
 			<ul>
-				<li><a href="https://pspwa.box.com/s/223vr288axxnpgpyzvxxprjd5k8sk1s9">Week 1 Legislative Update - January 12, 2015</a></li>
-				<li><a href="https://pspwa.box.com/s/sox6bt7x6gh5x7t1g5na0cepth37gcdt">Week 2 Legislative Update - January 19, 2015</a></li>
-				<li><a href="https://pspwa.box.com/s/eq302184lfffrjwa7vhw7tmewl31ixqm">Week 3 Legislative Update - January 26, 2015</a></li>
-				<li>Week 4 Legislative Update - February 2, 2015</li>
-				<li><a href="https://app.box.com/s/223vr288axxnpgpyzvxxprjd5k8sk1s9">Week 5 Legislative Update - February 9, 2015</a></li>
-				<li> <a href="https://app.box.com/s/cwuspu2updtqmrat0klzo5ln8pzvyj3j">Week 6 Legislative Update - February 16, 2015</a></li>
-				<li> <a href="https://app.box.com/s/148id4r6fmgzond2ca5qf6rbyk1q635k">Week 7 Legislative Update - February 23, 2015</a> </li>
-				<li> <a href="https://app.box.com/s/l7tcaxah89hj3ystaww16xdg5dc6ipms">Week 8 Legislative Update - March 2, 2015</a></li>
-				<li><a href="https://app.box.com/s/gujihq20vsn15o5ulwuk6y29et5s18qm" target="_blank">Week 9 and 10 Legislative Update - March 18, 2015</a> </li>
-				<li> <a href="https://app.box.com/s/7hpa2qmh7dkkciyr5l01bet15ogw40qg" target="_blank">Week 11 and 12 Legislative Update - April 6, 2015</a></li>
-				<li><a href="https://app.box.com/s/6m6l9ita9ekj66kocg2w0vtivw5qvum5">Special Edition Legislative Update April 9th, 2015</a></li>
-				<li><a href="https://app.box.com/s/edosu70lstg57e6t6twgikkyiapdxiz5">Special Edition Legislative Update May 6th, 2015</a></li>
-				<li><a href="https://app.box.com/s/0xmviclyorp3b37oujv7n31ruz1osctz">Special Edition Legislative Update June 5th, 2015</a></li>
-				<li><a href="https://app.box.com/s/aiv24rwhkhestcma4yx24kg52j70fxyj">Final Legislative Update July 10th, 2015</a></li>
+				<li>Coming soon</li>
 			</ul>
-			<h2>2015 Legislative Calendars </h2>
+			<h2>2016 Legislative Calendars </h2>
 			<ul>
-				<li>Week 1 Calendar</li>
-				<li><a href="https://pspwa.box.com/s/1b5bmdx2rzwb7m5j89krxohenmzkpbqu">Week 2 Calendar</a></li>
-				<li><a href="https://pspwa.box.com/s/ltis8m34p4qe2v9gdab512op18eiiqtb">Week 3 Calendar</a></li>
-				<li><a href="https://pspwa.box.com/s/wn8i8qhypl2e1dm5hrcsgvo3124y03tp">Week 4 Calendar</a></li>
-				<li><a href="https://pspwa.box.com/s/wn8i8qhypl2e1dm5hrcsgvo3124y03tp">Week 5 Calendar </a></li>
-				<li><a href="https://app.box.com/s/73ks47c7wi56q5sgoe18ta3bpjv8440q">Week 6 Calendar</a></li>
-				<li><a href="https://app.box.com/s/0lmstilx2eaplp72yzrxsz0to0a8d5cr">Week 7 Calendar</a></li>
-				<li><a href="https://app.box.com/s/c3ngeffe9ez902ppzumo52b7j7eyic3c">Week 8 Calendar</a></li>
-				<li><a href="https://app.box.com/s/1b5bmdx2rzwb7m5j89krxohenmzkpbqu">Week 9 Calendar</a></li>
-				<li><a href="https://app.box.com/s/d1z5fbig0jsk0opb8jrl7tdq9a8g1tou">Week 10 Calendar</a></li>
-				<li><a href="https://app.box.com/s/v95epgccc6mhn2bnht3qelalg7cnnos5">Week 11 Calendar</a></li>
-				<li><a href="https://app.box.com/s/gj6oxycvr6f2eejr8e2bet9g2vmkfscd">Week 12 Calendar</a></li>
-				<li><a href="https://app.box.com/s/zmjowjd91q6d3looln29n3nta2tdhdqw">Week 13 Calendar</a></li>
+				<li><a href="https://pspwa.box.com/s/xsrcyo7a0inik9yt4ujnuj6e4v3vn18e">Week 1 Calendar</a></li>
 			</ul>
-			
-			<h2>2014 Legislative Materials</h2>
+			<h2>Legislative Updates and Calendar Archives</h2>
+			<p><a href="https://pspwa.box.com/s/3fc8pyrlzpwzitsf4gc4hwss8dj5n2zf">View 2015 Materials</a></p>
+		
 			<p><a href="https://pspwa.box.com/s/e9rn1ckyd5u1ljol5he0t6obmjxlj35w">View 2014 materials</a></p>
-			<h2>2013 Legislative calenda</h2>
-			<p><a href="https://pspwa.box.com/s/i36bxnm4gogzeaye44345erld870ny5y">View 2013 materials</a></p>
-			<h2>2012 Legislative Updates</h2>
+		
+			<p><a href="https://pspwa.box.com/s/i36bxnm4gogzeaye44345erld870ny5y">View 2013 materials</a>			</p>
 			<p><a href="https://pspwa.box.com/s/m1ixjryou2m4la3j1sdocfj9umcsws41">View 2012 materials</a></p>
 		<!-- InstanceEndEditable --> </div>
 		
