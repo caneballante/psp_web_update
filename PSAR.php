e47e2252e66192a9b8928eb7e131640369df5242
caneballante <jonbridgman@gmail.com>
2016-01-13 13:33:32 -0800

    PSAR
    +added link to the 2017-19 large cap RFP
    +added placeholder link to SRC PSAR policies

--- PSAR.php
+++ PSAR.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
-<html lang="en"><!-- InstanceBegin template="/Templates/page-4-template.dwt" codeOutsideHTMLIsLocked="false" -->
+<html lang="en">
+<!-- InstanceBegin template="/Templates/page-4-template.dwt" codeOutsideHTMLIsLocked="false" -->
 <head>
 <meta charset="UTF-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -36,13 +37,13 @@
 <body>
 <?php include 'includes/modal-inc.html';?>
 <!-- START IMAGE HEADER --> 
- <!-- InstanceBeginEditable name="overviewphoto" -->
+<!-- InstanceBeginEditable name="overviewphoto" -->
 <header class="overview-page-image-style overview-page-image7">
-
-<div class="overview-quote pull-right"><h3>Investing in habitat restoration to bring back Puget Sound’s dynamic natural systems – for salmon, people, and community</h3></div>
-
- </header>
-<!-- InstanceEndEditable -->  
+	<div class="overview-quote pull-right">
+		<h3>Investing in habitat restoration to bring back Puget Sound’s dynamic natural systems – for salmon, people, and community</h3>
+	</div>
+</header>
+<!-- InstanceEndEditable -->
 <div class="container page-content padding-50-bottom">
 	<div class="row">
 		<div class="col-sm-3 padding-20-top"></div>
@@ -52,67 +53,56 @@
 			<!-- InstanceEndEditable --></div>
 		<div class="col-sm-2 padding-20-top"></div>
 	</div>
-	
-	<div class="row"> 
+	<div class="row">
 		<div class="col-sm-3">
 			<ul class="nav nav-stacked nav-pills nav-leftside-custom padding-left-0 margin-10-top">
 				<!-- InstanceBeginEditable name="left_nav" -->
 				<li role="presentation"> <a href="funding-programs.php">Puget Sound Funding Programs</a></li>
 				<li class="active" role="presentation"> <a href="PSAR.php">Puget Sound Recovery and Acquisition Fund (PSAR)</a></li>
-		
-			<!-- InstanceEndEditable -->
+				<!-- InstanceEndEditable -->
 			</ul>
 		</div>
-		 
 		<div class="col-sm-7 padding-20-top content-column"> <!-- InstanceBeginEditable name="6col_content" -->
-			
-
-     <p>The Puget Sound Acquisition and Restoration program was created in 2007 to help implement the most important habitat protection and restoration priorities for Puget Sound. Funding is appropriated by the Legislature through the Salmon Recovery Funding Board. The Puget Sound Partnership works with local entities to identify and prioritize projects. This funding is critical to advancing the most effective projects throughout our region.</p>
-<h2>Why is PSAR an effective investment?</h2>
-<p><strong>PSAR-funded projects restore critical habitat and salmon populations to Puget Sound. </strong>
-
-Because the projects are thoroughly reviewed, their benefit to improved salmon habitat in Puget Sound is significant. Since 2007, when PSAR fund was initiated, <strong>more than 300</strong> critical habitat projects have been completed. PSAR projects are developed with the input and review of scientists and environmental planners, who use <strong>monitoring data</strong> collected from key habitat indicators to inform project selection and completion.</p>
-<ul>
-		<li><strong>PSAR-funded projects benefit local communities. The projects:</strong></li>
-		<ul>
-				<li>Are <strong>locally vetted</strong> and locally supported</li>
-				<li>Are <strong>endorsed</strong> by citizens, cities, counties, tribes, non-profit organizations, and government agencies</li>
-				<li>Evolve from <strong>hours of volunteer effort</strong> in planning and labor</li>
-				<li>Produce <strong>significant community pride</strong>—and investment—in habitat restoration</li>
-				<li>Generate <strong>local jobs</strong></li>
-				<li>Improve <strong>public access</strong> to Puget Sound</li>
-				<li><strong>Reduce the risk of floods</strong> and damage to private property</li>
-		</ul>
-</ul>
-<ul>
-		<li><strong>PSAR projects make efficient use of public money. </strong></li>
-</ul>
-<p>Every $1 million invested in PSAR projects means at least 17 jobs and over $3 million in economic activity. For the 2013-15 biennium, PSAR projects produced 845 jobs.PSAR projects completed in the 2013-15 biennium restored and protected: 1,289 estuary and nearshore acres, 770 floodplain acres, and 14 river and stream miles.</p>
-<p>To be eligible for funding, proposed projects must first advance through a series of <strong>rigorous, science-driven reviews</strong>. The reviews occur at the local and regional level and ensure that all PSAR projects align with the <strong>Puget Sound Salmon Recovery Plan</strong>, as well as with the Puget Sound Partnership&rsquo;s ecosystem recovery targets, listed in the <strong>Action Agenda</strong>.<br />
-</p>
-<p><em>Staff with the Puget Sound Partnership manage the PSAR grant fund program; PSAR funds are administered through the Washington State Recreation and Conservation Office. For more information about the funding process, or to apply for a PSAR grant, go to <a href="http://www.rco.wa.gov/grants/salmon.shtml">http://www.rco.wa.gov/grants/salmon.shtml</a></em><br />
-</p>
-<p><img src="images/RCO.jpg" width="200" height="52" alt=""/></p>
-		<!-- InstanceEndEditable --> </div>
-		
+			<p>The Puget Sound Acquisition and Restoration program was created in 2007 to help implement the most important habitat protection and restoration priorities for Puget Sound. Funding is appropriated by the Legislature through the Salmon Recovery Funding Board. The Puget Sound Partnership works with local entities to identify and prioritize projects. This funding is critical to advancing the most effective projects throughout our region.</p>
+			<h2>Why is PSAR an effective investment?</h2>
+			<p><strong>PSAR-funded projects restore critical habitat and salmon populations to Puget Sound. </strong> Because the projects are thoroughly reviewed, their benefit to improved salmon habitat in Puget Sound is significant. Since 2007, when PSAR fund was initiated, <strong>more than 300</strong> critical habitat projects have been completed. PSAR projects are developed with the input and review of scientists and environmental planners, who use <strong>monitoring data</strong> collected from key habitat indicators to inform project selection and completion.</p>
+			<ul>
+				<li><strong>PSAR-funded projects benefit local communities. The projects:</strong></li>
+				<ul>
+					<li>Are <strong>locally vetted</strong> and locally supported</li>
+					<li>Are <strong>endorsed</strong> by citizens, cities, counties, tribes, non-profit organizations, and government agencies</li>
+					<li>Evolve from <strong>hours of volunteer effort</strong> in planning and labor</li>
+					<li>Produce <strong>significant community pride</strong>—and investment—in habitat restoration</li>
+					<li>Generate <strong>local jobs</strong></li>
+					<li>Improve <strong>public access</strong> to Puget Sound</li>
+					<li><strong>Reduce the risk of floods</strong> and damage to private property</li>
+				</ul>
+			</ul>
+			<ul>
+				<li><strong>PSAR projects make efficient use of public money. </strong></li>
+			</ul>
+			<p>Every $1 million invested in PSAR projects means at least 17 jobs and over $3 million in economic activity. For the 2013-15 biennium, PSAR projects produced 845 jobs.PSAR projects completed in the 2013-15 biennium restored and protected: 1,289 estuary and nearshore acres, 770 floodplain acres, and 14 river and stream miles.</p>
+			<p>To be eligible for funding, proposed projects must first advance through a series of <strong>rigorous, science-driven reviews</strong>. The reviews occur at the local and regional level and ensure that all PSAR projects align with the <strong>Puget Sound Salmon Recovery Plan</strong>, as well as with the Puget Sound Partnership&rsquo;s ecosystem recovery targets, listed in the <strong>Action Agenda</strong>.<br />
+			</p>
+			<p><em>Staff with the Puget Sound Partnership manage the PSAR grant fund program; PSAR funds are administered through the Washington State Recreation and Conservation Office. For more information about the funding process, or to apply for a PSAR grant, go to <a href="http://www.rco.wa.gov/grants/salmon.shtml">http://www.rco.wa.gov/grants/salmon.shtml</a></em><br />
+			</p>
+			<p><img src="images/RCO.jpg" width="200" height="52" alt=""/></p>
+			<!-- InstanceEndEditable --> </div>
 		<div class="col-sm-2 padding-20-top padding-0-right"> <!-- InstanceBeginRepeat name="right_nav_repeat" --><!-- InstanceBeginRepeatEntry --> <!-- InstanceBeginEditable name="right_nav_title" -->
-					<div class="right-nav-title">RELATED DOCUMENTS</div>
-				<!-- InstanceEndEditable -->
+			<div class="right-nav-title">RELATED DOCUMENTS</div>
+			<!-- InstanceEndEditable -->
 			<ul class="nav-rightside-custom">
 				<!-- InstanceBeginEditable name="right_nav_links" -->
-			          <li role="presentation"><a href="https://app.box.com/s/5h7adnq2hd7u3srmiqqd" target="new">PSAR FUND 2015-17 Ranked List and Map</a></li>
-				
-				
-					 <li role="presentation"><a href="https://app.box.com/s/ii5hkscxa6zk98juwz51" target="new">PSAR 2015-17 Project Snapshots Factsheets Individual</a> </li>
-					 <li role="presentation"><a href=" https://app.box.com/s/fct26xzwhsh3co3kuefl" target="new">PSAR 2015-17 Project Snapshots Factsheets Combined</a> </li>
-				 <li role="presentation"><a href="https://app.box.com/s/3pmck57jdlx5qho7ikwk" target="new">PSAR FUND Fact Sheet</a> </li>
-				
-				 <li role="presentation"><a href="https://app.box.com/s/31rg8lftqjecrrc6erv3" target="new">PSAR FUND Fact Sheet 11"x17" printable</a> </li>
-					 <li role="presentation"><a href="https://app.box.com/s/6bec8ax6ruaytrk4d5ms" target="new">PSAR 2013-15 Project Map</a> </li>
-                     <li role="presentation"><a href="https://pspwa.app.box.com/files/0/f/3135971089/1/f_47216460041" target="new">PSAR Return on Investment 2007-2014</a> </li>
-				
-				
-						<!-- InstanceEndEditable -->
+				<li role="presentation"><a href="https://pspwa.box.com/s/mdwgvfhr7zvlq7d3kngxyt7b8uai5pg3" target="new">Puget Sound Acquisition and Restoration (PSAR) large capital projects: request for proposals for the 2017-19 state biennium</a></li>
+				<li role="presentation">Salmon Recovery Council PSAR Policies (Coming Soon)</li>
+				<li role="presentation"><a href="https://app.box.com/s/5h7adnq2hd7u3srmiqqd" target="new">PSAR FUND 2015-17 Ranked List and Map</a></li>
+				<li role="presentation"><a href="https://app.box.com/s/ii5hkscxa6zk98juwz51" target="new">PSAR 2015-17 Project Snapshots Factsheets Individual</a> </li>
+				<li role="presentation"><a href=" https://app.box.com/s/fct26xzwhsh3co3kuefl" target="new">PSAR 2015-17 Project Snapshots Factsheets Combined</a> </li>
+				<li role="presentation"><a href="https://app.box.com/s/3pmck57jdlx5qho7ikwk" target="new">PSAR FUND Fact Sheet</a> </li>
+				<li role="presentation"><a href="https://app.box.com/s/31rg8lftqjecrrc6erv3" target="new">PSAR FUND Fact Sheet 11"x17" printable</a> </li>
+				<li role="presentation"><a href="https://app.box.com/s/6bec8ax6ruaytrk4d5ms" target="new">PSAR 2013-15 Project Map</a> </li>
+				<li role="presentation"><a href="https://pspwa.app.box.com/files/0/f/3135971089/1/f_47216460041" target="new">PSAR Return on Investment 2007-2014</a> </li>
+				<!-- InstanceEndEditable -->
 			</ul>
 			<!-- InstanceEndRepeatEntry --><!-- InstanceEndRepeat --> </div>
 	</div>
@@ -141,4 +131,5 @@ Because the projects are thoroughly reviewed, their benefit to improved salmon h
 
 </script>
 </body>
-<!-- InstanceEnd --></html>
+<!-- InstanceEnd -->
+</html>
