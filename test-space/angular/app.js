b6d34fa3d3ed81be7d68bef1b676399b0e4924a1
caneballante <jonbridgman@gmail.com>
2016-08-23 14:55:06 -0700

    Testing

---
+++ test-space/angular/app.js
@@ -0,0 +1,41 @@
+// JavaScript Document
+
+
+(function(){
+var app = angular.module('store', []);
+
+app.controller('StoreController', function() {
+	this.products = gems;
+	});
+	
+app.controller('PanelController', function() {
+	this.tab = 1;
+	this.selectTab = function (setTab) {
+		this.tab = setTab;	
+		};
+	this.isSelected = function (checkTab) {
+		return this.tab === checkTab;
+		};
+	});
+
+var gems = [
+		{
+			name: 'dodo',
+			price: 3.22,
+			description: 'this is a description',
+			canPurchase: false,
+			soldOut: false,
+			images : [
+				{
+					full: 'img/gem1_lg.jpg',
+					thumb: 'img/gem1_sm.jpg',
+				},
+				{
+					full: 'img/gem1_lg.jpg',
+					thumb: 'img/gem1_sm.jpg',
+				}
+			]
+		},
+		
+	]
+})();
