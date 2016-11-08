5924ca3e270e2157f4a018c562d2e671fc7017f4
caneballante <jonbridgman@gmail.com>
2016-11-08 14:57:17 -0800

    STRATEGIC INITIATIVES LEADS PAGE

---
+++ strategic-initiatives-leads.php
@@ -0,0 +1,234 @@
+<!DOCTYPE html>
+<html lang="en">
+<!-- InstanceBegin template="/Templates/page-4-template.dwt" codeOutsideHTMLIsLocked="false" -->
+<head>
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<!-- InstanceBeginEditable name="doctitle" -->
+<title>Puget Sound Partnership - Strategic Initiatives Leads</title>
+<!-- InstanceEndEditable -->
+<!-- Bootstrap -->
+<link href="css/bootstrap.css" rel="stylesheet">
+<link href="css/custom.css" rel="stylesheet" type="text/css">
+<link href="css/custom-erika.css" rel="stylesheet" type="text/css">
+<!-- loads the wf-loading class right away to minimize FOUT -->
+<script>document.documentElement.className += ' wf-loading';</script>
+<!-- Font PRENTON TYPEKIT -->
+<script src="https://use.typekit.net/srt5jze.js"></script>
+<script>try{Typekit.load({ async: true });}catch(e){}</script>
+<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+<!--[if lt IE 9]>
+	<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+	<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+<!-- InstanceBeginEditable name="head" -->
+<!-- InstanceEndEditable -->
+<!-- InstanceParam name="OptionalRegion1" type="boolean" value="true" -->
+<!-- InstanceParam name="OptionalRegion2" type="boolean" value="false" -->
+<!-- InstanceParam name="OptionalRegion3" type="boolean" value="true" -->
+<!-- InstanceParam name="OptionalRegion4" type="boolean" value="false" -->
+<!-- InstanceParam name="OptionalRegion5" type="boolean" value="true" -->
+<!-- InstanceParam name="OptionalRegion6" type="boolean" value="true" -->
+<!-- InstanceParam name="OptionalRegion7" type="boolean" value="false" -->
+<!-- InstanceParam name="OptionalRegion8" type="boolean" value="false" -->
+</head>
+<body>
+<?php include 'includes/modal-inc.html';?>
+<!-- START IMAGE HEADER --> 
+<!-- InstanceBeginEditable name="overviewphoto" -->
+<header class="overview-page-image-style overview-page-image10">
+	<div class="overview-quote pull-right"> </div>
+</header>
+<!-- InstanceEndEditable -->
+<div class="container page-content padding-50-bottom">
+	<div class="row">
+		<div class="col-sm-3 padding-20-top"></div>
+		<div class="col-sm-7 padding-20-top"> <!-- InstanceBeginEditable name="6col_header" -->
+			<h1> STRATEGIC INITIATIVES LEADS</h1>
+			<!-- InstanceEndEditable --></div>
+		<div class="col-sm-2 padding-20-top"></div>
+	</div>
+	<div class="row">
+		<div class="col-sm-3">
+			<div class="nav-leftside-custom">
+				<ul class="nav nav-stacked nav-pills nav-leftside-custom padding-left-0 margin-10-top">
+					<!-- InstanceBeginEditable name="left_nav" -->
+					<li role="presentation"> <a href="2014_action_agenda_download.php">2014-15 ACTION AGENDA</a></li>
+					<li class="active" role="presentation"> <a href="action_agenda_strategic_initiatives.php">STRATEGIC INITIATIVES</a></li>
+					<!-- InstanceEndEditable -->
+				</ul>
+			</div>
+		</div>
+		<div class="col-sm-7 padding-20-top content-column"> <!-- InstanceBeginEditable name="6col_content" -->
+			<p>Three Strategic Initiatives are emphasized as priority topics with issues critical to Puget Sound recovery within the Action Agenda - Stormwater, Habitat and Shellfish. Leads, described below, have been established for each Strategic Initiative. The SI Leads each facilitate an Advisory Team made up of technical and policy experts in the region, as well as work in coordination with one another and the Puget Sound Partnership, make and manage subawards, and implement work approved in the Action Agenda Implementation Plan.</p>
+			<p>Together the SI Leads and Advisory Teams are key to the development and implementation of the Action Agenda and have the following responsibilities:</p>
+			<ul>
+				<li>Identify regional recovery and protection priorities</li>
+				<li>Coordinate responses to issues that affect all three Strategic Initiatives (cross-cutting issues)</li>
+				<li>Establish the appropriate sequences of actions to lead from present conditions to long term goals</li>
+				<li>Solicit, identify, review and prioritize local and regional Near Term Actions</li>
+				<li>Develop and apply evaluation criteria for the review of Near Term Actions</li>
+				<li>Administration of National Estuary Program funds to implement priorities identified in the Action Agenda</li>
+				<li>Oversee development and adaptive management of Implementation Strategies</li>
+			</ul>
+			<p><a href="https://pspwa.box.com/s/il5i70rwisqvqqkunhq7nua3kh8klk0u">Draft Timeline</a> (Last Updated August 31, 2016)</p>
+			<p>Draft NTA Funding Recommendations for NEP Funding (anticipated November 10, 2016)	:</p>
+			<ul>
+				<li>Stormwater</li>
+				<li>Habitat</li>
+				<li>Shellfish </li>
+			</ul>
+			<h2>Stormwater Strategic Initiative</h2>
+			<p>Leads:  Led by the Washington State Department of Ecology (Ecology) in partnership with the Washington Stormwater Center at Washington State University (WSU), and the Department of Commerce (Commerce)</p>
+			<table class="table">
+				<tbody>
+					<tr>
+						<td>Contact</td>
+						<td>Agency</td>
+						<td>Lead/Co-Lead/Supporting Agency</td>
+					</tr>
+					<tr>
+						<td><br>
+							Derek Day, <br>
+							360-407-7612</td>
+						<td>Ecology</td>
+						<td>Lead</td>
+					</tr>
+					<tr>
+						<td>Heidi Siegelbaum, <br>
+							253-445-4502</td>
+						<td>WA Stormwater Center at WSU</td>
+						<td>Supporting</td>
+					</tr>
+					<tr>
+						<td>Doug Peters</td>
+						<td>Commerce</td>
+						<td>Supporting</td>
+					</tr>
+				</tbody>
+			</table>
+			<p><a href="https://pspwa.box.com/s/wxasucmo6rhue2ry7rwpryr3zw5kk8g1">Stormwater SIAT Roster</a></p>
+			<h2>Habitat Strategic Initiative</h2>
+			<p>Leads:  Co-led by the Washington State Departments of Fish &amp; Wildlife (WDFW) and Natural Resources (DNR)</p>
+			<table class="table">
+				<tbody>
+					<tr>
+						<td>Contact</td>
+						<td>Agency</td>
+						<td>Lead/Co-Lead/Supporting Agency</td>
+					</tr>
+					<tr>
+						<td><br>
+							Julie Watson, <br>
+						360-902-2580</td>
+						<td>WDFW</td>
+						<td>Policy Co-Lead</td>
+					</tr>
+					<tr>
+						<td>Kirsten Feifel, <br>
+						360-902-1055</td>
+						<td>DNR</td>
+						<td>Policy Co-Lead </td>
+					</tr>
+					<tr>
+						<td>Jennifer Griffiths,<br>
+360-902-2212</td>
+						<td>WDFW</td>
+						<td>Technical Co-Lead</td>
+					</tr>
+						<tr>
+						<td>Libby Gier,<br>
+360-902-1589</td>
+						<td>DNR</td>
+						<td>Technical Co-Lead</td>
+					</tr>
+						<tr>
+						<td>Kayla Jenkins,<br>
+360-902-8136</td>
+						<td>WDFW</td>
+						<td>Budget Analyst</td>
+					</tr>
+				</tbody>
+			</table>
+			<p><a href="https://pspwa.box.com/s/airlh6ihqoaira2dv7pux6ic5s0xx5ik">Habitat SIAT Roster</a></p>
+			
+			<h2>Shellfish Strategic Initiative</h2>
+			
+			<p>Leads:  Led by the Washington State Department of Health (Health) in partnership with the Departments of Ecology (Ecology) and Agriculture (Agriculture)</p>
+		<table class="table">
+				<tbody>
+					<tr>
+						<td>Contact</td>
+						<td>Agency</td>
+						<td>Lead/Co-Lead/Supporting Agency</td>
+					</tr>
+					<tr>
+						<td>
+							Emily Sanford, <br>
+							360-236-3319</td>
+						<td>Health</td>
+						<td>Lead</td>
+					</tr>
+					<tr>
+						<td>Stuart Glasoe, <br>
+							360-236-3377</td>
+						<td>health</td>
+						<td>Lead </td>
+					</tr>
+					<tr>
+						<td>Ron Cummings</td>
+						<td>Ecology</td>
+						<td>Supporting</td>
+					</tr>
+						<tr>
+						<td>Ginny Prest</td>
+						<td>Agriculture</td>
+						<td>Supporting</td>
+					</tr>
+				</tbody>
+			</table>
+			<p><a href="https://pspwa.box.com/s/gvjqtfbw0c4sjaz1y7ho2aet41ui0nt3">Shellfish SIAT Roster</a></p> 
+			<!-- InstanceEndEditable --> </div>
+		<div class="col-sm-2 padding-20-top padding-0-right"> <!-- InstanceBeginRepeat name="right_nav_repeat" --><!-- InstanceBeginRepeatEntry --> <!-- InstanceBeginEditable name="right_nav_title" -->
+			<div class="right-nav-title">SEE ALSO</div>
+			<!-- InstanceEndEditable -->
+			<ul class="nav-rightside-custom">
+				<!-- InstanceBeginEditable name="right_nav_links" -->
+				<li role="presentation"><a href="action_agenda_center.php">Action Agenda Overview</a></li>
+				<li role="presentation"><a href="2016_AA_update.php">2016 Action Agenda Update</a></li>
+				<li role="presentation"><a href="2016_AA_implementation_strategies.php">Implementation Strategies</a></li>
+				<li role="presentation"><a href="http://wa-geoservices.maps.arcgis.com/apps/Viewer/index.html?appid=4a40b5acc8884b849dd3c8dd65c2d985">Action Area Maps</a></li>
+				<li><a href="action_agenda_targets.php">Ecosystem Recovery Targets</a></li>
+				<!-- InstanceEndEditable -->
+			</ul>
+			<!-- InstanceEndRepeatEntry --><!-- InstanceEndRepeat --> </div>
+	</div>
+	<!--END OF ROW --> 
+</div>
+<!--END OF CONTENT CONTAINER -->
+
+<?php include 'includes/footer-inc.html';?>
+
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) --> 
+<script src="js/jquery-1.11.2.min.js"></script> 
+
+<!-- Include all compiled plugins (below), or include individual files as needed --> 
+<script src="js/bootstrap.js"></script> 
+<!-- custom js --> 
+<script src="js/custom.js"></script> 
+<!-- Google Tracking  --> 
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-69373425-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
+</body>
+<!-- InstanceEnd -->
+</html>
