77a7a2bae31a80bcd67425c9a697c72897cee6fd
caneballante <jonbridgman@gmail.com>
2016-11-09 10:34:59 -0800

    JONSTOOL
    update to the data object

--- test-space/jonstool/js/main_alt2.js
+++ test-space/jonstool/js/main_alt2.js
@@ -31,45 +31,201 @@ var projects = [
 		name: "Lower Dungeness Floodplain Restoration",
 		longitude: 48.14267633,
 		latitude: -123.1301177,
-		level: 1,
-		leadEntity: "North Olympic Peninsula / Clallam Co Community Dev" ,
+		level: 15,
+		leadEntity: "North Olympic Peninsula" , 
 		sponsor: "Clallam Co Community Dev" ,
 		marker: null,
 		funding: 3000000,
 		benefit: "placeholder benefits go here"
 	},
 	{
-		name: "Skillicum Creek 2",
-		longitude: 59.45522,
-		latitude: 18.33002,
-		level: 2,
-		sponsor: "Pork Chop" ,
-		marker: null
+		name: "Lower Russell Levee Setback & Habitat Restoration",
+		longitude: 000,
+		latitude: 000,
+		level: 6,
+		leadEntity: "WRIA 9/ King County" , 
+		sponsor: "sponsor" ,
+		marker: null,
+		funding: 10255524,
+		benefit: "placeholder benefits go here"
 	},
 		{
-		name: "Skillicum Creek 3",
-		longitude: 59.86522,
-		latitude: 18.33002,
-		level: 2,
-		sponsor: "Pork Chop" ,
-		marker: null
+		name: "Leque Island Estuary Restoration Construction",
+		longitude: 000,
+		latitude: 000,
+		level: 18,
+		leadEntity: "Stillaguamish / Dept of Fish & Wildlife" , 
+		sponsor: "sponsor" ,
+		marker: null,
+		funding: 6630991,
+		benefit: "placeholder benefits go here"
 	},
-		{
-		name: "Skillicum Creek 4",
-		longitude: 59.77522,
-		latitude: 18.33002,
+{
+		name: "Downey Farmstead Side Channel Restoration",
+		longitude: 000,
+		latitude: 000,
+		level: 1,
+		leadEntity: "WRIA 9/ City of Kent" , 
+		sponsor: "sponsor" ,
+		marker: null,
+		funding: 4835743,
+		benefit: "placeholder benefits go here"
+	},
+{
+		name: "USACE Skokomish Ecosystem Restoration Support 2",
+		longitude: 000,
+		latitude: 000,
+		level: 10,
+		leadEntity: "Hood Canal / Mason Conservation District" , 
+		sponsor: "sponsor" ,
+		marker: null,
+		funding: 6441322,
+		benefit: "placeholder benefits go here"
+	},
+{
+		name: "Pearson Shoreline",
+		longitude: 000,
+		latitude: 000,
+		level: 12,
+		leadEntity: "WRIA 6 / Whidbey Camano Land Trust" , 
+		sponsor: "sponsor" ,
+		marker: null,
+		funding: 1250000,
+		benefit: "placeholder benefits go here"
+	},
+{
+		name: "NF Nooksack (Xwqélém) Farmhouse Ph 4 Restoration",
+		longitude: 000,
+		latitude: 000,
 		level: 4,
-		sponsor: "Pork Chop 4" ,
-		marker: null
+		leadEntity: "WRIA 1 / Nooksack Indian Tribe" , 
+		sponsor: "sponsor" ,
+		marker: null,
+		funding: 3304422,
+		benefit: "placeholder benefits go here"
 	},
-		{
-		name: "Skillicum Creek 5",
-		longitude: 59.56562,
-		latitude: 18.33002,
+{
+		name: "Clear Creek Targeted Acquisition",
+		longitude: 000,
+		latitude: 000,
+		level: 17,
+		leadEntity: "Pierce Co Water Programs Div" , 
+		sponsor: "sponsor" ,
+		marker: null,
+		funding: 6400000,
+		benefit: "placeholder benefits go here"
+	},
+{
+		name: "Kilisut Harbor Restoration 2016",
+		longitude: 000,
+		latitude: 000,
+		level: 11,
+		leadEntity: "Hood Canal / North Olympic Salmon Coalition" , 
+		sponsor: "sponsor" ,
+		marker: null,
+		funding: 4093665,
+		benefit: "placeholder benefits go here"
+	},
+{
+		name: "West Oakland Bay Restoration",
+		longitude: 000,
+		latitude: 000,
+		level: 9,
+		leadEntity: "Mason Conservation / Squaxin Island Tribe" , 
+		sponsor: "sponsor" ,
+		marker: null,
+		funding: 3225750,
+		benefit: "placeholder benefits go here"
+	},
+{
+		name: "Barnum Point Acquisition",
+		longitude: 000,
+		latitude: 000,
+		level: 13,
+		leadEntity: "WRIA 6 / Whidbey Camano Land Trust" , 
+		sponsor: "sponsor" ,
+		marker: null,
+		funding: 2186728,
+		benefit: "placeholder benefits go here"
+	},
+{
+		name: "Chico Bridge - Keta restore",
+		longitude: 000,
+		latitude: 000,
+		level: 2,
+		leadEntity: "West Sound / Suquamish Tribe" , 
+		sponsor: "sponsor" ,
+		marker: null,
+		funding: 3442400,
+		benefit: "placeholder benefits go here"
+	},
+{
+		name: "Middle Fork Nooksack Fish Passage",
+		longitude: 000,
+		latitude: 000,
+		level: 3,
+		leadEntity: "WRIA 1/ City of Bellingham" , 
+		sponsor: "sponsor" ,
+		marker: null,
+		funding: 10904369,
+		benefit: "placeholder benefits go here"
+	},
+{
+		name: "Dungeness Off-Channel Reservoir: Final Design",
+		longitude: 000,
+		latitude: 000,
+		level: 16,
+		leadEntity: "North Olympic Peninsula / Clallam Conservation District" , 
+		sponsor: "sponsor" ,
+		marker: null,
+		funding: 1250000,
+		benefit: "placeholder benefits go here"
+	},
+{
+		name: "Hansen Creek Reach 5 Restoration",
+		longitude: 000,
+		latitude: 000,
+		level: 7,
+		leadEntity: "Skagit / Skagit County Public Works" , 
+		sponsor: "sponsor" ,
+		marker: null,
+		funding: 3681245,
+		benefit: "placeholder benefits go here"
+	},
+{
+		name: "Upper SF and Tributaries Corridor Acquisition",
+		longitude: 000,
+		latitude: 000,
 		level: 5,
-		sponsor: "Pork Chop" ,
-		marker: null
+		leadEntity: "WRIA 1 / Whatcom Land Trust" , 
+		sponsor: "sponsor" ,
+		marker: null,
+		funding: 1872911,
+		benefit: "placeholder benefits go here"
+	},
+{
+		name: "Morse Creek Riparian Conservation",
+		longitude: 000,
+		latitude: 000,
+		level: 14,
+		leadEntity: "North Olympic Peninsula / North Olympic Land Trust" , 
+		sponsor: "sponsor" ,
+		marker: null,
+		funding: 1107550,
+		benefit: "placeholder benefits go here"
+	},
+{
+		name: "Harper Estuary Bridge Construction",
+		longitude: 000,
+		latitude: 000,
+		level: 8,
+		leadEntity: "West Sound / Kitsap County" , 
+		sponsor: "sponsor" ,
+		marker: null,
+		funding: 2469844,
+		benefit: "placeholder benefits go here"
 	}
+
 ];
   
 //Google map  
