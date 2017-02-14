19181177171028c1f87b84c94775ccbdba4dce34
caneballante <jonbridgman@gmail.com>
2017-02-14 14:03:18 -0800

    VITAL SIGN PROTOTYPE 1.0

--- staff.php
+++ staff.php
@@ -87,7 +87,7 @@
    		
 			<h2>OPERATIONS</h2>
 			<h5>vacant</h5>
-			<p> Chief Financial Officer<br />
+			<p> Fiscal Division Manager<br />
 			</p><br />
             <!-- FISCAL-->
             <h3>FISCAL</h3>	    
