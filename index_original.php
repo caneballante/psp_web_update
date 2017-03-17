0649f63f4a008ecad15ed0a3061783b6df5f2be1
Erika Lari <erika.lari@psp.wa.gov>
2017-03-17 09:57:17 -0700

    update index page

--- index_original.php
+++ index_original.php
@@ -8,7 +8,7 @@
 
 <!-- Bootstrap -->
 <link href="css/bootstrap.css" rel="stylesheet">
-<link href="css/custom_DOH.css" rel="stylesheet" type="text/css">
+<link href="css/custom.css" rel="stylesheet" type="text/css">
 
 <!-- Font PRENTON TYPEKIT -->
 <script src="https://use.typekit.net/srt5jze.js"></script>
@@ -32,10 +32,8 @@
 		<div class="container">
 			<div class="row">
 				<div class="col-md-8 col-md-offset-2">
-					<h1 class="brand-heading">you’re invited<br>
-						<strong>2017 Puget Sound Day on The Hill</strong></h1>
-					<p>WASHINGTON DC | MAY 1-3</p>
-					<div class="learn-more-box"><a href="2017-day-on-the-hill.php">LEARN MORE</a></div>
+					<h1 class="brand-heading">TOGETHER, WE'RE PROTECTING THE NATION'S LARGEST ESTUARY</h1>
+					<div class="learn-more-box"><a href="puget-sound-recovery.php">LEARN MORE</a></div>
 				</div>
 			</div>
 		</div>
