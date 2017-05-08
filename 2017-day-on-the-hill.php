ee3a23bc1254464722ad2ab1cb4d288264020885
caneballante <jonbridgman@gmail.com>
2017-05-08 15:56:50 -0700

    day on the hill
    updated with cathy’s text

--- 2017-day-on-the-hill.php
+++ 2017-day-on-the-hill.php
@@ -67,6 +67,18 @@
 		</div>
 		 
 		<div class="col-sm-7 padding-20-top content-column"> <!-- InstanceBeginEditable name="6col_content" -->
+		<div style="width:500px;height:500px;text-align:center;margin:auto;" ><object width="500" height="500" classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000"  codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=6,0,40,0"> <param name="flashvars" value="offsite=true&amp;lang=en-us&amp;page_show_url=%2Fphotos%2Fdesigner_psp%2Falbums%2F72157633988825300%2Fshow&amp;page_show_back_url=%2Fphotos%2Fdesigner_psp%2Falbums%2F72157633988825300%2F&amp;user_id=26025902@N06&amp;set_id=72157633988825300" /> <param name="allowFullScreen" value="true" /> <param name="src" value="https://www.flickr.com/apps/slideshow/show.swf?v=71649" /> <embed width="500" height="500" type="application/x-shockwave-flash" src="https://www.flickr.com/apps/slideshow/show.swf?v=71649" flashvars="offsite=true&amp;lang=en-us&amp;page_show_url=%2Fphotos%2Fdesigner_psp%2Falbums%2F72157633988825300%2Fshow&amp;page_show_back_url=%2Fphotos%2Fdesigner_psp%2Falbums%2F72157633988825300%2F&amp;user_id=26025902@N06&amp;set_id=72157633988825300" allowFullScreen="true" /> </object><br /><small>Created with <a href="http://www.flickrslideshow.com">flickr slideshow</a>.</small></div>
+		<p>More than 60 people concerned about the future of Puget Sound met in Washington, D.C., May 1-3, 2017, to participate in Puget Sound Day on the Hill. The trip helped to raise the awareness of our nation’s decision-makers, ensuring them the opportunity to learn about the successes, opportunities, challenges and importance of the ongoing work for a clean, productive and resilient Puget Sound.</p>
+<p>Participants represented a broad range of interests: state and local governments, non-profit organizations, Puget Sound treaty tribes, and agricultural as well as small- and industrial-scale businesses.</p>
+<p>More than 150 individuals and organizations have also signed a letter supporting the awareness-raising purpose of the trip and acknowledging the energy shared by so many in our region for protecting the health of Puget Sound. The ever-growing list of signatories reflects the great diversity of support for federal funding for Puget Sound recovery.</p>
+<p>Participants met with Washington state senators and representatives, as well as with staff from congressional offices and committees and agency representatives. Topics of conversation included the importance of a healthy Puget Sound to the state’s economy and stories of innovation, community commitment, and project success in recovering Puget Sound.</p>
+ 
+<!--PULL QUOTE, Sheida Sahandy, Puget Sound Partnership Executive Director “It’s critically important that our nation’s policy-makers hear what we already know—that our wellbeing, our economic vitality and the health of our natural systems are inextricably interconnected.”
+PULL Quote “Beside this educational value, there is a lasting impression made when our national leaders see the dedication, commitment and true partnership among our attendees.”
+-->
+ 
+<!--LINKS
+-->
 			<blockquote class="twitter-tweet" data-lang="en"><p lang="en" dir="ltr">From the shores of Puget Sound all the way to the halls of Congress, we&#39;re working hard to <a href="https://twitter.com/hashtag/SaveOurSound?src=hash">#SaveOurSound</a>. It&#39;s a national treasure. <a href="https://t.co/Azq57yWKvd">pic.twitter.com/Azq57yWKvd</a></p>&mdash; Director Maia Bellon (@maiabellon) <a href="https://twitter.com/maiabellon/status/859581661528207362">May 3, 2017</a></blockquote> <script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
 			<blockquote class="twitter-tweet" data-lang="en"><p lang="en" dir="ltr">It&#39;s a great day to <a href="https://twitter.com/hashtag/SaveOurSound?src=hash">#SaveOurSound</a>! Part of the NW Straits team in DC this week, ready to talk <a href="https://twitter.com/hashtag/PugetSound?src=hash">#PugetSound</a> recovery. <a href="https://t.co/8Lz3BPhcbP">pic.twitter.com/8Lz3BPhcbP</a></p>&mdash; Northwest Straits (@NWStraits) <a href="https://twitter.com/NWStraits/status/859117347105157120">May 1, 2017</a></blockquote> <script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
 			<blockquote class="twitter-tweet" data-lang="en"><p lang="en" dir="ltr">Big shout out to the champions of Puget Sound, participating in Puget Sound Day on the Hill today in D.C.! <a href="https://twitter.com/hashtag/PugetSoundDay?src=hash">#PugetSoundDay</a> <a href="https://twitter.com/hashtag/SaveOurSound?src=hash">#SaveOurSound</a> <a href="https://t.co/LY4IISnPGU">pic.twitter.com/LY4IISnPGU</a></p>&mdash; PSPartnership (@PSPartnership) <a href="https://twitter.com/PSPartnership/status/859508362580328448">May 2, 2017</a></blockquote> <script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
@@ -74,7 +86,19 @@
 			<blockquote class="twitter-tweet" data-lang="en"><p lang="en" dir="ltr">60 of us are here in DC. We come from all sectors across WA &amp; speak with a common voice to <a href="https://twitter.com/hashtag/saveoursound?src=hash">#saveoursound</a> The energy is high. Let&#39;s do this! <a href="https://t.co/eYRQHL6G3p">pic.twitter.com/eYRQHL6G3p</a></p>&mdash; Jessie Israel (@JessieInSeattle) <a href="https://twitter.com/JessieInSeattle/status/859197198730448898">May 2, 2017</a></blockquote> <script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
 			<blockquote class="twitter-tweet" data-lang="en"><p lang="en" dir="ltr">Environmental justice caucus creator <a href="https://twitter.com/PramilaJayapal">@pramilajayapal</a> visited 60+ of us to talk environmental justice, infrastructure, budget. <a href="https://twitter.com/hashtag/SaveOurSound?src=hash">#SaveOurSound</a> <a href="https://t.co/SP6w6QyLaL">pic.twitter.com/SP6w6QyLaL</a></p>&mdash; NatureConservancy WA (@Conserve_WA) <a href="https://twitter.com/Conserve_WA/status/859509445772136449">May 2, 2017</a></blockquote> <script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>		<!-- InstanceEndEditable --> </div>
 		
-		<div class="col-sm-2 padding-20-top padding-0-right"> <!-- InstanceBeginRepeat name="right_nav_repeat" --><!-- InstanceEndRepeat --> </div>
+		<div class="col-sm-2 padding-20-top padding-0-right"> <!-- InstanceBeginRepeat name="right_nav_repeat" --><!-- InstanceBeginRepeatEntry --> <!-- InstanceBeginEditable name="right_nav_title" -->
+			<div class="right-nav-title margin-0-top ">2017 PUGET SOUND DAY ON THE HILL LINKS </div>
+			<!-- InstanceEndEditable -->
+			<ul class="nav-rightside-custom">
+				<!-- InstanceBeginEditable name="right_nav_links" -->
+				<li class="active" role="presentation"> <a href="https://pspwa.box.com/s/yh5cyeks0ab35zp4f5lhmowdu4tcezsg">Participant list</a></li>
+				<li role="presentation"> <a href="https://pspwa.box.com/s/8vn21gf0do1bv25z6808tvdfni9e1nhp">Puget Sound fact sheet</a></li>
+				<li role="presentation"> <a href="https://pugetsoundinnovationstories.blog">Puget Sound innovation stories</a></li>
+				<li role="presentation"> <a href="https://pspwa.box.com/s/cgy4fzfwmd40u5h2uj1r9608fr5fjbzx">Letter of support</a></li>
+			<!--	<li role="presentation"> <a href="#">KOMO radio interview with Sheida Sahandy</a></li>-->
+				<!-- InstanceEndEditable -->
+			</ul>
+			<!-- InstanceEndRepeatEntry --><!-- InstanceEndRepeat --> </div>
 	</div>
 	<!--END OF ROW --> 
 </div>
