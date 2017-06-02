41cd2eec5810f720458fa70a0a570309e74c82d8
caneballante <jonbridgman@gmail.com>
2017-06-02 15:00:02 -0700

    VS
    + added in the the chart with hard coded numbers.
    
    need to fix:
    +it loads when the page loads not when the button is clicked
    
    need to build:
    + pulling the rating data out of the dataVS
    + inserting the data into the chart

--- test-space/json-test/json_test_2.js
+++ test-space/json-test/json_test_2.js
@@ -1,12 +1,12 @@
 $(document).ready(function () {	
 	var dataJS;
 	$('#get-data').click(function () {
-   	
+   		//load headings
 		$.getJSON('vs_headings.json', function (data) {
 		headingsVS = data;
 		vsHeadingsShow();
 		});
-		
+		//load vital sign data
 		$.getJSON('vs_shoreline_armoring.json', function (data) {
 		dataVS = data;
 		vsDataShow();
@@ -38,7 +38,8 @@ $(document).ready(function () {
 		$('#show-assessment').html(vsAssessHtml);
 		
 		//rating
-		var vsRating = (dataVS['vitalSign']['rating']);
+		var vsRating = (dataVS['vitalSign']['progress-rating']);
+		console.log(vsRating);
 		
 		//highlights
 		var vsHighlights = (dataVS['vitalSign']['highlights']);
@@ -59,5 +60,41 @@ $(document).ready(function () {
 	function vsHeadingsShow () {
 		console.log("headings on the way")
 	}
+	var ctx = document.getElementById("myChart").getContext('2d');
+	var myChart = new Chart(ctx, {
+		type: 'bar',
+		data: {
+			labels: ["Making Progress", "Making Some Progress", "Not Changing", "Getting Somewhat Worse", "Getting Worse"],
+			datasets: [{
+				label: 'Percentage',
+				data: [0, 70, 15, 15, 0],
+				backgroundColor: [
+					'rgba(255, 99, 132, 0.2)',
+					'rgba(54, 162, 235, 0.2)',
+					'rgba(255, 206, 86, 0.2)',
+					'rgba(75, 192, 192, 0.2)',
+					'rgba(153, 102, 255, 0.2)'
+					
+				],
+				borderColor: [
+					'rgba(255,99,132,1)',
+					'rgba(54, 162, 235, 1)',
+					'rgba(255, 206, 86, 1)',
+					'rgba(75, 192, 192, 1)',
+					'rgba(153, 102, 255, 1)'
+				],
+				borderWidth: 1
+			}]
+		},
+		options: {
+			scales: {
+				yAxes: [{
+					ticks: {
+						beginAtZero:true
+					}
+				}]
+			}
+		}
+	});
 });
 
