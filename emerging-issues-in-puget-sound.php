d8d8a0cef10c54fd127d13462ee51576c9d563f8
caneballante <jonbridgman@gmail.com>
2017-06-06 15:57:44 -0700

    Emerging Issues new page

---
+++ emerging-issues-in-puget-sound.php
@@ -0,0 +1,168 @@
+<!DOCTYPE html>
+<html lang="en"><!-- InstanceBegin template="/Templates/page-4-template.dwt" codeOutsideHTMLIsLocked="false" -->
+<head>
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<!-- InstanceBeginEditable name="doctitle" -->
+<title>Puget Sound Partnership - Emerging Issues in Puget Sound</title>
+<!-- InstanceEndEditable -->
+<!-- Bootstrap -->
+<link href="css/bootstrap.css" rel="stylesheet">
+<link href="css/custom.css" rel="stylesheet" type="text/css">
+<link href="css/custom-erika.css" rel="stylesheet" type="text/css">
+<!-- loads the wf-loading class right away to minimize FOUT -->
+<script>document.documentElement.className += ' wf-loading';</script>
+<!-- Font PRENTON TYPEKIT -->
+<script src="https://use.typekit.net/srt5jze.js"></script>
+<script>try{Typekit.load({ async: true });}catch(e){}</script>
+<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+<!--[if lt IE 9]>
+	<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+	<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+<!-- InstanceBeginEditable name="head" -->
+<script>
+/*this  variable is used to set the proper nav to active. It should to the order the nav item is in the list*/
+  	navSelected = 2;
+</script>
+<!-- InstanceEndEditable -->
+<!-- InstanceParam name="OptionalRegion1" type="boolean" value="true" -->
+<!-- InstanceParam name="OptionalRegion2" type="boolean" value="false" -->
+<!-- InstanceParam name="OptionalRegion3" type="boolean" value="false" -->
+<!-- InstanceParam name="OptionalRegion4" type="boolean" value="true" -->
+<!-- InstanceParam name="OptionalRegion5" type="boolean" value="false" -->
+<!-- InstanceParam name="OptionalRegion6" type="boolean" value="false" -->
+<!-- InstanceParam name="OptionalRegion7" type="boolean" value="true" -->
+<!-- InstanceParam name="OptionalRegion8" type="boolean" value="false" -->
+</head>
+<body>
+<?php include 'includes/modal-inc.html';?>
+<!-- START IMAGE HEADER --> 
+ <!-- InstanceBeginEditable name="overviewphoto" -->
+<header class="overview-page-image-style overview-page-image101">
+	<div class="overview-quote pull-right">
+		<h3>&nbsp;</h3>
+	</div>
+</header>
+<!-- InstanceEndEditable -->  
+<div class="container page-content padding-50-bottom">
+	<div class="row">
+		<div class="col-sm-10 padding-20-top padding-0-left"> <!-- InstanceBeginEditable name="9col_header" -->
+			<h1>emerging issues in puget sound</h1>
+			<!-- InstanceEndEditable --></div>
+		<div class="col-sm-2 padding-20-top"></div>
+	</div>
+	
+	<div class="row">  
+		<div class="col-sm-10 padding-20-top padding-0-left content-column mobile-padding-20-left"> <!-- InstanceBeginEditable name="9col_content" -->
+			<!-- TOPIC -->
+			<h2 class="margin-0-top">No Discharge Zone </h2>
+			<img  class="floatright margin-20-left" src="images/marina-288x300.jpg" width="175" height="182" alt=""/>
+			<p><strong>Overview of the issue:</strong></p>
+			<p> Lorem ipsum dolor sit amet, in mea affert vivendo complectitur, in qui alii inciderint, et pro dicit saepe. An graeci animal alterum pri. Pro et dolor lucilius, vix animal gloriatur scriptorem te, ne consul contentiones ius. Facer denique qui at. Ne quod animal deseruisse eam.</p>
+			<p><strong>Partnership Statement:</strong></p>
+			<p>Included if we have a position we want to publicize.</p>
+			<p><strong>Links:</strong></p>
+			<p><a href="#">Some link to great information on this topic</a></p>
+			<p><a href="#">Some link to great information on this topic</a></p>
+			<hr>
+			<!-- TOPIC -->
+			<h2 class="padding-10-top">Snake River Dam</h2>
+			<img  class="floatright margin-20-left" src="images/snakeriver.jpg" width="175" height="182" alt=""/>
+			<p><strong>Overview of the issue:</strong></p>
+			<p> Lorem ipsum dolor sit amet, in mea affert vivendo complectitur, in qui alii inciderint, et pro dicit saepe. An graeci animal alterum pri. Pro et dolor lucilius, vix animal gloriatur scriptorem te, ne consul contentiones ius. Facer denique qui at. Ne quod animal deseruisse eam.</p>
+			<p><strong>Partnership Statement:</strong></p>
+			<p>Included if we have a position we want to publicize.</p>
+			<p><strong>Links:</strong></p>
+			<p><a href="#">Some link to great information on this topic</a></p>
+			<p><a href="#">Some link to great information on this topic</a></p>
+			<hr>
+			<!-- TOPIC -->
+			<h2 class="padding-10-top">Federal Budget</h2>
+			<img  class="floatright margin-20-left" src="images/washingtondc.jpg" width="175" height="182" alt=""/>
+			<p><strong>Overview of the issue:</strong></p>
+			<p> Lorem ipsum dolor sit amet, in mea affert vivendo complectitur, in qui alii inciderint, et pro dicit saepe. An graeci animal alterum pri. Pro et dolor lucilius, vix animal gloriatur scriptorem te, ne consul contentiones ius. Facer denique qui at. Ne quod animal deseruisse eam.</p>
+			<p><strong>Partnership Statement:</strong></p>
+			<p>Included if we have a position we want to publicize.</p>
+			<p><strong>Links:</strong></p>
+			<p><a href="#">Some link to great information on this topic</a></p>
+			<p><a href="#">Some link to great information on this topic</a></p>
+			<hr>
+			<!-- TOPIC -->
+			<h2 class="padding-10-top">State Budget</h2>
+			<img  class="floatright margin-20-left" src="images/olympia.jpg" width="175" height="182" alt=""/>
+			<p><strong>Overview of the issue:</strong></p>
+			<p> Lorem ipsum dolor sit amet, in mea affert vivendo complectitur, in qui alii inciderint, et pro dicit saepe. An graeci animal alterum pri. Pro et dolor lucilius, vix animal gloriatur scriptorem te, ne consul contentiones ius. Facer denique qui at. Ne quod animal deseruisse eam.</p>
+			<p><strong>Partnership Statement:</strong></p>
+			<p>Included if we have a position we want to publicize.</p>
+			<p><strong>Links:</strong></p>
+			<p><a href="#">Some link to great information on this topic</a></p>
+			<p><a href="#">Some link to great information on this topic</a></p>
+			<hr>
+			<!-- TOPIC -->
+			<h2 class="padding-10-top">Fish Passage Barriers</h2>
+			<img  class="floatright margin-20-left" src="images/fishpassage.jpg" width="175" height="182" alt=""/>
+			<p><strong>Overview of the issue:</strong></p>
+			<p> Lorem ipsum dolor sit amet, in mea affert vivendo complectitur, in qui alii inciderint, et pro dicit saepe. An graeci animal alterum pri. Pro et dolor lucilius, vix animal gloriatur scriptorem te, ne consul contentiones ius. Facer denique qui at. Ne quod animal deseruisse eam.</p>
+			<p><strong>Partnership Statement:</strong></p>
+			<p>Included if we have a position we want to publicize.</p>
+			<p><strong>Links:</strong></p>
+			<p><a href="#">Some link to great information on this topic</a></p>
+			<p><a href="#">Some link to great information on this topic</a></p>
+			<hr>
+			<!-- TOPIC -->
+			<h2 class="padding-10-top">Waste Water Treatment Plants</h2>
+			<img  class="floatright margin-20-left" src="images/wastewater.jpg" width="175" height="182" alt=""/>
+			<p><strong>Overview of the issue:</strong></p>
+			<p> Lorem ipsum dolor sit amet, in mea affert vivendo complectitur, in qui alii inciderint, et pro dicit saepe. An graeci animal alterum pri. Pro et dolor lucilius, vix animal gloriatur scriptorem te, ne consul contentiones ius. Facer denique qui at. Ne quod animal deseruisse eam.</p>
+			<p><strong>Partnership Statement:</strong></p>
+			<p>Included if we have a position we want to publicize.</p>
+			<p><strong>Links:</strong></p>
+			<p><a href="#">Some link to great information on this topic</a></p>
+			<p><a href="#">Some link to great information on this topic</a></p>
+			<!-- InstanceEndEditable --> </div>
+		
+		<div class="col-sm-2 padding-20-top padding-0-right"> <!-- InstanceBeginRepeat name="right_nav_repeat" --><!-- InstanceBeginRepeatEntry --> <!-- InstanceBeginEditable name="right_nav_title" -->
+			<div class="right-nav-title margin-0-top ">CONTACT</div>
+			<!-- InstanceEndEditable -->
+			<ul class="nav-rightside-custom">
+				<!-- InstanceBeginEditable name="right_nav_links" -->
+				<p class="right-nav-contact">For questions about Emerging Issues:<br />
+					<br>
+
+					<strong>Cathy Cochrane</strong>, <br>
+					Communication Lead,<br>
+					<a href="mailto:catherine.cochrane@psp.wa.gov">catherine.cochrane@psp.wa.gov</a><br>
+					360.790.7958 </p>
+				<!-- InstanceEndEditable -->
+			</ul>
+			<!-- InstanceEndRepeatEntry --><!-- InstanceEndRepeat --> </div>
+	</div>
+	<!--END OF ROW --> 
+</div>
+<!--END OF CONTENT CONTAINER -->
+
+<?php include 'includes/footer-inc.html';?>
+
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) --> 
+<script src="js/jquery-1.11.2.min.js"></script> 
+
+<!-- Include all compiled plugins (below), or include individual files as needed --> 
+<script src="js/bootstrap.js"></script> 
+<!-- custom js --> 
+<script src="js/custom.js"></script> 
+<!-- Google Tracking  --> 
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-69373425-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
+</body>
+<!-- InstanceEnd --></html>
