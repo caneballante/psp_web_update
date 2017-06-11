2b1c674e08a981daa51d0b439cbe9275a7a48539
caneballante <jonbridgman@gmail.com>
2017-06-11 16:20:13 -0700

    EMERGING ISSUES
    real content in place

--- emerging-issues-in-puget-sound.php
+++ emerging-issues-in-puget-sound.php
@@ -57,72 +57,81 @@
 	
 	<div class="row">  
 		<div class="col-sm-10 padding-20-top padding-0-left content-column mobile-padding-20-left"> <!-- InstanceBeginEditable name="9col_content" -->
-			<!-- TOPIC -->
-			<h2 class="margin-0-top">No Discharge Zone </h2>
-			<img  class="floatright margin-20-left" src="images/marina-288x300.jpg" width="175" height="182" alt=""/>
-			<p><strong>Overview of the issue:</strong></p>
-			<p> Lorem ipsum dolor sit amet, in mea affert vivendo complectitur, in qui alii inciderint, et pro dicit saepe. An graeci animal alterum pri. Pro et dolor lucilius, vix animal gloriatur scriptorem te, ne consul contentiones ius. Facer denique qui at. Ne quod animal deseruisse eam.</p>
-			<p><strong>Partnership Statement:</strong></p>
-			<p>Included if we have a position we want to publicize.</p>
-			<p><strong>Links:</strong></p>
-			<p><a href="#">Some link to great information on this topic</a></p>
-			<p><a href="#">Some link to great information on this topic</a></p>
-			<hr>
-			<!-- TOPIC -->
-			<h2 class="padding-10-top">Snake River Dam</h2>
-			<img  class="floatright margin-20-left" src="images/snakeriver.jpg" width="175" height="182" alt=""/>
-			<p><strong>Overview of the issue:</strong></p>
-			<p> Lorem ipsum dolor sit amet, in mea affert vivendo complectitur, in qui alii inciderint, et pro dicit saepe. An graeci animal alterum pri. Pro et dolor lucilius, vix animal gloriatur scriptorem te, ne consul contentiones ius. Facer denique qui at. Ne quod animal deseruisse eam.</p>
-			<p><strong>Partnership Statement:</strong></p>
-			<p>Included if we have a position we want to publicize.</p>
-			<p><strong>Links:</strong></p>
-			<p><a href="#">Some link to great information on this topic</a></p>
-			<p><a href="#">Some link to great information on this topic</a></p>
-			<hr>
-			<!-- TOPIC -->
-			<h2 class="padding-10-top">Federal Budget</h2>
-			<img  class="floatright margin-20-left" src="images/washingtondc.jpg" width="175" height="182" alt=""/>
-			<p><strong>Overview of the issue:</strong></p>
-			<p> Lorem ipsum dolor sit amet, in mea affert vivendo complectitur, in qui alii inciderint, et pro dicit saepe. An graeci animal alterum pri. Pro et dolor lucilius, vix animal gloriatur scriptorem te, ne consul contentiones ius. Facer denique qui at. Ne quod animal deseruisse eam.</p>
-			<p><strong>Partnership Statement:</strong></p>
-			<p>Included if we have a position we want to publicize.</p>
-			<p><strong>Links:</strong></p>
-			<p><a href="#">Some link to great information on this topic</a></p>
-			<p><a href="#">Some link to great information on this topic</a></p>
-			<hr>
-			<!-- TOPIC -->
-			<h2 class="padding-10-top">State Budget</h2>
+		<!--TOPIC STARTS HERE -->	
+ 
+			<h2>JLARC Audit Findings</h2>
+			<p>last update: June 6, 2017</p>
 			<img  class="floatright margin-20-left" src="images/olympia.jpg" width="175" height="182" alt=""/>
-			<p><strong>Overview of the issue:</strong></p>
-			<p> Lorem ipsum dolor sit amet, in mea affert vivendo complectitur, in qui alii inciderint, et pro dicit saepe. An graeci animal alterum pri. Pro et dolor lucilius, vix animal gloriatur scriptorem te, ne consul contentiones ius. Facer denique qui at. Ne quod animal deseruisse eam.</p>
-			<p><strong>Partnership Statement:</strong></p>
-			<p>Included if we have a position we want to publicize.</p>
-			<p><strong>Links:</strong></p>
-			<p><a href="#">Some link to great information on this topic</a></p>
-			<p><a href="#">Some link to great information on this topic</a></p>
-			<hr>
-			<!-- TOPIC -->
-			<h2 class="padding-10-top">Fish Passage Barriers</h2>
-			<img  class="floatright margin-20-left" src="images/fishpassage.jpg" width="175" height="182" alt=""/>
-			<p><strong>Overview of the issue:</strong></p>
-			<p> Lorem ipsum dolor sit amet, in mea affert vivendo complectitur, in qui alii inciderint, et pro dicit saepe. An graeci animal alterum pri. Pro et dolor lucilius, vix animal gloriatur scriptorem te, ne consul contentiones ius. Facer denique qui at. Ne quod animal deseruisse eam.</p>
-			<p><strong>Partnership Statement:</strong></p>
-			<p>Included if we have a position we want to publicize.</p>
-			<p><strong>Links:</strong></p>
-			<p><a href="#">Some link to great information on this topic</a></p>
-			<p><a href="#">Some link to great information on this topic</a></p>
-			<hr>
-			<!-- TOPIC -->
-			<h2 class="padding-10-top">Waste Water Treatment Plants</h2>
-			<img  class="floatright margin-20-left" src="images/wastewater.jpg" width="175" height="182" alt=""/>
-			<p><strong>Overview of the issue:</strong></p>
-			<p> Lorem ipsum dolor sit amet, in mea affert vivendo complectitur, in qui alii inciderint, et pro dicit saepe. An graeci animal alterum pri. Pro et dolor lucilius, vix animal gloriatur scriptorem te, ne consul contentiones ius. Facer denique qui at. Ne quod animal deseruisse eam.</p>
-			<p><strong>Partnership Statement:</strong></p>
-			<p>Included if we have a position we want to publicize.</p>
-			<p><strong>Links:</strong></p>
-			<p><a href="#">Some link to great information on this topic</a></p>
-			<p><a href="#">Some link to great information on this topic</a></p>
-			<!-- InstanceEndEditable --> </div>
+			<h3>Overview of the issue:</h3>
+ 
+			<p>The Puget Sound Partnership’s enabling legislation, Engrossed Substitute Senate Bill 5372, directed the Joint Legislative Audit and Review Committee (JLARC) to conduct two performance audits of the Partnership, the first in 2011 and the second in 2016. JLARC staff completed the 2011 performance audit, as well as a follow-up report in 2013. The report of the 2016 audit was released in May 2017.</p>
+ 
+			<p>Recognizing the first JLARC reviews were likely too early to measure progress in meeting the Legislature’s goals and objectives, the 2011 audit and 2013 follow-up report focused on key accountability directives assigned to the Partnership: would the Partnership be able to determine which restoration efforts were getting results? The Partnership agreed with the audit’s conclusion that much work was still needed to create and implement measures to understand what was and was not working in efforts to meet statutory goals and objectives.</p>
+ 
+			<p>The 2016 audit was designed to answer the following questions:</p>
+			<ul> 
+       			<li>What progress has been made in restoring Puget Sound by 2020?</li>
+				<li>Are restoration efforts and expenditures across the Puget Sound tracking with the priorities and strategic initiatives set by the Partnership to meet legislative goals?</li>
+				<li>How is the Partnership meeting key oversight and accountability requirements directed by statute? Specifically, how has it improved linking actions to progress on restoration goals, prioritizing actions, and monitoring effectiveness?</li>
+				<li>Has the Partnership identified any barriers to implementing the Action Agenda? If there are barriers, have solutions been identified and communicated to the Governor and/or Legislature?
+				<li>How is the Partnership utilizing monitoring and adaptive management in updating the Action Agenda?</li>
+ 			</ul>
+			<p>The audit included a review of other national ecosystem restoration efforts to inform these questions.</p>
+ 
+			<p>In their final report, the JLARC auditors noted that the Partnership’s ability to report data has been improved by adopting recovery targets that are linked to five of the six recovery goals identified by the Legislature; prioritizing near-term actions in the Puget Sound recovery plan (Action Agenda); and more clearly reporting on the status toward recovery targets. Areas that still need to be addressed encompass the following:</p>
+
+			<ul>
+				<li> Meeting the statutory requirements for identifying partner organizations whose actions are noncompliant with the Action Agenda or those that are making outstanding progress toward recovery goals</li>
+
+				<li>Fully integrating salmon recovery within overall recovery efforts</li>
+				<p>The report identifies three factors that hinder Puget Sound recovery efforts:</p>
+			</ul>
+			<ul>
+				<li>Short planning timeframes</li>
+				<li>An incomplete inventory and analysis of actions and funding</li>
+				<li>Unclear structure and execution of the monitoring program</li>
+			</ul>
+
+
+		    <p>JLARC’s recommendations encompass the following:</p>
+
+			<ul>
+				<li>Submit a plan to the Legislature that identifies and addresses needed revisions to the planning and recovery timeframes.</li>
+				<li>In collaboration with the Office of Financial Management, submit a plan to the Legislature that details how the Partnership will create a more complete inventory of recovery actions and funding.</li>
+				<li>Submit a plan to the Legislature that details how the Partnership will address the deficiencies in its ability to meet the essential requirements for a monitoring program, as identified by JLARC staff. The plan will also address how the Partnership will improve and clarify links between monitoring and planning.</li>
+			</ul>
+			 <p><a href="http://leg.wa.gov/jlarc/reports/2017/PSPartnership2016/pf/default.htm">Read the JLARC report</a></p>
+
+ 
+
+			<h3>In the news:</h3>
+
+<p>Kitsap Sun | <a href="http://pugetsoundblogs.com/waterways/2017/05/24/puget-sound-partnership-improves-but-some-changes-still-needed/">Watching Our Waterways blog: Puget Sound Partnership Improves but Some Changes Still Needed</a></p>
+ 
+			<h3>Partnership statement</h3>
+ 
+<p>We take the JLARC audits very seriously, and we have worked hard since the 2011 audit to meet the expectations of JLARC and the public. We are pleased that JLARC says we have improved since the last audit. To complete the 2016 audit, JLARC undertook a rigorous and professional review of our agency operations, provided us an opportunity to respond to comments and questions, and arrived at well-researched findings with which we concur. We will soon begin planning on how to implement the JLARC recommendations.</p>
+ <!--TOPIC ENDS HERE -->
+<!--TOPIC STARTS HERE -->
+
+			<h2>No Discharge Zone</h2>
+			<p>Last updated: June 2, 2017</p>
+			<img  class="floatright margin-20-left" src="images/marina-288x300.jpg" width="175" height="182" alt=""/>
+			<h3>Overview of the issue:</h3>
+			<p>On July 21, 2016, the Washington State Department of Ecology issued a petition to the U.S. Environmental Protection Agency (EPA) to establish a No Discharge Zone (NDZ) in Puget Sound area waters. The petition had long been supported by the Puget Sound Partnership and other state agencies. An NDZ is a designated body of water where the discharge of sewage from vessels is prohibited, whether treated or not. An NDZ is the only way for the state to gain authority to control this potential source of pollution. The decision to petition EPA came after more than four years of evaluation, extensive stakeholder outreach and a 2014 draft petition that went out for public comment. After review of the petition and extensive opportunity for public comment, in December 2016 the EPA determined that there are adequate facilities in the Puget Sound for vessels to pump out their sewage and Washington state may therefore move forward with an NDZ designation. With EPA’s approval in place, Ecology may now begin initiating rulemaking to make Puget Sound off limits to vessel sewage. The maritime industry – notably the tug and small cruise ship sectors – has expressed concerns about the cost of compliance. While EPA’s December determination was considered final, private interests are now petitioning EPA Administrator Scott Pruitt to rescind.</p>
+ 
+			<h3>In the news:</h3>
+<p>KING 5 news: <a href="http://www.king5.com/tech/science/environment/presidents-epa-counsel-calls-meeting-over-puget-sound/444319695">President's EPA counsel calls meeting over boat discharge in Puget Sound (June 2017)</a></p>
+<p>WorkBoat Magazine: <a href="https://www.workboat.com/news/government/operators-fear-cost-puget-sound-no-discharge-zone/">Operators fear cost implications of Puget Sound no discharge zone (March 2017)</a></p>
+<p>KNKX: <a href="http://knkx.org/post/epa-moving-ahead-no-discharge-zone-puget-sound">EPA Moving Ahead With No-Discharge Zone For Puget Sound (November 2016)</a></p>
+ 
+ 
+			<h3>What our partners are saying:</h3>
+			<p><a href="http://wagreen.p2a.co/lyrHluC">Washington Environmental Council</a></p> <p><a href="http://www.ecy.wa.gov/programs/wq/nonpoint/CleanBoating/ndzstatus.html">Department of Ecology</a></p>  
+ 
+			<h3>Puget Sound Partnership statement</h3>
+<p>We all want the same thing: a healthy, productive ecosystem. But the discharge of human waste into the waters in which we swim, fish and otherwise recreate is harmful for people and the environment.  The Puget Sound Partnership and our partners work on many fronts to minimize this type of pollution. Making Puget Sound a no-discharge zone for vessel sewage connects a missing piece in our strategy to clean up and restore the Sound. Years of study have gone into the concept of an NDZ. The research supports the need for an NDZ and also ensures there are adequate pump-out facilities in place (and more are planned) to make the transition as smooth as possible. The NDZ will help to protect our valuable shellfish beds, swimming beaches, and sensitive areas vulnerable to human pathogen pollution from vessel waste. There is broad support for establishing an NDZ, including that of the Puget Sound Partnership’s Ecosystem Coordination Board and the Leadership Council. Washington state has made large investments in sewage treatment, stormwater management, and in the control and prevention of industrial pollution and agricultural runoff. Banning sewage discharges from all sources supports the Puget Sound Action Agenda and Washington Shellfish Initiative.</p> 
+<p>Read our <a href="http://www.psp.wa.gov/blog/?p=707">July 2016 blog about the No Discharge Zone petition</a></p><!-- InstanceEndEditable --> </div>
 		
 		<div class="col-sm-2 padding-20-top padding-0-right"> <!-- InstanceBeginRepeat name="right_nav_repeat" --><!-- InstanceBeginRepeatEntry --> <!-- InstanceBeginEditable name="right_nav_title" -->
 			<div class="right-nav-title margin-0-top ">CONTACT</div>
