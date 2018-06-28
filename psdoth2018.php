23a058445b0bb240cd81978cff8d0100ead69fc3
Julia Luna <julia.luna@psplta001228.local>
2018-06-28 15:39:39 -0700

    update PSDOTH

--- psdoth2018.php
+++ psdoth2018.php
@@ -45,8 +45,8 @@
 	<div class="container-fluid page-content padding-50-bottom">
 		<div class="row">
 			<div class="col-sm-10 margin-40-left  padding-20-top padding-40-left">
-				<h1>2018 PUGET SOUND DAY ON THE HILL,<br>
-				SAVE THE DATE! MAY 23rd 2018</h1>
+				<h1>2018 PUGET SOUND DAY ON THE HILL<br>
+		MAY 23rd 2018</h1>
 
 				<p>&nbsp;</p>
 			</div>
@@ -74,11 +74,53 @@
 					</form>
 					<!-- /input-group --> 
 				</div>
-				<p>Once again, the President has proposed a budget to Congress that zeroes out all federal funding for Puget Sound recovery. Last year, we spoke up, and Congress listened. Join us again this year as we travel to Washington D.C. for Puget Sound Day on The Hill, May 23. We will meet with key decision makers to let them know the benefits a healthy Puget Sound brings to the entire nation, as well as to our region</p>
-		
+				<h3>Day on the Hill RECAP</h3>
+				<p>Thank you for doing your part in making Puget Sound Day on the Hill 2018 a success! The feedback we’ve received has been very positive, and I am proud of the great work our group was able to accomplish. Here’s a quick recap:<br>
+<li>
+We had the highest participation ever this year, with 60+ leaders from state, local, and tribal government, academia, business, and non-profit organizations meeting in Washington D.C. with key decision-makers. We were honored by and benefitted from strong tribal participation. Check out all of our attendees listed <a href=https://pspwa.app.box.com/file/292188788593>here.</a></li>
+<li>				
+Some unplanned excitement kicked off the trip as a flight with nearly half the group found themselves diverted from Washington D.C. hours before the first event. This group eventually made it to town safely the same evening. Meanwhile, the rest of the team joined the Puget Sound Congressional Caucus co-chairs for an informal welcome reception, at which <b>Congressmen Heck and Kilmer</b> welcomed us personally. Congressman Kilmer then led us on his legendary after-hours tour of the Capitol Building.</li>
+					<li>
+Wednesday, the first full day of Puget Sound Day on the Hill, was packed with addresses from our delegation, including <b>Senator Murray, Congressmen Heck, Kilmer, and Larsen, and Congresswomen DelBene and Jayapal.</b><b> Former Congressman Norm Dicks</b> delighted the group with a surprise visit and shared his thoughts about the state of affairs in DC. We then enjoyed a panel discussion with staff from the <b>S. Army Corps of Engineers (USACE), Environmental Protection Agency (EPA), and the United States Geologic Survey (USGS)</b>, as they talked about the federal agencies’ role in Puget Sound recovery, their ongoing efforts, and ways to strengthen our partnership to advance recovery.</li>
+					<li>
+Attendees also met with members of our delegation and their staff in smaller groups throughout the day on Wednesday and Thursday, including <b>Senator Cantwell, Congressmen Heck, Kilmer, Reichert, Larsen, Newhouse, and Smith, and Congresswomen Herrera Beutler, Jayapal, and DelBene</b>. These smaller group meetings allowed us to go into greater detail in our conversations with these leaders.</li>
+					
+			  <li>
+A separate meeting with the <b>USACE</b> was a great opportunity to stress the importance of infrastructure investments in Puget Sound, including addressing needs at Ballard Locks.</li>
+			  <li>
+The trip culminated on Thursday afternoon with a lunch panel presentation and discussion of Puget Sound innovations and public-private partnerships, with speakers from <b>The Nature Conservancy, Washington State University, Tacoma City Council, and Wilcox Farms</b>. Over 70 attendees packed a room with capacity of 50 to hear about Puget Sound as a case study for estuary restoration. <b>This lunch event was supported by Taylor Shellfish, Wilcox Farms, and the Salmon Safe program</b>. Several staffers from non-Washington members of Congress and various committees also joined us, helping expand the coalition of Puget Sound recovery champions. Joseph Gaydos, of The SeaDoc Society, donated copies of the recently published children’s book <a href=http://www.seadocsociety.org/salish-sea-kids-book/><i> Explore the Salish Sea, A Nature Guide for Kids</i>.</a> This compelling book helped us share the wonders of our Puget Sound with elected officials, especially those who have the misfortune of living elsewhere.</li>
+			  <li>
+              To ensure the voices of people who could not attend the trip would still be heard, we shared copies of the <b>PSDOTH Letter of Support</b> with people with whom we met. As of this writing, 74 people and organizations have signed onto a letter of support for Puget Sound recovery, which is addressed to key congressional and senate subcommittee chairs and ranking members. You can view the letter <a href=https://pspwa.app.box.com/file/291830612345> here.</a></li>
+					<li>
+                This year we incorporated a social media plan into the trip. Our data tells us that we received over <b>13,000 impressions</b> (this is the # of people who saw our content) and over <b>200 engagements</b> (this is the # of people who interacted with our posts) on <b>Twitter</b>. On <b>Facebook</b> we received over <b>500 impressions and 141 engagements</b>. </li>
+					
+                <p>A multi-state coalition followed up the PSDOTH with a Salmon Day on the Hill trip, which reemphasized the importance of<b> Pacific Coast Salmon Recovery Funds (PCSRF)</b> for salmon recovery.</p>
+                <li>
+                Representatives from Washington, Oregon, Idaho, and Alaska met with members of Congress and their staff, including <b>Senators Murray, Cantwell, Wyden, Merkley, Risch, and Crapo, and Representatives Kilmer, McMorris Rodgers, Newhouse, Jayapal, Larsen, Heck, Bonamichi, Walden, Blumenauer, DeFazio, Simpson, and Schrader</b>.</li>
+				<li>
+                Off the Hill, the multi-state coalition met with leadership and staff from the <b>National Oceanic and Atmospheric Administration, the U.S. Department of Agriculture, the White House Office of Management and Budget, the U.S. Forest Service, the U.S. Fish and Wildlife Service, and the U.S. Army Corps of Engineers</b>.</li>
+                <li>
+                The Congressional delegations expressed broad bipartisan support for PCSRF. Our coalition shared success stories and the prospect of delistings to demonstrate the value of PCSRF and the impact of PCSRF funding.</li> 
+                <b>Since the two trips, we have received great news:</b><br>
+                
+				<li>
+                The Senate Appropriations Committee passed an appropriation bill which <b>adds $5 million to PCSRF for a total of $70 million for FY19</b>. The bill now awaits a vote by the full Senate and consideration by the House.</li>
+				<li><a href=https://www.usace.army.mil/Missions/Civil-Works/Budget/>
+					   The US Army Corps of Engineers work plan was released </a>and it includes</li><br>
+	<ul>
+<li>$200k for the Puget Sound Nearshore Ecosystem Restoration Program (PSNERP) Project Engineering/Design (PED)</li>
+<li>An additional <b>$13M for Lake Washington Ship Canal (read Ballard Locks!)</b></li>
+<li>New start feasibility for Tacoma Harbor
+New start feasibility for Mill Creek
+$1M PED funds for Skokomish</li></ul>
 				
-				
-				<h3>ATTENDEE MATERIALS</h3>
+                If you were not able to attend this year, don’t forget to thank someone who did! We would not be able to maintain the support we get, and build the foundation for the support we need, without this kind of engagement and participation. We expect to begin planning for PSDOTH 2019 in the late fall and hope to see you in D.C. next year!
+  </p>
+                
+                
+                
+              </p>
+              <h3>ATTENDEE MATERIALS</h3>
 				<ul>
 					<li><a href="https://pspwa.box.com/s/0xhze4jkjufi9trb5frgmxghi9x7kb5y">Attendee list (5/22/18)</a></li>
 					<li><a href="https://pspwa.box.com/s/joqdu7meu36ktl3w7f29zpmdy5isjkze">Schedule (5/22/18)</a></li>
