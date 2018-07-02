217350c849f0ab7d347a0045b69f994caa515570
caneballante <jonbridgman@gmail.com>
2018-07-02 09:32:14 -0700

    Salmon Pages update - incomplete

---
+++ salmon-recovery-watersheds-beta.php
@@ -0,0 +1,243 @@
+<!DOCTYPE html>
+<html lang="en"><!-- InstanceBegin template="/Templates/page-4-template.dwt" codeOutsideHTMLIsLocked="false" -->
+<head>
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<!-- InstanceBeginEditable name="doctitle" -->
+<title>Puget Sound Watersheds</title>
+<!-- InstanceEndEditable -->
+<!-- Bootstrap -->
+<link href="css/bootstrap.css" rel="stylesheet">
+<link href="css/custom.css" rel="stylesheet" type="text/css">
+<link href="css/custom-erika.css" rel="stylesheet" type="text/css">
+<!-- loads the wf-loading class right away to minimize FOUT -->
+<script>document.documentElement.className += ' wf-loading';</script>
+<!-- Font PRENTON TYPEKIT -->
+<script src="https://use.typekit.net/srt5jze.js"></script>
+<script>try{Typekit.load({ async: true });}catch(e){}</script>
+<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+<!--[if lt IE 9]>
+	<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+	<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+<!-- InstanceBeginEditable name="head" -->
+<script>
+/*this variable is used to set the proper nav to active. It should to the order the nav item is in the list*/
+  	navSelected = 2;
+</script> 
+<!-- InstanceEndEditable -->
+<!-- InstanceParam name="OptionalRegion1" type="boolean" value="true" -->
+<!-- InstanceParam name="OptionalRegion2" type="boolean" value="false" -->
+<!-- InstanceParam name="OptionalRegion3" type="boolean" value="true" -->
+<!-- InstanceParam name="OptionalRegion4" type="boolean" value="false" -->
+<!-- InstanceParam name="OptionalRegion5" type="boolean" value="true" -->
+<!-- InstanceParam name="OptionalRegion6" type="boolean" value="true" -->
+<!-- InstanceParam name="OptionalRegion7" type="boolean" value="false" -->
+<!-- InstanceParam name="OptionalRegion8" type="boolean" value="false" -->
+</head>
+<body>
+<?php include 'includes/modal-inc.html';?>
+<!-- START IMAGE HEADER --> 
+ <!-- InstanceBeginEditable name="overviewphoto" -->
+<header class="overview-page-image-style overview-page-image8">
+	<div class="overview-quote pull-right"></div>
+</header>
+<!-- InstanceEndEditable -->  
+<div class="container-fluid page-content padding-50-bottom">
+	<div class="row">
+		<div class="col-sm-3 padding-20-top"></div>
+		<div class="col-sm-7 padding-20-top"> <!-- InstanceBeginEditable name="6col_header" -->
+			<h1>PUGET SOUND WATERSHEDS</h1>
+			<p>&nbsp;</p>
+			<!-- InstanceEndEditable --></div>
+		<div class="col-sm-2 padding-20-top"></div>
+	</div>
+	
+	<div class="row"> 
+		<div class="col-sm-3">
+			<div class="nav-leftside-custom">
+				<ul class="nav nav-stacked nav-pills nav-leftside-custom padding-left-0 margin-10-top">
+					<!-- InstanceBeginEditable name="left_nav" -->
+			<?php include 'includes/ln-salmon-beta.html';?>	
+				<!-- InstanceEndEditable -->
+				</ul>
+			</div>
+		</div>
+		 
+		<div class="col-sm-7 padding-20-top content-column"> <!-- InstanceBeginEditable name="6col_content" -->
+			<p>Puget Sound is a deep, vast, and complex ecosystem. The region encompasses mountains, farmlands, cities, rivers, forests, and wetlands. Within the Puget Sound region, 16 salmon recovery chapter areas are defined. In addition, there are nine local integrating organizations (LIOs) around Puget Sound that coordinate closely with the salmon recovery watersheds to advance priorities as strategies for local ecosystem recovery.</p>
+			<p>Each watershed has its own unique geographic, urban, suburban, and rural areas. The Puget Sound Partnership collaborates with partners throughout the entire region to ensure that salmon recovery efforts work toward protecting Chinook salmon, steelhead, and the rich biodiversity of species that rely on Puget Sound. It is within each of these local watersheds that recovery takes place. Tribes, lead entities, local jurisdictions, and community-based organizations work together to protect and restore habitat and get the word out about the importance of keeping Puget Sound clean and protected.</p>
+			<p>Lead entities are local, citizen-based, organizations that coordinate salmon recovery efforts in their local watersheds. Per statute (<a href="http://app.leg.wa.gov/RCW/default.aspx?cite=77.85.050">RCW 77.85.050</a>), lead entities are tasked with establishing a committee made up of habitat recovery interests in their area and developing a list of habitat recovery projects. In Puget Sound, lead entities work with local and state agencies, tribes, citizens, and other community groups to adaptively manage their <a href="http://www.psp.wa.gov/salmon-watershed-recovery-plans.php">watershed recovery plans</a> to recover salmon and ensure that salmon recovery actions are implemented on the ground.</p>
+			<p>Contact information for the lead entities is provided below. More information about the Lead Entity Program is available on the <a href="http://wwwtest2.rco.wa.gov/salmon_recovery/lead_entities.shtml">Washington State Recreation and Conservation Office website</a>.</p>
+			
+			<table class="table">
+				<tr>
+					<td width="122" class="tableheader"><strong>Watershed</strong></td>
+					<td width="107"  class="tableheader"><strong>Lead Entity Contact</strong></td>
+					<td width="104" class="tableheader"><strong>Phone</strong></td>
+					<td width="227" class="tableheader"><strong>Email</strong></td>
+					<td colspan="2" class="tableheader"><strong>Watershed Recovery Plan Chapters</strong></td>
+					
+				</tr>
+				<tr>
+					<td>Green/Duwamish Watershed</td>
+					<td>Doug Osterman, WRIA 9/King County </td>
+					<td>206-477-4793</td>
+					<td><a class="small-links" href="mailto:doug.osterman@kingcounty.gov">doug.osterman@kingcounty.gov</a></td>
+					<td>text</td>
+					<td>text</td>
+				</tr>
+				<tr>
+					<td>Hood Canal Watershed</td>
+					<td>Scott Brewer, Exec Dir</td>
+					<td>360-531-0575</td>
+					<td><a class="small-links" href="mailto:sbrewer@hccc.wa.gov">sbrewer@hccc.wa.gov</a></td>
+					<td>&nbsp;</td>
+					<td>&nbsp;</td>
+				</tr>
+				<tr>
+					<td>Island Watershed</td>
+					<td>Dawn Pucci, Island County</td>
+					<td>360-678-7916</td>
+					<td><a class="small-links"href="mailto:d.pucci@co.island.wa.us">d.pucci@co.island.wa.us</a></td>
+					<td>&nbsp;</td>
+					<td>&nbsp;</td>
+				</tr>
+				<tr>
+					<td>Lake Washington/Cedar/<br>
+						Sammamish Watershed</td>
+					<td>Jason Mulvihill-Kuntz, WRIA 8/King County</td>
+					<td>206-477-4780</td>
+					<td><a class="small-links" href="mailto:jason.mulvihill-kuntz@kingcounty.gov">jason.mulvihill-kuntz@kingcounty.gov</a></td>
+					<td>&nbsp;</td>
+					<td>&nbsp;</td>
+				</tr>
+				<tr>
+					<td>Nisqually River Watershed</td>
+					<td>Ashley Von Essen</td>
+					<td>360-456-5221</td>
+					<td><a class="small-links" href="mailto:vonessen.ashley@nisqually-nsn.gov">vonessen.ashley@nisqually-nsn.gov</a></td>
+					<td>&nbsp;</td>
+					<td>&nbsp;</td>
+				</tr>
+				<tr>
+					<td>Nooksack Watershed </td>
+					<td>Becky Peterson</td>
+					<td>360-392-1301         </td>
+					<td><a class="small-links" href="mailto:genevaconsulting@comcast.net" target="_blank">genevaconsulting@comcast.net</a> </td>
+					<td>&nbsp;</td>
+					<td>&nbsp;</td>
+				</tr>
+				<tr>
+					<td>North Olympic Peninsula Watershed</td>
+					<td>Cheryl Baumann, N. Olympic Peninsula Lead Entity for Salmon</td>
+					<td>360-417-2326</td>
+					<td><a class="small-links" href="mailto:cbaumann@co.clallam.wa.us">cbaumann@co.clallam.wa.us</a></td>
+					<td>&nbsp;</td>
+					<td>&nbsp;</td>
+				</tr>
+				<tr>
+					<td>Puyallup/White &amp; Chambers/Clover Watersheds</td>
+					<td>Lisa Spurrier, Pierce County</td>
+					<td>253-798-6158</td>
+					<td><a class="small-links" href="mailto:lspurri@co.pierce.wa.us">lspurri@co.pierce.wa.us</a></td>
+					<td>&nbsp;</td>
+					<td>&nbsp;</td>
+				</tr>
+				<tr>
+					<td>San Juan Watershed</td>
+					<td>Byron Rot, San Juan County</td>
+					<td>360-370-7593</td>
+					<td><a class="small-links" href="mailto:byronr@sanjuanco.com">byronr@sanjuanco.com</a></td>
+					<td>&nbsp;</td>
+					<td>&nbsp;</td>
+				</tr>
+				<tr>
+					<td>Skagit Watershed</td>
+					<td>Richard Brocksmith, Skagit Watershed Council </td>
+					<td>360-826-2164</td>
+					<td><a class="small-links" href="mailto:rbrocksmith@skagitwatershed.org" target="_blank">rbrocksmith@skagitwatershed.org</a></td>
+					<td>&nbsp;</td>
+					<td>&nbsp;</td>
+				</tr>
+				<tr>
+					<td>Snohomish Watershed</td>
+					<td>Gretchen Glaub, Snohomish Basin Salmon Recovery Forum<br /></td>
+					<td>425-330-0311 <br />
+						<br />
+						<br /></td>
+					<td><a class="small-links" href="mailto:gretchen.glaub@snoco.org">gretchen.glaub@snoco.org</a>
+						&nbsp;</td>
+					<td>&nbsp;</td>
+					<td>&nbsp;</td>
+				</tr>
+				<tr>
+					<td>South Sound Watersheds</td>
+					<td>Amy Hatch-Winecka, Mason County Conservation District/
+						Thurston County Conservation District</td>
+					<td>360-754-3588, Ext. 103</td>
+					<td><a class="small-links" href="mailto:AmyHW@thurstoncd.com">AmyHW@thurstoncd.com</a></td>
+					<td>&nbsp;</td>
+					<td>&nbsp;</td>
+				</tr>
+				<tr>
+					<td>Stillaguamish Watershed</td>
+					<td>Donald “Kit” Crump<br />
+							Senior Planner-PW<br />
+							Coordinator, Stillaguamish River Salmon Recovery Co-Lead Entity<br />
+						</td>
+					<td>425-388-3464 Ext. 4658</td>
+					<td><a class="small-links" href=" Donald.Crump@snoco.org"> Donald.Crump@snoco.org</a></td>
+					<td>&nbsp;</td>
+					<td>&nbsp;</td>
+				</tr>
+				<tr>
+					<td>West Sound Watersheds</td>
+					<td>Marian Berejikian</td>
+					<td>&nbsp;</td>
+					<td> <a class="small-links" href="mberejikian@co.kitsap.wa.us" target="_blank">mberejikian@co.kitsap.wa.us</a></td>
+					<td>&nbsp;</td>
+					<td>&nbsp;</td>
+				</tr>
+			</table>
+		<!-- InstanceEndEditable --> </div>
+		
+		<div class="col-sm-2 padding-20-top padding-0-right"> <!-- InstanceBeginRepeat name="right_nav_repeat" --><!-- InstanceBeginRepeatEntry --> <!-- InstanceBeginEditable name="right_nav_title" -->
+			<div class="right-nav-title">SEE ALSO</div>
+			<!-- InstanceEndEditable -->
+			<ul class="nav-rightside-custom">
+				<!-- InstanceBeginEditable name="right_nav_links" -->
+			<li role="presentation"><a href="SR_about.php">Salmon Recovery Council</a></li>
+				<li role="presentation"><a href="http://www.westcoast.fisheries.noaa.gov/protected_species/salmon_steelhead/recovery_planning_and_implementation/puget_sound/PS_steelhead_recovery_team.html">Puget Sound Steelhead Recovery Team</a></li>
+				<!-- InstanceEndEditable -->
+			</ul>
+			<!-- InstanceEndRepeatEntry --><!-- InstanceEndRepeat --> </div>
+	</div>
+	<!--END OF ROW --> 
+</div>
+<!--END OF CONTENT CONTAINER -->
+
+<?php include 'includes/footer-inc.html';?>
+
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) --> 
+<script src="js/jquery-1.11.2.min.js"></script> 
+
+<!-- Include all compiled plugins (below), or include individual files as needed --> 
+<script src="js/bootstrap.js"></script> 
+<!-- custom js --> 
+<script src="js/custom.js"></script> 
+<!-- Google Tracking  --> 
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-69373425-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
+</body>
+<!-- InstanceEnd --></html>
