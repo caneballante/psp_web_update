bb49f96ac7e803dca2d5488ea7015b89b352b8d5
jon bridgman <jon.bridgman@psp.wa.gov>
2019-09-04 10:31:50 -0700

    Puget Sound SOS

--- puget-sound-SOS.php
+++ puget-sound-SOS.php
@@ -55,12 +55,17 @@
 
 		<div class="row">
 			<div class="col-sm-9 margin-40-left padding-20-top padding-40-right content-column mobile-padding-20-left">
+				<div class="width-300-govdelivery-doth border-thick-blue floatright padding-10-all">
+				<p><a href="https://pspwa.box.com/s/f12g2ktyf9ep5ejxkow0b7js0h7pcrhh">Letter of Support from the Puget Sound Recovery Community</a></p>
+				<hr>
+				<p><a href="https://pspwa.box.com/s/dj6bg16xk2r9modsbtrclm7u9f0h21fq">Puget Sound SOS bill</a></p>
+				<p><a href="https://transportation.house.gov/committee-activity/hearings/the-subcommittee-on-water-resources-and-environment-hearing-on_--protecting-and-restoring-americas-iconic-waters">Video of Laura Blackmore's testimony</a>
+					
+		        </div>
 				<h3 class="margin-0-top">Testimony of Laura L. Blackmore<br>
-Executive Director, Puget Sound Partnership<br>
-Before the House Committee on Transportation and Infrastructure<br>
-Subcommittee on Water Resources and Environment<br>
-Hearing on Protecting and Restoring America’s Iconic Waters<br>
-Tuesday, June 25, 2019</h3>
+				Executive Director, Puget Sound Partnership<br><br>
+				Before the House Committee on Transportation and Infrastructure Subcommittee on Water Resources and Environment<br><br>
+			  Hearing on Protecting and Restoring America’s Iconic Waters, Tuesday, June 25, 2019</h3>
 				
 				<p>Chair Napolitano, Ranking Member Westerman, and distinguished members of the subcommittee, thank you for inviting me here today. On behalf of the Puget Sound Partnership and our hundreds of partners, I want to thank you for convening this important hearing today.</p>
 				
@@ -92,19 +97,20 @@ Tuesday, June 25, 2019</h3>
 				  <li>
 				    <p>Abundant water quantity</p>
 			      </li>
-				  <li>
-				  <p>Healthy water quality</p>
-				    
-				  <p>Congress designated Puget Sound as an Estuary of National Significance in 1988. The Puget Sound Partnership participates in the EPA&rsquo;s National Estuary Program (NEP), created by Congress in 1987. This highly effective program, which incorporates 28 estuaries from every coast, charges us with developing and implementing a collaborative, non-regulatory blueprint for restoring and protecting this iconic water body.</p>
-				   
-				    <h3>We fulfill these responsibilities in three primary ways:</h3>
-				    
-				    <p>Chart the course – Action Agenda and Puget Sound Salmon Recovery Plan</p>
-				    <p>The 2018-2022 Action Agenda for Puget Sound, which serves as the Sound&rsquo;s Comprehensive Conservation &amp; Management Plan as authorized by the NEP, charts the course to achieving a resilient Puget Sound. It outlines regional strategies and specific actions required to make progress toward recovery. The actions proposed for funding in the Action Agenda offer the promise of effective investment in Puget Sound protection and restoration. As required under the NEP, the Partnership convenes a Management Conference composed of federal, tribal, state and local government agencies, businesses, the environmental community, the agricultural and timber industries, academic institutions, fishermen, shellfish growers, and other partners to develop and manage the implementation of the Action Agenda.</p>
-				    <p>The Partnership&rsquo;s Leadership Council also oversees the implementation of the Puget Sound Salmon Recovery Plan, approved by NOAA in 2007 as the region&rsquo;s recovery plan for Chinook salmon under the Endangered Species Act. The Salmon Recovery Plan includes strategies for recovering Chinook salmon populations in each watershed of Puget Sound. With federal and state funding, the Partnership supports local councils that manage each of these watershed-scale strategies.</p>
-				    <p>Promote shared measures – State of the Sound report</p>
-				    <p>The biennial State of the Sound report improves understanding across the Management Conference and among decision-makers about how well the recovery effort is going. The State of the Sound answers the following questions:</p>
+					<li>
+				    <p>Healthy water quality</p>
 			      </li>
+				</ul>
+			<p>Congress designated Puget Sound as an Estuary of National Significance in 1988. The Puget Sound Partnership participates in the EPA&rsquo;s National Estuary Program (NEP), created by Congress in 1987. This highly effective program, which incorporates 28 estuaries from every coast, charges us with developing and implementing a collaborative, non-regulatory blueprint for restoring and protecting this iconic water body.</p>
+				   
+				<h3>We fulfill these responsibilities in three primary ways:</h3>
+				    
+				<p>Chart the course – Action Agenda and Puget Sound Salmon Recovery Plan</p>
+				<p>The 2018-2022 Action Agenda for Puget Sound, which serves as the Sound&rsquo;s Comprehensive Conservation &amp; Management Plan as authorized by the NEP, charts the course to achieving a resilient Puget Sound. It outlines regional strategies and specific actions required to make progress toward recovery. The actions proposed for funding in the Action Agenda offer the promise of effective investment in Puget Sound protection and restoration. As required under the NEP, the Partnership convenes a Management Conference composed of federal, tribal, state and local government agencies, businesses, the environmental community, the agricultural and timber industries, academic institutions, fishermen, shellfish growers, and other partners to develop and manage the implementation of the Action Agenda.</p>
+				<p>The Partnership&rsquo;s Leadership Council also oversees the implementation of the Puget Sound Salmon Recovery Plan, approved by NOAA in 2007 as the region&rsquo;s recovery plan for Chinook salmon under the Endangered Species Act. The Salmon Recovery Plan includes strategies for recovering Chinook salmon populations in each watershed of Puget Sound. With federal and state funding, the Partnership supports local councils that manage each of these watershed-scale strategies.</p>
+				<p>Promote shared measures – State of the Sound report</p>
+				 <p>The biennial State of the Sound report improves understanding across the Management Conference and among decision-makers about how well the recovery effort is going. The State of the Sound answers the following questions:</p>
+			     <ul>
 				  <li>
 				    <p>How is the ecosystem doing?</p>
 			      </li>
@@ -122,7 +128,7 @@ Tuesday, June 25, 2019</h3>
 			      </li>
 			  </ul>
 				
-			  <p>Support partners – mobilize funding, communicate effectively, remove barriers</p>
+			  <h3>Support partners – mobilize funding, communicate effectively, remove barriers</h3>
 				<p>The Partnership supports the collective effort of our partners by advocating for enhanced and diversified funding sources, funding science and monitoring work to answer pressing questions, evaluating the effectiveness of recovery actions, convening forums to confront difficult issues, and ensuring effective communication throughout our partner network.</p>
 				
 				<h3>Funding Shortfalls Threaten Puget Sound Recovery</h3>
@@ -131,8 +137,7 @@ Tuesday, June 25, 2019</h3>
 				<p>This year, Washington State legislators listened to their constituents and to Governor Inslee, to the pleas of the Governor&rsquo;s Southern Resident Orca Task Force, and to our Leadership Council and the multitude of Management Conference members. They passed significant policy and budget bills aimed at orca recovery. Because of the bold actions on the part of our state elected officials, we have hope that we will stave off extinction for the Southern Residents. But state resources alone are not enough. Federal funding is crucial. Here&rsquo;s why:</p>
 				<p>Scientists say that we can still recover Puget Sound, but only if we act boldly now. We know what we need to do. The primary barrier between us and more food for orcas, clean and sufficient water for people and fish, sustainable working lands, and harvestable shellfish is funding. We cannot wait any longer to fully fund the Action Agenda and the Puget Sound Salmon Recovery Plan.</p>
 				
-			  <p>The primary source of funding to implement the Action Agenda is the Puget Sound Geographic Program. Over the past several fiscal years, Congress has appropriated $28 million annually into this fund, managed by the EPA. National Estuary Programs nationwide leverage $19 for every $1 in federal funding,ii and we are no exception. While this funding is significant and appreciated, estimates of the actual need to fully implement the Action Agenda show that the funding received falls far short of the need: the funding gap for the 2014-2015 Action Agenda was 68 percent, and for the 2016-2018 Action Agenda it was 73 percentiii. The funding gap for salmon recovery is about 84 percentiv. Our monitoring</p>
-				<p>shows that at these funding levels, we are barely holding our ground against further degradation, if not managing decline of the ecosystem.</p>
+			  <p>The primary source of funding to implement the Action Agenda is the Puget Sound Geographic Program. Over the past several fiscal years, Congress has appropriated $28 million annually into this fund, managed by the EPA. National Estuary Programs nationwide leverage $19 for every $1 in federal funding,ii and we are no exception. While this funding is significant and appreciated, estimates of the actual need to fully implement the Action Agenda show that the funding received falls far short of the need: the funding gap for the 2014-2015 Action Agenda was 68 percent, and for the 2016-2018 Action Agenda it was 73 percentiii. The funding gap for salmon recovery is about 84 percentiv. Our monitoring shows that at these funding levels, we are barely holding our ground against further degradation, if not managing decline of the ecosystem.</p>
 			
 			  <p>The single greatest step we could take to ensure a durable, systematic, and science-based effort for Puget Sound recovery is to fully fund the implementation of the Action Agenda and Salmon Recovery Plan on an on-going basis.</p>
 				<p>The Promoting United Government Efforts To Save Our Sound (PUGET SOS) Act (H.R. 2247), introduced by Congressmen Heck and Kilmer this year, would authorize up to $50 million in funding for Puget Sound recovery, a significant and very welcome jump from the $28 million per year that Congress has appropriated for the last several fiscal years.</p>
@@ -140,20 +145,20 @@ Tuesday, June 25, 2019</h3>
 				<h3>Why Passage of the PUGET SOS Act is Critical to Puget Sound Recovery</h3>
 			
 			  <p>Puget Sound is a national treasure, as long as it is healthy. A dying Puget Sound is a national disgrace. Our Governor, state Legislature, local elected officials, Tribes, and network of organizations and individuals have proven their commitment to recovering Puget Sound. We need commensurate investment at the federal level. Passage of the PUGET SOS Act would demonstrate that federal commitment. Here&rsquo;s why this bill would be such a boon to Puget Sound:</p>
-				<p>PUGET SOS aligns federal agency brainpower and resources. These are tremendous assets. Ensuring they are coordinated, setting goals, and holding each other accountable will help increase their effectiveness and provide yet another boost to Puget Sound recovery. Establishing the Puget Sound Program Office at EPA and requiring a Federal Task Force promises that these goals will be met.</p>
-				<p>PUGET SOS protects and sustains a cherished resource and a cherished way of life. The investment of up to $50 million authorized in the PUGET SOS bill will enable us and our partners to more effectively plan and implement the projects that will recover Puget Sound.</p>
-				<p>PUGET SOS demonstrates to the nation that Puget Sound is vital to the economic, cultural, and environmental security of the United States. By investing significantly in the health and well-being of Puget Sound, on par with other great waters like the Great Lakes and Chesapeake Bay, federal decision- makers demonstrate that Puget Sound is worth saving. They affirm that it is of critical importance to national well-being, and that they too are concerned for the future of their children and grandchildren. They demonstrate that recovering an ecosystem is more than a one-time effort, that our fates are interlinked with the environment we live in, and that we must stay ever vigilant and ever active in protecting and restoring our home.</p>
-			
+				<ul>
+				  <li>PUGET SOS aligns federal agency brainpower and resources. These are tremendous assets. Ensuring they are coordinated, setting goals, and holding each other accountable will help increase their effectiveness and provide yet another boost to Puget Sound recovery. Establishing the Puget Sound Program Office at EPA and requiring a Federal Task Force promises that these goals will be met.</li>
+				  <li>PUGET SOS protects and sustains a cherished resource and a cherished way of life. The investment of up to $50 million authorized in the PUGET SOS bill will enable us and our partners to more effectively plan and implement the projects that will recover Puget Sound.</li>
+				  <li>PUGET SOS demonstrates to the nation that Puget Sound is vital to the economic, cultural, and environmental security of the United States. By investing significantly in the health and well-being of Puget Sound, on par with other great waters like the Great Lakes and Chesapeake Bay, federal decision- makers demonstrate that Puget Sound is worth saving. They affirm that it is of critical importance to national well-being, and that they too are concerned for the future of their children and grandchildren. They demonstrate that recovering an ecosystem is more than a one-time effort, that our fates are interlinked with the environment we live in, and that we must stay ever vigilant and ever active in protecting and restoring our home.</li>
+			  </ul>
 			  <p>Thank you again for the opportunity to appear before you today, and I look forward to your questions.</p>
+				
+				<p>Laura Blackmore<br>
+				Executive Director, Puget Sound Partnership</p><br><br>
 				
 			
 			  <p>i <a href="http://www.seattletimes.com/seattle-news/data/big-city-growth-slows-across-u-s-but-seattle-still-ranks-no-2-" target="_blank">https://</a><a href="http://www.seattletimes.com/seattle-news/data/big-city-growth-slows-across-u-s-but-seattle-still-ranks-no-2-" target="_blank">www.seattletimes.com/seattle-news/data/big-city-growth-slows-across-u-s-but-seattle-still-ranks-no-2-</a></p>
-				<p>in-2018/</p>
 				<p>ii US Environmental Protection Agency, 2018. National Estuary Program website, Financing Strategies Used by the National Estuary Program. Last updated June 4, 2018. Accessed June 20, 2019. <a href="http://www.epa.gov/nep/financing-strategies-used-national-estuary-program" target="_blank">https://www.epa.gov/nep/financing-strategies-used-national-estuary-program</a></p>
 				<p>iii <a href="http://www.psp.wa.gov/sos" target="_blank">Puget Sound Partnership, 2017. 2017 State of the Sound. Olympia, Washington, November 2017. 84pp. </a><a href="http://www.psp.wa.gov/sos" target="_blank">www.psp.wa.gov/sos</a></p>
-				<p><br>
-			  </p>
-				<p><img width="624" height="1" alt="image" src="Image_003.png"></p>
 				<p>iv <a href="https://stateofsalmon.wa.gov/exec-summary/">Governor&rsquo;s Salmon Recovery Office, 2018. State of the Salmon Report, Executive Summary, page 9. Accessed June 20, 2019.</a> </p>
 			
 		
