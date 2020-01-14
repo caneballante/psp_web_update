7254382c4004e97cc0fa320f8ba2f6bf9d81695e
Kevin Hyde <kevin.hyde@psp.wa.gov>
2020-01-14 10:29:29 -0800

    Created new page for PSDOTH 2020
    I created a new page for Puget Sound Day on the Hill 2020 and added the link to it to the footer and modal html.

---
+++ psdoth2020.php
@@ -0,0 +1,123 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Puget Sound Partnership - 2019 Puget Sound Day on the Hill</title>
+	<!-- Bootstrap -->
+	<link href="css/bootstrap.css" rel="stylesheet">
+	<link href="css/custom.css" rel="stylesheet" type="text/css">
+	<link href="css/custom-erika.css" rel="stylesheet" type="text/css">
+	<!-- loads the wf-loading class right away to minimize FOUT -->
+	<script>
+		document.documentElement.className += ' wf-loading';
+	</script>
+	<!-- Font PRENTON TYPEKIT -->
+	<script src="https://use.typekit.net/srt5jze.js"></script>
+	<script>
+		try {
+			Typekit.load( {
+				async: true
+			} );
+		} catch ( e ) {}
+	</script>
+	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+	<!--[if lt IE 9]>
+	<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+	<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+	<script>
+		/*this variable is used to set the proper nav to active. It should to the order the nav item is in the list*/
+		navSelected = 1;
+	</script>
+</head>
+
+<body>
+	<?php include 'includes/modal-inc.html';?>
+	<!-- START IMAGE HEADER -->
+	<header class="overview-page-image-style overview-page-image110"> </header>
+	<div class="row">
+		<div class="col-sm-12 orange"></div>
+	</div>
+	<div class="container-fluid page-content padding-50-bottom">
+		<div class="row">
+			<div class="col-sm-10 margin-40-left  padding-20-top padding-40-left">
+				<h1>2020 PUGET SOUND DAY ON THE HILL<br>
+					APRIL, WASHINGTON D.C.
+				</h1>
+
+				<p>&nbsp;</p>
+			</div>
+			<div class="col-sm-2 padding-20-top"></div>
+		</div>
+
+		<div class="row">
+			<div class="col-sm-9 margin-40-left padding-20-top padding-40-right content-column mobile-padding-20-left">
+				<div class="width-300-govdelivery-doth border-thick-blue floatright padding-10-all">
+					<form id="GD-snippet-form" action="https://public.govdelivery.com/accounts/WAPSP/subscribers/qualify" accept-charset="UTF-8" method="post">
+						<input name="utf8" type="hidden" value="&#x2713;" />
+						<input type="hidden" name="authenticity_token" value="nOYBKnmWNQvieUlEVV4W6duHYH0RnJgtlvjmL4+9PgYyAL3d7msuMDW3SKUDVIM4+JB2/yCASiTVzcv4a1KTyQ==" />
+						<input type="hidden" name="topic_id" id="topic_id" value="WAPSP_126" />
+						<fieldset>
+							<p> <span class="margin-0-top">SIGN UP TO RECEIVE UPDATES FOR PUGET SOUND DAY ON THE HILL</span> </p>
+							<ol class='form'>
+								<li class='email_fields' style='display: block'>
+									<label for="email"><img class="required" src="https://content.govdelivery.com/images/required.gif" alt="Required" />Email Address</label>
+									<input type="text" name="email" id="email" class="long" />
+								</li>
+							</ol>
+							<div class='button_panel'>
+								<input type="submit" name="commit" value="Submit" class="form_button" />
+							</div>
+						</fieldset>
+					</form>
+				</div>
+					
+				<h3 class="margin-0-top"></h3>
+				<p>The Puget Sound Partnership and Northwest Indian Fisheries Commission invite you to the 2020 Puget Sound Day on the Hill, together with Salmon Days on the Hill, in Washington, D.C., this April. Attendees will come together this year to celebrate victories—like the increased funding in 2020 for the Environmental Protection Agency’s Puget Sound Geographic Program and for implementation of the Pacific Salmon Treaty —and build momentum for Puget Sound and salmon recovery. Puget Sound Day on the Hill raises awareness about the need for sustained federal investment in Puget Sound recovery. Over the last five years, the event has grown in prominence and drawn more participants. Representatives from tribes, state and local governments, nonprofits, and businesses, along with concerned citizens, pay their own way to attend the event and lend their voices in support of action to save the Sound.
+</p>
+								
+				<hr>
+				<h3 class="margin-0-top">&nbsp;</h3>
+				<h3>&nbsp;</h3>
+<br>
+			</div>
+
+		
+		</div>
+		<!--END OF ROW -->
+	</div>
+	<!--END OF CONTENT CONTAINER -->
+
+	<?php include 'includes/footer-inc.html';?>
+
+	<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+	<script src="js/jquery-1.11.2.min.js"></script>
+
+	<!-- Include all compiled plugins (below), or include individual files as needed -->
+	<script src="js/bootstrap.js"></script>
+	<!-- custom js -->
+	<script src="js/custom.js"></script>
+	<!-- Google Tracking  -->
+	<script>
+		( function ( i, s, o, g, r, a, m ) {
+			i[ 'GoogleAnalyticsObject' ] = r;
+			i[ r ] = i[ r ] || function () {
+				( i[ r ].q = i[ r ].q || [] ).push( arguments )
+			}, i[ r ].l = 1 * new Date();
+			a = s.createElement( o ),
+				m = s.getElementsByTagName( o )[ 0 ];
+			a.async = 1;
+			a.src = g;
+			m.parentNode.insertBefore( a, m )
+		} )( window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga' );
+
+		ga( 'create', 'UA-69373425-1', 'auto' );
+		ga( 'send', 'pageview' );
+	</script>
+</body>
+
+</html>
