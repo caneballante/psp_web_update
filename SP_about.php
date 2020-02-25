7f764dfdd152e31c7fe79c91cfbbea0c805267a0
jon bridgman <jon.bridgman@psp.wa.gov>
2020-02-25 13:59:47 -0800

    Removing depracated boards pages

--- SP_about.php
+++ SP_about.php
@@ -67,8 +67,8 @@
 		 
 		<div class="col-sm-7 padding-20-top content-column"> <!-- InstanceBeginEditable name="6col_content" -->
 		<p>The Science Panel's expertise and advice are critical to the Puget Sound Partnership&rsquo;s efforts to develop a comprehensive, science-based plan to restore Puget Sound. The members, appointed by the Leadership Council, were chosen from the top scientists in Washington State. 2020 Meeting schedules can be found <a href="board_meetings.php">here.</a><br></p>
-		<!--START MEMBER-->
-	``<IFRAME WIDTH=1000 HEIGHT=700 FRAMEBORDER=0 SRC="https://app.smartsheet.com/b/publish?EQBCT=34b79c1749f94979b7fcc38cb37bdce0"></IFRAME>
+		<!--START INCLUDE-->
+	       <IFRAME WIDTH=1000 HEIGHT=700 FRAMEBORDER=0 SRC="https://app.smartsheet.com/b/publish?EQBCT=34b79c1749f94979b7fcc38cb37bdce0"></IFRAME>
 			<h3>Board contact</h3>
 			<p>For questions about the Partnership's boards contact:<br />
 	   		<strong>Deb Hagen, <br>
@@ -77,12 +77,12 @@
 			360.819.0057
 			</p>
 			<p><strong>Jillian Reitz, <br>
-Boards Policy Specialist<br>
+            Boards Policy Specialist<br>
             </strong> <a href="mailto:jillian.reitz">jillian.reitz@psp.wa.gov</a><br>
-360.742.2936 </p>
+            360.742.2936 </p>
 			<h3>Board Materials</h3>
 			<p><a href="https://pspwa.box.com/s/gabtcrbzo9i5yybkeyi6lx6cez0bh10o"> Board's documents, meeting materials, summaries, and Resolutions</a>			</p>
-		 <!--END MEMBER-->
+		 <!--END INCLUDE-->
 			<!-- InstanceEndEditable --> </div>
 		
 		<div class="col-sm-2 padding-20-top padding-0-right"> <!-- InstanceBeginRepeat name="right_nav_repeat" --><!-- InstanceEndRepeat --> </div>
