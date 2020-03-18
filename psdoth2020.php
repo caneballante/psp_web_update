01aa986d541d3876ea09700c5c4d2fd2fd1018ac
Kevin Hyde <kevin.hyde@psp.wa.gov>
2020-03-18 14:30:12 -0700

    Added new text to PSDOTH 2020 page and updated format
    Also removed the sponsors image at the bottom for the time being

--- psdoth2020.php
+++ psdoth2020.php
@@ -56,18 +56,23 @@
 		</div> --> 
         <div class="row">
 		    	<div class="col-sm-12">
-				<h2 class="margin-0-top">2020 Puget Sound Day on the Hill Postponed</h2>
+				<h2 class="margin-2-top">2020 Puget Sound Day on the Hill Postponed</h2>
+					<p><strong>Why was Puget Sound Day on the Hill postponed?</strong></p>
                 
-					<p>Dear Puget Sound recovery community,<br><br>
-					With deep regret, Justin Parker and I are postponing Puget Sound Day on the Hill 2020 to comply with social distancing guidelines.  We hope to move the event to this fall; please stay tuned for updates!  I’ll also send around an updated version of the sign-on letter for review and comment in the next day or two.<br><br> Please, all of you stay safe and healthy. We’ll do what we can this spring to thank our delegation and urge their continued support for Puget Sound recovery from here in beautiful Washington State.<br><br>
-Best,<br>
-Laura Blackmore
+					<p>Puget Sound Day on the Hill 2020 was postponed to comply with social distancing guidelines during the COVID-19 outbreak. We hope to move the event to the fall. Please stay tuned for more updates on our plans.</p><br>
+					
+					<p><strong>How are we supporting Puget Sound?</strong></p>
+					<p>For the past five years, we’ve sent a letter to Congress with the signatures of those who support Puget Sound recovery. With the postponement of an in-person Puget Sound Day on the Hill 2020, this letter takes on added importance.<br><br>
+We have <a href="https://pspwa.box.com/shared/static/9eid4ap2va257utdxezlxyyms3pe534r.docx">updated the letter</a> this year with input from our tribal partners and others.  New elements include paragraphs about reauthorization of the FAST Act and funding for the Pacific Salmon-Hatchery and Genetic Management Plan line in NOAA’s budget; everything else is carried over from previous years.<br><br>
+Please take a moment to review it; if you have questions or concerns, please contact Laura Blackmore (<a href="mailto:laura.blackmore@psp.wa.gov">laura.blackmore@psp.wa.gov</a>) by March 25, 2020. As you review, please remember that all of the text in this letter has been carefully negotiated over time.  Changes aren’t simple, but we want to know if there’s an issue that might prevent you from signing the letter. We’d like to circulate the final letter for signature on March 30, 2020.
+</p><br>
+					<p><strong>What about my travel accommodations?</strong></p>
+					<p>We have guidance from <a href="https://www.alaskaair.com/content/advisories/travel-advisories?int=AS_HOMEADVISORY_-prodID:Awareness">Alaska Airlines on cancelling flights</a>. Be sure to cancel your hotel, rental car, or any airport shuttle you may have reserved.</p>
  
                 </p><br>
                     
 				<hr>
-                <img class="img-responsive" src="images/PSDOTH2020/2020-PSDOTH-mail-footer.png" width="1000" height="291" alt=""/><br>
-                <hr>
+                
            </div>
         </div>
         
