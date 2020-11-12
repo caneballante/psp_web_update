3419326e5ed42047c376078d3ac96a8d40a5fbf6
Kevin Hyde <kevin.hyde@psp.wa.gov>
2020-11-12 15:56:02 -0800

    Added strategic plan page to the inclue menu and revised the title of the page

--- strategic-plan.php
+++ strategic-plan.php
@@ -5,7 +5,7 @@
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <!-- InstanceBeginEditable name="doctitle" -->
-<title>Technology Strategic Plan</title>
+<title>Strategic Plan</title>
 <!-- InstanceEndEditable -->
 <!-- Bootstrap -->
 <link href="css/bootstrap.css" rel="stylesheet">
