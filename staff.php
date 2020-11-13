fde4ebf59a7c493fefb99568598110fa5ef1cfd5
Kevin Hyde <kevin.hyde@psp.wa.gov>
2020-11-13 15:13:01 -0800

    Edited heading size

--- staff.php
+++ staff.php
@@ -137,7 +137,7 @@
 			<p>Visual Communications Lead<br>
 			   360.480.1020 | <a href="mailto:chase.nuuhiwa@psp.wa.gov">chase.nuuhiwa@psp.wa.gov</a></p>
             <!--PLANNING TEAM-->
-			<h3 class="padding-10-top">INTEGRATED PLANNING PROGRAM</h3>
+			<h2>INTEGRATED PLANNING PROGRAM</h2>
 			<h5>Setphanie Suter</h5>
 			<p>Planning Program Director<br>
 			360.791.3154 | <a href="mailto:stephanie.suter@psp.wa.gov">stephanie.suter@psp.wa.gov</a></p>
