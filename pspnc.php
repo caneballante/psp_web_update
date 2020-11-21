a7141d0f1788a6fe2554efd50e52adf35b53b5aa
Kevin Hyde <kevin.hyde@psp.wa.gov>
2020-11-20 17:06:56 -0800

    Revised language on PSPNC page

--- pspnc.php
+++ pspnc.php
@@ -88,7 +88,7 @@
 <p>One of the other advantages of the marketplace is that it provides a method for restoration work to happen close to the permit site. Creosote-soaked piers and pilings are widespread throughout Puget Sound, so the conservation projects funded by the sale of credits through the PSPNC program will be located in the same area as the permitted project. 
 </p>
 			
-		<h2>The PSPNC provides conservation credits for obligations under Endangered Species Act consultations</h2>
+		<h2>The PSPNC provides conservation credits for obligations under Endangered Species Act Section 7 consultations</h2>
 			<p>The Puget Sound Partnership provides conservation credits for obligations under Endangered Species Act (ESA) consultations. If other federal, local, or state agencies also require mitigation, and that mitigation was not already included in the conservation calculator, it is the applicant's responsibility to clarify with those agencies whether the conservation credits provided by the Puget Sound Partnership would meet those obligations. For example, Puget Sound Partnership Nearshore Credits may not be used to satisfy compensatory mitigation obligations under Section 404 of the Clean Water Act.</p>
 
 <p>The PSPNC is not a mitigation bank, or an in-lieu fee program. The PSPNC conservation credits cannot be used for Clean Water Act mitigation. 
