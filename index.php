171baad7011900f084aca448ff4bbbf2e472ea59
Kevin Hyde <kevin.hyde@psp.wa.gov>
2021-02-05 08:22:48 -0800

    Updated board meeting dates

--- index.php
+++ index.php
@@ -243,20 +243,6 @@ and recovering Puget Sound</h1>
 			<div class="newsbox calendarbox">
 				<!--news content -->
 				<div class="newsbox-date">FEB<br>
-			  3</div>
-				<div class="newsbox-text"><a href="board_meetings.php">SCIENCE PANEL, <br>
-					Online<br>
-							</a><br>
-				</div>
-				<!--END news content-->
-			</div>
-		</div>
-		<!--COLUMN END-->	
-		<!--COLUMN START-->
-		<div class="col-md-3 col-sm-4">
-			<div class="newsbox calendarbox">
-				<!--news content -->
-				<div class="newsbox-date">FEB<br>
 			  18</div>
 				<div class="newsbox-text"><a href="board_meetings.php">LEADERSHIP COUNCIL, <br>
 					Online<br>
@@ -265,8 +251,8 @@ and recovering Puget Sound</h1>
 				<!--END news content-->
 			</div>
 		</div>
-		<!--COLUMN END-->
-			<!--COLUMN START-->
+		<!--COLUMN END-->	
+		<!--COLUMN START-->
 		<div class="col-md-3 col-sm-4">
 			<div class="newsbox calendarbox">
 				<!--news content -->
@@ -287,6 +273,20 @@ and recovering Puget Sound</h1>
 				<div class="newsbox-date">APR<br>
 			  14-15</div>
 				<div class="newsbox-text"><a href="board_meetings.php">SCIENCE PANEL, <br>
+					Online<br>
+							</a><br>
+				</div>
+				<!--END news content-->
+			</div>
+		</div>
+		<!--COLUMN END-->
+			<!--COLUMN START-->
+		<div class="col-md-3 col-sm-4">
+			<div class="newsbox calendarbox">
+				<!--news content -->
+				<div class="newsbox-date">MAY<br>
+			  12</div>
+				<div class="newsbox-text"><a href="board_meetings.php">ECOSYSTEM COORDINATION BOARD, <br>
 					Online<br>
 							</a><br>
 				</div>
