246491052fa1c162404ebb87d5c51d4af0154f63
Kevin Hyde <kevin.hyde@psp.wa.gov>
2021-02-04 17:08:49 -0800

    Removed link to feedback from, per Don Gourlie's request.

--- priority-outcomes.php
+++ priority-outcomes.php
@@ -84,8 +84,8 @@ navSelected = 2;
 			<h3>How were the desired outcomes identified?</h3>
 			<p>The draft list of desired outcomes was identified using existing plans, including Implementation Strategies and Local Integrating Organization (LIO) plans as initial sources. Partnership staff worked in collaboration with representatives of Puget Sound tribes, LIOs, state agencies, and the Environmental Protection Agency (EPA) to assemble data about the stressors and sources that are addressed through existing plans. The outcomes in Implementation Strategies, LIO plans, and other plans are all created through well-vetted, science-informed, and collaborative processes led by subject matter experts that capture knowledge from around the region.</p>
 		<h3>How can you weigh in?</h3>
-			<p>Partners can review the draft list of desired outcomes and provide input.</p>
-			<button class="button button1"><a href="https://survey.zohopublic.com/zs/vcCsGo" target="_blank" style="color:white"><strong>Click here to provide input on the draft desired outcomes</strong></a></button>
+			<p>Partners had the opportunity to comment on draft outcomes through Feb. 4.</p>
+			
 		
 		<h3>How can groups best prepare to engage in phase 3? </h3>
 			<p>Desired outcomes will serve as the basis for identifying strategies and actions in the next phases of the Action Agenda update process. Where desired outcomes describe what we intend to accomplish, the strategies and actions will describe how to best achieve those outcomes. Phase 3 will include exercises such as public workshops, partner meetings, and online engagement tools and will be designed to leverage the extensive work completed to date to identify strategies and actions that advance Puget Sound recovery, including existing plans and ongoing programs.</p>
