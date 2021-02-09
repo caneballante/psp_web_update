1dbedfbdbfbbcefedeb0c0b1fa1d02447ea36ba5
Kevin Hyde <kevin.hyde@psp.wa.gov>
2021-02-09 12:50:20 -0800

    Added new PSDOTH page

---
+++ psdoth2021.php
@@ -0,0 +1,111 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Puget Sound Partnership - 2021 Puget Sound Days on the Hill</title>
+	<!-- Bootstrap -->
+	<link href="css/bootstrap.css" rel="stylesheet">
+	<link href="css/custom.css" rel="stylesheet" type="text/css">
+	<link href="css/custom-erika.css" rel="stylesheet" type="text/css">
+	<!-- loads the wf-loading class right away to minimize FOUT -->
+	<script>
+		document.documentElement.className += ' wf-loading';
+	</script>
+	<!-- Font PRENTON TYPEKIT -->
+	<script src="https://use.typekit.net/srt5jze.js"></script>
+	<script>
+		try {
+			Typekit.load( {
+				async: true
+			} );
+		} catch ( e ) {}
+	</script>
+	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+	<!--[if lt IE 9]>
+	<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+	<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+	<script>
+		/*this variable is used to set the proper nav to active. It should to the order the nav item is in the list*/
+		navSelected = 1;
+	</script>
+</head>
+
+<body>
+	<?php include 'includes/modal-inc.html';?>
+	<!-- START IMAGE HEADER -->
+	
+	<div class="container page-content padding-50-all"><br><br>
+		<div class="row">
+            <div class="col-sm-8">
+                <img class="img-responsive" src="images/PSDOTH2020/2020-virtual-psdoth-web-banner_original-JULY.jpg" width="1200" height="424" alt=""/>
+            </div>
+			<div class="col-sm-4">
+                
+			</div>
+			<div class="col-sm-2 padding-20-top"></div>
+		</div>
+      <!--   <div class="row">
+			<div class="col-sm-12 padding-20-top">
+				<p>Sponsors | Travel and Lodging Information | Schedule | Materials<br></p>
+			</div>      
+		</div> --> 
+        <div class="row">
+		    	<div class="col-sm-12">
+				<h2 class="margin-2-top">2021 Virtual Puget Sound Days on the Hill</h2>
+                
+					<p>Since we can’t gather together in person, this year's Puget Sound Day on the Hill will be a series of virtual events with the Washington State congressional delegation and special guests.</p>
+					<p>Puget Sound Partnership and the Northwest Indian Fisheries Commission invite you to participate in our Virtual Puget Sound Days on the Hill, starting on Friday, April 23. We’ll discuss Puget Sound restoration and protection, salmon recovery, environmental justice, climate change adaptation and mitigation, and infrastructure, among other topics.</p>
+					
+					<p>We will announce registration info soon. Check this page for the latest information or <a href="https://public.govdelivery.com/accounts/WAPSP/subscriber/new">sign up for our listserv</a>.</p>
+					
+						
+											
+					<hr>
+					<div><img src="images/PSDOTH2020/2020-virtual-PSDOTH-web-FOOTER.jpg" width="700" height="333" alt=""/></div>
+           </div>
+        </div>
+        
+		
+        </div>
+         
+        </div>
+			
+	
+		<!--END OF ROW -->
+	</div>
+	<!--END OF CONTENT CONTAINER -->
+
+	<?php include 'includes/footer-inc.html';?>
+
+	<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+	<script src="js/jquery-1.11.2.min.js"></script>
+
+	<!-- Include all compiled plugins (below), or include individual files as needed -->
+	<script src="js/bootstrap.js"></script>
+	<!-- custom js -->
+	<script src="js/custom.js"></script>
+	<!-- Google Tracking  -->
+	<script>
+		( function ( i, s, o, g, r, a, m ) {
+			i[ 'GoogleAnalyticsObject' ] = r;
+			i[ r ] = i[ r ] || function () {
+				( i[ r ].q = i[ r ].q || [] ).push( arguments )
+			}, i[ r ].l = 1 * new Date();
+			a = s.createElement( o ),
+				m = s.getElementsByTagName( o )[ 0 ];
+			a.async = 1;
+			a.src = g;
+			m.parentNode.insertBefore( a, m )
+		} )( window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga' );
+
+		ga( 'create', 'UA-69373425-1', 'auto' );
+		ga( 'send', 'pageview' );
+	</script>
+</body>
+
+</html>
