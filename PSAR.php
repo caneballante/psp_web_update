71fa2b7e847e065a1d21fdf764e80556aba68e5a
Kevin Hyde <kevin.hyde@psp.wa.gov>
2021-03-12 09:31:06 -0800

    Added links for other video tours

--- PSAR.php
+++ PSAR.php
@@ -70,7 +70,9 @@
 				<p class="padding-10-all"><span class="new-red">NEW!</span>&nbsp;Links to Puget Sound Acquisition and Restoration Large Capital Project legislative tours<br />
 				<ul>
 				<li><a href="https://kingcounty.gov/services/environment/animals-and-plants/restoration-projects/projects/fall-city-floodplain-restoration.aspx" target="_blank">Fall City Floodplain Restoration</a></li>
+				<li><a href="https://youtu.be/cBvJSBVW-ts" target="_blank">Florence Island Tidal Wetland Acquisition</a></li>
 				<li><a href="https://connects.sumnerwa.gov/white-river-restoration-project" target="_blank">Sumner White River Restoration</a></li>
+				<li><a href="https://youtu.be/cBvJSBVW-ts" target="_blank">Port Susan Bay Restoration</a></li>
 				<li><a href="https://www.youtube.com/watch?v=VObtMVew2HY" target="_blank">Skookum Creek Valley Phase 2 Conservation</a></li>
 				<li><a href="https://www.kentwa.gov/Home/Components/FacilityDirectory/FacilityDirectory/404/568" target="_blank">Downey Farmstead Side Channel Restoration</a></li>
 				<li><a href="https://www.youtube.com/watch?v=VObtMVew2HY" target="_blank">West Oakland Bay Restoration</a></li>
