faa42dabc9a586410ca458181ac1f1b45efc9239
Kevin Hyde <kevin.hyde@psp.wa.gov>
2021-03-26 15:08:31 -0700

    Added new leg call recording and presentation

--- legislative_updates.php
+++ legislative_updates.php
@@ -120,6 +120,7 @@
 			<p><a href="https://pspwa.box.com/s/f7inacr461jimdfw61ja4qm0l9f8g6e2" target="_blank">March 5, 2021 call recording</a> and <a href="https://pspwa.box.com/s/t9kzby5q0uwu2r9megmzslf8c288o9tb" target="_blank">presentation</a></p>
 			<p><a href="https://pspwa.box.com/s/gpp5cp43i6zsrl8qcqlo3ah16a2mee4z" target="_blank">March 12, 2021 call recording</a> and <a href="https://pspwa.box.com/s/cw3xpfq534qdcn6a8mppp1emqywr3ent" target="_blank">presentation</a></p>
 			<p><a href="https://pspwa.box.com/s/794wgtlufnjqn3sd8ao60xe1jyaqldwc" target="_blank">March 19, 2021 call recording</a> and <a href="https://pspwa.box.com/s/95xf581w2mgo1n8lw0j452ouvzt7b56p" target="_blank">presentation</a></p>
+			<p><a href="https://pspwa.box.com/s/mels0qzaxzp3vq4a1zqqaap6xljljh6q" target="_blank">March 26, 2021 call recording</a> and <a href="https://pspwa.box.com/s/tyct6c0ngurra98cc5pnmh2d8rv5ufio" target="_blank">presentation</a></p>
             
             
            
