fadab1b67c5c46ac31f9611b0dc73b46fb620ca0
Kevin Hyde <kevin.hyde@psp.wa.gov>
2021-04-07 11:26:41 -0700

    Added new page for monitoring to accelerate recovery

---
+++ monitoring-accelerate-recovery.php
@@ -0,0 +1,220 @@
+<!DOCTYPE html>
+<html lang="en"><!-- InstanceBegin template="/Templates/page-4-template.dwt" codeOutsideHTMLIsLocked="false" -->
+<head>
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<!-- InstanceBeginEditable name="doctitle" -->
+<title>Puget Sound Partnership - Monitoring to Accelerate Recovery</title>
+<!-- InstanceEndEditable -->
+<!-- Bootstrap -->
+<link href="css/bootstrap.css" rel="stylesheet">
+<link href="css/custom.css" rel="stylesheet" type="text/css">
+<link href="css/custom-erika.css" rel="stylesheet" type="text/css">
+<!-- loads the wf-loading class right away to minimize FOUT -->
+<script>document.documentElement.className += ' wf-loading';</script>
+<!-- Font PRENTON TYPEKIT -->
+<script src="https://use.typekit.net/srt5jze.js"></script>
+<script>try{Typekit.load({ async: true });}catch(e){}</script>
+<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+<!--[if lt IE 9]>
+	<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+	<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+<!-- InstanceBeginEditable name="head" -->
+<!-- InstanceEndEditable -->
+<!-- InstanceParam name="OptionalRegion1" type="boolean" value="true" -->
+<!-- InstanceParam name="OptionalRegion2" type="boolean" value="false" -->
+<!-- InstanceParam name="OptionalRegion3" type="boolean" value="true" -->
+<!-- InstanceParam name="OptionalRegion4" type="boolean" value="false" -->
+<!-- InstanceParam name="OptionalRegion5" type="boolean" value="true" -->
+<!-- InstanceParam name="OptionalRegion6" type="boolean" value="true" -->
+<!-- InstanceParam name="OptionalRegion7" type="boolean" value="false" -->
+<!-- InstanceParam name="OptionalRegion8" type="boolean" value="false" -->
+</head>
+<body>
+<?php include 'includes/modal-inc.html';?>
+<!-- START IMAGE HEADER --> 
+ <!-- InstanceBeginEditable name="overviewphoto" -->
+<header class="overview-page-image-style overview-page-image16">
+
+<div class="overview-quote pull-right"><h3>&nbsp;</h3></div>
+
+ </header>
+<!-- InstanceEndEditable -->  
+<div class="container-fluid page-content padding-50-bottom">
+	<div class="row">
+		<div class="col-sm-3 padding-20-top"></div>
+		<div class="col-sm-7 padding-20-top"> <!-- InstanceBeginEditable name="6col_header" -->
+			<h1> Puget Sound Partnership 2021 Request for Collaborative Project Proposals: Monitoring to Accelerate Recovery</h1>
+			<!-- InstanceEndEditable --></div>
+		<div class="col-sm-2 padding-20-top"></div>
+	</div>
+	
+	<div class="row"> 
+		<div class="col-sm-3">
+			<div class="nav-leftside-custom">
+				<ul class="nav nav-stacked nav-pills nav-leftside-custom padding-left-0 margin-10-top">
+					<!-- InstanceBeginEditable name="left_nav" -->
+				
+			
+		
+			<!-- InstanceEndEditable -->
+				</ul>
+			</div>
+		</div>
+		 
+		<div class="col-sm-7 padding-20-top content-column"> <!-- InstanceBeginEditable name="6col_content" -->
+			<h3>Funding opportunity for the 2021-2023 biennium</h3>
+			<p>The Puget Sound Partnership requests project proposals that address priority monitoring information needs to inform and accelerate recovery of Puget Sound.</p>
+			
+			<p>All projects are expected to support the objectives of the Puget Sound Ecosystem Monitoring Program (PSEMP) Strategic Plan to increase collaboration, support adaptive management, and improve communication. </p>
+			
+			<p>Projects should also address cross-cutting principles of integrating biophysical and social sciences, JEDI, and climate change.</p>
+			
+			<p>For this 2021 opportunity, approximately $750,000 will be available over a two-year period to support an estimated 14 to 25 projects in total, with individual project budgets ranging from a minimum of $30,000 to $80,00 maximum. </p>
+			
+			<p>Project leads must be affiliated with an organization, agency, or tribal government that is licensed to do business in the State of Washington.</p>
+			
+			<p>Projects must address priority information needs under one or both project categories and include a detailed engagement plan.
+			<ul>
+			<li>Category 1: Monitoring, assessment, and reporting of the status and time trends of Vital Sign indicators and potential future indicators</li>
+			<li>Category 2: Synthesis and evaluation of ecosystem change; evaluation of cumulative effects; effectiveness assessment</li></ul></p>
+		
+		<button class="button button1"><a href="https://pspwa.box.com/s/lwk71kpaaoo9off1iw5ancj3m7xrcp45" target="_blank" style="color:white"><strong>Access the full solicitation here for details.</strong></a></button>
+		<br>
+		<br>
+		<table class="table" width="774" border="2">
+        <tbody>
+          <tr>
+            <th width="50%" scope="col"><p>Timeline</p></th>
+            <th width="50%" scope="col"><p>Date</p></th>
+          </tr>
+          <tr>
+            <td><p>Solicitation released</p></td>
+            <td><p>Tuesday, April 13</p></td>
+          </tr>
+          <tr>
+            <td><p>Information session (webinar will be recorded</p></td>
+            <td><p>Tuesday, April 27, 12:00 p.m. - 1:00 p.m.</p></td>
+          </tr>
+          <tr>
+            <td><p>Letter of intent due</p></td>
+            <td><p>Friday, May 14, by 11:59 p.m.</p></td>
+          </tr>
+          <tr>
+            <td><p>Response to letters of intent</p></td>
+            <td><p>Tuesday, June 15, by 11:59 p.m.</p></td>
+          </tr>
+          <tr>
+            <td><p>Full proposals due</p></td>
+            <td><p>Friday, August 6, by 11:59 p.m.</p></td>
+          </tr>
+          <tr>
+            <td><p>Full proposal evaluation and selection</p></td>
+            <td><p>August 9 - September 10</p></td>
+          </tr>
+			<tr>
+			<td><p>Project scoping and contract negotiation</p></td>
+			<td><p>September 13 - October 15</p></td>
+			</tr>
+		<tr>
+			<td><p>Project start date</p></td>
+			<td><p>Dependent on contract negotiation</p></td>
+			</tr>
+			<tr>
+			<td><p>Project end date</p></td>
+			<td><p>Dependent on fund source but not later than June 30, 2023, or September 30, 2023</p></td>
+				</tr>
+        </tbody>
+        </table>
+		<br>
+		
+		<h3>Information session</h3>
+		<p>The Partnership will host one information session on Tuesday, April 27 12:00 pm to 1:00 pm
+to brief interested project leads on the solicitation process and provide an opportunity to ask questions. The information will be recorded and posted below. </p>
+		
+		<button class="button button1"><a href="https://zoom.us/meeting/register/tJcoce2pqz0sG9GgQDMJQKIld1jO7AIXXCOL" target="_blank" style="color:white"><strong>To attend the information session, please register at this link.</strong></a></button>
+		<hr>
+		<h3>PSEMP and Vital Signs Projects Funded in 2019-21:</h3>
+		<p>The Partnership funded the following projects using PSEMP program funding and Vital Signs reporting program funding during the 2019-21 biennium:</p>
+		<h4>PSEMP products:</h4>
+		<ul>
+		<li><strong>Nearshore Summit and Synthesis</strong>
+			<p>There has been a long-standing need for a synthesis and evaluation of past nearshore and estuary projects to inform the direction of future restoration projects; this virtual three-day nearshore summit highlighted the results of research projects in Puget Sound and provided a forum for dialogue between researchers and restoration practitioners. The summit produced a proceedings document, including a synopsis of recent social science research related to nearshore restoration.</p>
+			<p>Project Leads: Tish Conway-Cranos, Washington Department of Fish and Wildlife; Jason Toft, University of Washington</p></li>
+			<li><strong>Metrics for Ecosystem Observation</strong>
+			<p>Climate change poses a serious threat to Puget Sound recovery efforts. To adapt recovery efforts to these changes, we need metrics that show why change is occurring. The project will use data from ongoing oceanographic and atmospheric measurement programs to develop six fundamental environmental metrics and compare current status updates to historical values. When finished, the metrics will be available to the public via a web-based dashboard that will concisely summarize current conditions. </p>
+				<p>Project Lead: John Mickett, University of Washington</p></li>
+			<li><strong>Marine Waters Annual Overview</strong>
+			<p>The Marine Waters Overview Report and annual workshop will communicate results from regional monitoring programs through a virtual workshop and published report of technical summaries and high-level information synthesis across monitoring programs and scientific disciplines. The annual workshop and report provide an integrative understanding of current trends in Puget Sound marine water quality and impacts through the food web, link to the Puget Sound Vital Signs and goals for ecosystem recovery, and share critical information for making evidence-based decisions about recovery efforts.</p>
+			<p>Project Leads: Jude Apple, Padilla Bay National Estuarine Research Reserve; Rachel Wold, University of Washington</p></li>
+			<li><strong>Avian Monitoring in Puget Sound</strong>
+			<p>Puget Sound and the Salish Sea support hemispherically significant concentrations of shorebirds, waterfowl, and other marine bird species. Despite this importance, birds have received little attention by state and federal recovery efforts. This project develops science products that support the management needs of estuary birds, including end-user stakeholder engagement, avian-habitat suitability models, and the initiation of a regional avian monitoring framework for river delta estuaries. A monitoring framework will facilitate targeted data collection that allows for avian conservation planning and management at local and regional scales and supports integration of birds in estuary recovery efforts.</p>
+			<p>Project Lead: Amanda Summers, Stillaguamish Tribes of Indians; Trina Bayard, Audubon Washington </p></li>
+			<li><strong>Contaminants of Emerging Concern</strong>
+			<p>This project will identify and prioritize a suite of contaminants of emerging concern (CECs), based on biological relevance, to focus future monitoring efforts and provide the information necessary for a management response. Hundreds of CECs such as pharmaceuticals, plasticizers, pesticides, and others have been detected in Puget Sound. It is critical that we prioritize limited funding to address the chemicals of greatest concern and for future regulatory requirements. </p>
+			<p>Project Leads: Andy James, University of Washington Tacoma; Ruth Sofield, Western Washington University</p></li>
+			<li><strong>Juvenile Salmonid Monitoring in Puget Sound Rivers</strong>
+			<p>Watershed-scale juvenile salmon monitoring provides crucial information on population abundance, productivity, and life history diversity, which is relevant to many salmon conservation and management decisions. Ongoing salmon outmigration studies are often described, evaluated, and funded individually. </p>
+			<p>Project Leads: Joe Anderson, Washington Department of Fish and Wildlife; Dawn Pucci, Island County</p></li>
+		</ul>
+		<h4>Vital Signs reporting program products:</h4>
+		<ul>
+		<li><strong>Birds Vital Sign, Abundance of marine birds indicator</strong>
+			<p>Process annual mid-winter WDFW PSAMP seabird and waterfowl survey flight data; marine and terrestrial bird indicator updates; winter birds web map; Pigeon Guillemot citizen science data gathering, storage and summary tools. </p>
+			<p>Project Lead: Scott Pearson, Washington Department of Fish and Wildlife.</p></li>
+			<li><strong>Drinking Water Vital Sign, Index of vulnerability for elevated nitrates in groundwater indicator</strong>
+			<p>Model for index of vulnerability to nitrates in groundwater; first Vital Sign indicator report for the vulnerability index indicator. </p>
+				<p>Project Lead: Rick Dinicola, United States Geological Survey.</p></li>
+			<li><strong>Economic Vitality Vital Sign, Employment, output and percent employment in natural resource industries indicator</strong>
+			<p>Data compilation and analysis; annual update for three economic indicators in 2022. Vital Sign key messages. </p>
+			<p>Project Leads: Katrina Wellman, Northern Economics Inc; Susan Burke, EcoResource Group and Green Economics. </p></li>
+			<li><strong>Outdoor Activities Vital Sign, Condition of swimming beaches indicator</strong>
+			<p>Maintain monitoring, data compilation, analyses and reporting at high-use, high-risk Puget Sound marine beaches for fecal indicator bacteria and communicate to the public when a risk of disease is identified. Indicator update in PS Info. </p>
+				<p>Project Leads: Julianne Ruffner, Washington Department of Ecology.</p></li>
+			<li><strong>Shoreline Armor Vital Sign, HPA based indicator of new and removed armor</strong>
+			<p>HPA-based data compilation; Vital Sign indicator reporting for 2019 and 2020; Vital Sign decadal summary report and final data product. </p>
+				<p>Project Lead: Doris Small, Washington State Department of Fish and Wildlife.</p></li>
+			<li><strong>Cultural Wellbeing, Good Governance, Sense of Place, Sound Stewardship, Local Foods and Outdoor Activity Vital Signs, subjective human wellbeing indicators</strong>
+				<p>Second general survey (in 2020) and report in support of the subjective human wellbeing indicators to report on. Survey of latinx communities. Draft and final updates to each Vital Sign indicator reports, submitted via PS Info, based on general subjective HWB VS survey; Draft and final updates to each Vital Sign indicator reports via PS Info, based on Latinx survey; Key messages for Sense of Place submitted via PS Info; Presentations to Social Sciences Advisory Group; Inputs to State of the Sound report. Indicators: Participation in cultural practices; Good Governance Index; Locally harvestable foods; Nature-based recreation; Nature-based work; Overall life satisfaction; Psychological Wellbeing Index; Sense of Place Index; Engagement in stewardship activities). </p>
+				<p>Project Lead: Kelly Biedenweg, Oregon State University.</p></li>
+				<li><strong>Toxics in Fish Vital Sign, Contaminants in juvenile salmon indicator</strong>
+			<p>Assess compatibility of historical data; assess time trends; indicator update for contaminants in juvenile salmon; draft manuscript and communication product.</p>
+			</li>
+		</ul>
+
+			<p class="last-update">Last updated: 04/07/21</p>
+ 
+			 
+		<!-- InstanceEndEditable --> </div>
+		
+		<div class="col-sm-2 padding-20-top padding-0-right"> <!-- InstanceBeginRepeat name="right_nav_repeat" --><!-- InstanceEndRepeat --> </div>
+	</div>
+	<!--END OF ROW --> 
+</div>
+<!--END OF CONTENT CONTAINER -->
+
+<?php include 'includes/footer-inc.html';?>
+
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) --> 
+<script src="js/jquery-1.11.2.min.js"></script> 
+
+<!-- Include all compiled plugins (below), or include individual files as needed --> 
+<script src="js/bootstrap.js"></script> 
+<!-- custom js --> 
+<script src="js/custom.js"></script> 
+<!-- Google Tracking  --> 
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-69373425-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
+</body>
+<!-- InstanceEnd --></html>
