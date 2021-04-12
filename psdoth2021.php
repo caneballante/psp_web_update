ef20fadf5f71ba25910456a5e78f03b8923d5b4b
Kevin Hyde <kevin.hyde@psp.wa.gov>
2021-04-12 12:10:39 -0700

    Updated PSDOTH page to link to the new site

--- psdoth2021.php
+++ psdoth2021.php
@@ -59,9 +59,9 @@
 				<h2 class="margin-2-top">2021 Virtual Puget Sound Days on the Hill</h2>
                 
 					<p>Since we still can't convene in person, this year's Puget Sound Day on the Hill will again be a series of virtual events with the Washington congressional delegation, federal officials, and special guests.</p>
-					<p>Puget Sound Partnership and the Northwest Indian Fisheries Commission invite you to participate in our virtual Puget Sound Days on the Hill. These events are currently scheduled on <strong>Fridays from 1:00–2:30 p.m. (Pacific Time), April 23–May 21</strong>. Additional notes will be provided prior to each session. We’ll discuss Puget Sound restoration and protection, salmon recovery efforts, climate change adaptation and mitigation, and infrastructure, among other topics.</p>
+					<p>Puget Sound Partnership and the Northwest Indian Fisheries Commission invite you to participate in our <a href="https://www.psdoth.org/">virtual Puget Sound Days on the Hill</a>. These events are currently scheduled on <strong>Fridays from 1:00–2:30 p.m. (Pacific Time), April 23–May 21</strong>. Additional notes will be provided prior to each session. We’ll discuss Puget Sound restoration and protection, salmon recovery efforts, climate change adaptation and mitigation, and infrastructure, among other topics.</p>
 					
-					<p>We will announce registration info soon. Check this page for the latest information or <a href="https://public.govdelivery.com/accounts/WAPSP/subscriber/new">sign up for our listserv</a>.</p>
+					<p><a href="https://www.psdoth.org/">Register now for 2021 Puget Sound Days on the Hill</a>, and keep checking the new Puget Sound Days on the Hill site for the latest information. You can also <a href="https://public.govdelivery.com/accounts/WAPSP/subscriber/new">sign up for our listserv</a>.</p>
 					
 						
 											
