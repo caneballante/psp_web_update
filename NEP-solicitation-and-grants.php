4f2e4f00eb99be989520561bf8df21389246f2a2
Kevin Hyde <kevin.hyde@psp.wa.gov>
2021-05-20 16:49:59 -0700

    Made changes based on Don Gourlie's suggestions.

--- NEP-solicitation-and-grants.php
+++ NEP-solicitation-and-grants.php
@@ -69,73 +69,17 @@
 		</div>
 		 
 		<div class="col-sm-7 padding-20-top content-column"> <!-- InstanceBeginEditable name="6col_content" -->
-			<p>Under the National Estuary Program, the U.S. Environmental Protection Agency (EPA) provides funding to Washington state agencies as Strategic Initiative Leads and to the Northwest Indian Fisheries Commission to implement the Puget Sound Action Agenda. Strategic Initiative Leads develop and implement six-year strategies for four categories of ecosystem protection and restoration. EPA funding to the Strategic Initiative Leads and the Fisheries Commission totals about $115 million since 2010. EPA can provide additional funding for the next two years of the Strategic Initiative Lead six year strategies, depending on congressional appropriations. Strategic Initiative Leads investments implement the 2012 Puget Sound Action Agenda and the priorities set through the strategic initiative process. The three strategic initiatives for 2012/2013 include:</p>
-			<ul>
-				<li>Prevent pollution from urban stormwater runoff</li>
-				<li>Protect and restore habitat</li>
-				<li>Recover shellfish beds<br />
-				</li>
-			</ul>
-			<p>Strategic Initiative Leads use competitive processes, and in some cases direct awards, to fund work identified in their six-year strategies. The <a href="http://blogs.nwifc.org/psp/about/">Northwest Indian Fisheries Commission </a> awards grants to the 19 federally recognized tribes in Puget Sound and any authorized consortium of these tribes to implement high priority projects identified in the Action Agenda.</p>
-			<p>The Puget Sound Partnership also has cooperative agreements with EPA focused on regional engagement, stewardship and managing the implementation of the Action Agenda. </p>
-			<p>EPA funding is a small part of the overall funding needed to  protect and restore Puget Sound. The 2013 State of the Sound report estimates a $390 million shortfall over a three year period to implement near-term actions identified in the 2012 Action Agenda. </p>
-			<p>For more information on other grant and loan funding for protection and restoration projects in Puget Sound visit:</p>
-			<ul>
-				<li><a href="http://www.rco.wa.gov/grants/index.shtml">Washington state Recreation and Conservation Office website</a></li>
-				<li><a href="http://www.ecy.wa.gov/puget_sound/index.html">Washington state Department of Ecology website</a><br />
-					<br />
-				</li>
-			</ul>
-			<h2><a name="1" id="1"></a><strong>Funding Opportunities</strong></h2>
-			<p>Click here to receive email notification of funding opportunities through the Strategic Initiative Leads. </p>
-			<table width="100%" border="0" cellpadding="0" bordercolor="#89B7D5">
-				<tr>
-					<td><a href="sign-up.php"><strong>Sign up to receive RFP notification by email</strong></a></td>
-				</tr>
-			</table>
-			<p>&nbsp;</p>
-			<p>Specific information about funding opportunities, including how to apply is available at the links below:</p>
-			<p><strong><a href="http://wdfw.wa.gov/grants/ps_marine_nearshore/">Marine and nearshore protection and restoration</a></strong> Strategic Initiative Lead focuses on: Effective regulation and stewardship, strategic capital investments, programs that address high priority threats to the marine and nearshore ecosystem, and adaptive management. The state departments of Fish and Wildlife and Natural Resources are the Strategic Initiative Leads for this category.</p>
-			<ul>
-				<li><a href="https://pspwa.box.com/LOWorkPlanMarineNearshore">Six-Year Investment Strategy to Protect and Restore Marine and Nearshore Habitat FFY2014-15</a></li>
-				<li><a href="https://pspwa.box.com/LOWorkPlanMarineNearshoreTable">Six-Year Investment Strategy to Protect and Restore Marine and Nearshore Habitat Table FFY2014-15</a></li>
-			</ul>
-			<p>For more information, please contact:<br />
-				Patricia Jatczak, WDFW: <a href="mailto:Patricia.Jatczak@dfw.wa.gov">Patricia.Jatczak@dfw.wa.gov</a>, 360.902.2597</p>
-			<p> </p>
-			<p><a href="http://www.ecy.wa.gov/puget_sound/grants_fed_watershed.html"><strong>Watershed protection and restoration</strong></a> focuses on: Watershed characterization, land use and working lands, strategies to manage stormwater and strategies to protect and restore watershed ecosystems. The state departments of Ecology and Commerce are the Strategic Initiative Leads for this work.</p>
-			<ul>
-				<li><a href="https://pspwa.box.com/LOWorkPlanWatershed">Six Year Investment Strategy to Protect and Restore Watersheds FFY2014-15</a></li>
-			</ul>
-			<p>For more information, please contact:</p>
-			<p> <br />
-				Carrie Byron, ECY: <a href="mailto:chum461@ECY.WA.GOV">carrie.byron@ecy.wa.gov</a>, 425.649.4451<br />
-				<br />
-				Doug Peters, Department of Commerce: <a href="mailto:doug.peters@commerce.wa.gov">doug.peters@commerce.wa.gov</a>, 360.725.3046</p>
-			<p> </p>
-			<p><a href="http://www.ecy.wa.gov/puget_sound/grants_fed_toxics.html"><strong>Toxics and nutrients prevention, reduction and control</strong></a> Strategic Initiative Lead focuses on:  Reducing toxic chemicals in products, preventing toxic chemicals in stormwater, determining the how human sources of nutrients affect Puget Sound, and reducing nutrient loadings in a prioritized fashion. The state Department of Ecology is the Strategic Initiative Lead for this category.</p>
-			<ul>
-				<li><a href="https://pspwa.box.com/LOWorkPlanPathogens">Six year Investment Strategy to Prevent, Reduce and Control Toxics and Nutrient Pollution FFY2014-15</a></li>
-			</ul>
-			<p>For more information, please contact:<br />
-				Blake Nelson, Department of Ecology: <a href="mailto:bnel461@ECY.WA.GOV">bnel461@ECY.WA.GOV</a>, 360.407.6940</p>
-			<p> </p>
-			<p><a href="http://www.doh.wa.gov/CommunityandEnvironment/Shellfish/EPAGrants/PathogensGrant.aspx"><strong>Pathogen prevention, reduction and control </strong></a>Strategic Initiative Lead focuses on:  Implementing local on-site sewage management plans, establishing sustainable local nonpoint pollution identification and correction programs, and reducing pathogen loadings through improved manure management. The state departments of Health and Ecology are the Strategic Initiative Leads.</p>
-			<ul>
-				<li><a href="https://pspwa.box.com/LOWorkPlanPathogens">Six-Year Investment Strategy to Prevent, Reduce and Control Pathogen Pollution FFY2014-15</a></li>
-			</ul>
-			<p>For more information, please contact:<br />
-				Mary Knackstedt, Department of Health: <a href="mailto:Mary.Knackstedt@DOH.WA.GOV">Mary.Knackstedt@DOH.WA.GOV</a>, 360.236.3319</p>
-			<p><br />
-			</p>
+			<p>Under the National Estuary Program, the U.S. Environmental Protection Agency (EPA) provides funding to Washington state agencies as Strategic Initiative Leads and to the Northwest Indian Fisheries Commission to implement the Puget Sound Action Agenda.</p>
+			
+			<p>The Strategic Initiatives use competitive processes, and in some cases direct awards, to fund work. The <a href="https://nwifc.org/about-us/" target="_blank">Northwest Indian Fisheries Commission</a> awards grants to the 19 federally recognized tribes in Puget Sound and any authorized consortium of these tribes to implement high priority projects identified in the Action Agenda.</p>
+			
+			<p>EPA funding is a small part of the overall funding needed to  protect and restore Puget Sound.</p>
+			<p>For a comprehensive list of other grant and loan programs that contribute to protection and restoration projects in Puget Sound, visit the <a href="https://www.pugetsoundinfo.wa.gov/Fund/Index" target="_blank"></a>Puget Sound Info funds page.</p>
+			
 			<p><strong>Projects Funded by Strategic Initiative Leads</strong><br />
-				<a href="http://psp.wa.gov/gis/NEPAtlas/ShowMeData">Click here</a> to find a list of projects that Strategic Initiative Leads have funded to implement the Puget Sound Action Agenda. <br />
+				<a href="https://nepatlas.pugetsoundinfo.wa.gov/" target="_blank">Click here</a> to find a list of recent projects that Strategic Initiative Leads have funded to implement the Puget Sound Action Agenda. <br />
 			</p>
-			<table width="100%" border="0" cellpadding="0" bordercolor="#89B7D5">
-				<tr>
-					<td><a href="http://psp.wa.gov/gis/NEPAtlas/NEPActivities"><strong>EPA Puget Sound Funding Map</strong></a></td>
-				</tr>
-			</table>
+			
 			<!-- InstanceEndEditable --> </div>
 		
 		<div class="col-sm-2 padding-20-top padding-0-right"> <!-- InstanceBeginRepeat name="right_nav_repeat" --><!-- InstanceEndRepeat --> </div>
