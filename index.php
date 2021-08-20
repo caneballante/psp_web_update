51071d1fa19adfaa2d2b6dd3d29bc315f09eba29
Kevin Hyde <kevin.hyde@psp.wa.gov>
2021-08-20 11:35:00 -0700

    Added Orca Recovery Day promotion on front page

--- index.php
+++ index.php
@@ -114,9 +114,9 @@ and recovering Puget Sound</h1>
 				<div class="newsbox newsbox-with-img">
 					<div class="newsbox-text"> 
 						<!--news content BOX --> 
-					  <img class="img-responsive img-rounded" src="images/news-images/PSEMP_logonews.jpg"  alt="Logo for the Puget Sound Ecosystem Monitoring Program"/> 
+					  <img class="img-responsive img-rounded" src="images/news-images/j16j26noaaNEWS.jpg"  alt="Photo of J16 Southern Resident orca breaching while J26 swims nearby. Photo from NOAA Fisheres West Coast. Photo credit: Katy Foster, NOAA Fisheries."/> 
 					<strong>ANNOUNCEMENT</strong><br>
-						<a href="https://content.govdelivery.com/accounts/WAPSP/bulletins/2eb3766" target="_blank">The Puget Sound Ecosystem Monitoring Program Steering Committee is seeking new members</a><br>
+						<a href="https://betterground.org/in-your-community/events/ord/" target="_blank">Orca Recovery Day is October 16, 2021</a><br>
 						<!--END news content BOX-->  
 					</div>
 					<div class="clearfix"></div>
@@ -129,10 +129,25 @@ and recovering Puget Sound</h1>
 				<div class="newsbox newsbox-with-img">
 					<div class="newsbox-text"> 
 						<!--news content BOX --> 
+					  <img class="img-responsive img-rounded" src="images/news-images/PSEMP_logonews.jpg"  alt="Logo for the Puget Sound Ecosystem Monitoring Program"/> 
+					<strong>ANNOUNCEMENT</strong><br>
+						<a href="https://content.govdelivery.com/accounts/WAPSP/bulletins/2eb3766" target="_blank">The Puget Sound Ecosystem Monitoring Program Steering Committee is seeking new members</a><br>
+						<!--END news content BOX-->   
+					</div>
+					<div class="clearfix"></div>
+				</div>
+				
+			</div>
+		<!--COLUMN END-->
+			<!--COLUMN START-->
+			<div class="col-md-3 col-sm-4">
+				<div class="newsbox newsbox-with-img">
+					<div class="newsbox-text"> 
+						<!--news content BOX --> 
 					  <img class="img-responsive img-rounded" src="images/news-images/BillyFrankJrNews.jpg"  alt="Photo Billy Frank Jr. laughing. Photo credit: Northwest Indian Fisheries Commission."/> 
 					<strong>BLOG</strong><br>
 						<a href="https://medium.com/puget-sound-partnership/remembering-billy-frank-jr-6115d04791e0" target="_blank">Remembering Billy Frank Jr.</a><br>
-						<!--END news content BOX-->   
+						<!--END news content BOX-->    
 					</div>
 					<div class="clearfix"></div>
 				</div>
@@ -147,14 +162,14 @@ and recovering Puget Sound</h1>
 					  <img class="img-responsive img-rounded" src="images/news-images/PSARDashboardNews.jpg"  alt="Screenshot of the new PSAR Dashboard website"/> 
 					<strong>BLOG</strong><br>
 						<a href="https://medium.com/puget-sound-partnership/explore-the-new-dashboard-for-puget-sound-acquisition-and-restoration-projects-af9378876fe8" target="_blank">Explore the new dashboard for Puget Sound Acquisition and Restoration projects</a><br>
-						<!--END news content BOX-->   
+						<!--END news content BOX-->     
 					</div>
 					<div class="clearfix"></div>
 				</div>
 				
 			</div>
 		<!--COLUMN END-->
-			<!--COLUMN START-->
+		<!--COLUMN START-->
 			<div class="col-md-3 col-sm-4">
 				<div class="newsbox newsbox-with-img">
 					<div class="newsbox-text"> 
@@ -168,8 +183,8 @@ and recovering Puget Sound</h1>
 				</div>
 				
 			</div>
-		<!--COLUMN END-->
-		<!--COLUMN START-->
+		<!--COLUMN END-->	
+			<!--COLUMN START-->
 			<div class="col-md-3 col-sm-4">
 				<div class="newsbox newsbox-with-img">
 					<div class="newsbox-text"> 
@@ -177,21 +192,6 @@ and recovering Puget Sound</h1>
 					  <img class="img-responsive img-rounded" src="images/news-images/PSP-logo-rgbNews.jpg"  alt="Logo for the Puget Sound Partnership"/> 
 					<strong>ANNOUNCEMENT</strong><br>
 						<a href="https://content.govdelivery.com/accounts/WAPSP/bulletins/2d7782b">Two Science and Monitoring Funding Opportunities from the Puget Sound Partnership - additional information and upcoming deadlines</a><br>
-						<!--END news content BOX-->   
-					</div>
-					<div class="clearfix"></div>
-				</div>
-				
-			</div>
-		<!--COLUMN END-->	
-			<!--COLUMN START-->
-			<div class="col-md-3 col-sm-4">
-				<div class="newsbox newsbox-with-img">
-					<div class="newsbox-text"> 
-						<!--news content BOX --> 
-					  <img class="img-responsive img-rounded" src="images/news-images/EPAannounce.jpg"  alt="Aerial photo of Puget Sound water and shorelines. Photo credit: Kiliii Yuyan"/> 
-					<strong>ANNOUNCEMENT</strong><br>
-						<a href="https://pugetsoundestuary.wa.gov/what-we-do/funded-projects/">Strategic Initiatives of the Puget Sound National Estuary Program Funded Projects</a><br>
 						<!--END news content BOX--> 
 					</div>
 					<div class="clearfix"></div>
@@ -204,10 +204,10 @@ and recovering Puget Sound</h1>
 				<div class="newsbox newsbox-with-img">
 					<div class="newsbox-text"> 
 						<!--news content BOX --> 
-					  <img class="img-responsive img-rounded" src="images/news-images/PSP-logo-rgbNews.jpg"  alt="Logo for Puget Sound Partnership"/> 
-					<strong>NEWS RELEASE</strong><br>
-						<a href="https://content.govdelivery.com/accounts/WAPSP/bulletins/2c139e4" target="_blank">Governor Inslee appoints Will Hall to the Puget Sound Partnership's Leadership Council</a><br>
-						<!--END news content BOX-->       
+					  <img class="img-responsive img-rounded" src="images/news-images/EPAannounce.jpg"  alt="Aerial photo of Puget Sound water and shorelines. Photo credit: Kiliii Yuyan"/> 
+					<strong>ANNOUNCEMENT</strong><br>
+						<a href="https://pugetsoundestuary.wa.gov/what-we-do/funded-projects/">Strategic Initiatives of the Puget Sound National Estuary Program Funded Projects</a><br>
+						<!--END news content BOX-->        
 					</div>
 					<div class="clearfix"></div>
 				</div>
