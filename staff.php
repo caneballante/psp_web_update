6fbe6fd62505fad017378129a71b678ff69e2371
Kevin Hyde <kevin.hyde@psp.wa.gov>
2021-08-30 12:15:23 -0700

    Changed last updated date

--- staff.php
+++ staff.php
@@ -285,7 +285,7 @@
 						<h5>Justine Sharp</h5>
 						<p>IT Specialist<br />
 							360.490.9680 | <a href="mailto:justine.sharp@psp.wa.gov">justine.sharp@psp.wa.gov</a> </p>
-	<p class="last-update">Last updated: 08/13/21</p>
+	<p class="last-update">Last updated: 08/30/21</p>
 
 		<!-- InstanceEndEditable --> </div>
 		
