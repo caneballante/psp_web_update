2402381027786dddeee14a8ff6affd8ef43c7c8b
Kevin Hyde <kevin.hyde@psp.wa.gov>
2021-09-27 14:31:40 -0700

    Added new page for Action Agenda communications, per Kaitlin Harris request

---
+++ identifying-actions.php
@@ -0,0 +1,181 @@
+<!DOCTYPE html>
+<html lang="en"><!-- InstanceBegin template="/Templates/page-4-template.dwt" codeOutsideHTMLIsLocked="false" -->
+<head>
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<!-- InstanceBeginEditable name="doctitle" -->
+<title>Idenfitying actions for Puget Sound Recovery</title>
+<!-- InstanceEndEditable -->
+<!-- Bootstrap -->
+<link href="css/bootstrap.css" rel="stylesheet">
+<link href="css/custom.css" rel="stylesheet" type="text/css">
+<link href="css/custom-erika.css" rel="stylesheet" type="text/css">
+<!-- loads the wf-loading class right away to minimize FOUT -->
+<script>document.documentElement.className += ' wf-loading';</script>
+<!-- Font PRENTON TYPEKIT -->
+<script src="https://use.typekit.net/srt5jze.js"></script>
+<script>try{Typekit.load({ async: true });}catch(e){}</script>
+<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+<!--[if lt IE 9]>
+	<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+	<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+<!-- InstanceBeginEditable name="head" -->
+<script>
+/*this variable is used to set the proper nav to active. It should to the order the nav item is in the list*/
+navSelected = 1;
+	
+</script>
+<meta property="og:image" content="http://www.psp.wa.gov/images/inside_image_99_sm.jpg" />
+<!-- InstanceEndEditable -->
+<!-- InstanceParam name="OptionalRegion1" type="boolean" value="true" -->
+<!-- InstanceParam name="OptionalRegion2" type="boolean" value="false" -->
+<!-- InstanceParam name="OptionalRegion3" type="boolean" value="true" -->
+<!-- InstanceParam name="OptionalRegion4" type="boolean" value="false" -->
+<!-- InstanceParam name="OptionalRegion5" type="boolean" value="true" -->
+<!-- InstanceParam name="OptionalRegion6" type="boolean" value="true" -->
+<!-- InstanceParam name="OptionalRegion7" type="boolean" value="false" -->
+<!-- InstanceParam name="OptionalRegion8" type="boolean" value="true" -->
+</head>
+<body>
+<?php include 'includes/modal-inc.html';?>
+<!-- START IMAGE HEADER --> 
+ <!-- InstanceBeginEditable name="overviewphoto" -->
+<header class="overview-page-image-style overview-page-image114">
+</header>
+<!-- InstanceEndEditable -->  <!-- InstanceBeginEditable name="EditRegion11" -->
+<div class="row">
+	<div class="col-xs-12 orange-fill align-center overview-logo-banner"><img src="images/2016-AA-banner-logo-none-small.png" width="146" height="30" alt=""/></div>
+</div>
+<!-- InstanceEndEditable -->
+<div class="container-fluid page-content padding-50-bottom">
+	<div class="row">
+		<div class="col-sm-3 padding-20-top"></div>
+		<div class="col-sm-7 padding-20-top"> <!-- InstanceBeginEditable name="6col_header" -->
+			<h1>Identifying actions for Puget Sound recovery</h1>
+			<!-- InstanceEndEditable --></div>
+		<div class="col-sm-2 padding-20-top"></div>
+	</div>
+	
+	<div class="row"> 
+		<div class="col-sm-3">
+			<div class="nav-leftside-custom">
+				<ul class="nav nav-stacked nav-pills nav-leftside-custom padding-left-0 margin-10-top">
+					<!-- InstanceBeginEditable name="left_nav" -->
+					<?php include 'includes/ln-2022-AA.html';?>
+					<!-- InstanceEndEditable -->
+				</ul>
+			</div>
+		</div>
+		 
+		<div class="col-sm-7 padding-20-top content-column"> <!-- InstanceBeginEditable name="6col_content" -->
+			<h2>Overall vision, scope, and timeline for the 2022-2026 Action Agenda update</h2>
+
+<p>The 2022-2026 Action Agenda will be an update to the Puget Sound recovery plan for protected habitat, thriving species, and healthy people. The purpose of the 2022-2026 Action Agenda is to concentrate our efforts, energy, and investments on key transformational changes that will enable us to collectively advance progress toward the statutory goals and Vital Signs that guide Puget Sound recovery. In June 2020, the Leadership Council adopted a concept and schedule for updating the 2022-2026 Action Agenda. The approved schedule includes multiple phases of work (figure 1).</p><br>
+			<img class="img-responsive" src="https://pspwa.box.com/shared/static/uoeewhy6v4831exjd3uufd86pv21dszy.png" width="1491" height="421" alt=""/>
+
+<br><p>Phase 2 focused on identifying science-informed <a href="https://pspwa.box.com/s/lwk71kpaaoo9off1iw5ancj3m7xrcp45" target="_blank">desired recovery outcomes</a> that describe what we intend to accomplish to advance Puget Sound recovery. Phase 3 focused on identifying the strategies necessary to make progress toward the Action Agenda desired outcomes, the <a href="https://vitalsigns.pugetsoundinfo.wa.gov/" target="_blank">Puget Sound Vital Signs</a>, and <a href="https://psp.wa.gov/puget-sound-recovery.php" target="_blank">statutory goals</a>. In phase 4—the second to last phase of the 2022-2026 Action Agenda update process—we will identify the actions to advance strategies.</p>
+			
+			<h2>Actions describe the activities that are a shared focus for implementing strategies </h2>
+			<p>For the purposes of the 2022-2026 Action Agenda update, an action describes the activities that will be a shared focus for implementing each strategy from 2022-2026. This could include restoration and acquisition program development, improvement, or implementation; education; outreach; research; legislative or policy improvements; or other types of activities. Actions are intended to guide partner implementation and innovation and will inform the focus of public and private funding and implementation support by the boards and regional partners. </p>
+			
+			<h2>How will Action Agenda actions be identified?  </h2>
+			<p>The phase 4 process is designed to promote co-generation of Action Agenda actions that will implement strategies. Actions will be identified through public workshops and will utilize Implementation Strategies and other existing plans  – as well as approved desired outcomes and approved strategies – as a starting point for discussion. Public workshops will take place between early November and mid-December. Each workshop will be approximately two to three hours in length and will be held virtually: </p>
+
+		
+        <tbody>
+          <tr>
+            <th width="13%" scope="col"><p>Timeframe</p></th>
+            <th width="24%" scope="col"><p>Phase</p></th>
+            <th width="53%" scope="col"><p>Detail</p></th>
+            <th width="10%" scope="col"><p>Status</p></th>
+          </tr>
+          <tr>
+            <td><p>Jan - June 2020</p></td>
+            <td><p>Phase 1: Scope and schedule</p></td>
+            <td><p>Determine the overall scope and schedule of the Action Agenda, including the intended purpose and use of the Action Agenda and major process phases and concepts. </p></td>
+            <td class="orange-fill white"><p>Completed</p></td>
+          </tr>
+          <tr>
+            <td><p>June 2020 - Feb 2021</p></td>
+            <td><p>Phase 2: Identify desired outcomes for recovery</p></td>
+            <td><p>Identify desired outcomes that describe <em>what </em>we as the recovery  community hope to accomplish. Desired outcomes will guide the identification of strategies and actions that are necessary to make progress toward <a href="https://psp.wa.gov/puget-sound-recovery.php">Puget Sound recovery goals </a>and <a href="https://vitalsigns.pugetsoundinfo.wa.gov/" target="_blank">Vital Signs</a>. <a href="https://www.psp.wa.gov/priority-outcomes.php">More detail is available here.</a></p>
+            <td class="orange-fill white"><p>Completed</p></td>
+          </tr>
+          <tr>
+            <td><p>Mar 2021 - Sept 2021</p></td>
+            <td><p>Phase 3: Identify strategies to advance desired outcomes</p></td>
+            <td><p>Identify strategies that tell us <em>how </em>to advance progress toward desired outcomes (phase 2) and overall  recovery. <a href="https://psp.wa.gov/identifying-strategies.php">More detail is available here.</a></p>
+				</td>
+            </td>
+            <td class="orange-fill white"><p>Completed</p></td>
+          </tr>
+          <tr>
+            <td><p>Sept - Dec 2021</p></td>
+            <td><p>Phase 4: Identify actions and commitments that align with strategies.</p></td>
+            <td><p>Identify actions and programs that require additional investment; necessary policy changes; and commitments from the boards and partners to support priority actions for recovery. </p>
+            </td>
+           <td class="blue-fill white"><p>In progress</p></td>
+          </tr>
+          <tr>
+            <td><p>Aug 2021 - Feb 2022</p></td>
+            <td><p>Phase 5: Develop draft Action Agenda</p></td>
+            <td><p>Bring together content developed in phases 2-4 into a draft Action  Agenda; partners and public provide feedback on draft.</p></td>
+           <td class="light-blue-fill white"><p>Not started</p></td>
+          </tr>
+          <tr>
+            <td><p>Feb - June 2022</p></td>
+            <td><p>Phase 6: Public comment and adoption</p></td>
+            <td><p>Provide time for public comment and input from each of the Partnership's  Boards and partner groups.</p></td>
+           <td class="light-blue-fill white"><p>Not started</p></td>
+          </tr>
+        </tbody>
+        </table>
+		<H2>We invite your participation</H2>
+		<p>Every phase of Action Agenda development will build from existing plans developed by partners. We  and our partners are also  developing an engagement plan and establishing mechanisms for  collaboration and participation in developing the outcomes (phase 2),  strategies (phase 3), and actions (phase 4) for the 2022-2026 Action Agenda.  Throughout 2020 and 2021, Partnership staff who are leading the development of  the Action Agenda will use existing meeting schedules to present to partner  groups, host workshops where partners can convene around particular topic  areas, and provide online mechanisms to gather independent feedback at the  convenience of partners&rsquo; schedules.&nbsp;			</p>
+			<strong>Stay tuned for more information on engagement opportunities as  the process is developed.</strong></p>
+		<p><strong>Ask for a briefing or a conversation. </strong>We would be happy to talk with you or your organization about the  2022-2026 Action Agenda development process and get your ideas. Please contact  Dan Stonington (<a href="mailto:dan.stonington@psp.wa.gov">dan.stonington@psp.wa.gov</a>), Kaitlin Harris (<a href="mailto:kaitlin.harris@psp.wa.gov">kaitlin.harris@psp.wa.gov</a>), Wren McNally (<a href="mailto:wren.mcnally@psp.wa.gov">wren.mcnally@psp.wa.gov</a>), or your other Partnership staff connections  to request a briefing.</p>
+			<p class="last-update">Last updated: 09/10/21</p>
+
+        <!-- InstanceEndEditable --> </div>
+		
+		<div class="col-sm-2 padding-20-top padding-0-right"> <!-- InstanceBeginRepeat name="right_nav_repeat" --><!-- InstanceBeginRepeatEntry --> <!-- InstanceBeginEditable name="right_nav_title" -->
+			<div class="right-nav-title margin-0-top ">SEE ALSO</div>
+			<!-- InstanceEndEditable -->
+			<ul class="nav-rightside-custom">
+				<!-- InstanceBeginEditable name="right_nav_links" -->
+				<li class="active" role="presentation">Implementation and funding status of actions from the 2018-2022 Action Agenda can be found in the <a href="https://actionagenda.pugetsoundinfo.wa.gov/Results/FundingStatus" target="_blank">Action Agenda Tracker</a></li>
+				<li class="active" role="presentation"><a href="https://pspwa.box.com/s/v8x6pg3oijn203lf8i9wjs267kn9f0rl" target="_blank"><strong>Action Agenda Frequently Asked Questions</strong></a></li>
+	
+				<!-- InstanceEndEditable -->
+			</ul>
+	  <!-- InstanceEndRepeatEntry --><!-- InstanceEndRepeat --> </div>
+	</div>
+	<!--END OF ROW --> 
+</div>
+<!--END OF CONTENT CONTAINER -->
+
+<?php include 'includes/footer-inc.html';?>
+
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) --> 
+<script src="js/jquery-1.11.2.min.js"></script> 
+
+<!-- Include all compiled plugins (below), or include individual files as needed --> 
+<script src="js/bootstrap.js"></script> 
+<!-- custom js --> 
+<script src="js/custom.js"></script> 
+<!-- Google Tracking  --> 
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-69373425-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
+</body>
+<!-- InstanceEnd --></html>
