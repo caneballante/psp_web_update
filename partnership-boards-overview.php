6a2a8c720b26370f63bf7484358f60f7d65a28b6
Kevin Hyde <kevin.hyde@psp.wa.gov>
2021-10-05 10:52:11 -0700

    Changed boards contact info on the overview page

--- partnership-boards-overview.php
+++ partnership-boards-overview.php
@@ -92,7 +92,7 @@
 			<ul class="nav-rightside-custom">
 				<!-- InstanceBeginEditable name="right_nav_links" -->
 					<li>
-					<?php include 'includes/board_contact.html';?>
+					<?php include 'includes/board_overviewcontact.html';?>
 				</li>
 				<li><a href="https://pspwa.box.com/s/gabtcrbzo9i5yybkeyi6lx6cez0bh10o"> Board's documents, meeting materials, summaries, and Resolutions</a></li>
 				<li><a href="https://pspwa.box.com/s/87rfvgjwf6wahymd806v7lrkh0a89zjt">Puget Sound Environmental Caucus members </a></li>
