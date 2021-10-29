f83f660ad3acde445f7f8917c11d02b70726684e
Kevin Hyde <kevin.hyde@psp.wa.gov>
2021-10-29 07:25:07 -0700

    Adjusted text

--- legislative-calendar.php
+++ legislative-calendar.php
@@ -5,7 +5,7 @@
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <!-- InstanceBeginEditable name="doctitle" -->
-<title>Puget Sound Partnership - Government Relations</title>
+<title>Puget Sound Partnership - Legislative Calendar</title>
 <!-- InstanceEndEditable -->
 <!-- Bootstrap -->
 <link href="css/bootstrap.css" rel="stylesheet">
@@ -55,7 +55,7 @@
 	<div class="row">
 		<div class="col-sm-3 padding-20-top"></div>
 		<div class="col-sm-7 padding-20-top"> <!-- InstanceBeginEditable name="6col_header" -->
-			<h1>GOVERNMENT RELATIONS</h1>
+			<h1>Legislative Calendar</h1>
 			<p>&nbsp;</p>
 			<!-- InstanceEndEditable --></div>
 		<div class="col-sm-2 padding-20-top"></div>
@@ -74,17 +74,8 @@
 		 
 		<div class="col-sm-7 padding-20-top content-column"> <!-- InstanceBeginEditable name="6col_content" -->
 			
-			<p>The Puget Sound Partnership connects and leverages the efforts of hundreds of organizations working on a shared mission to protect and restore the health of Puget Sound. We are a science-based, non-regulatory state agency bringing people together to advance this shared mission by setting goals and achieving them through actions specified in the common framework of the Puget Sound Action Agenda. During Legislative Session each year, we work to advance policy and mobilize funding to protect and restore Puget Sound. We also monitor proposed legislation to determine whether and how it might affect the health of Puget Sound, then inform decision-makers about the positive and negative effects of such legislation on Puget Sound. Between sessions, we work with legislators to help them understand the issues the ecosystem faces and how state resources are being used to address these issues. </p>
-			<p><strong>The Partnership supports:</strong></p>
-			<ul>
-				<li>Legislation and investments to implement the Action Agenda, the regional plan to restore the health of Puget Sound.</li>
-				<li>Policies, strategies, and investments that recognize and support Tribal Treaty rights.</li>
-				<li>Policies, strategies, and investments that support a coordinated investment strategy—advancing Puget Sound recovery while also fostering job creation, public health, safety, and the wellbeing of current and future generations.</li>
-				<li>Authority and resources for the Partnership to support and enable programs and projects led by other agencies that help implement the Action Agenda, with an emphasis on increasing the implementation of high-priority regional actions and evaluation and measurement of outcomes and the effectiveness of recovery efforts.</li>
-				<li>Incentives and market-based mechanisms to help landowners, businesses, and residents advance regional Puget Sound recovery goals through local actions. </li>
-				<li>Policies and strategies that account for environmental externalities, recognizing the importance of allocating and understanding full and true costs and impacts of available alternatives when making policy choices.</li>
-			</ul>
-			<h2 class="margin-0-top">&nbsp;</h2>
+			<p>Listed below are the legislative committee activities the Partnership will be following in the near future. Double click on any calendar item for more information, including date, time, agenda, and hyperlinks to materials and access the meeting.</p>
+			<p>If you plan to attend a committee meeting, be sure to check the latest committee schedule before you log on/leave home. Committee schedules change frequently and are updated at <a href="https://app.leg.wa.gov/committeeschedules/" target="_blank">https://app.leg.wa.gov/committeeschedules/</a></p>
 			<!-- InstanceEndEditable --> </div>
 		
 		<div class="col-sm-2 padding-20-top padding-0-right"> <!-- InstanceBeginRepeat name="right_nav_repeat" --><!-- InstanceBeginRepeatEntry --> <!-- InstanceBeginEditable name="right_nav_title" -->
