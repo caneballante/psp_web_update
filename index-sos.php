ed28346a62b08f4feab531964f2656342f9dc778
jon bridgman <jonbridgman@gmail.com>
2021-10-28 19:03:49 -0700

    2021 SOS homepage

---
+++ index-sos.php
@@ -0,0 +1,333 @@
+ <!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Puget Sound Partnership</title>
+
+<!-- Bootstrap -->
+<link href="css/bootstrap.css" rel="stylesheet">
+<link href="css/custom-2021.css" rel="stylesheet" type="text/css">
+
+<!-- Font PRENTON TYPEKIT -->
+<script src="https://use.typekit.net/srt5jze.js"></script>
+<script>try{Typekit.load({ async: true });}catch(e){}</script>
+
+<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+<!--[if lt IE 9]>
+	<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+	<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+<body>
+
+<!-- logo and menu and modal navigation gets included here -->
+&nbsp;
+<?php include 'includes/modal-inc.html';?>
+
+<!-- Intro Header -->
+<header class="intro-test">
+	<div class="intro-body">
+		<div class="container">
+         
+			<div class="row">
+				<div class="col-md-8 col-md-offset-2">
+					<h1 class="emsize-3 fontweight-400">2021 State of the Sound<br>
+				  </h1>
+ 					<div class="learn-more-box no-icon fontweight-400"><a href="https://www.stateofthesound.wa.gov">READ THE REPORT</a></div>  &nbsp;
+					<div class="learn-more-box no-icon fontweight-400"><a href="https://www.youtube.com">WATCH THE VIDEO</a></div> 
+					
+
+					
+ 				</div>
+			</div>
+		</div>
+	</div>
+</header>
+<div class="container-fluid">
+	<div class="row"> 
+		<div class="col-xs-12 orange-fill"><br>
+		</div>
+	</div>
+</div>
+<div class="container padding-40-bottom" id="home-squares">
+	<div class="row"> 
+		<div class="col-sm-3 padding-40-top">
+			<img src="images/home_roadmap.png" width="264" height="264" alt="satellite view of the Salish Sea" class="img-responsive img-rounded"/>
+		</div>
+		<div class="col-sm-3 padding-10-top">
+			<h1>CHARTING THE COURSE TO RECOVERY</h1>
+			<div class="padding-5-bottom"><a href="action_agenda_center.php"> 2018-2022 Action&nbsp;Agenda&nbsp;for Puget&nbsp;Sound</a></div>
+			<div class="padding-5-bottom"><a href="2022AAupdate.php"> 2022-2026 Action Agenda update</a></div>
+			<div class="padding-5-bottom"><a href="implementation-strategies.php">Implementation Strategies</a> </div>
+			<div class="padding-5-bottom"><a href="science-adaptive-management-framework.php">Results-based management</a></div>
+		</div>
+		<div class="col-sm-3 padding-40-top">
+			<img src="images/home_local.png" width="264" height="264" alt="construction of an engineered log jam" class="img-responsive img-rounded"/>
+		</div>
+		<div class="col-sm-3 padding-10-top">
+			<h1>ACTIONS ACROSS THE SOUND</h1>
+			<div class="padding-5-bottom"><a href="salmon-recovery-overview.php"> Salmon recovery</a></div>
+			<div class="padding-5-bottom"><a href="PSAR.php">Puget Sound Acquisition and Restoration (PSAR))&nbsp;program</a></div>
+			<div class="padding-5-bottom"><a href="NEP-overview.php">National Estuary Program (NEP)</a></div>
+			<div class="padding-5-bottom"><a href="LIO-overview.php">Local recovery and Local Integrating Organizations (LIOs)</a></div>
+			<div><a href="pspnc.php">Puget Sound Nearshore Credits</a></div>
+		</div>
+	</div><!-- end row -->
+	<div class="row">
+		<div class="col-sm-3 padding-40-top">
+			<img src="images/home_measure.png" width="264" height="264" alt="water testing" class="img-responsive img-rounded"/>
+		</div>
+			<div class="col-sm-3 padding-10-top">
+				<h1>MEASURING RECOVERY</h1>
+				
+				<div class="padding-5-bottom"><a href="evaluating-actions-to-recover-puget-sound.php">Evaluating actions to recovery Puget Sound</a></div>
+				<div class="padding-5-bottom"><a href="evaluating-vital-signs.php">Puget Sound Vital Signs</a></div>
+				<div class="padding-5-bottom"><a href="/PSEMP-overview.php">Ecosystem monitoring (PSEMP)</a> </div>
+				<div class="padding-5-bottom"><a href="science-and-evaluation-overview.php">Science and evaluation overview</a></div>
+				<div class="padding-5-bottom"><a href="https://www.stateofthesound.wa.gov">2019 State of the Sound</a> </div>
+				<div class="padding-5-bottom"><a href="https://www.pugetsoundinfo.wa.gov">Puget Sound Info</a></div>
+			</div>
+		<div class="col-sm-3 padding-40-top">
+			<img src="images/home_support.png" width="264" height="264" alt="community meeting" class="img-responsive img-rounded"/>
+		</div>
+			<div class="col-sm-3 padding-10-top">
+			<h1>SUPPORTING OUR PARTNERS</h1>
+			<div class="padding-5-bottom"><a href="partnership-boards-overview.php">Partnership boards</a></div>
+			<div class="padding-5-bottom"><a href="legislative_updates.php">2021 Legislative session</a></div>
+			<div class="padding-5-bottom"><a href="psdoth2021.php">2021 Puget Sound Day on the Hill</a></div>	
+			<div class="padding-5-bottom"><span class="new-red">NEW</span><a href="https://pspwa.box.com/shared/static/wmdpr9w05ow7072pqym99ufwmo39akpf.pdf">Download the 2021-23 budget rankings </a></div>
+			
+		</div>
+	</div>	<!-- end row -->
+</div><!-- end container -->
+
+<div class="container-fluid light-blue-fill margin-20-top padding-40-bottom light-icon">
+	<div class="container"> 
+		<!--copy column to column to create new blog post-->
+		<!--ROW START-->
+		<div class="row margin-20-top">
+				<!--COLUMN START-->
+			<div class="col-md-3 col-sm-4">
+				<div class="newsbox newsbox-with-img">
+					<div class="newsbox-text"> 
+						<!--news content BOX --> 
+					  <img class="img-responsive img-rounded" src="images/news-images/ps-marine-waters-report-2020.jpg"  alt="Cover image from the Puget Sound Marine Waters 2020 Overview"/> 
+					<strong>ANNOUNCEMENT</strong><br>
+						<a href="https://psp.wa.gov/psmarinewatersoverview.php">Puget Sound Marine Waters 2020 Overview</a><br>
+						<!--END news content BOX-->  
+					</div>
+					<div class="clearfix"></div>
+				</div>
+				
+			</div>
+		    <!--COLUMN END-->
+			<!--COLUMN START-->
+			<div class="col-md-3 col-sm-4">
+				<div class="newsbox newsbox-with-img">
+					<div class="newsbox-text"> 
+						<!--news content BOX --> 
+					  <img class="img-responsive img-rounded" src="images/news-images/action-agenda-news-image.jpg"  alt="Photo of Puget Sound shoreline with birds flying above the beach."/> 
+					<strong>ANNOUNCEMENT</strong><br>
+						<a href="https://survey.zohopublic.com/zs/9zCsRU" target="_blank">Participate in the co-generation of actions to advance Puget Sound recovery</a><br>
+						<!--END news content BOX-->  
+					</div>
+					<div class="clearfix"></div>
+				</div>
+				
+			</div>
+		<!--COLUMN END-->
+			<!--COLUMN START-->
+			<div class="col-md-3 col-sm-4">
+				<div class="newsbox newsbox-with-img">
+					<div class="newsbox-text"> 
+						<!--news content BOX --> 
+					  <img class="img-responsive img-rounded" src="images/news-images/PSP-logo-rgbNews.jpg"  alt="Puget Sound Partnership logo."/> 
+					<strong>ANNOUNCEMENT</strong><br>
+						<a href="https://content.govdelivery.com/accounts/WAPSP/bulletins/2f0be74" target="_blank">Announcing apparent successful project proposals for two Puget Sound Partnership solicitations of science and monitoring projects</a><br>
+						<!--END news content BOX-->   
+					</div>
+					<div class="clearfix"></div>
+				</div>
+				
+			</div>
+		<!--COLUMN END-->
+			<!--COLUMN START-->
+			<div class="col-md-3 col-sm-4">
+				<div class="newsbox newsbox-with-img">
+					<div class="newsbox-text"> 
+						<!--news content BOX --> 
+					  <img class="img-responsive img-rounded" src="images/news-images/BillyFrankJrNews.jpg"  alt="Photo Billy Frank Jr. laughing. Photo credit: Northwest Indian Fisheries Commission."/> 
+					<strong>BLOG</strong><br>
+						<a href="https://medium.com/puget-sound-partnership/remembering-billy-frank-jr-6115d04791e0" target="_blank">Remembering Billy Frank Jr.</a><br>
+						<!--END news content BOX-->     
+					</div>
+					<div class="clearfix"></div>
+				</div>
+				
+			</div>
+		<!--COLUMN END-->
+		<!--COLUMN START-->
+			<div class="col-md-3 col-sm-4">
+				<div class="newsbox newsbox-with-img">
+					<div class="newsbox-text"> 
+						<!--news content BOX --> 
+					  <img class="img-responsive img-rounded" src="images/news-images/PSARDashboardNews.jpg"  alt="Screenshot of the new PSAR Dashboard website"/> 
+					<strong>BLOG</strong><br>
+						<a href="https://medium.com/puget-sound-partnership/explore-the-new-dashboard-for-puget-sound-acquisition-and-restoration-projects-af9378876fe8" target="_blank">Explore the new dashboard for Puget Sound Acquisition and Restoration projects</a><br>
+						<!--END news content BOX-->         
+					</div>
+					<div class="clearfix"></div>
+				</div>
+				
+			</div>
+		<!--COLUMN END-->	
+			<!--COLUMN START-->
+			<div class="col-md-3 col-sm-4">
+				<div class="newsbox newsbox-with-img">
+					<div class="newsbox-text"> 
+						<!--news content BOX --> 
+					  <img class="img-responsive img-rounded" src="images/news-images/leg-calendar.png"  alt="Photo of the Capitol building with overlaid text that says Legislative Calendar"/> 
+					<strong>LEGISLATIVE SESSION</strong><br>
+						<a href="https://pspwa.box.com/s/ijmtcakxz74czarprxc690osiztrdlxm" target="_blank">Legislative Wrap-Up, May 7, 2021</a><br>
+						<!--END news content BOX-->
+					</div>
+					<div class="clearfix"></div>
+				</div>
+				
+			</div>
+		<!--COLUMN END-->	
+		<!--COLUMN START-->
+			<div class="col-md-3 col-sm-4">
+				<div class="newsbox newsbox-with-img">
+					<div class="newsbox-text"> 
+						<!--news content BOX --> 
+					  <img class="img-responsive img-rounded" src="images/news-images/PSPSNC_news.jpg"  alt="Photo of herring swimming through eelgrass. Photo credit: Florian Graner."/> 
+					<strong>BLOG</strong><br>
+						<a href="https://medium.com/puget-sound-partnership/puget-sound-partnership-credits-will-fund-nearshore-conservation-398c573bea50" target="_blank">Puget Sound Partnership credits will fund nearshore conservation</a><br>
+						<!--END news content BOX-->       
+					</div>
+					<div class="clearfix"></div>
+				</div>
+				
+			</div>
+		<!--COLUMN END-->
+		
+		<!--COLUMN START-->
+			<div class="col-md-3 col-sm-4">
+				<div class="newsbox newsbox-with-img">
+					<div class="newsbox-text"> 
+						<!--news content BOX --> 
+						<img class="img-responsive img-rounded" src="images/news-images/innovation-stories.jpg"  alt=""/> 
+						<strong>PUGET SOUND INNOVATION STORIES<br></strong>
+						<a href="https://innovationstories.psp.wa.gov/">Learn about the people and projects saving the Sound</a><br>
+						<!--END news content BOX--> 
+					</div>
+				</div>
+			</div>
+		<!--COLUMN END-->
+		</div>
+		<!--ROW END--> 
+		<!--ROW START-->
+		<div class="row light-blue-fill">
+		
+		
+		<!--COLUMN START-->
+		<div class="col-md-3 col-sm-4">
+			<div class="newsbox calendarbox">
+				<!--news content -->
+				<div class="newsbox-date">OCT<br>
+			  28</div>
+				<div class="newsbox-text"><a href="board_meetings.php">ECOSYSTEM COORDINATION BOARD, <br>
+					Online<br>
+							</a><br>
+				</div>
+				<!--END news content-->
+			</div>
+		</div>
+		<!--COLUMN END-->	
+		<!--COLUMN START-->
+		<div class="col-md-3 col-sm-4">
+			<div class="newsbox calendarbox">
+				<!--news content -->
+				<div class="newsbox-date">NOV<br>
+			  18</div>
+				<div class="newsbox-text"><a href="board_meetings.php">SALMON RECOVERY COUNCIL, <br>
+					Online<br>
+							</a><br>
+				</div>
+				<!--END news content-->
+			</div>
+		</div>
+		<!--COLUMN END-->
+			<!--COLUMN START-->
+		<div class="col-md-3 col-sm-4">
+			<div class="newsbox calendarbox">
+				<!--news content -->
+				<div class="newsbox-date">DEC<br>
+			  9</div>
+				<div class="newsbox-text"><a href="board_meetings.php">LEADERSHIP COUNCIL, <br>
+					Online<br>
+							</a><br>
+				</div>
+				<!--END news content-->
+			</div>
+		</div>
+		<!--COLUMN END-->
+			<!--COLUMN START-->
+		<div class="col-md-3 col-sm-4">
+			<div class="newsbox calendarbox">
+				<!--news content -->
+				<div class="newsbox-date">DEC<br>
+			  16</div>
+				<div class="newsbox-text"><a href="board_meetings.php">SCIENCE PANEL, <br>
+					Online<br>
+							</a><br>
+				</div>
+				<!--END news content-->
+			</div>
+		</div>
+		<!--COLUMN END-->
+		
+	</div>
+	<!--ROW END-->
+	
+		
+	
+	
+	
+	
+	
+	
+	
+	
+	</div>
+<!--CONTAINER END--> 
+</div>
+<!--CONTAINER END--> 
+
+<!-- FOOTER INCLUDE -->
+<?php include 'includes/footer-inc.html';?>
+
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) --> 
+<script src="js/jquery-1.11.2.min.js"></script> 
+
+<!-- Include all compiled plugins (below), or include individual files as needed --> 
+<script src="js/bootstrap.js"></script> 
+<!-- custom js --> 
+<script src="js/custom.js"></script> 
+<!-- Google Tracking  --> 
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-69373425-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
+</body>
+</html>
