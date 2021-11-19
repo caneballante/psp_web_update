336b7f3b460341fddc5b333641247aeac16559aa
Kevin Hyde <kevin.hyde@psp.wa.gov>
2021-11-19 10:46:14 -0800

    Added AA3 job to the site

--- employment.php
+++ employment.php
@@ -72,6 +72,7 @@
 			
 		<h2 class="margin-0-top">Employment opportunities</h2>
 					
+			<p><a href="https://www.governmentjobs.com/careers/washington/jobs/3315162/administrative-assistant-3" target="_blank">Administrative Assistant 3</a></p>
 			<p><a href="https://www.governmentjobs.com/careers/washington/jobs/3298986/fiscal-program-and-contracts-coordinator-exempt" target="_blank">Fiscal Program and Contracts Coordinator</a></p>
 			
 			
@@ -94,7 +95,7 @@
 		
 	
 	<p></p>
-			<p class="last-update">Last updated: 11/5/21</p>
+			<p class="last-update">Last updated: 11/19/21</p>
 		<!-- InstanceEndEditable --> </div>
 		
 		<div class="col-sm-2 padding-20-top padding-0-right"> <!-- InstanceBeginRepeat name="right_nav_repeat" --><!-- InstanceBeginRepeatEntry --> <!-- InstanceBeginEditable name="right_nav_title" -->
