996a4bb8c5ac5dae78adadc6ffbb2b1ed0692bca
Kevin Hyde <kevin.hyde@psp.wa.gov>
2021-11-23 13:14:14 -0800

    Updated board meetings

--- index.php
+++ index.php
@@ -238,9 +238,9 @@
 		<div class="col-md-3 col-sm-4">
 			<div class="newsbox calendarbox">
 				<!--news content -->
-				<div class="newsbox-date">NOV<br>
-			  18</div>
-				<div class="newsbox-text"><a href="board_meetings.php">SALMON RECOVERY COUNCIL, <br>
+				<div class="newsbox-date">DEC<br>
+			  9</div>
+				<div class="newsbox-text"><a href="board_meetings.php">LEADERSHIP COUNCIL, <br>
 					Online<br>
 							</a><br>
 				</div>
@@ -252,20 +252,6 @@
 		<div class="col-md-3 col-sm-4">
 			<div class="newsbox calendarbox">
 					<!--news content -->
-				<div class="newsbox-date">DEC<br>
-			  9</div>
-				<div class="newsbox-text"><a href="board_meetings.php">LEADERSHIP COUNCIL, <br>
-					Online<br>
-							</a><br>
-				</div>
-				<!--END news content-->
-			</div>
-		</div>
-		<!--COLUMN END-->
-			<!--COLUMN START-->
-		<div class="col-md-3 col-sm-4">
-			<div class="newsbox calendarbox">
-				<!--news content -->
 				<div class="newsbox-date">DEC<br>
 			  16</div>
 				<div class="newsbox-text"><a href="board_meetings.php">SCIENCE PANEL, <br>
@@ -283,6 +269,20 @@
 				<div class="newsbox-date">JAN<br>
 			  27</div>
 				<div class="newsbox-text"><a href="board_meetings.php">SALMON RECOVERY COUNCIL, <br>
+					Online<br>
+							</a><br>
+				</div>
+				<!--END news content-->
+			</div>
+		</div>
+		<!--COLUMN END-->
+			<!--COLUMN START-->
+		<div class="col-md-3 col-sm-4">
+			<div class="newsbox calendarbox">
+				<!--news content -->
+				<div class="newsbox-date">FEB<br>
+			  2</div>
+				<div class="newsbox-text"><a href="board_meetings.php">SCIENCE PANEL, <br>
 					Online<br>
 							</a><br>
 				</div>
