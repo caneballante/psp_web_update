a73448216f459a3be031c262595ea30455c9edbf
Kevin Hyde <kevin.hyde@psp.wa.gov>
2021-12-08 14:52:43 -0800

    Adjusted width of embed

--- puget-sound-budget.php
+++ puget-sound-budget.php
@@ -88,7 +88,7 @@
 			
 			<br>
 			
-			<iframe title="BudgetPBI_2022 - Page 1" width="850" height="700" src="https://app.powerbigov.us/view?r=eyJrIjoiNTFmOTI3ZDAtNzNhZi00ZmFlLTgxNzQtMzg3MTAxMWI1OGQ3IiwidCI6IjExZDBlMjE3LTI2NGUtNDAwYS04YmEwLTU3ZGNjMTI3ZDcyZCJ9" frameborder="0" allowFullScreen="true"></iframe>
+			<iframe title="BudgetPBI_2022 - Page 1" width="900" height="700" src="https://app.powerbigov.us/view?r=eyJrIjoiNTFmOTI3ZDAtNzNhZi00ZmFlLTgxNzQtMzg3MTAxMWI1OGQ3IiwidCI6IjExZDBlMjE3LTI2NGUtNDAwYS04YmEwLTU3ZGNjMTI3ZDcyZCJ9" frameborder="0" allowFullScreen="true"></iframe>
 			
 			<br>
 			<br>
