11b14b3389f13f949b1ebf0d9b307036e9392c71
Kevin Hyde <kevin.hyde@psp.wa.gov>
2022-01-04 11:10:15 -0800

    Adjusted layout

--- PSEMP-resources-test.php
+++ PSEMP-resources-test.php
@@ -94,8 +94,9 @@
 					<div class="card">
 						<div class="card-body">
 							<div class="d-inline">
-					<h6 class="margin-0-top">2018 SALISH SEA TOXICS MONITORING SYNTHESIS</h6> 
+					
 					<span class="no-icon"><a href="https://www.eopugetsound.org/articles/2018-salish-sea-toxics-monitoring-synthesis"><img src="images/PSEMP/toxics-synthesis-cover-2018.jpg" width="200" height="150" alt=""/></a> 
+						<h6 class="margin-0-top">2018 SALISH SEA TOXICS MONITORING SYNTHESIS</h6> 
 					<p><a href="https://www.eopugetsound.org/articles/2018-salish-sea-toxics-monitoring-synthesis">Download</a></p></span>
 							</div></div></div></div></div> <!--end col-->
 			
