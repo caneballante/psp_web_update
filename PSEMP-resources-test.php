88616ae3382c1ab2d283d3e5c163d8a2a879b875
Kevin Hyde <kevin.hyde@psp.wa.gov>
2022-01-04 11:41:24 -0800

    Added dividers

--- PSEMP-resources-test.php
+++ PSEMP-resources-test.php
@@ -100,6 +100,7 @@
 							</div></div></div></div></div> <!--end col-->
 			
 					 <!--end col-->
+			<hr>
 			<div class="row text-left">	
 			<div class="col-md-4">
 					<div class="card">
@@ -131,6 +132,7 @@
 							</div></div></div></div> <!--end col-->
 				
 	</div><!--end row-->
+			<hr>
 			<div class="row text-left">	
 			<div class="col-md-4">
 					<div class="card">
