a5e0c0ae0c77e0ac1c326acece3b8134c79a26eb
Kevin Hyde <kevin.hyde@psp.wa.gov>
2022-01-14 14:13:53 -0800

    Modified layout

--- PSEMP-resources-test.php
+++ PSEMP-resources-test.php
@@ -86,7 +86,7 @@
 						<div class="card-body">
 							<div class="d-inline">
 					<h6 class="margin-0-top">Prioritization of Contaminants of Emerging Concern</h6> 
-					<span class="no-icon"><a href="https://vitalsigns.pugetsoundinfo.wa.gov/VitalSign/Detail/22"><img src="images/PSEMP/CECS.jpg" class="img-responsive img-rounded" alt=""/></a> 
+					<span class="no-icon"><img src="images/PSEMP/CECS.jpg" class="img-responsive img-rounded" alt=""/></a> 
 					<p class="psemp-products"><a href="https://pspwa.box.com/s/dtw2o073imaqivbbqm6ikzytstuo7y7q">Overview</a> | <a href="https://pspwa.box.com/s/d7w99q8sxit90rja5vpig3umgve3wnck">Video</a></p></span>
 								<p class="psemp-products">This project prioritizes contaminants of emerging concern (CECs) for monitoring efforts and management responses. Hundreds of CECs such as pharmaceuticals, plasticizers, and pesticides have been found in Puget Sound.</p><br>
 					
