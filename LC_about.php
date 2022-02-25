34de2de0ac1604416d160171ed9b9abbc2cbfb88
Kevin Hyde <kevin.hyde@psp.wa.gov>
2022-02-25 07:36:22 -0800

    Edited text on the page per Boards team

--- LC_about.php
+++ LC_about.php
@@ -65,35 +65,35 @@
 		</div>
 		 
 		<div class="col-sm-7 padding-20-top content-column"> <!-- InstanceBeginEditable name="6col_content" -->
-			<p>The Leadership Council is the governing body of the Puget Sound Partnership. Its seven members are leading citizens chosen from around the Sound and appointed by the Governor to serve four-year terms. Jay Manning currently chairs the Leadership Council.<br>
+			<p>The Leadership Council is the governing body of the Puget Sound Partnership. Its seven members are leading citizens chosen from around the Sound. Members are appointed by the Governor to serve four-year terms, but may continue to serve until being officially reappointed or replaced by a new member. Jay Manning currently chairs the Leadership Council.<br>
 			</p>
 			<h2>Chair: Jay Manning</h2>
-			<p><strong>Term: </strong>Four-year term, ends June 25, 2022<sup>*</sup><br />
+			<p><strong>Term ends: </strong>June 25, 2022<sup>*</sup><br />
 				<strong>Biography:</strong> Jay has been working on restoring Puget Sound for most of his career.  Jay, currently a partner with Cascadia Law Group, spent many years working for the State of Washington, first in the Ecology Division of the Washington Attorney General’s Office and later as Director of the Department of Ecology. During his time at Ecology, Jay made Puget Sound restoration one of the agency’s highest priorities. Gov. Gregoire also appointed Jay to co-chair, with Bill Ruckelshause and Billy Frank Jr., of the original Puget Sound Partnership – the stakeholder group that recommended the creation of the Partnership as a state agency. Later, as Chief of Staff for Gov. Gregoire’s administration, Jay worked hard to maintain the administration’s focus on Puget Sound as the state went through the recession.  Jay has a degree in political science from Eastern Washington University and earned his law degree from the University of Oregon. </p>
 			<h2>Vice Chair: Dennis McLerran</h2>
-			<p><strong>Term:</strong> Four-year term, ends June   25, 2022<br />
+			<p><strong>Term ends:</strong> June 25, 2022<br />
 				<strong>Biography:</strong> Dennis McLerran served as the Regional Administrator for the U.S. Environmental Protection Agency’s Region 10, which encompasses Alaska, Idaho, Oregon and Washington, and Pacific Northwest Indian Country from 2010 until January of 2017. He currently serves on the Board of Directors for the Stockholm Environment Institute US, a research affiliate of Tufts University specializing in sustainable development and environmental issues. His previous experiences include serving as executive director of the Puget Sound Clean Air Agency, the regional air quality agency for King, Kitsap, Pierce and Snohomish counties; president of the National Association of Clean Air Agencies; and chairman of the Land Use and Environmental Law Section of the Washington State Bar Association. He holds a law degree from Seattle University School of Law and a bachelor’s degree in urban planning from the University of Washington.</p>
 			<h2>Member: Kate Dean</h2>
-			<p><strong>Term: </strong>Four-year term, ends June 25, 2025<br />
+			<p><strong>Term ends: </strong>June 25, 2025<br />
 				<strong>Biography:</strong> Dean is a Jefferson County Commissioner for District 1, Port Townsend, a position she was elected to in 2017, and has served as a member of the Partnership's Ecosystem Coordination Board. She moved to Jefferson County in 1999 and spent 10 years farming and working to grow the local food economy through businesses she co-founded, including FinnRiver Farm and Mt. Townsend Creamery. Her experience as an entrepreneur is critical to her understanding of the local economy and community.</p>
 <p>Dean left the farm but didn’t go far; she started a consulting business that had her working on natural resource and rural economic development issues locally and regionally. She coordinated the Jefferson Landworks Collaborative (a farmland preservation and enterprise development initiative), managed Washington State University Extension’s Small Farm Program, worked for Washington State Department of Labor and Industries, and was the regional director for the North Olympic Development Council, a council of governments tasked with community and economic development. Dean holds her Master of Public Administration degree from the Evans School of Public Policy and Governance at the University of Washington. Her publications include <em>USDA Farmland Changing Hands</em> and <em>Preparing for Climate Change on the North Olympic Peninsula</em>. In her spare time, Dean can be found gardening, riding her bike, or in the mountains with her two teenagers.</p>
 			<h2>Member: Will Hall</h2>
-			<p><strong>Term:</strong> Four-year term, ends June 25, 2025<sup>*</sup><br />
+			<p><strong>Term ends:</strong> June 25, 2025<sup>*</sup><br />
 				<strong>Biography:</strong> Will Hall has been protecting and restoring Puget Sound in many roles over the years. He wrote one of the first salmon recovery plans in the state, for the Snohomish River Basin. He established a marine resources committee that engages hundreds of volunteers in Puget Sound recovery. As director of the third-largest stormwater utility in Washington, he was responsible for planning, outreach, and monitoring, and for capital projects to protect and restore water quality, salmon habitat, and marine resources across five major watersheds draining to Puget Sound.</p>
 <p>Hall is an expert at local government planning and land use. He has served on the Shoreline City Council for 11 years, including three as mayor. He has been active on many boards, including The Coastal Society, the Society for Ecological Restoration, the Association of Washington Cities, the Southern Resident Orca Task Force, and the Puget Sound Partnership Ecosystem Coordination Board, which he chaired for four years.
 Hall has an undergraduate degree in physics from the University of Chicago and a Master of Marine Affairs degree from the University of Washington School of Marine and Environmental Affairs. Hall and his wife, Laurie, grew up near Puget Sound and enjoy many outdoor activities including hiking, biking, skiing, and scuba diving.
  </p>
 			<h2>Member: Russell Hepfer</h2>
-			<p><strong>Term:</strong> Four-year term, ends June 25, 2023<br />
+			<p><strong>Term ends:</strong> June 25, 2023<br />
 				<strong>Biography:</strong> Russell Hepfer, a Lower Elwha Klallam Tribe member, grew up on the Lower Elwha Klallam Reservation and has exercised his treaty harvest rights for his entire life, harvesting fish and shellfish, including geoduck.
 				He taught himself how to fish and then passed those skills on to several of his sons and a nephew. Russell started in natural resources as a water quality technician in 1995, then soon was asked to represent his tribe as a commissioner on the Northwest Indian Fisheries Commission.</p>
 			<p>While serving the commission for the past 18 years, he has also served on his tribal council for 16 years, formerly as chair and currently as vice chair. Since 2005, he has served as his tribe's delegate to the Indian Policy Advisory Committee for the Washington State Department of Social & Health Services. He has also been his tribe's delegate to the Coast Salish Gathering since its inception in 2006. When he's not working, he loves spending time in the outdoors, camping and fishing. </p>
 			<h2>Member: DEBORAH JENSEN</h2>
-			<p><strong>Term:</strong> Four-year term, ends June 25, 2023<br />
+			<p><strong>Term ends:</strong> June 25, 2023<br />
 				<strong>Biography:</strong> Deborah Jensen has extensive experience as an executive leading conservation and scientific organizations in Washington state and internationally. Named by Seattle Business Magazine as a recipient of the 2015 Executive Excellence Award and by the Puget Sound Business Journal as a recipient of the 2009 Women of Influence Award, Ms. Jensen currently serves as principal for Jensen and Associates. The firm offers consulting services on business and conservation strategy, organizational change, and board development. She is also a Distinguished Practitioner in Residence at the University of Washington, College of the Environment. From 2002 to 2015, Ms. Jensen shepherded the 100-year-old Woodland Park Zoo in its transition to nonprofit management and built strong education and conservation programs. Before moving to the Northwest, she served as Vice President of The Nature Conservancy&rsquo;s Conservation Science Division, at its world headquarters, in Arlington, Virginia. She holds masters and doctorate degrees in Energy and Resources and an undergraduate degree in Botany from the University of California Berkeley. Ms. Jensen&rsquo;s community involvement includes serving as current Vice-Chair of the Washington Wildlife and Recreation Coalition. She has also served as a member of the Seattle Community Development Roundtable; member of the Board of Directors, Association of Zoos and Aquariums; and as President and Board member, Society for Conservation Biology. </p>
 			
 			<h2>Member: Toby Murray </h2>
-			<p><strong>Term:</strong> Four-year term, ends June   25, 2022<br />
+			<p><strong>Term ends:</strong> June 25, 2022<br />
 				<strong>Biography:</strong> Born and raised in Lakewood, Toby graduated from Charles Wright Academy in 1971, and in 1975 graduated cum laude from Claremont McKenna College in Southern California.  He also attended the Harvard Business School Program for Management Development in 1983.
  
               <p> Toby began his career with Murray Pacific Corporation in 1976 when he started as a Timber Cruiser/Log Buyer for the company.  From 1977-99 he worked as a Tree Farm Forester, and in 1980-82 he became Assistant General Manager of the Timber Products Division.  Toby served as Vice President from 1983-2000.  During this time major career highlights included the development of a Habitat Conservation Plan for the Northern Spotted Owl (1993), and the nation’s first Multi-Species Habitat Conservation Plan (1995).  Toby recently retired from his position as President/CEO of Murray Pacific Corporation.</p>
@@ -104,7 +104,7 @@ Hall has an undergraduate degree in physics from the University of Chicago and a
 		
 		<p><sup>*</sup> Pending formal re-appointment by the Governor's Office of Boards and Commissions</p>
 		
-		<p class="last-update">Last updated: 02/24/2022</p>
+		<p class="last-update">Last updated: 02/25/2022</p>
 			
 			<!-- InstanceEndEditable --> </div>
 		
