2eca60c96c89f3845d0030028b134ad39efdbafd
kevinhydepsp <58960211+kevinhydepsp@users.noreply.github.com>
2022-05-24 07:14:18 -0700

    Added new open position to the employment page

--- employment.php
+++ employment.php
@@ -71,8 +71,7 @@
 		<div class="col-sm-7 padding-20-top content-column"> <!-- InstanceBeginEditable name="6col_content" -->
 			
 		<h2 class="margin-0-top">Employment opportunities	    </h2>
-		<p><a href="https://www.governmentjobs.com/careers/washington/jobs/3530370/monitoring-data-coordinator-and-analyst-exempt" target="new">Monitoring Data Coordinator and Analyst - closes 5/10/2022</a></p>              
-		<p><a href="https://www.governmentjobs.com/careers/washington/jobs/3530287/administrative-assistant-3" target="new">Administrative Assistant 3 - closes 5/10/2022</a></p>              
+		<p><a href="https://www.governmentjobs.com/careers/washington/jobs/3558913/contracts-coordinator" target="new">Contracts Coordinator - closes 6/2/2022</a></p>      
 				
 			
 		<h3>We are an Employer of Choice</h3>
@@ -88,7 +87,7 @@
 			 <img src="images/Employer-of-choice.jpg" width="753" height="707" alt="Puget Sound Partnership employer of choice benefits"/><br>
 		<br>
 		<br>
-		<p class="last-update">Last updated: 3/15/22</p>
+		<p class="last-update">Last updated: 5/24/22</p>
 		<!-- InstanceEndEditable --> </div>
 		
 		<div class="col-sm-2 padding-20-top padding-0-right"> <!-- InstanceBeginRepeat name="right_nav_repeat" --><!-- InstanceBeginRepeatEntry --> <!-- InstanceBeginEditable name="right_nav_title" -->
