2ae5310358324c2d265ccacf9967ae225e12c8d9
jon bridgman <jonbridgman@gmail.com>
2022-06-03 14:09:06 -0700

    2022 Action Agenda

--- 2022AAupdate.php
+++ 2022AAupdate.php
@@ -73,11 +73,11 @@ navSelected = 1;
 		<div class="col-sm-7 padding-20-top content-column"> <!-- InstanceBeginEditable name="6col_content" -->
 	
 		<div class="highlightbox">
-			<p>The 2022-2026 Action Agenda pages are currently being updated. Links to the 2022-2026 Action Agenda will be available before the June 8th, 2022 Leadership Council meeting. For assistance, please contact <a href="mailto:info@psp.wa.gov">info@psp.wa.gov</a></p>
+			<p>The 2022-2026 Action Agenda pages are currently being updated. Links to the 2022-2026 Action Agenda will be available before the <a href="https://pspwa.box.com/s/lptcpapdm5o1tctqegplo07ybbgah8gm">June 8th, 2022 Leadership Council meeting</a>. For assistance, please contact <a href="mailto:info@psp.wa.gov">info@psp.wa.gov</a></p>
 		</div>
 			
 		<h2>What is the Action Agenda? </h2>
-		<p>As  our community&rsquo;s shared plan for advancing Puget Sound recovery over the next  four years, the 2022-2026 Action Agenda charts the course for  Puget Sound recovery.</p>
+		<p>As our community&rsquo;s shared plan for advancing Puget Sound recovery over the next  four years, the 2022-2026 Action Agenda charts the course for  Puget Sound recovery.</p>
 			<img class="floatright" src="images/2022-AA-cover.jpg" width="288" height="370" alt=""/>
 <!--ADD THUMBNAIL HERE	[Thumbnail image of 2022-2026 AA, with links to]: </p>-->
 		<ul class="bullet-size-fix">
