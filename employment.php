87d410adc9f424e76c7b7ae78c98c8db0a5c5f4c
kevinhydepsp <58960211+kevinhydepsp@users.noreply.github.com>
2022-06-09 13:44:28 -0700

    Updated last update date

--- employment.php
+++ employment.php
@@ -87,7 +87,7 @@
 			 <img src="images/Employer-of-choice.jpg" width="753" height="707" alt="Puget Sound Partnership employer of choice benefits"/><br>
 		<br>
 		<br>
-		<p class="last-update">Last updated: 5/24/22</p>
+		<p class="last-update">Last updated: 6/09/22</p>
 		<!-- InstanceEndEditable --> </div>
 		
 		<div class="col-sm-2 padding-20-top padding-0-right"> <!-- InstanceBeginRepeat name="right_nav_repeat" --><!-- InstanceBeginRepeatEntry --> <!-- InstanceBeginEditable name="right_nav_title" -->
