4e187cabb1d3f08d8bd186043c5c6b3729761a83
kevinhydepsp <58960211+kevinhydepsp@users.noreply.github.com>
2022-08-18 07:26:41 -0700

    Took off the link for Spanish AA

--- 2022AAupdate.php
+++ 2022AAupdate.php
@@ -95,7 +95,7 @@ navSelected = 1;
 			<ul class="bullet-size-fix">
 			<li><a href="https://pspwa.box.com/shared/static/7svj5x4xtrj6duyccqgo2y0ryekk9zjr.pdf">Download the 2022-2026 Action Agenda Executive Summary</a> </li>
 			<li><a href="https://pspwa.box.com/shared/static/8zak4wiakdy94vc6104er8l3kn9bdxkw.pdf">Download the full 2022-2026 Action Agenda PDF</a></li>
-			<li><a href="https://pspwa.box.com/shared/static/mejeolwllwccjj3rmc8kiduia604vxos.pdf">Lea la Agenda de Acción 2022-2026 en español</a></li>
+			
 			
 			</ul>
 			</div>
