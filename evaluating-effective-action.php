adef14a53c3469ba3f81f48a912dd0bc3f6ba541
kevinhydepsp <58960211+kevinhydepsp@users.noreply.github.com>
2022-08-26 14:39:28 -0700

    Changed page title

--- evaluating-effective-action.php
+++ evaluating-effective-action.php
@@ -5,7 +5,7 @@
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <!-- InstanceBeginEditable name="doctitle" -->
-<title>Puget Sound Partnership - Effective Actions in Puget Sound</title>
+<title>Puget Sound Partnership - Evaluating Recovery Effectiveness</title>
 <!-- InstanceEndEditable -->
 <!-- Bootstrap -->
 <link href="css/bootstrap.css" rel="stylesheet">
