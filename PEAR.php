e99b4952c882d3875587d9cf492da4cf17c331e6
jon bridgman <jonbridgman@gmail.com>
2022-08-30 14:20:15 -0700

    PEAR

---
+++ PEAR.php
@@ -0,0 +1,141 @@
+<!DOCTYPE html>
+<html lang="en"><!-- InstanceBegin template="/Templates/page-4-template.dwt" codeOutsideHTMLIsLocked="false" -->
+<head>
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<!-- InstanceBeginEditable name="doctitle" -->
+<title>Pro-Equity Anti-Racism (PEAR)</title>
+<!-- InstanceEndEditable -->
+<!-- Bootstrap -->
+<link href="css/bootstrap.css" rel="stylesheet">
+<link href="css/custom.css" rel="stylesheet" type="text/css">
+<link href="css/custom-erika.css" rel="stylesheet" type="text/css">
+<!-- loads the wf-loading class right away to minimize FOUT -->
+<script>document.documentElement.className += ' wf-loading';</script>
+<!-- Font PRENTON TYPEKIT -->
+<script src="https://use.typekit.net/srt5jze.js"></script>
+<script>try{Typekit.load({ async: true });}catch(e){}</script>
+<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+<!--[if lt IE 9]>
+	<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+	<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+<!-- InstanceBeginEditable name="head" -->
+<script>
+/*this variable is used to set the proper nav to active. It should to the order the nav item is in the list*/
+  	navSelected = 1;
+</script> 
+<!-- InstanceEndEditable -->
+<!-- InstanceParam name="OptionalRegion1" type="boolean" value="true" -->
+<!-- InstanceParam name="OptionalRegion2" type="boolean" value="false" -->
+<!-- InstanceParam name="OptionalRegion3" type="boolean" value="true" -->
+<!-- InstanceParam name="OptionalRegion4" type="boolean" value="false" -->
+<!-- InstanceParam name="OptionalRegion5" type="boolean" value="true" -->
+<!-- InstanceParam name="OptionalRegion6" type="boolean" value="true" -->
+<!-- InstanceParam name="OptionalRegion7" type="boolean" value="false" -->
+<!-- InstanceParam name="OptionalRegion8" type="boolean" value="false" -->
+</head>
+<body>
+<?php include 'includes/modal-inc.html';?>
+<!-- START IMAGE HEADER --> 
+ <!-- InstanceBeginEditable name="overviewphoto" -->
+<header class="overview-page-image-style overview-page-image120"> </header>
+<!-- InstanceEndEditable -->  
+<div class="container-fluid page-content padding-50-bottom">
+	<div class="row">
+		<div class="col-sm-3 padding-20-top"></div>
+		<div class="col-sm-7 padding-20-top"> <!-- InstanceBeginEditable name="6col_header" -->
+			<h1>Pro-Equity Anti-Racism (PEAR)</h1> 
+			<!-- InstanceEndEditable --></div>
+		<div class="col-sm-2 padding-20-top"></div>
+	</div>
+	
+	<div class="row"> 
+		<div class="col-sm-3">
+			<div class="nav-leftside-custom">
+				<ul class="nav nav-stacked nav-pills nav-leftside-custom padding-left-0 margin-10-top">
+					<!-- InstanceBeginEditable name="left_nav" -->
+				<!--<?php include 'includes/ln-boards.html';?>-->	
+	
+			<!-- InstanceEndEditable -->
+				</ul>
+			</div>
+		</div>
+		 
+		<div class="col-sm-7 padding-20-top content-column"> <!-- InstanceBeginEditable name="6col_content" -->
+
+       
+        <h2 class="margin-0-top">The Partnership's PEAR statement</h2>
+        <div class="floatright-block blue-outline-5px padding-10-all margin-10-left"><img src="images/PEAR.jpg" width="299" height="171" alt=""/><br>
+        <a href="https://pspwa.box.com/s/hxfglg57ny33e9qfvc8u5malexh1vr9d" target="new">Video recorded staff presentation </a><br>
+        of the PEAR statement 8/18/2022</div>    
+        <p>On March 21, 2022, Governor Inslee signed Executive Order 22-04, Implementing the Washington State Pro-Equity Anti-Racism (PEAR) Plan  &amp; Playbook. The Executive Order  directs all Cabinet agencies to develop a PEAR Strategic Action Plan, implement  it, and report our progress on implementation.</p>
+        <p>In this Executive Order,  the Governor clearly  states his intention  that we and our sister state agencies  do our part to create a PEAR ecosystem in the state of Washington. Our  founding statute also directs us to support a healthy human population and a  vibrant quality of life. Achieving  these goals will help us achieve our mission, but it&rsquo;s also quite simply the  right and deeply decent thing to do.</p>
+        <p>I and the Executive Team are  committed to implementing this Executive Order. While responsibilities may vary across our staff for  implementing the specifics of the PSP PEAR Strategic Plan, we know that each of us must do our own anti-racism, pro-equity work. We look forward  to continued work with the Equity &amp; Environmental Justice  Team, supervisors, and all staff to build diversity, equity, inclusion, and  environmental justice into the programs and products we lead and every staff  member&rsquo;s work plan.</p>
+        <p>We also plan the following:</p>
+        <ul class="bullet-size-fix">
+            <li>The Executive Team will continue  working with the Equity &amp; Environmental Justice  Team to develop our DEI Action  Plan, and with all of you to implement it.</li>
+            <li>Executive Team members plan to reflect on the  state&rsquo;s diversity, equity, and inclusion competencies, assess our personal  progress toward them, and make individual action  plans based on them. We&rsquo;ll  make ours available to others as a model for the agency.</li>
+            <li>In 2023, we will update our agency&rsquo;s  strategic plan, and ensure that we infuse  pro-equity, anti- racism and  environmental justice work into it, in accordance with the HEAL Act and our own  values.</li>
+        </ul>
+        <p>I am looking forward to  continuing this journey with each of you, our partners, and the Office of  Equity. Please don&rsquo;t hesitate to  reach out to me or any member  of the Executive Team or our PEAR Team – Mel,  Lea Anne, Denise, and me – with questions or ideas.</p>
+        <p>Laura Blackmore<br>
+            <em> Executive Director</em><br>
+        The Puget Sound Partnership</p>
+            
+        <h2>Partnership PEAR team</h2>
+            <table class="table-striped">
+                <tr>
+                    <td><p>Laura Blackmore</p></td>
+                    <td><p>Executive Director</p></td>
+                </tr>
+                <tr>
+                    <td><p>Mellisa Schutten</p></td>
+                    <td><p>Equity and Environmental Justice Manager</p></td>
+                </tr>
+                <tr>
+                    <td><p>Lea Anne Burke</p></td>
+                    <td><p>Tribal Liaison</p></td>
+                </tr>
+                <tr>
+                    <td><p>Denise Ross</p></td>
+                    <td><p>Human Resource Manager</p></td>
+                </tr>
+                <tr>
+                    <td><p>Community Member (vacant)</p></td>
+                    <td><p></p></td>
+                </tr>
+            </table>
+		
+			<!-- InstanceEndEditable --> </div>
+		
+		<div class="col-sm-2 padding-20-top padding-0-right"> <!-- InstanceBeginRepeat name="right_nav_repeat" --><!-- InstanceEndRepeat --> </div>
+	</div>
+	<!--END OF ROW --> 
+</div>
+<!--END OF CONTENT CONTAINER -->
+
+<?php include 'includes/footer-inc.html';?>
+
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) --> 
+<script src="js/jquery-1.11.2.min.js"></script> 
+
+<!-- Include all compiled plugins (below), or include individual files as needed --> 
+<script src="js/bootstrap.js"></script> 
+<!-- custom js --> 
+<script src="js/custom.js"></script> 
+<!-- Google Tracking  --> 
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-69373425-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
+</body>
+<!-- InstanceEnd --></html>
