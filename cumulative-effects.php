9d9b3dad3e4520dbbf1fd0060c59d6ba7b8a85e3
kevinhydepsp <58960211+kevinhydepsp@users.noreply.github.com>
2022-09-06 11:55:32 -0700

    Changed nav order

--- cumulative-effects.php
+++ cumulative-effects.php
@@ -25,7 +25,7 @@
 <!-- InstanceBeginEditable name="head" -->
 <script>
 /*this variable is used to set the proper nav to active. It should to the order the nav item is in the list*/
-  	navSelected = 5;
+  	navSelected = 6;
 </script>
 <!-- InstanceEndEditable -->
 <!-- InstanceParam name="OptionalRegion1" type="boolean" value="true" -->
