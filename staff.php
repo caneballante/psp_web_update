298498bcc6fbaaa197b67994c6b8bfe8ed170f25
kevinhydepsp <58960211+kevinhydepsp@users.noreply.github.com>
2022-09-16 10:48:07 -0700

    Added Kate Loy to the staff list

--- staff.php
+++ staff.php
@@ -251,6 +251,9 @@
 						<h5>Annelise Del Rio</h5>
 						<p>	Monitoring Performance Analyst/Salmon Scientist <br>
 							360.584.5306 | <a href="mailto:annelise.delrio@psp.wa.gov">annelise.delrio@psp.wa.gov</a></p>
+						<h5>Kate Loy</h5>
+						<p>Keystone Fellow<br>
+							360.789.3324 | <a href="mailto:kate.loy@psp.wa.gov">kate.loy@psp.wa.gov</a></p>
 						
 
 						<!--INTERNAL OPERATIONS-->
