76c31d58383ae7fa958dac83669329e01425b993
kevinhydepsp <58960211+kevinhydepsp@users.noreply.github.com>
2022-11-07 11:19:57 -0800

    Adjusted page

--- PSAR.php
+++ PSAR.php
@@ -67,7 +67,10 @@
 		 
 		<div class="col-sm-9 padding-20-top padding-40-right content-column mobile-padding-20-left"> <!-- InstanceBeginEditable name="9col_content" -->
 						
-			
+			</div>
+			<div class="highlightbox">
+			<p><span class="new-red">NEW!</span>&nbsp;<a href="https://pspwa.box.com/s/ckw368t070rmlcdl182999fnbi0dtun2" target="new">2023-2025 Puget Sound Acquisition and Restoration (PSAR) Large Capital Project ranked list and project factsheets are available here.</a></p>
+			</div>
 			<div class="sidebarbox margin-20-top margin-20-left margin-20-bottom padding-10-all orange-fill">
 				<h3 class="margin-0-top">PSAR ACCOMPLISHMENTS</h3>
 				<p>$254 million invested in Puget Sound recovery<br>
@@ -79,10 +82,7 @@
 				Vegetation planted: <strong>3,096 acres</strong><br>
 				Habitat area connected: <strong>5,417 acres</strong><br>
 				Jobs produced: <strong>3,413</strong></p>
-			</div>
-			<div class="highlightbox">
-			<p><span class="new-red">NEW!</span>&nbsp;<a href="https://pspwa.box.com/s/ckw368t070rmlcdl182999fnbi0dtun2" target="new">2023-2025 Puget Sound Acquisition and Restoration (PSAR) Large Capital Project ranked list and project factsheets are available here.</a></p>
-			</div>
+			
 			<p class="margin-20-top"><strong>Investing in habitat restoration and protection to bring back Puget Sound's natural systems-for salmon, people and community</strong></p>
 			<p class="margin-20-top">The Puget Sound Acquisition and Restoration (PSAR) fund supports projects that recover salmon and protect and recover salmon habitat in Puget Sound. The state legislature appropriates money for PSAR every 2 years in the Capital Budget. PSAR is co-managed by the Puget Sound Partnership and the Recreation and Conservation Office. Local entities identify and propose PSAR projects. The Salmon Recovery Funding Board prioritizes projects for funding.</p>
 		
