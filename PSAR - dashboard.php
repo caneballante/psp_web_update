8ab64403df2ed27ce901149917d8db95133ae206
kevinhydepsp <58960211+kevinhydepsp@users.noreply.github.com>
2022-12-08 09:55:57 -0800

    Added test page

---
+++ PSAR - dashboard.php
@@ -0,0 +1,170 @@
+<!DOCTYPE html>
+<html lang="en"><!-- InstanceBegin template="/Templates/page-4-template.dwt" codeOutsideHTMLIsLocked="false" -->
+<head>
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<!-- InstanceBeginEditable name="doctitle" -->
+<title>Puget Sound Partnership - PSAR Fund</title>
+<!-- InstanceEndEditable -->
+<!-- Bootstrap -->
+<link href="css/bootstrap.css" rel="stylesheet">
+<link href="css/custom.css" rel="stylesheet" type="text/css">
+<link href="css/custom-erika.css" rel="stylesheet" type="text/css">
+<!-- loads the wf-loading class right away to minimize FOUT -->
+<script>document.documentElement.className += ' wf-loading';</script>
+<!-- Font PRENTON TYPEKIT -->
+<script src="https://use.typekit.net/srt5jze.js"></script>
+<script>try{Typekit.load({ async: true });}catch(e){}</script>
+<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+<!--[if lt IE 9]>
+	<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+	<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+<!-- InstanceBeginEditable name="head" -->
+<!-- InstanceEndEditable -->
+<!-- InstanceParam name="OptionalRegion1" type="boolean" value="true" -->
+<!-- InstanceParam name="OptionalRegion2" type="boolean" value="false" -->
+<!-- InstanceParam name="OptionalRegion3" type="boolean" value="true" -->
+<!-- InstanceParam name="OptionalRegion4" type="boolean" value="false" -->
+<!-- InstanceParam name="OptionalRegion5" type="boolean" value="true" -->
+<!-- InstanceParam name="OptionalRegion6" type="boolean" value="false" -->
+<!-- InstanceParam name="OptionalRegion7" type="boolean" value="true" -->
+<!-- InstanceParam name="OptionalRegion8" type="boolean" value="false" -->
+</head>
+<body>
+<?php include 'includes/modal-inc.html';?>
+<!-- START IMAGE HEADER --> 
+ <!-- InstanceBeginEditable name="overviewphoto" -->
+<header class="overview-page-image-style overview-page-image7">
+	<div class="overview-quote pull-right">
+		<h3>Investing in habitat restoration to bring back Puget Sound’s dynamic natural systems – for salmon, people, and community</h3>
+	</div>
+</header>
+<!-- InstanceEndEditable -->  
+<div class="container-fluid page-content padding-50-bottom">
+	<div class="row">
+		<div class="col-sm-3 padding-20-top"></div>
+		<div class="col-sm-7 padding-20-top"> <!-- InstanceBeginEditable name="6col_header" -->
+			<h1>PUGET SOUND ACQUISITION AND RESTORATION (PSAR) FUND</h1>
+			<!-- InstanceEndEditable --></div>
+		<div class="col-sm-2 padding-20-top"></div>
+	</div>
+	
+	<div class="row"> 
+		<div class="col-sm-3">
+			<div class="nav-leftside-custom">
+				<ul class="nav nav-stacked nav-pills nav-leftside-custom padding-left-0 margin-10-top">
+					<!-- InstanceBeginEditable name="left_nav" -->
+				
+				<li class="active" role="presentation"> <a href="PSAR.php">PSAR OVERVIEW</a></li>
+				<li class="active" role="presentation"> <a href="PSAR-resources.php">PSAR RESOURCES</a></li>
+				<!-- InstanceEndEditable -->
+				</ul>
+			</div>
+		</div>
+		 
+		<div class="col-sm-9 padding-20-top padding-40-right content-column mobile-padding-20-left"> <!-- InstanceBeginEditable name="9col_content" -->
+			<div class="highlightbox margin0-top">
+			<p class="padding-10-all"><span class="new-red">NEW!</span>&nbsp;<a href="https://pspwa.box.com/s/ckw368t070rmlcdl182999fnbi0dtun2" target="new">2023-2025 Puget Sound Acquisition and Restoration (PSAR) Large Capital Project ranked list and project factsheets are available here.</a></p>
+			</div>			
+			
+			<div class="sidebarbox margin-20-top margin-20-left margin-20-bottom padding-10-all orange-fill">
+				<h3 class="margin-0-top">PSAR ACCOMPLISHMENTS</h3>
+				<p>$254 million invested in Puget Sound recovery<br>
+				Projects implemented: <strong>620</strong><br>	
+				River shoreline restored: <strong>6,449 acres</strong><br>
+				Estuary habitat restored<strong>: 3,260 acres</strong><br>
+				Watershed habitat protected: <strong>12,837 acres</strong><br>
+				Fish passage restored: <strong>142 river miles</strong><br>
+				Vegetation planted: <strong>3,096 acres</strong><br>
+				Habitat area connected: <strong>5,417 acres</strong><br>
+				Jobs produced: <strong>3,413</strong></p>
+			
+			<p class="margin-20-top"><strong>Investing in habitat restoration and protection to bring back Puget Sound's natural systems-for salmon, people and community</strong></p>
+				</div>
+			
+			<p class="margin-20-top">The Puget Sound Acquisition and Restoration (PSAR) fund supports projects that recover salmon and protect and recover salmon habitat in Puget Sound. The state legislature appropriates money for PSAR every 2 years in the Capital Budget. PSAR is co-managed by the Puget Sound Partnership and the Recreation and Conservation Office. Local entities identify and propose PSAR projects. The Salmon Recovery Funding Board prioritizes projects for funding.</p>
+			
+			
+		
+		<button class="button button1"><a href="https://www.pugetsoundinfo.wa.gov/PSARDashboard" target="_blank" style="color:white"><strong>Puget Sound Acquisition and Restoration Dashboard on Puget Sound Info</strong></a></button>
+		
+			<hr /><br><br><br><br><br><br><br><br><br><br>
+		<div class="row margin-10-top"> 
+			<div class="col-sm-3"><a href="https://innovationstories.psp.wa.gov/"><h4>Puget Sound Innovation Recovery Stories</h4></a></div>
+			<div class="col-sm-3"><a href="https://innovationstories.psp.wa.gov/index.php/2017/08/24/calistoga-levee-setback/"><img class="img-responsive" src="images/PSAR/ps-innovation-calistoga.jpg" width="874" height="743" alt=""/></a></div>
+			<div class="col-sm-3"><a href="https://innovationstories.psp.wa.gov/index.php/2017/04/21/leque-island-estuary-2/"><img class="img-responsive" src="images/PSAR/ps-innovation-leque.jpg" width="874" height="743" alt=""/></a></div>
+			<div class="col-sm-3"><a href="https://innovationstories.psp.wa.gov/index.php/2017/05/17/smith-island-2/"><img class="img-responsive" src="images/PSAR/ps-innovation-smith.jpg" width="874" height="743" alt=""/></a></div>
+		</div>
+		<hr />
+		<h2>COMMUNITY INVOLVEMENT IN PSAR PROJECTS</h2>
+		<p><strong>COMMUNITY INVOLVEMENT IN PSAR PROJECTS</strong><br>
+			<strong>Each community decides which projects matter most. </strong><br>
+			Washington approaches salmon recovery in a unique way. People in communities around Puget Sound organized themselves to address Endangered Species Act listings of Chinook and other salmonids in Washington state. This local approach to a difficult problem is known as &ldquo;the Washington Way.&rdquo;</p>
+			<p>The 15 community-based groups that organized in Puget Sound are called <a href="https://www.rco.wa.gov/salmon_recovery/lead_entities.shtml">Lead Entities</a>. Lead Entities are supported by regional entities called <a href="http://www.psp.wa.gov/salmon-recovery-overview.php">regional recovery organizations</a>. There are seven around the state, including two in the Puget Sound region. The Puget Sound Partnership is the regional recovery organization for all of Puget Sound except the Hood Canal area. Recovery organizations are accepted by NOAA as the owners of each region&rsquo;s recovery and are responsible to carrying out salmon recovery plans. </p>
+		<h2>LESSONS LEARNED</h2>
+			<p><strong>The PSAR program continues to improve with feedback from Local Entities.</strong><br>
+			As managers of the PSAR program, RCO and the Partnership continuously look for opportunities to improve how PSAR works. Some of the improvements over the past 10 years include the following:</p>
+		<ul>
+			<li>Developed methods to more clearly communicate how requested PSAR funds are used throughout the region</li>
+			<li>Improved the reporting and data management of PSAR projects to help project partners and constituents visualize results in their area and where money has been spent</li>
+			<li>Updated PSAR Large Capital criteria assist reviewers in selecting only the best projects shown to benefit salmon in Puget Sound and advance regional restoration targets </li>
+			<li>Synthesis of the most effective restoration actions further our knowledge of what&rsquo;s working for salmon recovery and help guide our actions and priorities in the region</li>
+		</ul>
+		<p>All this and more improvements are underway. We are constantly evaluating our activities to develop an understanding of effectiveness and impact that PSAR has on salmon recovery.<strong> </strong></p>
+		<h2>PROJECT EVALUATION</h2>
+			<p><strong>Evaluation helps verify project successes.</strong><br>
+			By monitoring PSAR project sites and evaluating the resulting data, scientists are observing many ecosystem successes, including the following: </p>
+		<ul>
+			<li>Eelgrass beds have expanded, water quality has improved, and shellfish areas are improving in the Skokomish River estuary, as a result of a PSAR-funded restoration project. <a href="https://pspwa.box.com/s/fdxzgqwtsct1xy4l6ohfpeepjg4ho42a" target="new">VIEW FACT SHEET</a></li>
+			<li>In the South Fork of the Skagit River, 461 estuary acres were restored and now support an additional 160,000 young Chinook each year; this represents about 12 percent of the salmon recovery goal for this area. <a href="https://pspwa.box.com/s/crre7nvc9ru3z14qegb5zojf1ofjtdon" target="new">VIEW FACT SHEET </a></li>
+			<li>An estimated 65,000 young Chinook salmon are expected to be produced annually after restoration is complete at Fir Island Farm in Skagit County, which holds some of the largest runs of Chinook, pink, and chum in the state. <a href="https://pspwa.box.com/s/zfbfd0ynmc3wec4cije7a89dop7zsr7k">VIEW FACT SHEET</a></li>
+		</ul>
+		<h2>PSAR PROJECT BENEFITS </h2>
+		<p><strong>PSAR benefits extend beyond the local ecosystem:</strong></p>
+		<ul>
+			<li>Every $1 million invested in watershed restoration produces more than 16 new or sustained jobs roughly $2.5 million in total economic activity. </li>
+			<li>80 percent of the funds invested in restoration projects stays in the county where the projects are located, providing needed cash in more rural and distressed counties. </li>
+		</ul><br><br>
+		
+		<p><strong>For more information contact:</strong></p>
+			<h5>Alison Agness</h5>
+			<p>PSAR Program Manager<br />
+			360.968.9673 | <a href="mailto:alison.agness@psp.wa.gov">alison.agness@psp.wa.gov</a></p>
+			<p>&nbsp;</p>
+			<div>
+				<div> </div>
+			</div>
+	<p class="last-update">Last updated: 11/7/22</p>
+		<!-- InstanceEndEditable --> </div>
+		
+		<div class="col-sm-2 padding-20-top padding-0-right"> <!-- InstanceBeginRepeat name="right_nav_repeat" --><!-- InstanceEndRepeat --> </div>
+	</div>
+	<!--END OF ROW --> 
+</div>
+<!--END OF CONTENT CONTAINER -->
+
+<?php include 'includes/footer-inc.html';?>
+
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) --> 
+<script src="js/jquery-1.11.2.min.js"></script> 
+
+<!-- Include all compiled plugins (below), or include individual files as needed --> 
+<script src="js/bootstrap.js"></script> 
+<!-- custom js --> 
+<script src="js/custom.js"></script> 
+<!-- Google Tracking  --> 
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-69373425-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
+</body>
+<!-- InstanceEnd --></html>
