0d98ec5ae53ce97d5d0efee9996f97cbc37b3c5c
jon bridgman <jonbridgman@gmail.com>
2023-01-23 11:47:31 -0800

    test pages

---
+++ 2018-2022-action-agenda-explorer.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Puget Sound Partnership 2018-2022 Action Agenda Explorer</title>
+<!-- Bootstrap -->
+<link href="css/bootstrap.css" rel="stylesheet">
+<link href="css/custom.css" rel="stylesheet" type="text/css">
+<link href="css/custom-erika.css" rel="stylesheet" type="text/css">
+<!-- loads the wf-loading class right away to minimize FOUT -->
+<script>document.documentElement.className += ' wf-loading';</script>
+<!-- Font PRENTON TYPEKIT -->
+<script src="https://use.typekit.net/srt5jze.js"></script>
+<script>try{Typekit.load({ async: true });}catch(e){}</script>
+<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+<!--[if lt IE 9]>
+	<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+	<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+<script>
+/*this variable is used to set the proper nav to active. It should to the order the nav item is in the list*/
+  	navSelected = 7;
+</script>
+</head>
+<body>
+<?php include 'includes/modal-inc.html';?>
+<!-- START IMAGE HEADER --> 
+
+<div class="container-fluid page-content padding-50-bottom">
+	<div class="row">
+		<div class="col-sm-1 padding-20-top"></div>
+		<div class="col-sm-11 padding-20-top">
+<h1 class="grey">STATE OF THE SOUND</h1>
+			<p>&nbsp;</p>
+			</div>
+		<div class="col-sm-1 padding-20-top"></div>
+	</div>
+	
+	<div class="row"> 
+		<div class="col-sm-1">
+			
+		</div>
+		 
+		<div class="col-sm-11 padding-20-top content-column">
+			<iframe title="AA Tracker NTA Detail page" width="1140" height="541.25" src=https://app.powerbigov.us/reportEmbed?reportId=0c6df853-1191-4e07-bef5-2f0b5915918f&autoAuth=true&ctid=11d0e217-264e-400a-8ba0-57dcc127d72d frameborder="0" allowFullScreen="true"></iframe>
+			<p>&nbsp;</p>
+			</div>
+		
+		<div class="col-sm-1 padding-20-top padding-0-right"> </div>
+	</div>
+	<!--END OF ROW --> 
+</div>
+<!--END OF CONTENT CONTAINER -->
+
+<?php include 'includes/footer-inc.html';?>
+
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) --> 
+<script src="js/jquery-1.11.2.min.js"></script> 
+
+<!-- Include all compiled plugins (below), or include individual files as needed --> 
+<script src="js/bootstrap.js"></script> 
+<!-- custom js --> 
+<script src="js/custom.js"></script> 
+<!-- Google Tracking  --> 
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-69373425-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
+</body>
+</html>
---
+++ LIO-overview-updatetest.php
@@ -0,0 +1,137 @@
+<!DOCTYPE html>
+<html lang="en"><!-- InstanceBegin template="/Templates/page-4-template.dwt" codeOutsideHTMLIsLocked="false" -->
+<head>
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-BGEZ5L3EJY"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-BGEZ5L3EJY');
+</script>
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<!-- InstanceBeginEditable name="doctitle" -->
+<title>Puget Sound Partnership - Local Integrating Organizations Overview</title>
+<!-- InstanceEndEditable -->
+<!-- Bootstrap -->
+<link href="css/bootstrap.css" rel="stylesheet">
+<link href="css/custom.css" rel="stylesheet" type="text/css">
+<link href="css/custom-erika.css" rel="stylesheet" type="text/css">
+<!-- loads the wf-loading class right away to minimize FOUT -->
+<script>document.documentElement.className += ' wf-loading';</script>
+<!-- Font PRENTON TYPEKIT -->
+<script src="https://use.typekit.net/srt5jze.js"></script>
+<script>try{Typekit.load({ async: true });}catch(e){}</script>
+<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+<!--[if lt IE 9]>
+	<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+	<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+<!-- InstanceBeginEditable name="head" -->
+<script>
+/*this variable is used to set the proper nav to active. It should to the order the nav item is in the list*/
+  	navSelected = 1;
+</script>
+<!-- InstanceEndEditable -->
+<!-- InstanceParam name="OptionalRegion1" type="boolean" value="true" -->
+<!-- InstanceParam name="OptionalRegion2" type="boolean" value="false" -->
+<!-- InstanceParam name="OptionalRegion3" type="boolean" value="true" -->
+<!-- InstanceParam name="OptionalRegion4" type="boolean" value="false" -->
+<!-- InstanceParam name="OptionalRegion5" type="boolean" value="true" -->
+<!-- InstanceParam name="OptionalRegion6" type="boolean" value="true" -->
+<!-- InstanceParam name="OptionalRegion7" type="boolean" value="false" -->
+<!-- InstanceParam name="OptionalRegion8" type="boolean" value="false" -->
+</head>
+<body>
+<?php include 'includes/modal-inc.html';?>
+<!-- START IMAGE HEADER --> 
+ <!-- InstanceBeginEditable name="overviewphoto" -->
+<header class="overview-page-image-style overview-page-image13"> </header>
+<!-- InstanceEndEditable -->  
+<div class="container-fluid page-content padding-50-bottom">
+	<div class="row">
+		<div class="col-sm-3 padding-20-top"></div>
+		<div class="col-sm-7 padding-20-top"> <!-- InstanceBeginEditable name="6col_header" -->
+			<h1>LOCAL INTEGRATING ORGANIZATIONS (LIOs)</h1>
+			<p>&nbsp;</p>
+			<!-- InstanceEndEditable --></div>
+		<div class="col-sm-2 padding-20-top"></div>
+	</div>
+	
+	<div class="row"> 
+		<div class="col-sm-3">
+			<div class="nav-leftside-custom">
+				<ul class="nav nav-stacked nav-pills nav-leftside-custom padding-left-0 margin-10-top">
+					<!-- InstanceBeginEditable name="left_nav" -->
+				<?php include 'includes/ln-local-recovery-efforts.html';?>
+				<!-- InstanceEndEditable -->
+				</ul>
+			</div>
+		</div>
+		 
+		<div class="col-sm-7 padding-20-top content-column"> <!-- InstanceBeginEditable name="6col_content" -->
+			<p>Local Integrating Organizations are local forums that meet regularly throughout the year to collaboratively work to develop, coordinate, and implement strategies and actions that that contribute to the protection and recovery of the local ecosystem. Up until December 2018, there were nine LIOs. In Dec of 2018, the Leadership Council approved a tenth LIO, the Puyallup-White River LIO (PWR LIO).</p>
+			<p>Each LIO regularly updates their 5-year Ecosystem Recovery Plan that outlines specific strategies and actions that guide local ecosystem recovery and advises regional scale recovery.</p>
+			<p>The LIOs committees are recognized as the local expert bodies for ecosystem recovery. While each LIO structure may differ as a result of the needs of the specific geography, most have an executive committee and a technical committee. Members may include elected officials, tribal staff, city and county government staff, non-profit organizations, land trusts and conservation districts, marine resource committees, local businesses, interest groups, citizens, and educational organizations.</p>
+			<p>Each LIO coordinates with local salmon recovery bodies and other ecosystem recovery entities to align local priorities and goals.</p>
+			<p>The Puget Sound Partnership supports LIOs through capacity funding, resources, and liaison support via Ecosystem Recovery Coordinators (Puget Sound Partnership Staff)</p>
+			<p style="font-weight: bold">Local Integrating Organizations:</p>
+			<ul>
+				<li><a href="http://www.sanjuanlio.com" target="new">San Juan Action Agenda Oversight Group </a></li>
+				<li><a href="http://www.govlink.org/sc-puget-sound-action-area/Index.htm" target="new">South Central Action Area Caucus Group</a></li>
+				<li><a href="http://www.healthysouthsound.org/" target="new">Alliance for a Healthy South Sound</a></li>
+				<li><a href="https://hccc.wa.gov/" target="new">Hood Canal Coordinating Council </a></li>
+				<li><a href="https://westsoundpartners.org/" target="new">West Sound Partners for Ecosystem Recovery</a></li>
+				<li><a href="https://wria1project.whatcomcounty.org/home" target="new">Whatcom LIO</a></li>
+				<li><a href="http://www.straitern.org/">Strait Ecosystem Recovery Network </a></li>
+				<li><a href="https://www.islandcountywa.gov/Health/DNR/ILIO/Pages/Home.aspx" target="new">Island LIO</a></li>
+				<li><a href="http://snohomishcountywa.gov/831/Local-Integrating-Organization" target="new">Snohomish/Stillaguamish LIO </a></li>
+				<li><a href="https://puyallupwatershed.org/" target="new">Puyallup-White River LIO (PWR LIO)</a></li>
+			</ul>
+			<p><a href="https://wa-psp.maps.arcgis.com/apps/webappviewer/index.html?id=bce72e6f26c640e5a485db8951951117" target="new"><img class="img-responsive" src="images/LIOsPresentation.jpg" width="1000" height="900" alt=""/></a></p>
+			<p>&nbsp;</p>
+		<!-- InstanceEndEditable --> </div>
+		
+		<div class="col-sm-2 padding-20-top padding-0-right"> <!-- InstanceBeginRepeat name="right_nav_repeat" --><!-- InstanceBeginRepeatEntry --> <!-- InstanceBeginEditable name="right_nav_title" -->
+			<div class="right-nav-title margin-0-top ">SEE ALSO</div>
+			<!-- InstanceEndEditable -->
+			<ul class="nav-rightside-custom">
+				<!-- InstanceBeginEditable name="right_nav_links" -->
+				<li class="active" role="presentation"> <a href="https://pspwa.box.com/s/nxli7o61pnjxx4rkmo7nokcjh9huc0kf">LIO Ecosystem Recovery Plans</a></li>
+				<li class="active" role="presentation"> <a href="https://wa-psp.maps.arcgis.com/apps/webappviewer/index.html?id=bce72e6f26c640e5a485db8951951117" target="new">LIO Geography Map</a></li>
+				<li class="active" role="presentation"> <a href="https://pspwa.box.com/v/ECONetInfo">ECO Net Information</a></li>
+				
+				<!-- InstanceEndEditable -->
+			</ul>
+			<!-- InstanceEndRepeatEntry --><!-- InstanceEndRepeat --> </div>
+	</div>
+	<!--END OF ROW --> 
+</div>
+<!--END OF CONTENT CONTAINER -->
+
+<?php include 'includes/footer-inc.html';?>
+
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) --> 
+<script src="js/jquery-1.11.2.min.js"></script> 
+
+<!-- Include all compiled plugins (below), or include individual files as needed --> 
+<script src="js/bootstrap.js"></script> 
+<!-- custom js --> 
+<script src="js/custom.js"></script> 
+<!-- Google Tracking  --> 
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-69373425-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
+</body>
+<!-- InstanceEnd --></html>
