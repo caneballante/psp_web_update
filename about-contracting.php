6b987365365de18778551f33dc110462aff54904
jon bridgman <jonbridgman@gmail.com>
2023-02-06 09:34:13 -0800

    funding

---
+++ about-contracting.php
@@ -0,0 +1,129 @@
+<!DOCTYPE html>
+<html lang="en"><!-- InstanceBegin template="/Templates/page-4-template.dwt" codeOutsideHTMLIsLocked="false" -->
+<head>
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-BGEZ5L3EJY"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-BGEZ5L3EJY');
+</script>	
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<!-- InstanceBeginEditable name="doctitle" -->
+<title>Puget Sound Partnership - Funding Opportunities</title>
+<!-- InstanceEndEditable -->
+<!-- Bootstrap -->
+<link href="css/bootstrap.css" rel="stylesheet">
+<link href="css/custom.css" rel="stylesheet" type="text/css">
+<link href="css/custom-erika.css" rel="stylesheet" type="text/css">
+<!-- loads the wf-loading class right away to minimize FOUT -->
+<script>document.documentElement.className += ' wf-loading';</script>
+<!-- Font PRENTON TYPEKIT -->
+<script src="https://use.typekit.net/srt5jze.js"></script>
+<script>try{Typekit.load({ async: true });}catch(e){}</script>
+<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+<!--[if lt IE 9]>
+	<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+	<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+<!-- InstanceBeginEditable name="head" -->
+		<script>
+/*this variable is used to set the proper nav to active. It should to the order the nav item is in the list*/
+  	navSelected = 2;
+</script> 
+<!-- InstanceEndEditable -->
+<!-- InstanceParam name="OptionalRegion1" type="boolean" value="true" -->
+<!-- InstanceParam name="OptionalRegion2" type="boolean" value="false" -->
+<!-- InstanceParam name="OptionalRegion3" type="boolean" value="true" -->
+<!-- InstanceParam name="OptionalRegion4" type="boolean" value="false" -->
+<!-- InstanceParam name="OptionalRegion5" type="boolean" value="true" -->
+<!-- InstanceParam name="OptionalRegion6" type="boolean" value="true" -->
+<!-- InstanceParam name="OptionalRegion7" type="boolean" value="false" -->
+<!-- InstanceParam name="OptionalRegion8" type="boolean" value="false" -->
+</head>
+<body>
+<?php include 'includes/modal-inc.html';?>
+<!-- START IMAGE HEADER --> 
+ <!-- InstanceBeginEditable name="overviewphoto" -->
+<header class="overview-page-image-style overview-page-image16">
+
+<div class="overview-quote pull-right"><h3>&nbsp;</h3></div>
+
+ </header>
+<!-- InstanceEndEditable -->  
+<div class="container-fluid page-content padding-50-bottom">
+	<div class="row">
+		<div class="col-sm-3 padding-20-top"></div>
+		<div class="col-sm-7 padding-20-top"> <!-- InstanceBeginEditable name="6col_header" -->
+			<h1>CONTRACTING WITH THE PARTNERSHIP</h1>
+			<!-- InstanceEndEditable --></div>
+		<div class="col-sm-2 padding-20-top"></div>
+	</div>
+	
+	<div class="row"> 
+		<div class="col-sm-3">
+			<div class="nav-leftside-custom">
+				<ul class="nav nav-stacked nav-pills nav-leftside-custom padding-left-0 margin-10-top">
+					<!-- InstanceBeginEditable name="left_nav" -->
+				
+				<?php include 'includes/ln-funding.html';?>	
+			
+		
+			<!-- InstanceEndEditable -->
+				</ul>
+			</div>
+		</div>
+		 
+		<div class="col-sm-7 padding-20-top content-column"> <!-- InstanceBeginEditable name="6col_content" -->
+			
+
+
+<p>The Partnership works collaboratively with all levels of government, tribes, businesses and citizen groups in its charge to lead and coordinate efforts to protect and restore Puget Sound. Our goal is to make Puget Sound healthy again, and create a roadmap for how to get it done. In order to help achieve this goal, we fund partner organizations through contracts and grants. <a href="https://omwbe.wa.gov/" target="_blank">The Washington State Office of Minority and Women's Business Enterprises (OMWBE)</a> is charged with certifying small, minority and women-owned businesses to facilitate their participation in public contracting and procurement. If you have a small, minority or women-owned business and would like to apply for state or federal certification, <a href="https://omwbe.wa.gov/" target="_blank">please see OMWBE's site.</a><br /></p>
+            
+
+			
+ 
+			 
+		<!-- InstanceEndEditable --> </div>
+		
+		<div class="col-sm-2 padding-20-top padding-0-right"> <!-- InstanceBeginRepeat name="right_nav_repeat" --><!-- InstanceBeginRepeatEntry --> <!-- InstanceBeginEditable name="right_nav_title" -->
+			<div class="right-nav-title margin-0-top ">SEE ALSO</div>
+			<!-- InstanceEndEditable -->
+			<ul class="nav-rightside-custom">
+				<!-- InstanceBeginEditable name="right_nav_links" -->
+			<a href="https://app.powerbigov.us/view?r=eyJrIjoiNjE3ZGJlYTktMGFkYy00OWNlLTgyYWUtYjMyMDBkNWM0ZDE5IiwidCI6IjExZDBlMjE3LTI2NGUtNDAwYS04YmEwLTU3ZGNjMTI3ZDcyZCJ9&pageName=ReportSection4135b7dbe5123b276369">Bipartisan Infrastructure Law (BIL) funding opportunities for Puget Sound recovery</a>
+				<!-- InstanceEndEditable -->
+			</ul>
+			<!-- InstanceEndRepeatEntry --><!-- InstanceEndRepeat --> </div>
+	</div>
+	<!--END OF ROW --> 
+</div>
+<!--END OF CONTENT CONTAINER -->
+
+<?php include 'includes/footer-inc.html';?>
+
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) --> 
+<script src="js/jquery-1.11.2.min.js"></script> 
+
+<!-- Include all compiled plugins (below), or include individual files as needed --> 
+<script src="js/bootstrap.js"></script> 
+<!-- custom js --> 
+<script src="js/custom.js"></script> 
+<!-- Google Tracking  --> 
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-69373425-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
+</body>
+<!-- InstanceEnd --></html>
---
+++ funding-test.php
@@ -0,0 +1,211 @@
+<!DOCTYPE html>
+<html lang="en"><!-- InstanceBegin template="/Templates/page-4-template.dwt" codeOutsideHTMLIsLocked="false" -->
+<head>
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-BGEZ5L3EJY"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-BGEZ5L3EJY');
+</script>	
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<!-- InstanceBeginEditable name="doctitle" -->
+<title>Puget Sound Partnership - Funding Opportunities</title>
+<!-- InstanceEndEditable -->
+<!-- Bootstrap -->
+<link href="css/bootstrap.css" rel="stylesheet">
+<link href="css/custom.css" rel="stylesheet" type="text/css">
+<link href="css/custom-erika.css" rel="stylesheet" type="text/css">
+<!-- loads the wf-loading class right away to minimize FOUT -->
+<script>document.documentElement.className += ' wf-loading';</script>
+<!-- Font PRENTON TYPEKIT -->
+<script src="https://use.typekit.net/srt5jze.js"></script>
+<script>try{Typekit.load({ async: true });}catch(e){}</script>
+<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+<!--[if lt IE 9]>
+	<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+	<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+<!-- InstanceBeginEditable name="head" -->
+	<script>
+/*this variable is used to set the proper nav to active. It should to the order the nav item is in the list*/
+  	navSelected = 1;
+</script> 
+<!-- InstanceEndEditable -->
+<!-- InstanceParam name="OptionalRegion1" type="boolean" value="true" -->
+<!-- InstanceParam name="OptionalRegion2" type="boolean" value="false" -->
+<!-- InstanceParam name="OptionalRegion3" type="boolean" value="true" -->
+<!-- InstanceParam name="OptionalRegion4" type="boolean" value="false" -->
+<!-- InstanceParam name="OptionalRegion5" type="boolean" value="true" -->
+<!-- InstanceParam name="OptionalRegion6" type="boolean" value="true" -->
+<!-- InstanceParam name="OptionalRegion7" type="boolean" value="false" -->
+<!-- InstanceParam name="OptionalRegion8" type="boolean" value="false" -->
+</head>
+<body>
+<?php include 'includes/modal-inc.html';?>
+<!-- START IMAGE HEADER --> 
+ <!-- InstanceBeginEditable name="overviewphoto" -->
+<header class="overview-page-image-style overview-page-image16">
+
+<div class="overview-quote pull-right"><h3>&nbsp;</h3></div>
+
+ </header>
+<!-- InstanceEndEditable -->  
+<div class="container-fluid page-content padding-50-bottom">
+	<div class="row">
+		<div class="col-sm-3 padding-20-top"></div>
+		<div class="col-sm-7 padding-20-top"> <!-- InstanceBeginEditable name="6col_header" -->
+			<h1> Funding opportunities</h1>
+			<!-- InstanceEndEditable --></div>
+		<div class="col-sm-2 padding-20-top"></div>
+	</div>
+	
+	<div class="row"> 
+		<div class="col-sm-3">
+			<div class="nav-leftside-custom">
+				<ul class="nav nav-stacked nav-pills nav-leftside-custom padding-left-0 margin-10-top">
+					<!-- InstanceBeginEditable name="left_nav" -->
+				
+				<?php include 'includes/ln-funding.html';?>	
+			
+		
+			<!-- InstanceEndEditable -->
+				</ul>
+			</div>
+		</div>
+		 
+		<div class="col-sm-7 padding-20-top content-column"> <!-- InstanceBeginEditable name="6col_content" -->		
+			
+			
+<h2 class="margin-0-top">1/23/2023 - Monitoring to Accelerate Recovery (PSEMP and Vital Signs affiliation)</h2>
+	<p><strong>Timeline:</strong> Responses are due by 11:59 pm on April 14, 2023. </p>
+	
+	<p><strong>Funding amount:</strong> Approximately $750,000 will be allocated for selected projects with a minimum of $50,000 and a maximum of $120,000 total compensation payable per project. We anticipate supporting up to 10 projects in total for the period of July 1, 2023, to June 30, 2025. </p>
+			
+	<p><strong>Informational webinar:</strong> Thursday, February 23, 2023, 1:00 pm to 2:00 pm PST. <a href="https://us06web.zoom.us/meeting/register/tZIsdOqoqDIqG9bvcLUxuNZRlXlA394Y2WTq" target="new">Registration is required</a></p>
+			<p><strong>Details: </strong>The Partnership is pleased to announce a <a href="https://pspwa.box.com/s/jq3z2x11d9uvdthg4jiu0std4uw4qz05" target="new">Request for Information</a> (RFI) for collaborative monitoring-related projects to address priority information needs to support and accelerate the recovery of the Puget Sound ecosystem. </p>
+	
+	<p>Specifically, the Partnership seeks proposals that fall under one of the following two categories:  </p>
+		<ul class="bullet-size-fix">
+			<li>Category 1 – Developing and first-time reporting of Vital Sign Indicators, and  </li>
+			<li>Category 2 - Addressing questions to inform and assess progress toward desired outcomes and statutory goals for Puget Sound recovery.  </li>
+		</ul>
+	
+	<p>All projects are expected to support the objectives of the Puget Sound Ecosystem Monitoring Program (PSEMP) strategic plan to increase collaboration, support adaptive management of Puget Sound recovery efforts, and improve communication to improve access to credible information to guide recovery decisions.</p>
+	
+	<p>As an implementing agency of Washington State’s Healthy Environment for All (HEAL) Act, the Partnership encourages respondents to submit proposals that create environmental benefits for overburdened and vulnerable populations, build collaborations with community scientists, and provide opportunities for tribes, vulnerable populations, and overburdened communities to participate. </p>
+	
+	<br><br>
+	
+<h2>1/23/2023 - Puget Sound Salmon Science Investigations</h2>
+	<p><strong>Timeline: </strong>Open from December 22, 2022 until March 13, 2023. Awards will be announced in April for work anticipated to begin in July 2023 and be completed by June 30, 2025.</p>
+	<p><strong>Funding amount: </strong>Up to $2 million will be awarded for projects up to $400,000 each.</p>
+	<p><strong>Details: </strong>The Partnership and Salmon Science Advisory Group are pleased to announce a <a href="https://pspwa.box.com/s/dmbng1beev0iiowfhrw3fdi91uj9v6vf" target="new">Request for Information</a> for collaborative proposals to conduct salmon scientific investigations. Investigations could include but are not limited to modeling, research, and syntheses to address emerging issues of concern, factors limiting recovery, and other critical science and monitoring questions to advance Puget Sound salmon recovery. Outcomes of the proposed investigations should support ongoing recovery planning and adaptive management efforts to ensure self-sustaining, harvestable, salmon runs that sustain people and orcas in Puget Sound. Proposed investigations are encouraged to address environmental justice, tribal needs, and treaty rights in relation to salmon recovery.</p>
+	
+	<p>Specifically, the Partnership is seeking project proposals that address one or more priority information needs: </p>
+			<ol class="bullet-size-fix">
+				<li>Relationships between salmon populations, salmon habitats, and recovery actions;</li>
+				<li>Marine survival and marine food webs;</li>
+				<li>Salmon production potential across landscapes; and</li>
+				<li>Human dimensions of salmon recovery and Indigenous knowledge.</li>
+			</ol>
+			
+	<p>&nbsp;</p>
+			
+<h2>Upcoming Funding Overview</h2>
+			
+<h3>Puget Sound Scientific Research (Science Panel Affiliation)</h3>
+	<ul class="bullet-size-fix">
+	<li><strong>Funding content focus: </strong>Specific content is still under discussion, but will rely on the <a href="https://pspwa.box.com/s/e81y0ap941ntik8o0me8o1lo6v12act1">2020-2024 Science Work Plan</a> to inform priority areas of investigation.</li>
+	<li><strong>Estimated or determined timeline: </strong>Estimated release of RFP is early 2023 with awards granted in the spring and work to begin July 1, 2023.   </li>
+	<li><strong>Estimated or determined funding amount: </strong>Total award amount is still uncertain. In 2021, six projects identified through a Partnership solicitation and nine projects selected by Washington Sea Grant were awarded a total of nearly $1.7 million. </li>
+	<li><strong>Additional background information: </strong>Two-year awards from July 1, 2023-June 30, 2025.  To learn more about 2019-2021 projects, please see <a href="https://pspwa.box.com/s/jin0hqljzm6n5wvocm5xaxy7dcw0117p">this Science Panel Memo</a>; to learn about the previous solicitation and evaluation process and awarded projects in 2021, please read <a href="https://pspwa.box.com/s/3jtyia0fdp7d725y9eshi3yfsjtzfgn8">this memo</a> or <a href="https://pspwa.box.com/s/buf4pf2pge4b2lg9zadplxq2q0rfp79d">this Science Panel memo</a>; October 2022 thinking about the 2023-2025 solicitation can be found <a href="https://pspwa.box.com/s/3pds75au1trckastvi0r60let8du9944">here</a>.</li>
+	</ul>
+			
+			
+<h3>Strategic Initiative Leads Requests for Proposals (Strategic Initiative Leads Affiliation)</h3>
+	<ul class="bullet-size-fix">
+	<li><strong>Funding content focus: </strong>These RFPs will solicit for proposals to advance our investment priorities, described in the joint SILs Investment Plan for FFY2021-2022, that were designed to protect and restore habitat, water quality, and harvestable shellfish beds and to result in a more resilient, equitable, and sustainable future for Puget Sound communities and ecosystems.</li>
+	<li><strong>Estimated or determined timeline: </strong>There are three separate timelines depending on the SIL of interest.</li>
+		<ul class="bullet-size-fix">
+			<li><strong>Shellfish SIL RFP: </strong>Coming winter 2023 for proposals supporting Investment Priority D to integrate shellfish bed classification priorities in wastewater facility function, planning, and/or upgrades.</li>
+			<li><strong>Stormwater SIL RFP: </strong>Multiple grant opportunities coming spring 2023 including Chemical Action Plan Implementation, Research for Chemicals of Emerging Concern, and Exploring a Toxics and Stormwater Infrastructure Fund.  </li>
+			
+		</ul>
+		<li><strong>Estimated or determined funding amount:</strong></li>
+		<ul class="bullet-size-fix">
+			<li><strong>Shellfish SIL RFP: </strong>Up to $1 million for proposals between $100,000 to $700,000 for up to 4 years. </li>
+			<li><strong>Stormwater SIL RFP: </strong></li>
+				<ul class="bullet-size-fix">
+					<li>Up to $1.5 million for proposals up to $500,00 for Chemical Action Plan Implementation </li>
+					<li>Up to $1 million for proposals between $200,000 and $750,000 for Research for Chemicals of Emerging Concern</li>
+					<li>Up to $300,000 for Explore a Toxics and Stormwater Infrastructure Fund</li>
+				</ul>
+			
+		</ul>
+	<li><strong>Additional background information: </strong>More information about the funding is available on their <a href="https://pugetsoundestuary.wa.gov/rfp/">website</a>.</li>
+	</ul>
+
+<h3>Washington Sea Grant (WSG) 2023 Request for Proposals</h3>
+	<ul class="bullet-size-fix">
+	<li><strong>Funding content focus: </strong>Funded projects will contribute to WSG and state priority information needs by advancing knowledge in one of the four WSG focus areas: environmental literacy and workforce development, resilient communities and economies, healthy coastal ecosystems, and sustainable fisheries and aquaculture. </li>
+	<li><strong>Estimated or determined timeline: </strong>The request for proposals will be released in early January 2023. Preliminary proposals are due February 16, 2023 and full proposals are due May 4, 2023. </li>
+	<li><strong>Estimated or determined funding amount: </strong>Approximately $2 million will be awarded during the two-year funding cycle.  </li>
+	<li><strong>Additional background information: </strong>More information can be found on the <a href="https://wsg.washington.edu/funding/apply-for-funding/" target="new">WSG website.</a></li>
+	</ul>
+			
+<h3>Salmon Recovery Funding Board (SRFB) Monitoring Program (SRFB, Lead Entities, and Watersheds Affiliation)</h3>
+	<ul class="bullet-size-fix">
+	<li><strong>Funding content focus: </strong>Regional monitoring projects must address high priority information needs or data gaps identified within a recovery plan; associated regional research, monitoring, and evaluation plan; or lead entity strategy. Regional monitoring projects should complement, enhance, or leverage ongoing monitoring efforts. Regional monitoring projects must be consistent or compatible with data collection, analysis, and management methods and protocols being used in the region, and shall, to the maximum extent practicable, be consistent or compatible with methods and protocols in common use throughout the state.</li>
+	<li><strong>Estimated or determined timeline: </strong>Applications are through the local lead entity and will follow that specific timeline. Applications are as early as January 2022 in some watersheds and as late as May in others. Funding approval will be by SRFB September 2023.</li>
+	<li><strong>Estimated or determined funding amount: </strong>Funding amounts are to be determined but previously have been $350,000 for the state.  </li>
+	<li><strong>Additional background information: </strong>More information can be found in the <a href="https://rco.wa.gov/wp-content/uploads/2019/05/SAL-Manual18.pdf" target="new">Recreation and Conservation Office’s Manual 18.</a></li>
+	</ul>
+			
+	<p class="last-update">Last updated: 02/1/23</p>
+
+ 
+			 
+		<!-- InstanceEndEditable --> </div>
+		
+		<div class="col-sm-2 padding-20-top padding-0-right"> <!-- InstanceBeginRepeat name="right_nav_repeat" --><!-- InstanceBeginRepeatEntry --> <!-- InstanceBeginEditable name="right_nav_title" -->
+			<div class="right-nav-title margin-0-top ">SEE ALSO</div>
+			<!-- InstanceEndEditable -->
+			<ul class="nav-rightside-custom">
+				<!-- InstanceBeginEditable name="right_nav_links" -->
+			<a href="https://app.powerbigov.us/view?r=eyJrIjoiNjE3ZGJlYTktMGFkYy00OWNlLTgyYWUtYjMyMDBkNWM0ZDE5IiwidCI6IjExZDBlMjE3LTI2NGUtNDAwYS04YmEwLTU3ZGNjMTI3ZDcyZCJ9&pageName=ReportSection4135b7dbe5123b276369">Bipartisan Infrastructure Law (BIL) funding opportunities for Puget Sound recovery</a>
+				<!-- InstanceEndEditable -->
+			</ul>
+			<!-- InstanceEndRepeatEntry --><!-- InstanceEndRepeat --> </div>
+	</div>
+	<!--END OF ROW --> 
+</div>
+<!--END OF CONTENT CONTAINER -->
+
+<?php include 'includes/footer-inc.html';?>
+
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) --> 
+<script src="js/jquery-1.11.2.min.js"></script> 
+
+<!-- Include all compiled plugins (below), or include individual files as needed --> 
+<script src="js/bootstrap.js"></script> 
+<!-- custom js --> 
+<script src="js/custom.js"></script> 
+<!-- Google Tracking  --> 
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-69373425-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
+</body>
+<!-- InstanceEnd --></html>
