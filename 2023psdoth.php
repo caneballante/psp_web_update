8ebeba1c62cb540b0802158c9dd8e4519c11f1aa
jon bridgman <jonbridgman@gmail.com>
2023-03-30 13:45:05 -0700

    2023 PSDOTH

---
+++ 2023psdoth.php
@@ -0,0 +1,143 @@
+<!DOCTYPE html>
+<html lang="en"><!-- InstanceBegin template="/Templates/page-4-template.dwt" codeOutsideHTMLIsLocked="false" -->
+<head>
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-BGEZ5L3EJY"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-BGEZ5L3EJY');
+</script>	
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<!-- InstanceBeginEditable name="doctitle" -->
+<title>2023 Puget Sound Partnership</title>
+<!-- InstanceEndEditable -->
+<!-- Bootstrap -->
+<link href="css/bootstrap.css" rel="stylesheet">
+<link href="css/custom.css" rel="stylesheet" type="text/css">
+<link href="css/custom-erika.css" rel="stylesheet" type="text/css">
+<!-- loads the wf-loading class right away to minimize FOUT -->
+<script>document.documentElement.className += ' wf-loading';</script>
+<!-- Font PRENTON TYPEKIT -->
+<script src="https://use.typekit.net/srt5jze.js"></script>
+<script>try{Typekit.load({ async: true });}catch(e){}</script>
+<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+<!--[if lt IE 9]>
+	<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+	<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+<!-- InstanceBeginEditable name="head" -->
+<!-- InstanceEndEditable -->
+<!-- InstanceParam name="OptionalRegion1" type="boolean" value="true" -->
+<!-- InstanceParam name="OptionalRegion2" type="boolean" value="false" -->
+<!-- InstanceParam name="OptionalRegion3" type="boolean" value="true" -->
+<!-- InstanceParam name="OptionalRegion4" type="boolean" value="false" -->
+<!-- InstanceParam name="OptionalRegion5" type="boolean" value="true" -->
+<!-- InstanceParam name="OptionalRegion6" type="boolean" value="true" -->
+<!-- InstanceParam name="OptionalRegion7" type="boolean" value="false" -->
+<!-- InstanceParam name="OptionalRegion8" type="boolean" value="false" -->
+</head>
+<body>
+<?php include 'includes/modal-inc.html';?>
+<!-- START IMAGE HEADER --> 
+ <!-- InstanceBeginEditable name="overviewphoto" -->
+<header class="overview-page-image-style overview-page-image120">
+
+<div class="overview-quote pull-right"><h3>&nbsp;</h3></div>
+
+ </header>
+<!-- InstanceEndEditable -->  
+<div class="container-fluid page-content padding-50-bottom">
+	<div class="row">
+		<div class="col-sm-3 padding-20-top"></div>
+		<div class="col-sm-7 padding-20-top"> <!-- InstanceBeginEditable name="6col_header" -->
+			<h1>2023 Puget Sound Day on the HIll</h1>
+			<!-- InstanceEndEditable --></div>
+		<div class="col-sm-2 padding-20-top"></div>
+	</div>
+	
+	<div class="row"> 
+		<div class="col-sm-3">
+			<div class="nav-leftside-custom">
+				<ul class="nav nav-stacked nav-pills nav-leftside-custom padding-left-0 margin-10-top">
+					<!-- InstanceBeginEditable name="left_nav" -->
+					<!--<?php include 'includes/ln-HEAL.html';?>-->
+			
+		
+			<!-- InstanceEndEditable -->
+				</ul>
+			</div>
+		</div>
+		 
+		<div class="col-sm-7 padding-20-top content-column"> <!-- InstanceBeginEditable name="6col_content" -->
+			<h2 class="margin-0-top">Raising awareness to recover Puget Sound!</h2>
+			<p>Dates: May 8-11, 2023</p>
+			<p>Location: Washington, D.C.</p>
+			<p>Join us! Puget Sound Day on the Hill raises awareness about the need for sustained federal investment in Puget Sound restoration and salmon recovery. Representatives from tribes, state agencies, local governments, nonprofits, and businesses, along with concerned residents, lend their voices in support of action to save Puget Sound and uphold tribal treaty rights.</p>
+			<p><a href="https://docs.google.com/forms/d/e/1FAIpQLSf3wmHgEdvBUcLv8RGOphoB5GR8q6PX4WxgNA0e5zOyYVJvKg/viewform" target="new" class="btn btn-default btn-lg" role="button">Register Now</a></p><br>
+
+			<p>We are asking individuals, governments, and organizations to demonstrate their support by signing on to our federal appropriations requests. To be included as an official signatory to any of the letters, please complete this form by 5pm, Friday March 24th</p>
+			<p><a href="https://docs.google.com/forms/d/e/1FAIpQLSeEpwjE5cdm6Tz6dgwgofErozrjForaV76mWobh_4djU_D3rA/viewform" target="new" class="btn btn-default btn-lg" role="button">Sign the Letter</a></p><br>
+
+			<p>Need some guidance with travel arrangements? Don't miss this chance to make your voice heard and connect with other leaders in the Puget Sound community. Click here to make travel arrangements and secure your spot at Puget Sound Day on the Hill!</p><br>
+
+			<p><a href="https://pspwa.box.com/shared/static/r6ptvhhm1zg0db97ijzej9bj7lw8c023.pdf" target="new" class="btn btn-default btn-lg" role="button">Click to download the latest date travel information</a></p>
+			<p class="last-update">Last updated: 3/17/23</p>
+			
+		<!-- InstanceEndEditable --> </div>
+		
+		<div class="col-sm-2 padding-20-top padding-0-right"> <!-- InstanceBeginRepeat name="right_nav_repeat" --><!-- InstanceBeginRepeatEntry --> <!-- InstanceBeginEditable name="right_nav_title" -->
+			
+			<!-- InstanceEndEditable -->
+			<ul class="nav-rightside-custom">
+				<!-- InstanceBeginEditable name="right_nav_links" -->
+				
+				
+				<!-- InstanceEndEditable -->
+			</ul>
+			<!-- InstanceEndRepeatEntry --><!-- InstanceBeginRepeatEntry --> <!-- InstanceBeginEditable name="right_nav_title" -->
+			<div class="right-nav-title margin-0-top ">For more information:</div>
+			<!-- InstanceEndEditable -->
+			<ul class="nav-rightside-custom">
+				<!-- InstanceBeginEditable name="right_nav_links" -->
+				<p> <strong>Ahren Stroming</strong> <br>
+					Special Assistant for Federal Affairs<br>
+					360.918.1337   	    <a href="mailto:ahren.stroming@psp.wa.gov">ahren.stroming@psp.wa.gov</a><br>
+				</p>
+
+
+				<!-- InstanceEndEditable -->
+			</ul>
+			<!-- InstanceEndRepeatEntry --><!-- InstanceEndRepeat --> </div>
+	</div>
+	<!--END OF ROW --> 
+</div>
+<!--END OF CONTENT CONTAINER -->
+
+<?php include 'includes/footer-inc.html';?>
+
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) --> 
+<script src="js/jquery-1.11.2.min.js"></script> 
+
+<!-- Include all compiled plugins (below), or include individual files as needed --> 
+<script src="js/bootstrap.js"></script> 
+<!-- custom js --> 
+<script src="js/custom.js"></script> 
+<!-- Google Tracking  --> 
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-69373425-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
+</body>
+<!-- InstanceEnd --></html>
