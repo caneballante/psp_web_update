fb5c4cee8f71cc4d302ef4df994f657e4d1329d2
kevinhydepsp <58960211+kevinhydepsp@users.noreply.github.com>
2023-05-10 11:30:19 -0700

    Added link to sign up for legislative updates

--- legislative_updates.php
+++ legislative_updates.php
@@ -243,6 +243,7 @@
 					Legislative Policy Director <br>
 					<a href="mailto:don.gourlie@psp.wa.gov">don.gourlie@psp.wa.gov</a><br>
 					360.688.3253<br><br>
+						<a href="https://public.govdelivery.com/accounts/WAPSP/subscriber/new?topic_id=WAPSP_27" target="new">To sign up for future Puget Sound legislative updates, click here.</a><br><br>
 				<a href="https://app.smartsheet.com/b/form/ab94add956f34fbdaa9114193168738e" target="new">Click here</a> to provide feedback on the content and functionality of this page.</p>
 				
 				
