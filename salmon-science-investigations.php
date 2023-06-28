e37279793ce4065f8ef32f7e28ce40e1691a7575
kevinhydepsp <58960211+kevinhydepsp@users.noreply.github.com>
2023-06-28 12:01:14 -0700

    Created page and modified it

---
+++ salmon-science-investigations.php
@@ -0,0 +1,235 @@
+<!DOCTYPE html>
+<html lang="en"><!-- InstanceBegin template="/Templates/page-4-template.dwt" codeOutsideHTMLIsLocked="false" -->
+<head>
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-BGEZ5L3EJY"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-BGEZ5L3EJY');
+</script>	
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<!-- InstanceBeginEditable name="doctitle" -->
+<title>Salmon Science Investigations</title>
+<!-- InstanceEndEditable -->
+<!-- Bootstrap -->
+<link href="css/bootstrap.css" rel="stylesheet">
+<link href="css/custom.css" rel="stylesheet" type="text/css">
+<link href="css/custom-erika.css" rel="stylesheet" type="text/css">
+<!-- loads the wf-loading class right away to minimize FOUT -->
+<script>document.documentElement.className += ' wf-loading';</script>
+<!-- Font PRENTON TYPEKIT -->
+<script src="https://use.typekit.net/srt5jze.js"></script>
+<script>try{Typekit.load({ async: true });}catch(e){}</script>
+<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+<!--[if lt IE 9]>
+	<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+	<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+<!-- InstanceBeginEditable name="head" -->
+<script>
+/*this variable is used to set the proper nav to active. It should to the order the nav item is in the list*/
+  	navSelected = 1;
+</script> 
+<!-- InstanceEndEditable -->
+<!-- InstanceParam name="OptionalRegion1" type="boolean" value="true" -->
+<!-- InstanceParam name="OptionalRegion2" type="boolean" value="false" -->
+<!-- InstanceParam name="OptionalRegion3" type="boolean" value="true" -->
+<!-- InstanceParam name="OptionalRegion4" type="boolean" value="false" -->
+<!-- InstanceParam name="OptionalRegion5" type="boolean" value="true" -->
+<!-- InstanceParam name="OptionalRegion6" type="boolean" value="true" -->
+<!-- InstanceParam name="OptionalRegion7" type="boolean" value="false" -->
+<!-- InstanceParam name="OptionalRegion8" type="boolean" value="false" -->
+</head>
+<body>
+<?php include 'includes/modal-inc.html';?>
+<!-- START IMAGE HEADER --> 
+ <!-- InstanceBeginEditable name="overviewphoto" -->
+<header class="overview-page-image-style overview-page-image109">
+
+<div class="overview-quote pull-right"></div>
+
+ </header>
+<!-- InstanceEndEditable -->  
+<div class="container-fluid page-content padding-50-bottom">
+	<div class="row">
+		<div class="col-sm-3 padding-20-top"></div>
+		<div class="col-sm-7 padding-20-top"> <!-- InstanceBeginEditable name="6col_header" -->
+			<h1>Salmon Science Investigations</h1>
+			<p>&nbsp;</p>
+			<!-- InstanceEndEditable --></div>
+		<div class="col-sm-2 padding-20-top"></div>
+	</div>
+	
+	<div class="row"> 
+		<div class="col-sm-3">
+			<div class="nav-leftside-custom">
+				<ul class="nav nav-stacked nav-pills nav-leftside-custom padding-left-0 margin-10-top">
+					<!-- InstanceBeginEditable name="left_nav" -->
+			<?php include 'includes/ln-salmon.html';?>	
+			<!-- InstanceEndEditable -->
+				</ul>
+			</div>
+		</div>
+		 
+		<div class="col-sm-7 padding-20-top content-column"> <!-- InstanceBeginEditable name="6col_content" -->
+		<h2 class="margin-0-top">Purpose</h2>
+		
+		<p>The Partnership funds salmon science investigations each biennium that will advance Puget Sound salmon recovery. Funds are awarded through a competitive solicitation process in the form of a Request for Information (RFI). The RFI is developed in collaboration with the Salmon Science Advisory Group to address priority salmon science needs for the region.</p>
+		<p>Investigations can include but are not limited to modeling, research, and syntheses to address emerging issues of concern, factors limiting recovery, and other critical science and monitoring questions to address knowledge gaps and critical uncertainties in salmon recovery. Outcomes of the proposed investigations should support ongoing recovery planning and adaptive management efforts to ensure self-sustaining, harvestable, salmon runs that sustain people and orcas in Puget Sound. Proposed investigations are strongly encouraged to address environmental justice, tribal priorities, and treaty rights in relation to salmon recovery.</p>
+		
+		<h2>Projects funded in 2023-2025</h2>
+			
+			<p>For the 2023-2025 biennium, the Puget Sound Partnership requested proposals for projects that address priority information needs to advance regional salmon recovery under the Puget Sound Salmon Science Investigations RFI (RFI #2023-165). The RFI allocated up to $2,000,000 for proposals up to $400,000 each. </p>
+			<p>The following 6 proposals were selected for funding:</p><br>
+			
+			<table class="table-responsive table-condensed table-bordered">
+					<thead>
+						<tr>
+						<th>Project title</th>
+						<th>Description</th>
+						<th>Project leads and affiliations</th>
+						</tr>
+					</thead>
+					<tbody>
+					<tr>
+						<td><p class="psemp-mar-font">Modeling population-level impacts from Contaminants of Emerging Concern exposures to endangered salmonids</p></td>
+						<td><p class="psemp-mar-font">Contaminants of Emerging Concern (CECs) are a group of anthropogenic chemicals that occur in the environment, have the potential to cause harm to exposed organisms, and are poorly regulated. The objective of this proposed project is to develop and apply a modeling framework that will evaluate the population-level impacts of CEC exposures on Chinook salmon.  A population matrix model will be developed to analyze how exposures to CECs under different scenarios affects population outcomes and stable age distributions. The models will be applied to a highly impacted system, the Puyallup River, and a lightly impacted system, the Nisqually. </p></td>
+						<td><p class="psemp-mar-font">Andy James, University of Washington and Ruth Sofield, Western Washington University </p></td>
+						
+					</tr>
+					<tr>
+						<td><p class="psemp-mar-font">Impacts of spring storms on juvenile salmon health in an urbanized watershed</p></td>
+						<td><p class="psemp-mar-font">Adult coho salmon spawning in urban-impacted streams have been documented to die prematurely due to inputs of untreated roadway runoff and exposures to 6PPD-quinone - a product found in tires. However, the mortality of juvenile coho from exposures to runoff in streams has not been studied. This project includes field exposures, water quality sampling, and laboratory exposure studies to better understand the risks of runoff for juvenile coho, using Miller Creek as a representative system. The results will provide a first assessment of the acute risks of degraded water quality to juvenile coho in runoff-impacted streams and an understanding of the relative role of various tire- derived contaminants.</p></td>
+						<td><p class="psemp-mar-font">Jen McIntyre, Washington State University; John Hansen, U.S. Geological Survey; and Ed Kolodziej, University of Washington</p></td>
+						
+					</tr>
+					<tr>
+						<td><p class="psemp-mar-font">Evaluation of experimental hatchery rearing strategies to benefit Chinook salmon in Puget Sound</p></td>
+						<td><p class="psemp-mar-font">In Washington State, approximately 80% of returning adult Chinook are of hatchery origin. As a result, the near-term health of salmon-dependent ecosystems, fisheries, and Southern Resident Killer Whales rely on Puget Sound hatcheries. Building from Salish Sea Marine Survival Project findings, seven hatcheries in Puget Sound launched experiments to evaluate the impact of variable rearing and release strategies on size-at-age, age-at-maturity, and marine survival of Chinook salmon. The project is collecting and analyzing data from these ongoing experiments to lay the groundwork for a final synthesis report in 2027 that will include an evaluation of the various release strategies and guidance on optimizing hatchery management to increase Chinook abundance in Puget Sound.</p></td>
+						<td><p class="psemp-mar-font">Liz Duffy, Long Live the Kings</p></td>
+						
+					</tr>
+					<tr>
+						<td><p class="psemp-mar-font">Testing cumulative effects of restoration for multiple salmon populations in the Whidbey Basin using causal analysis</p></td>
+						<td><p class="psemp-mar-font">This project will use a causal analysis within a Cumulative Effects Evaluation framework to test the hypotheses that (i) cumulative restoration is increasing the availability and structure of intertidal habitats and (ii) juvenile Chinook salmon distributions from multiple river systems benefit from these actions. Multiple lines of evidence will be developed to evaluate the hypotheses including 1) qualitative findings of published literature, 2) quantitative analysis of published studies, 3) spatial analysis, 4) habitat change analysis, 5) statistical analysis of fish monitoring data, and 6) simulation modeling. This study will result in a deeper understanding of the benefits of tidal delta and nearshore restoration for juvenile salmon, where restoration will best benefit multiple populations, and how restoration efforts benefit population and ecosystem recovery in the Whidbey Basin.</p></td>
+						<td><p class="psemp-mar-font">Mike LeMoine, Skagit River System Cooperative</p></td>
+						
+					</tr>
+					<tr>
+						<td><p class="psemp-mar-font">Using bioenergetics and landscape connectivity to plan effective tidal delta restoration projects for Chinook salmon under a changing climate</p></td>
+						<td><p class="psemp-mar-font">This project will develop an analytical framework using hydraulic, landscape connectivity-abundance, and bioenergetic models to compare the abundance and growth potential of juvenile Chinook salmon across a suite of restoration and climate scenarios. The framework addresses two hypotheses: 1) Well placed restoration projects support increased juvenile Chinook salmon use beyond the intended project area by increasing connectivity among sites or creating areas with high growth potential; and 2) Warming water temperatures counteract restoration efforts by increasing the metabolic demand of juvenile Chinook salmon in estuarine deltas, which may be further impacted by poor project placement. Design plans, abundance estimates, and growth potential results will be used to evaluate which recovery actions provide cumulative, broad-scale restoration benefits and remain resilient to future climate change. </p></td>
+						<td><p class="psemp-mar-font">Emily Howe, The Nature Conservancy</p></td>
+						
+					</tr>
+					<tr>
+						<td><p class="psemp-mar-font">Investigation of sediment contaminant impacts on the growth and survival of juvenile Puget Sound Chinook salmon</p></td>
+						<td><p class="psemp-mar-font">To undergo the migration from freshwater to the ocean, salmon must successfully complete the stressful process of smoltification. However, juvenile salmon undergoing smoltification are faced with multiple stressors including exposure to legacy contaminants, contaminants of emerging concern, and altered prey availability. This work will first evaluate how Duwamish River sediment impacts the survival of field-collected polychaetes and amphipods, invertebrate prey species for juvenile Chinook. The invertebrate work will inform laboratory studies addressing how juvenile Chinook health is impacted by three key factors – contaminated prey, reduced food availability, and the physiological stress of smolting. The study results will inform the potential for environmental stressors and chemical exposures to limit the survival of outmigrant Chinook salmon.</p></td>
+						<td><p class="psemp-mar-font">John Stark, Washington State University</p></td>
+						
+					</tr>
+					</tbody>
+				</table><br>
+			
+					<p>These proposals were selected for funding following an evaluation process from technical evaluators and a review panel. We would like to thank our science colleagues from around the Salish Sea and Partnership staff who contributed their time and expertise as proposal evaluators, and the Salmon Science Advisory Group who contributed their regional perspectives as members of the review panel.</p>
+				
+					<p>We are excited to support this cohort of projects and look forward to working with the project teams in implementing their studies. Updates from project teams will also be shared at future PSEMP Salmonids Work Group meetings (PSEMP Salmonids Work Group (box.com).</p>
+			
+					<h2>Projects funded in 2022-2023</h2>
+			
+					<p>In summer 2022 the Partnership announced a Salmon Science RFI awarding up to $390,000 for projects up to $100,000 each. The projects funded in 2022 are listed below:</p><br>
+			
+					<table class="table-responsive table-condensed table-bordered">
+					<thead>
+						<tr>
+						<th>Project title</th>
+						<th>Description</th>
+						<th>Project leads and affiliations</th>
+						</tr>
+					</thead>
+					<tbody>
+					<tr>
+						<td><p class="psemp-mar-font">Evaluating early marine growth and size/condition as an indicator of overall survival in Puget Sound Chinook salmon</p></td>
+						<td><p class="psemp-mar-font">Documenting and quantifying relationships between early life history conditions, individual performance, and adult return rates would be advantageous for predicting how residence of juvenile salmon in Puget Sound’s changing ecosystem affects recovery trajectories and fisheries management. The project goal is to determine whether the link between early marine performance and marine survival is best indexed by individual growth or size, independently, or by assessing, size, growth, and the interactions between them to inform survival assessments. It will also evaluate the appropriate spatial and temporal resolution for potential marine survival indicators and ecosystem indicators related to growth.</p></td>
+						<td><p class="psemp-mar-font">Josh Chamberlin and Correigh Greene, National Oceanic and Atmospheric Administration</p></td>
+						
+					</tr>
+						<tr>
+						<td><p class="psemp-mar-font">Evaluating the effects of experimental hatchery management on hatchery Chinook salmon</p></td>
+						<td><p class="psemp-mar-font">The overarching goal of this project is to evaluate the effects of experimental hatchery rearing and release strategies on survival and size- and age-at-return of hatchery Chinook salmon. This research will include sampling, data analysis, experiment coordination efforts, and active communication of findings with regional resource managers through an initial synthesis report. This project is part of a larger, multi-year, collaborative study across tribal, state, and LLTK hatcheries. </p></td>
+						<td><p class="psemp-mar-font">Liz Duffy, Long Live the Kings</p></td>
+						
+					</tr>
+					<tr>
+						<td><p class="psemp-mar-font">Yearling Chinook salmon habitat use in the Skagit River</p></td>
+						<td><p class="psemp-mar-font">This project is analyzing six field seasons of observational data collected on the Skagit River to better understand the specific habitat use for parr and yearling migrant juvenile Chinook salmon. The field data collected for this study will provide insight and quantification of the relationships between stream type juvenile Chinook and habitat preferences during different seasons. This project will provide practical information about habitat use for yearling migrant Chinook salmon to improve and enhance restoration efforts in the Skagit River, and other watersheds with the stream type life history pattern.</p></td>
+						<td><p class="psemp-mar-font">Jen O’Neal, Natural Systems Design</p></td>
+						
+					</tr>
+					<tr>
+						<td><p class="psemp-mar-font">Investigation of sediment contaminant impacts on the growth and survival of juvenile Puget Sound Chinook salmon</p></td>
+						<td><p class="psemp-mar-font">The goal of this project is to conduct toxicity tests to determine if food restriction and exposure to a Duwamish Waterway sediment toxic chemical mixture that overlaps with smoltification will have a compounding stress impact on juvenile chinook salmon, reducing their overall health and possibly contributing to increased mortality. This research on the contribution of PCBs, PBDEs, and complex sediment chemical mixtures to estuarine-reared-fry growth, swimming performance, and health status will allow WRIA 9 to better evaluate the potential for contaminants to be limiting survival of outmigrant Chinook.</p></td>
+						<td><p class="psemp-mar-font">John Stark, Washington State University</p></td>					
+						
+					</tr>
+					
+					
+					</tbody>
+				</table><br>
+			
+					<p>For more information about the proposals or solicitation process, please contact Annelise Del Rio at annelise.delrio@psp.wa.gov.
+</p>
+			
+			<p class="last-update">Last updated: 01/05/23</p>
+		
+
+		<!-- InstanceEndEditable --> </div>
+		
+		<div class="col-sm-2 padding-20-top padding-0-right"> <!-- InstanceBeginRepeat name="right_nav_repeat" --><!-- InstanceBeginRepeatEntry --> <!-- InstanceBeginEditable name="right_nav_title" -->
+					<div class="right-nav-title">SEE ALSO</div>
+				<!-- InstanceEndEditable -->
+			<ul class="nav-rightside-custom">
+				<!-- InstanceBeginEditable name="right_nav_links" -->
+						<li role="presentation"><a href="SR_about.php">Salmon Recovery Council</a></li>
+				<li role="presentation"><a href="https://pspwa.box.com/shared/static/k0xpbegydhwww61vq3xzjc36y3fawfwx.pdf">Puget Sound Nearshore Chinook Salmon Strategies</a></li>
+				<li role="presentation"><a href="http://www.westcoast.fisheries.noaa.gov/protected_species/salmon_steelhead/recovery_planning_and_implementation/puget_sound/PS_steelhead_recovery_team.html">Puget Sound Steelhead Recovery Team</a></li>
+				<li role="presentation"><a href="https://pspwa.box.com/s/k50wb4xz16sbbzfjz8zcutfrqnfvhdc3">Lead Entity guidance on Chinook salmon and climate change</a></li>
+				<li role="presentation"><a href="https://pspwa.box.com/s/aikkubhd6at4eo7yibqdsmvdrwcsrr4z">Lead Entity guidance on Chinook salmon and climate change flow chart</a></li>
+				<li role="presentation"><a href="http://www.psp.wa.gov/shared-salmon-strategy/index.htm">Shared Salmon Strategy archived website</a></li>
+				
+				
+				<!-- InstanceEndEditable -->
+			</ul>
+			<!-- InstanceEndRepeatEntry --><!-- InstanceEndRepeat --> </div>
+	</div>
+	<!--END OF ROW --> 
+</div>
+<!--END OF CONTENT CONTAINER -->
+
+<?php include 'includes/footer-inc.html';?>
+
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) --> 
+<script src="js/jquery-1.11.2.min.js"></script> 
+
+<!-- Include all compiled plugins (below), or include individual files as needed --> 
+<script src="js/bootstrap.js"></script> 
+<!-- custom js --> 
+<script src="js/custom.js"></script> 
+<!-- Google Tracking  --> 
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-69373425-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
+</body>
+<!-- InstanceEnd --></html>
