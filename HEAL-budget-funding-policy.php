1bf1834dfcdd2e619de3ca07f174b577eaa3275d
kevinhydepsp <58960211+kevinhydepsp@users.noreply.github.com>
2023-06-29 08:32:35 -0700

    Added new page

---
+++ HEAL-budget-funding-policy.php
@@ -0,0 +1,169 @@
+<!DOCTYPE html>
+<html lang="en"><!-- InstanceBegin template="/Templates/page-4-template.dwt" codeOutsideHTMLIsLocked="false" -->
+<head>
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-BGEZ5L3EJY"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-BGEZ5L3EJY');
+</script>	
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<!-- InstanceBeginEditable name="doctitle" -->
+<title>Puget Sound Partnership - HEAL Environmental Justice Assessments</title>
+<!-- InstanceEndEditable -->
+<!-- Bootstrap -->
+<link href="css/bootstrap.css" rel="stylesheet">
+<link href="css/custom.css" rel="stylesheet" type="text/css">
+<link href="css/custom-erika.css" rel="stylesheet" type="text/css">
+<!-- loads the wf-loading class right away to minimize FOUT -->
+<script>document.documentElement.className += ' wf-loading';</script>
+<!-- Font PRENTON TYPEKIT -->
+<script src="https://use.typekit.net/srt5jze.js"></script>
+<script>try{Typekit.load({ async: true });}catch(e){}</script>
+<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+<!--[if lt IE 9]>
+	<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+	<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+<!-- InstanceBeginEditable name="head" -->
+<script>
+/*this variable is used to set the proper nav to active. It should to the order the nav item is in the list*/
+navSelected = 4;
+</script> 
+<!-- InstanceEndEditable -->
+<!-- InstanceParam name="OptionalRegion1" type="boolean" value="true" -->
+<!-- InstanceParam name="OptionalRegion2" type="boolean" value="false" -->
+<!-- InstanceParam name="OptionalRegion3" type="boolean" value="true" -->
+<!-- InstanceParam name="OptionalRegion4" type="boolean" value="false" -->
+<!-- InstanceParam name="OptionalRegion5" type="boolean" value="true" -->
+<!-- InstanceParam name="OptionalRegion6" type="boolean" value="true" -->
+<!-- InstanceParam name="OptionalRegion7" type="boolean" value="false" -->
+<!-- InstanceParam name="OptionalRegion8" type="boolean" value="false" -->
+</head>
+<body>
+<?php include 'includes/modal-inc.html';?>
+<!-- START IMAGE HEADER --> 
+ <!-- InstanceBeginEditable name="overviewphoto" -->
+<header class="overview-page-image-style overview-page-image120">
+
+<div class="overview-quote pull-right"><h3>&nbsp;</h3></div>
+
+ </header>
+<!-- InstanceEndEditable -->  
+<div class="container-fluid page-content padding-50-bottom">
+	<div class="row">
+		<div class="col-sm-3 padding-20-top"></div>
+		<div class="col-sm-7 padding-20-top"> <!-- InstanceBeginEditable name="6col_header" -->
+			<h1>Environmental Justice Assessments</h1>
+			<!-- InstanceEndEditable --></div>
+		<div class="col-sm-2 padding-20-top"></div>
+	</div>
+	
+	<div class="row"> 
+		<div class="col-sm-3">
+			<div class="nav-leftside-custom">
+				<ul class="nav nav-stacked nav-pills nav-leftside-custom padding-left-0 margin-10-top">
+					<!-- InstanceBeginEditable name="left_nav" -->
+					<?php include 'includes/ln-HEAL.html';?>
+			
+		
+			<!-- InstanceEndEditable -->
+				</ul>
+			</div>
+		</div>
+		 
+		<div class="col-sm-7 padding-20-top content-column"> <!-- InstanceBeginEditable name="6col_content" -->
+			
+			
+			<h2 class="margin-0-top">Public  Comment on Types of Significant Agency Actions for Environmental Justice  Assessments</h2>
+			<div class="floatright">
+				<div class="highlightbox margin-10-left">
+				<p><a href="https://youtu.be/mmicbUH07TQ"><img src="images/HEAL-sig-actions-thumb.jpg" width="200" height="111" alt=""/><br>
+				Presentation explaining the <br>
+				Partnership Significant Actions</a></p></div>
+			</div>
+			
+			<p><strong>Public comments will  be accepted until 11:59pm on June 15, 2023</strong></p>
+			<p>	Washington State&rsquo;s  statewide environmental justice law, the&nbsp;<a href="https://app.leg.wa.gov/RCW/default.aspx?cite=70A.02.005">Healthy Environment for All (HEAL) Act</a>&nbsp;instructs the Puget Sound Partnership (The Partnership) to  conduct environmental justice assessments when considering Significant Agency Actions.</p>
+			<p>Starting July 1, 2023,  we are required to complete an environmental justice assessment for the  following actions named in the law:</p>
+			<ul class="bullet-size-fix">
+				<li>Adopting or developing       significant legislative rules as defined in&nbsp;<a href="https://app.leg.wa.gov/RCW/default.aspx?cite=34.05.328">RCW 34.05.328</a>.</li>
+				<li>Adopting or developing new       grant or loan programs initiated after July 1, 2023.</li>
+				<li>Designing or awarding capital       projects, grants, or loans of $12 million or more.</li>
+				<li>Developing agency request       legislation.</li>
+				<li>Any other actions deemed       significant consistent with RCW&nbsp;<a href="http://app.leg.wa.gov/RCW/default.aspx?cite=70A.02.060">70A.02.060</a>. </li>
+			</ul>
+			<p>The Partnership can  identify additional types of significant agency actions, in addition to the  above, which will require environmental justice assessments starting July 1,  2025. We are beginning to identify actions that may benefit from an  environmental justice assessment and there will be additional opportunities for  the public to comment in the future. Our initial list of specific significant  agency actions include:</p>
+			<p>	Per <a href="https://gcc02.safelinks.protection.outlook.com/?url=https%3A%2F%2Fapp.leg.wa.gov%2FRCW%2Fdefault.aspx%3Fcite%3D70A.02.010&data=05%7C01%7Clea-anne.burke%40psp.wa.gov%7C1d86ba148c604c448ab408db53130195%7C11d0e217264e400a8ba057dcc127d72d%7C0%7C0%7C638195113315120507%7CUnknown%7CTWFpbGZsb3d8eyJWIjoiMC4wLjAwMDAiLCJQIjoiV2luMzIiLCJBTiI6Ik1haWwiLCJXVCI6Mn0%3D%7C3000%7C%7C%7C&sdata=b6pK6Z8jtPz0cfn1Mk84UsvwC4qs5Zzs23%2B8UqxSKhw%3D&reserved=0" target="_blank">RCW 70A.02.010 (12)</a> (c)</p>
+			<ul class="bullet-size-fix">
+				<li>Puget Sound Acquisition and Restoration biennial grant round</li>
+			</ul>
+			<p>Per <a href="https://gcc02.safelinks.protection.outlook.com/?url=https%3A%2F%2Fapp.leg.wa.gov%2FRCW%2Fdefault.aspx%3Fcite%3D70A.02.010&data=05%7C01%7Clea-anne.burke%40psp.wa.gov%7C1d86ba148c604c448ab408db53130195%7C11d0e217264e400a8ba057dcc127d72d%7C0%7C0%7C638195113315120507%7CUnknown%7CTWFpbGZsb3d8eyJWIjoiMC4wLjAwMDAiLCJQIjoiV2luMzIiLCJBTiI6Ik1haWwiLCJXVCI6Mn0%3D%7C3000%7C%7C%7C&sdata=b6pK6Z8jtPz0cfn1Mk84UsvwC4qs5Zzs23%2B8UqxSKhw%3D&reserved=0" target="_blank">RCW 70A.02.010 (12)</a> (e) and <a href="https://app.leg.wa.gov/RCW/default.aspx?cite=70A.02.060">RCW 70A.02.060</a></p>
+			<ul class="bullet-size-fix">
+				<li>Development of the 2025-2028 Science Work Plan</li>
+				<li>Development of the 2026-2030 Action  Agenda</li>
+			</ul>
+			<p><strong><em>We are seeking your comments  and feedback on the following:</em></strong></p>
+			<ul class="bullet-size-fix">
+				<li>The above list of significant agency actions.</li>
+				<li>Other types of Partnership actions you think would benefit from an assessment starting in 2025.</li>
+				<li>How the Partnership can best engage with communities impacted by significant agency actions.</li>
+				<li>How you would like to stay informed about environmental justice assessments.</li>
+			</ul>
+			<p>While this comment  period is open through June 15, 2023, to meet required deadlines, we invite you  to reach out to us at any time. We will use input to help inform decisions on  conducting assessments and how we communicate and engage.</p>
+			<p><strong>Submit written  comments to&nbsp;Melissa Schutten, Equity and Environmental Justice Manager at:  melissa.schutten@psp.wa.gov.</strong>&nbsp;A summary of comments and Partnership responses will be  posted to our Partnership webpage by June 30, 2023.</p>
+			
+			<h2>The HEAL Act</h2>
+			<p>The passage of the Healthy  Environment for All (HEAL) Act in 2021 is a historic step toward eliminating  environmental health disparities and more equitably distributing health and environmental  benefits among communities of color and low-income households.&nbsp;<strong>It is  the first law in Washington to create a coordinated state agency approach to  environmental justice.</strong></p>
+			<p>The law covers seven  state agencies: the Puget Sound Partnership, and the Washington State  departments of Health, Agriculture, Commerce, Ecology, Natural Resources, and  Transportation. It allows other agencies to opt in.</p>
+			<p> more information  and continued updates on HEAL Act implementation, see the Partnership&rsquo;s  <a href="https://psp.wa.gov/HEAL.php">HEAL  Act webpage</a>.<br>
+			<h2>Environmental Justice  Assessments</h2>
+			<p> HEAL Act requires  staff of covered agencies to conduct environmental justice assessments.&nbsp;<strong>Environmental  justice assessments are a process that staff will be required to follow to  determine the impacts of significant agency actions on overburdened communities  and vulnerable populations.</strong>&nbsp;HEAL Act staff are working on creating the  exact process for environmental justice assessments, which will be finalized by  July 2023 when it needs to be implemented by the agency. Environmental justice  assessments are intended to assist the agency with understanding  disproportionate impacts, equitably distributing environmental benefits,  reducing environmental harms, and addressing environmental and health  disparities. This process should not delay the timeline nor completion of the  action being assessed.&nbsp;</p>
+<p class="last-update">Last updated: 5/23/23</p>
+		<!-- InstanceEndEditable --> </div>
+		
+		<div class="col-sm-2 padding-20-top padding-0-right"> <!-- InstanceBeginRepeat name="right_nav_repeat" --><!-- InstanceBeginRepeatEntry --> <!-- InstanceBeginEditable name="right_nav_title" -->
+			<div class="right-nav-title margin-0-top nonresponsive-object-hide-rightnav">CONTACT</div>
+			<!-- InstanceEndEditable -->
+			<ul class="nav-rightside-custom">
+				<!-- InstanceBeginEditable name="right_nav_links" -->
+				<div class="nonresponsive-object-hide-rightnav-li">
+				<li class="active" role="presentation"> <?php include 'includes/ln-HEAL-contact.html';?></li>
+				</div>
+				
+				<!-- InstanceEndEditable -->
+			</ul>
+			<!-- InstanceEndRepeatEntry --><!-- InstanceEndRepeat --> </div>
+	</div>
+	<!--END OF ROW --> 
+</div>
+<!--END OF CONTENT CONTAINER -->
+
+<?php include 'includes/footer-inc.html';?>
+
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) --> 
+<script src="js/jquery-1.11.2.min.js"></script> 
+
+<!-- Include all compiled plugins (below), or include individual files as needed --> 
+<script src="js/bootstrap.js"></script> 
+<!-- custom js --> 
+<script src="js/custom.js"></script> 
+<!-- Google Tracking  --> 
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-69373425-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
+</body>
+<!-- InstanceEnd --></html>
