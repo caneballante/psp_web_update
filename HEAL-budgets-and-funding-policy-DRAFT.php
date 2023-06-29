3a62c270606487efd9268f67cc36a8f21d7eb0e2
jon bridgman <jonbridgman@gmail.com>
2023-06-29 13:07:59 -0700

    Update HEAL-budgets-and-funding-policy-DRAFT.php

--- HEAL-budgets-and-funding-policy-DRAFT.php
+++ HEAL-budgets-and-funding-policy-DRAFT.php
@@ -95,11 +95,11 @@ navSelected = 5;
 				<li>Considering a broad scope</li>
 				<li>Establishing a goal of 40 percent</li>
 			</ul>
-			<p>You can view the Puget Sound Partnership HEAL Budget and Funding Policy below or <a href="#">click here to download</a></p>
-			<iframe src="https://pspwa.app.box.com/embed/s/hks6d5o8aeyoyuj36q0iedkdiluengrk?sortColumn=date" width="600" height="600" frameborder="0" allowfullscreen webkitallowfullscreen msallowfullscreen>
+			<p align="center">You can view the Puget Sound Partnership HEAL Budget and Funding Policy below or <a href="#">click here to download</a></p>
+			<p><iframe src="https://pspwa.app.box.com/embed/s/hks6d5o8aeyoyuj36q0iedkdiluengrk?sortColumn=date" width="600" height="600" frameborder="0" allowfullscreen webkitallowfullscreen msallowfullscreen>
 			<p>&nbsp;</p>
 			<p>&nbsp;</p>
-			</iframe>
+			</iframe></p>
 			
 			<h2>The HEAL Act</h2>
 			<p>The passage of the Healthy  Environment for All (HEAL) Act in 2021 is a historic step toward eliminating  environmental health disparities and more equitably distributing health and environmental  benefits among communities of color and low-income households.&nbsp;<strong>It is  the first law in Washington to create a coordinated state agency approach to  environmental justice.</strong></p>
