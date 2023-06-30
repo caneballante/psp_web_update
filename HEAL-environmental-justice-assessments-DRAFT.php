19205936ac5ae3c60e5868ff33933d503445ecb1
kevinhydepsp <58960211+kevinhydepsp@users.noreply.github.com>
2023-06-30 10:04:35 -0700

    Changed title

--- HEAL-environmental-justice-assessments-DRAFT.php
+++ HEAL-environmental-justice-assessments-DRAFT.php
@@ -60,7 +60,7 @@ navSelected = 4;
 	<div class="row">
 		<div class="col-sm-3 padding-20-top"></div>
 		<div class="col-sm-7 padding-20-top"> <!-- InstanceBeginEditable name="6col_header" -->
-			<h1>SIGNIFICANT AGENCY ACTION FOR Environmental Justice Assessments</h1>
+			<h1>Environmental Justice Assessments</h1>
 			<!-- InstanceEndEditable --></div>
 		<div class="col-sm-2 padding-20-top"></div>
 	</div>
