1f47fb5876166688c0f6389db80ad94cfd660f84
kevinhydepsp <58960211+kevinhydepsp@users.noreply.github.com>
2023-09-06 13:45:41 -0700

    Adjusted page

--- pspnc-draft-in-lieu-fee-program.php
+++ pspnc-draft-in-lieu-fee-program.php
@@ -145,7 +145,7 @@
 			
 			<p>Additionally, federal permit applicants may generate their own conservation (onsite or offsite) with approval from NOAA Fisheries and the U.S. Fish and Wildlife Service.</p>
 			
-			<button class="button7"><a href="/pspnc-credits.php" target="_blank" style="color:white"><p class="PSPNC-font">The Partnership's in-lieu fee program</p></a></button><br>
+			<button class="button6"><a href="/pspnc-credits.php" target="_blank" style="color:white"><p class="PSPNC-font">Federal action</p></a></button><br>
 			
 			<button class="button4"><a href="/pspnc-credits.php" target="_blank" style="color:white"><p class="PSPNC-font">Credit prices</p></a></button><br>
 			
