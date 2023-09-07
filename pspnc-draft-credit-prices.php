8f55f04c78c3d704e4a6c3ded979e5012021966f
kevinhydepsp <58960211+kevinhydepsp@users.noreply.github.com>
2023-09-07 13:26:19 -0700

    Added new page

---
+++ pspnc-draft-credit-prices.php
@@ -0,0 +1,205 @@
+<!DOCTYPE html>
+<html lang="en"><!-- InstanceBegin template="/Templates/page-4-template.dwt" codeOutsideHTMLIsLocked="false" -->
+<head>
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-BGEZ5L3EJY"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-BGEZ5L3EJY');
+</script>	
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<!-- InstanceBeginEditable name="doctitle" -->
+<title>Puget Sound Partnership Nearshore Conservation Credit Program</title>
+<!-- InstanceEndEditable -->
+<!-- Bootstrap -->
+<link href="css/bootstrap.css" rel="stylesheet">
+<link href="css/custom.css" rel="stylesheet" type="text/css">
+<link href="css/custom-erika.css" rel="stylesheet" type="text/css">
+<!-- loads the wf-loading class right away to minimize FOUT -->
+<script>document.documentElement.className += ' wf-loading';</script>
+<!-- Font PRENTON TYPEKIT -->
+<script src="https://use.typekit.net/srt5jze.js"></script>
+<script>try{Typekit.load({ async: true });}catch(e){}</script>
+<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+<!--[if lt IE 9]>
+	<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+	<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+<!-- InstanceBeginEditable name="head" -->
+<script>
+/*this variable is used to set the proper nav to active. It should to the order the nav item is in the list*/
+  	navSelected = 9;
+</script> 
+<!-- InstanceEndEditable -->
+<!-- InstanceParam name="OptionalRegion1" type="boolean" value="true" -->
+<!-- InstanceParam name="OptionalRegion2" type="boolean" value="false" -->
+<!-- InstanceParam name="OptionalRegion3" type="boolean" value="true" -->
+<!-- InstanceParam name="OptionalRegion4" type="boolean" value="false" -->
+<!-- InstanceParam name="OptionalRegion5" type="boolean" value="true" -->
+<!-- InstanceParam name="OptionalRegion6" type="boolean" value="true" -->
+<!-- InstanceParam name="OptionalRegion7" type="boolean" value="false" -->
+<!-- InstanceParam name="OptionalRegion8" type="boolean" value="false" -->
+</head>
+<body>
+<?php include 'includes/modal-inc.html';?>
+<!-- START IMAGE HEADER --> 
+ <!-- InstanceBeginEditable name="overviewphoto" -->
+<header class="overview-page-image-style overview-page-image8">
+	<div class="overview-quote pull-right">
+		<h3>&nbsp;</h3>
+	</div>
+</header>
+<!-- InstanceEndEditable -->  
+<div class="container-fluid page-content padding-50-bottom">
+	<div class="row">
+		<div class="col-sm-3 padding-20-top"></div>
+		<div class="col-sm-7 padding-20-top"> <!-- InstanceBeginEditable name="6col_header" -->
+			<h1>Puget Sound Partnership Nearshore Conservation Credit Program</h1>
+			
+			<!-- InstanceEndEditable --></div>
+		<div class="col-sm-2 padding-20-top"></div>
+	</div>
+	
+	<div class="row"> 
+		<div class="col-sm-3">
+			<div class="nav-leftside-custom">
+				<ul class="nav nav-stacked nav-pills nav-leftside-custom padding-left-0 margin-10-top">
+					<!-- InstanceBeginEditable name="left_nav" -->
+					<?php include 'includes/ln-pspnc-left.html';?>
+					
+				<!-- InstanceEndEditable -->
+				</ul>
+			</div>
+		</div>
+		 
+		<div class="col-sm-7 padding-20-top content-column"> <!-- InstanceBeginEditable name="6col_content" -->
+		
+			<h2 class="margin-0-top">The Partnership's In-Lieu Fee Program</h2>
+			
+			<h3>How does the Partnership's Nearshore Conservation Credits program work?</h3>
+						
+			<p>The Puget Sound Partnership Nearshore Credits Program is an In-Lieu Fee (ILF) program. We sell conservation credits to help federal permit applicants meet obligations to offset adverse impacts to important nearshore habitat. An ILF program allows a permittee to pay a fee to a third party instead of conducting mitigation themselves. This is similar to paying a fee to a conservation bank. However, while a bank constructs mitigation prior to impacts, an ILF program constructs the mitigation after sufficient funds from different permittees have been collected. The Puget Sound Partnership uses funds from those credit sales to carry out equivalent conservation projects as soon as possible and within a maximum of six years. We aggregate funds from credit purchases, select nearshore conservation projects to generate credits, track their progress, and report on those projects.</p>
+			
+			<img src="images/nearshore-graphic.jpg" class="img-responsive" width=100% alt="A graphic that shows how the Partnership Nearshore Credit Program works. Applicants seeking federal permits for maintenance and development work that will adversely affect nearshore habitat can quantify those impacts using a conservation calculator developed by NOAA scientists. Permit applicants may offset outstanding impacts with either on-site or off-site conservation. Applicants may also choose to purchase conservation credits through the Partnership Nearshore Credits program. The Partnership aggregates funds from credits purchase, selects conservation projects to generate credits, tracks their progress, and reports on those projects."/><br>
+			
+			
+			<h3>Can you tell me more about In-Lieu Fee programs in Washington state?</h3>
+				
+				<p>While Mitigation and Conservation Banks are often for-profit businesses, ILF sponsorship is limited to government agencies or non-profit organizations with expertise in natural resource management. Users of ILFs include Federal agencies like the Navy, state agencies, businesses, and private entities.</p>
+			
+				<p>Other than the Partnership, there are currently three other ILFs in Washington State approved for wetland mitigation. There is one public use ILF in Hood Canal, and two private use ILF’ in the Puget Sound area, the King County ILF, and Pierce County ILF. The Puget Sound Partnership Nearshore Credits Program provides mitigation for salmonid habitat. You can read more about wetland ILFs <a href="https://ecology.wa.gov/Water-Shorelines/Wetlands/Mitigation/In-lieu-fee-mitigation#:~:text=In%2Dlieu%20fee%20(ILF),from%20a%20wetland%20mitigation%20bank." target="new">here</a>.</p>
+			
+			<h3>What conservation projects will the Partnership support to generate conservation?</h3>
+			
+			<p>Initially, we will work with the <a href="https://www.dnr.wa.gov/programs-and-services/aquatics/restoration/creosote-removal" target="new">Department of Natural Resources' Creosote Removal Program</a> to bring about conservation through the removal of creosote-soaked pilings and piers. Creosote-treated wood leaks chemicals into sediments and water, causing adverse effects in juvenile Chinook and high mortality and developmental abnormalities in herring eggs. Herring are an important forage fish in Puget Sound and a key food source for our migrating salmon, which in turn are the primary food source for endangered Southern Resident orcas.</p>
+			
+			<p>Candidate projects for near-term funding include the <a href="https://www.dnr.wa.gov/news/legislation-remove-derelict-structures-would-promote-healthier-environment-communities" target="new">“Filthy Four”</a> identified by the Department of Natural Resources:</p>
+			
+			<ul class="bullet-size-fix">
+				<li>Former Dickman Lumber Mill site, Tacoma</li>
+				<li>Ray’s Boathouse pier, Seattle</li>
+				<li>Former Triton-America pier, Anacortes</li>
+				<li>Former High Tides Seafood pier, Neah Bay</li>
+			
+			</ul>
+			
+			<p>In addition to the initial creosote removal projects, we will explore opportunities to remove shoreline armoring. Shoreline armoring disrupts natural erosion, which supplies much of the sand and gravel that forms our beaches and creates habitat for many other species.</p>
+			
+			<h3>Can credit sales from one service area be used to fund conservation projects in other service areas?</h3>
+			
+			<p>No. Conservation must occur within the same <a href="https://experience.arcgis.com/experience/3ad3ed63a1d541a5bc1793887aa38a85" target="new">marine service area</a> as the impacting project.</p>
+			
+			<h3>How were the service areas determined?</h3>
+				
+			<p>Mitigation is generally sited in close proximity to an impact. The goal is to place the mitigation in the same ecoregion as the impact or ensure that the same population that is impacted will benefit from the mitigation. This concept has been termed service area. The service area is the geographic area where mitigation can provide compensation to offset the functions lost through the impact. NOAA Fisheries determined the marine service areas based on <a href="https://www.fisheries.noaa.gov/resource/map/salmon-habitat-status-and-trend-monitoring-program-data" target="new">NOAA Salmon Habitat Status and Trends Monitoring Program (SHSTMP) Puget Sound Marine Basins</a>.</p>
+			
+			<h3>How many credits has the Partnership sold?</h3>
+			
+			<p>As of August 2023, we have sold 2,090 conservation credits. Credit sales by service area are shown below:</p>
+			
+			<img src="images/nearshore-credits-sold.jpg" class="img-responsive" width=60% alt="A map graphic that shows how many conservation credits the Partnership has sold in each marine service area. The Partnership has sold 1,382 credits in the North Puget Sound service area; O credits in the Strait of Juan de Fuca service area; 31 credits in the Whidbey service area; 307 credits in the Hood Canal service area; and 370 credits in the South Central Puget Sound service area."/><br>
+			
+			<h3>Have any conservation projects been fully implemented?</h3>
+			
+			<p>Not yet. As with any “In-Lieu Fee-style” program, it can take years of credit sales to build up the amount of funding needed to carry out an effective conservation project. While the exact timing depends on the rate of credit sales, we hope we’ll be able to begin construction on one or more projects in 2024.</p>
+			
+			<h3>When will the Partnership complete its conservation projects?</h3>
+			
+			<p>Under our ILF agreement with the NOAA Fisheries and the US Fish and Wildlife Service, we must implement conservation within six years of selling the first credit for an impact within the service area.</p>
+			
+			<h3>Will the Partnership run out of credits?</h3>
+			
+			<p>No. As of August 2023, we have identified potential credit-generating conservation projects that will create about 8,700 credits. That estimate only includes projects that have been measured using the NOAA Conservation Calculator. Many more projects could be eligible for funding projects that create conservation credits in the future!</p>
+			
+			<p>“Running out of credits” would mean that there are no more opportunities to restore the nearshore habitat of Puget Sound. We remain very far from fully restoring nearshore habitat function (and running out of all opportunities to generate conservation credits).</p>
+			
+			<h3>Are there other credit providers besides the Partnership?</h3>
+			
+			<p>Yes, the Blue Heron Slough Conservation Bank, the Hood Canal In-Lieu Fee Program, and the Port of Tacoma all offer conservation credits for sale to impacting projects that fall within their service areas.</p>
+			
+			<p>Additionally, federal permit applicants may generate their own conservation (onsite or offsite) with approval from NOAA Fisheries and the U.S. Fish and Wildlife Service.</p><br>
+			
+			<button class="button6"><a href="/pspnc-credits.php" target="_blank" style="color:white"><p class="PSPNC-font">Federal action</p></a></button><br>
+			
+			<button class="button4"><a href="/pspnc-credits.php" target="_blank" style="color:white"><p class="PSPNC-font">Credit prices</p></a></button><br>
+			
+			<button class="button5"><a href="/pspnc-credits.php" target="_blank" style="color:white"><p class="PSPNC-font">Purchase credits</p></a></button><br>
+			
+			<button class="button6"><a href="/pspnc-credits.php" target="_blank" style="color:white"><p class="PSPNC-font">Suggest conservation projects for funding</p></a></button><br>
+			
+
+			<p class="last-update">Last updated: 10/25/22</p>
+			
+			<!-- InstanceEndEditable --> </div>
+		
+		<div class="col-sm-2 padding-20-top padding-0-right"> <!-- InstanceBeginRepeat name="right_nav_repeat" --><!-- InstanceBeginRepeatEntry --> <!-- InstanceBeginEditable name="right_nav_title" -->
+			
+			<div class="right-nav-title margin-0-top nonresponsive-object-hide-rightnav">CONTACT</div>
+			<!-- InstanceEndEditable -->
+			<ul class="nav-rightside-custom">
+				<!-- InstanceBeginEditable name="right_nav_links" -->
+				<div class="nonresponsive-object-hide-rightnav-li">
+				<li class="active" role="presentation"> <?php include 'includes/pspnc_contact.html';?></li>
+				<li class="active" role="presentation"><button class=button><a href="https://www.fisheries.noaa.gov/west-coast/habitat-conservation/puget-sound-nearshore-habitat-conservation-calculator" target="_blank" style="color:white">Puget Sound Nearshore Habitat Conservation Calculator</a></button></li>
+				<div class="right-nav-title margin-0-top nonresponsive-object-hide-rightnav">SEE ALSO</div>
+				<li class="active" role="presentation"><a href="https://vimeo.com/637105177/d4713aa7ee" target="new"><strong>A new conservation marketplace in the Puget Sound</strong></a></li>
+				<li class="active" role="presentation"><a href="https://www.noaa.gov/news-release/army-and-noaa-to-advance-endangered-species-act-consultations" target="new"><strong>Army and NOAA to advance Endangered Species Act consulations</strong></a></li>
+				<li class="active" role="presentation"><a href="https://medium.com/puget-sound-partnership/puget-sound-partnership-credits-will-fund-nearshore-conservation-398c573bea50" target ="_blank"><strong>Puget Sound Partnership credits will fund nearshore conservation</strong></a></li>
+				<li class="active" role="presentation"><a href="https://wdfw.wa.gov/species-habitats/habitat-recovery/puget-sound/nearshore-program" target="new"><strong>Washington State Department of Fish and Wildlife Nearshore Program</strong></a></li>
+				</div>
+				<!-- InstanceEndEditable -->
+			</ul>
+			<!-- InstanceEndRepeatEntry --><!-- InstanceEndRepeat --> </div>
+	</div>
+	<!--END OF ROW --> 
+</div>
+<!--END OF CONTENT CONTAINER -->
+
+<?php include 'includes/footer-inc.html';?>
+
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) --> 
+<script src="js/jquery-1.11.2.min.js"></script> 
+
+<!-- Include all compiled plugins (below), or include individual files as needed --> 
+<script src="js/bootstrap.js"></script> 
+<!-- custom js --> 
+<script src="js/custom.js"></script> 
+<!-- Google Tracking  --> 
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-69373425-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
+</body>
+<!-- InstanceEnd --></html>
