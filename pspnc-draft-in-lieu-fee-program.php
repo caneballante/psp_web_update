263db4be498a94dfdc8114138b9b42d0c0097526
kevinhydepsp <58960211+kevinhydepsp@users.noreply.github.com>
2023-09-07 14:32:11 -0700

    Adjusted page

--- pspnc-draft-in-lieu-fee-program.php
+++ pspnc-draft-in-lieu-fee-program.php
@@ -145,16 +145,16 @@
 			
 			<p>Additionally, federal permit applicants may generate their own conservation (onsite or offsite) with approval from NOAA Fisheries and the U.S. Fish and Wildlife Service.</p><br>
 			
-			<button class="button6"><a href="/pspnc-credits.php" target="_blank" style="color:white"><p class="PSPNC-font">Federal action</p></a></button><br>
+			<button class="button6"><a href="/pspnc-draft-federal-action.php" target="_blank" style="color:white"><p class="PSPNC-font">Federal action</p></a></button><br>
 			
-			<button class="button4"><a href="/pspnc-credits.php" target="_blank" style="color:white"><p class="PSPNC-font">Credit prices</p></a></button><br>
+			<button class="button4"><a href="/pspnc-draft-credit-prices.php" target="_blank" style="color:white"><p class="PSPNC-font">Credit prices</p></a></button><br>
 			
-			<button class="button5"><a href="/pspnc-credits.php" target="_blank" style="color:white"><p class="PSPNC-font">Purchase credits</p></a></button><br>
+			<button class="button5"><a href="/pspnc-draft-purchase-credits.php" target="_blank" style="color:white"><p class="PSPNC-font">Purchase credits</p></a></button><br>
 			
-			<button class="button6"><a href="/pspnc-credits.php" target="_blank" style="color:white"><p class="PSPNC-font">Suggest conservation projects for funding</p></a></button><br>
+			<button class="button6"><a href="/pspnc-draft-suggest-conservation-projects.php" target="_blank" style="color:white"><p class="PSPNC-font">Suggest conservation projects for funding</p></a></button><br>
 			
 
-			<p class="last-update">Last updated: 10/25/22</p>
+			<p class="last-update">Last updated: 9/7/23</p>
 			
 			<!-- InstanceEndEditable --> </div>
 		
