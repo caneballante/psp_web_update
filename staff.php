a7b95332eeed3c145da653cf11476434f0f03ddd
kevinhydepsp <58960211+kevinhydepsp@users.noreply.github.com>
2023-09-13 07:09:43 -0700

    Removed link

--- staff.php
+++ staff.php
@@ -347,7 +347,7 @@
 		<!-- InstanceEndEditable --> </div>
 		
 		<div class="col-sm-2 padding-20-top padding-0-right"> <!-- InstanceBeginRepeat name="right_nav_repeat" --><!-- InstanceBeginRepeatEntry --> <!-- InstanceBeginEditable name="right_nav_title" -->
-			<div class="right-nav-title margin-20-top">SEE ALSO</div>
+			<!-- <div class="right-nav-title margin-20-top">SEE ALSO</div> -->
 			<!-- InstanceEndEditable -->
 			<ul class="nav-rightside-custom">
 				<!-- InstanceBeginEditable name="right_nav_links" -->
