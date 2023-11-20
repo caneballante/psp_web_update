ff969c669b86b5b7645392d67f8c23a23f1ac6bf
kevinhydepsp <58960211+kevinhydepsp@users.noreply.github.com>
2023-11-20 14:26:58 -0800

    Added new page

---
+++ about-MART.php
@@ -0,0 +1,187 @@
+<!DOCTYPE html>
+<html lang="en"><!-- InstanceBegin template="/Templates/page-4-template.dwt" codeOutsideHTMLIsLocked="false" -->
+<head>
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-K5R1WLJ6VJ"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-K5R1WLJ6VJ');
+</script>
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<!-- InstanceBeginEditable name="doctitle" -->
+<title>Puget Sound Partnership - Multi-Agency Review Team</title>
+<!-- InstanceEndEditable -->
+<!-- Bootstrap -->
+<link href="css/bootstrap.css" rel="stylesheet">
+<link href="css/custom.css" rel="stylesheet" type="text/css">
+<link href="css/custom-erika.css" rel="stylesheet" type="text/css">
+<!-- loads the wf-loading class right away to minimize FOUT -->
+<script>document.documentElement.className += ' wf-loading';</script>
+<!-- Font PRENTON TYPEKIT -->
+<script src="https://use.typekit.net/srt5jze.js"></script>
+<script>try{Typekit.load({ async: true });}catch(e){}</script>
+<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+<!--[if lt IE 9]>
+	<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+	<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+<!-- InstanceBeginEditable name="head" -->
+<script>
+/*this variable is used to set the proper nav to active. It should to the order the nav item is in the list*/
+  	navSelected = 1;
+</script>
+<!-- InstanceEndEditable -->
+<!-- InstanceParam name="OptionalRegion1" type="boolean" value="true" -->
+<!-- InstanceParam name="OptionalRegion2" type="boolean" value="false" -->
+<!-- InstanceParam name="OptionalRegion3" type="boolean" value="true" -->
+<!-- InstanceParam name="OptionalRegion4" type="boolean" value="false" -->
+<!-- InstanceParam name="OptionalRegion5" type="boolean" value="true" -->
+<!-- InstanceParam name="OptionalRegion6" type="boolean" value="true" -->
+<!-- InstanceParam name="OptionalRegion7" type="boolean" value="false" -->
+<!-- InstanceParam name="OptionalRegion8" type="boolean" value="false" -->
+</head>
+<body>
+<?php include 'includes/modal-inc.html';?>
+<!-- START IMAGE HEADER --> 
+ <!-- InstanceBeginEditable name="overviewphoto" -->
+<header class="overview-page-image-style overview-page-image13"> </header>
+<!-- InstanceEndEditable -->  
+<div class="container-fluid page-content padding-50-bottom">
+	<div class="row">
+		<div class="col-sm-3 padding-20-top"></div>
+		<div class="col-sm-7 padding-20-top"> <!-- InstanceBeginEditable name="6col_header" -->
+			<h1>Multi-Agency Review Team</h1>
+			<p>&nbsp;</p>
+			<!-- InstanceEndEditable --></div>
+		<div class="col-sm-2 padding-20-top"></div>
+	</div>
+	
+	<div class="row"> 
+		<div class="col-sm-3">
+			<div class="nav-leftside-custom">
+				<ul class="nav nav-stacked nav-pills nav-leftside-custom padding-left-0 margin-10-top">
+					<!-- InstanceBeginEditable name="left_nav" -->
+				<?php include 'includes/ln-local-recovery-efforts.html';?>
+				<!-- InstanceEndEditable -->
+				</ul>
+			</div>
+		</div>
+		 
+		<div class="col-sm-7 padding-20-top content-column"> <!-- InstanceBeginEditable name="6col_content" -->
+			<h2 class="margin-0-top">Multi-Agency Review Team</h2>
+			<p>The Multi-Agency Review Team (MART) is a team of federal and state regulatory staff working together to streamline the permitting process for Puget Sound Basin habitat recovery projects. The MART uses a facilitated, coordinated interagency approach to move projects through the entire permit process expeditiously and efficiently.</p>
+			
+			<h3>Benefits</h3>
+			<ul class="bullet-size-fix">
+				<li>Direct access to team of federal, state, and local permitting agency contacts</li>
+				<li>MART projects may be prioritized by permitting agencies who are responsible for permitting those projects</li>
+				<li>Potential reduction in submittal redundancy and possibly permit process time</li>
+				<li>MART tracks projects monthly and adaptively resolves issues that arise, where possible</li>
+			</ul><br>
+			
+			<div class="container-fluid">
+		
+		
+		<div class="row text-left">
+			
+			<div class="col-md-4">
+					<div class="card">
+							<div class="card-body">
+							<div class="d-inline">
+								<h5 class="margin-0-top">Multi-Agency Review Team</h5>
+								<p class="mart-font">The intent of the MART is to promote and implement habitat restoration as quickly and efficiently as possible within the Puget Sound Basin.</p>
+					
+					
+						</div></div></div></div>
+			
+			<div class="col-md-4">
+					<div class="card">
+							<div class="card-body">
+							<div class="d-inline">
+								<h5 class="margin-0-top">How It Works</h5>
+					<p class="mart-font">Federal and state agencies in coordination with local agencies work with applicants to concurrently process permits to save time and effort.</p>
+					
+						</div></div></div></div>
+			
+			<div class="col-md-4">
+					<div class="card">
+						<div class="card-body">
+							<div class="d-inline">
+					<h5 class="margin-0-top">Have a project in mind?</h5> 
+					<p class="mart-font">The MART works on ecologically beneficial projects in marine or freshwater environments that are permit ready and promote the recovery of Puget Sound Basin. Tell us more about your project to see if the MART can assist you.</p>
+					
+							</div></div></div></div> <!--end col-->
+			
+			</div><!--end row-->	
+				
+			</div><!--end container-->
+			<br>
+			
+			<button class="button6"><a href="" target="_blank" style="color:white"><p class="PSPNC-font">Let us help you streamline the permitting process with our multi-agency team. </p></a></button><br>
+			
+			<h2>About the MART</h2>
+			
+			<p>The Multi-Agency Review Team (MART) is a working group of the Puget Sound Federal Leadership Task Force (PSFLTF). The PSFLTF is a 16 federal agency group that collaborates with the Tribes and Washington State agencies on Puget Sound recovery actions and treaty rights. MART is a team of federal and state regulatory staff working together to streamline the permitting process for habitat recovery projects. The MART started as a 2-year pilot to test a facilitated, coordinated team approach to help permit ecologically beneficial restoration projects on marine shorelines. Due to its success, the MART is implementing this process and expanded its scope to freshwater restoration projects. Efforts focus on streamlining the federal permitting process in coordination with state and local permits.</p>
+			
+			<h3>MART members</h3>
+			
+			<p>Environmental Protection Agency (EPA), U.S. Army Corps of Engineers (USACE), National Marine Fisheries Service (NMFS), U.S. Fish and Wildlife Service (USFWS), Federal Emergency Management Agency (FEMA), WA Department of Fish and Wildlife (WDFW), WA Department of Ecology (Ecology), WA Department of Natural Resources (WDNR), Puget Sound Partnership, and coordinating with local jurisdictions.</p>
+			
+			<h3>Partner programs</h3>
+			
+			<p>Habitat Recovery Pilot Program – streamlined state and local permitting for habitat recovery projects.</p>
+
+<p>Fish Habitat Enhancement Project (FHEP) - expedited Hydraulic Project Approval permit for fish enhancement projects.</p>
+
+<p>Shore Friendly program - helps landowners replace bulkheads with softshore armor or restored shorelines.
+</p>
+			
+			
+			<p class="last-update">Last updated: 11/20/23</p>
+		<!-- InstanceEndEditable --> </div>
+		
+		<div class="col-sm-2 padding-20-top padding-0-right"> <!-- InstanceBeginRepeat name="right_nav_repeat" --><!-- InstanceBeginRepeatEntry --> <!-- InstanceBeginEditable name="right_nav_title" -->
+			<div class="right-nav-title margin-0-top ">SEE ALSO</div>
+			<!-- InstanceEndEditable -->
+			<ul class="nav-rightside-custom">
+				<!-- InstanceBeginEditable name="right_nav_links" -->
+				<li class="active" role="presentation"> <a href="https://pspwa.box.com/s/nxli7o61pnjxx4rkmo7nokcjh9huc0kf">LIO Ecosystem Recovery Plans</a></li>
+				<li class="active" role="presentation"> <a href="https://wa-psp.maps.arcgis.com/apps/webappviewer/index.html?id=bce72e6f26c640e5a485db8951951117" target="new">LIO Geography Map</a></li>
+				<li class="active" role="presentation"> <a href="https://pspwa.box.com/v/ECONetInfo">ECO Net Information</a></li>
+				
+				<!-- InstanceEndEditable -->
+			</ul>
+			<!-- InstanceEndRepeatEntry --><!-- InstanceEndRepeat --> </div>
+	</div>
+	<!--END OF ROW --> 
+</div>
+<!--END OF CONTENT CONTAINER -->
+
+<?php include 'includes/footer-inc.html';?>
+
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) --> 
+<script src="js/jquery-1.11.2.min.js"></script> 
+
+<!-- Include all compiled plugins (below), or include individual files as needed --> 
+<script src="js/bootstrap.js"></script> 
+<!-- custom js --> 
+<script src="js/custom.js"></script> 
+<!-- Google Tracking  --> 
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-69373425-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
+</body>
+<!-- InstanceEnd --></html>
