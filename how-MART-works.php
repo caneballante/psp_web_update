3f36c0f76a6765cb840c16da130d877294575682
kevinhydepsp <58960211+kevinhydepsp@users.noreply.github.com>
2023-11-20 15:00:02 -0800

    Adjusted page

--- how-MART-works.php
+++ how-MART-works.php
@@ -34,7 +34,7 @@
 <!-- InstanceBeginEditable name="head" -->
 <script>
 /*this variable is used to set the proper nav to active. It should to the order the nav item is in the list*/
-  	navSelected = 1;
+  	navSelected = 2;
 </script>
 <!-- InstanceEndEditable -->
 <!-- InstanceParam name="OptionalRegion1" type="boolean" value="true" -->
@@ -67,7 +67,7 @@
 			<div class="nav-leftside-custom">
 				<ul class="nav nav-stacked nav-pills nav-leftside-custom padding-left-0 margin-10-top">
 					<!-- InstanceBeginEditable name="left_nav" -->
-				<?php include 'includes/ln-local-recovery-efforts.html';?>
+				<?php include 'includes/ln-MART.html';?>
 				<!-- InstanceEndEditable -->
 				</ul>
 			</div>
