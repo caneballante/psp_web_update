2ce44e7e74869ed992a3636edbc2a825f4c4ad40
kevinhydepsp <58960211+kevinhydepsp@users.noreply.github.com>
2024-01-02 12:56:30 -0800

    Updated date

--- SP_about.php
+++ SP_about.php
@@ -75,7 +75,7 @@
 		</div>
 		 
 		<div class="col-sm-7 padding-20-top content-column"> <!-- InstanceBeginEditable name="6col_content" -->
-			<p>The Science Panel's expertise and advice are critical to the Puget Sound Partnership&rsquo;s efforts to develop a comprehensive, science-based plan to restore Puget Sound. The members, appointed by the Leadership Council, were chosen from the top scientists in Washington State. 2022 Meeting schedules can be found <a href="board_meetings.php">here.</a><br></p>
+			<p>The Science Panel's expertise and advice are critical to the Puget Sound Partnership&rsquo;s efforts to develop a comprehensive, science-based plan to restore Puget Sound. The members, appointed by the Leadership Council, were chosen from the top scientists in Washington State. 2024 Meeting schedules can be found <a href="board_meetings.php">here.</a><br></p>
 		<!--START IFRAME-->
 	       <IFRAME WIDTH=1000 HEIGHT=700 FRAMEBORDER=0 SRC="https://app.smartsheet.com/b/publish?EQBCT=34b79c1749f94979b7fcc38cb37bdce0"></IFRAME>
 			 <!--END IFRAME-->
@@ -87,7 +87,7 @@
             360.742.2936 </p>
 			<h3>Board Materials</h3>
 			<p><a href="https://pspwa.box.com/s/gabtcrbzo9i5yybkeyi6lx6cez0bh10o"> Board's documents, meeting materials, summaries, and Resolutions</a>			</p>
-			<p class="last-update">Last updated: 01/25/21</p>
+			<p class="last-update">Last updated: 1/2/24</p>
 		
 			<!-- InstanceEndEditable --> </div>
 		
