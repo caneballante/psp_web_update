8672f3ef103faa6f877a15ec12ca1a179e049cb1
kevinhydepsp <58960211+kevinhydepsp@users.noreply.github.com>
2024-01-17 07:56:40 -0800

    Update MART.php
    Changed image back

--- MART.php
+++ MART.php
@@ -50,7 +50,7 @@
 <?php include 'includes/modal-inc.html';?>
 <!-- START IMAGE HEADER --> 
  <!-- InstanceBeginEditable name="overviewphoto" -->
-<header class="overview-page-image-style overview-page-image123"> </header>
+<header class="overview-page-image-style overview-page-image13"> </header>
 <!-- InstanceEndEditable -->  
 <div class="container-fluid page-content padding-50-bottom">
 	<div class="row">
