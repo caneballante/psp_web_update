ca55b2a99e446ac77cd7dcf640f8aa2410276567
kevinhydepsp <58960211+kevinhydepsp@users.noreply.github.com>
2024-01-23 08:41:17 -0800

    Update PSEMP-resources.php
    Adding new content

--- PSEMP-resources.php
+++ PSEMP-resources.php
@@ -77,6 +77,48 @@
 		<div class="col-sm-7 padding-20-top content-column"> <!-- InstanceBeginEditable name="6col_content" -->
 			
 			<div class="container-fluid">
+				
+		<div class="row text-left">
+			
+			<div class="col-md-4">
+					<div class="card">
+							<div class="card-body">
+							<div class="d-inline">
+								<h6 class="margin-0-top">THIRD SURVEY FOR VITAL SIGN INDICATORS OF HUMAN WELLBEING</h6>
+					<span class="no-icon"><a href="https://pspwa.box.com/s/meadbna20m1xyigt9dan7839lmn6x4gm" target="new"><img src="images/psemp/human-wellbeing.jpg" class="img-responsive img-rounded" alt="Photo of people enjoying Owen Beach at Tacoma's Point Defiance Park."/></a>
+	 	 		    
+					<p class="psemp-products"><a href="https://pspwa.box.com/s/meadbna20m1xyigt9dan7839lmn6x4gm">2022 Survey Report</a></p></span>
+								<p class="psemp-products">This report covers the third Human Wellbeing survey iteration to monitor the subjective human wellbeing vital signs created by the Puget Sound Partnership and Oregon State University. </p><br>
+					
+						</div></div></div></div>
+			
+			<div class="col-md-4">
+					<div class="card">
+							<div class="card-body">
+							<div class="d-inline">
+								<h6 class="margin-0-top">2022 SALISH SEA TOXICS MONITORING SYNTHESIS</h6>
+					<span class="no-icon"><a href="https://www.eopugetsound.org/articles/2022-salish-sea-toxics-monitoring-synthesis-selection-research" target="new"><img src="images/psemp/toxics-monitoring.jpg" class="img-responsive img-rounded" alt="The cover photo from the 2022 Salish Sea Toxics Monitoring Synthesis report, which shows a heron standing on a shoreline with a fish in its beak."/></a>
+	 	 		    
+					<p class="psemp-products"><a href="https://www.eopugetsound.org/articles/2022-salish-sea-toxics-monitoring-synthesis-selection-research">Report</a></p></span>
+								<p class="psemp-products">An overview of selected recent monitoring and research activities focused on toxic contaminants in the Salish Sea.</p><br>
+					
+						</div></div></div></div>
+			
+			<div class="col-md-4">
+					<div class="card">
+							<div class="card-body">
+							<div class="d-inline">
+								<h6 class="margin-0-top">PUGET SOUND MARINE WATERS 2021 OVERVIEW</h6>
+					<span class="no-icon"><a href="https://www.psp.wa.gov/PSmarinewatersoverview.php" target="new"><img src="images/ps-marine-waters-report-2021.jpg" class="img-responsive img-rounded" alt="The cover photo from the Puget Sound Marine Waters 2021 Overview report, showing the silhouette of a seaplane flying over blue, green, and orange-colored Puget Sound water."/></a>
+	 	 		    
+					<p class="psemp-products"><a href="https://www.psp.wa.gov/PSmarinewatersoverview.php">Report</a></p></span>
+								<p class="psemp-products">The Puget Sound Marine Waters Overview report, prepared by the Puget Sound Ecosystem Monitoring Program’s Marine Waters Work Group, provides a comprehensive overview of marine conditions in 2021.</p><br>
+					
+						</div></div></div></div>
+			
+			
+			
+			</div><!--end row-->
 		
 		
 		<div class="row text-left">
