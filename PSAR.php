835e2eb4115475ff1bbdef1340dff0da3a5e9f37
jon bridgman <jonbridgman@gmail.com>
2024-01-23 16:44:12 -0800

    Update PSAR.php

--- PSAR.php
+++ PSAR.php
@@ -70,7 +70,7 @@
 				<li class="active" role="presentation"> <a href="PSAR.php">PSAR OVERVIEW</a></li>
 				<li class="active" role="presentation"> <a href="PSAR-resources.php">PSAR RESOURCES</a></li>
 					<hr/>
-					<p class="caption left-align">The PSAR program is supported with funding from Washington’s <strong>Climate Commitment Act</strong>. The CCA supports Washington’s climate action efforts by putting cap-and-invest dollars to work reducing climate pollution, creating jobs, and improving public health. Information about the CCA is available at <a href="www.climate.wa.gov">www.climate.wa.gov</a>.<br>
+					<p class="caption left-align">The PSAR program is supported with funding from Washington’s <strong>Climate Commitment Act</strong>. The CCA supports Washington’s climate action efforts by putting cap-and-invest dollars to work reducing climate pollution, creating jobs, and improving public health. Information about the CCA is available at <a href="http://www.climate.wa.gov">www.climate.wa.gov</a>.<br>
 					<img class="img-responsive" src="images/CCA-primary-logo.png" width="309" height="156" alt=""/></p>
 					<hr/>
 				<!-- InstanceEndEditable -->
