407054de18824e8f6dd156c54defb64bca7aca77
kevinhydepsp <58960211+kevinhydepsp@users.noreply.github.com>
2024-02-22 09:09:09 -0800

    Added new page

---
+++ future-scenarios-test.php
@@ -0,0 +1,220 @@
+<!DOCTYPE html>
+<html lang="en"><!-- InstanceBegin template="/Templates/page-4-template.dwt" codeOutsideHTMLIsLocked="false" -->
+<head>
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-K5R1WLJ6VJ"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-K5R1WLJ6VJ');
+</script>
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<!-- InstanceBeginEditable name="doctitle" -->
+<title>Puget Sound Partnership - Future Scenarios Project</title>
+<!-- InstanceEndEditable -->
+<!-- Bootstrap -->
+<link href="css/bootstrap.css" rel="stylesheet">
+<link href="css/custom.css" rel="stylesheet" type="text/css">
+<link href="css/custom-erika.css" rel="stylesheet" type="text/css">
+<!-- loads the wf-loading class right away to minimize FOUT -->
+<script>document.documentElement.className += ' wf-loading';</script>
+<!-- Font PRENTON TYPEKIT -->
+<script src="https://use.typekit.net/srt5jze.js"></script>
+<script>try{Typekit.load({ async: true });}catch(e){}</script>
+<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+<!--[if lt IE 9]>
+	<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+	<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+<!-- InstanceBeginEditable name="head" -->
+<script>
+/*this variable is used to set the proper nav to active. It should to the order the nav item is in the list*/
+  	navSelected = 6;
+</script>
+<!-- InstanceEndEditable -->
+<!-- InstanceParam name="OptionalRegion1" type="boolean" value="true" -->
+<!-- InstanceParam name="OptionalRegion2" type="boolean" value="false" -->
+<!-- InstanceParam name="OptionalRegion3" type="boolean" value="true" -->
+<!-- InstanceParam name="OptionalRegion4" type="boolean" value="false" -->
+<!-- InstanceParam name="OptionalRegion5" type="boolean" value="true" -->
+<!-- InstanceParam name="OptionalRegion6" type="boolean" value="true" -->
+<!-- InstanceParam name="OptionalRegion7" type="boolean" value="false" -->
+<!-- InstanceParam name="OptionalRegion8" type="boolean" value="false" -->
+</head>
+<body>
+<?php include 'includes/modal-inc.html';?>
+<!-- START IMAGE HEADER --> 
+ <!-- InstanceBeginEditable name="overviewphoto" -->
+<header class="overview-page-image-style overview-page-image25"> </header>
+<!-- InstanceEndEditable -->  
+<div class="container-fluid page-content padding-50-bottom">
+	<div class="row">
+		<div class="col-sm-3 padding-20-top"></div>
+		<div class="col-sm-7 padding-20-top"> <!-- InstanceBeginEditable name="6col_header" -->
+			<h1>Alternative futures for Puget Sound</h1>
+			<p>&nbsp;</p>
+			<!-- InstanceEndEditable --></div>
+		<div class="col-sm-2 padding-20-top"></div>
+	</div>
+	
+	<div class="row"> 
+		<div class="col-sm-3">
+			<div class="nav-leftside-custom">
+				<ul class="nav nav-stacked nav-pills nav-leftside-custom padding-left-0 margin-10-top">
+					<!-- InstanceBeginEditable name="left_nav" -->
+				
+				<!-- InstanceEndEditable -->
+				</ul>
+			</div>
+		</div>
+		 
+		<div class="col-sm-7 padding-20-top content-column"> <!-- InstanceBeginEditable name="6col_content" -->
+			<div class="highlightbox margin-0-top margin-20-bottom">
+			
+			<h2 class="margin-0-top"><span class="new-red">NEW:</span> Future Scenarios and Smart Growth Workshops</h2>
+			
+			<p>On Nov. 7 the Future Scenarios project will host a day-long workshop intended to use the alternative futures to explore smart growth. The workshop will look at the updated versions of the future scenarios which are as follows.</p>
+			
+			<h3>Alternative 1 – Salmon Forward:</h3> <p>Explores a future where both government cohesion and willingness to act and human perceptions & behaviors are positive toward Puget Sound. This creates a mutually reinforcing virtuous cycle that unlocks large scale changes in how big cities are developed to minimize their impacts and maximize their livability in a way that calls people in to dense urban living and maximizes protection and restoration of land outside growth centers in existing urban areas. Land use is tightly managed, and protection of critical areas is high. <a href="https://pspwa.box.com/s/5w0i44z9toz52ay5kpder8urzwsuiknv">Read more</a>.</p>
+			
+			<h3>Alternative 2 – Networked Growth:</h3> <p>Government cohesion and willingness to act remain positive toward Puget Sound but human perceptions and behaviors are not. This alternative explores a future where people are less willing to live in dense urban centers and instead distribute themselves more evenly across the Puget Sound region and are relatively unengaged in considering the impacts of dispersed growth. Government responds to this by developing new planning models that ensure small and medium sized cities can make the infrastructure investments needed to manage new growth and land use continues to be tightly managed. <a href="https://pspwa.box.com/s/uuhaahsb4g8zqacbhg9eshmfa54lymas">Read more</a>.</p>  
+			
+			<h3>Alternative 3 – Stewardship and Rural Living:</h3> <p>Explores a future where government cohesion and willingness to act is low but where human perceptions and behaviors are very aligned and positive toward Puget Sound. In this future, people distribute themselves more evenly across the Puget Sound region and prefer rural living to urban centers. Cities grow less rapidly and are less dense. Non- and quasi-government actors play a strong role in supporting individuals in implementing best management practices and taking other individual actions to slow Puget Sound decline and implement neighborhood-scale restorations. <a href="https://pspwa.box.com/s/xspjum0qy94973s5fisrxbft2t9r988k">Read more</a>.</p>
+			
+			<p>See the rest of the <a href="https://pspwa.box.com/s/96qnz9sdesctdcvrklpnljnim9xxjdw8">workshop materials here</a>. For more information or to register for the workshop please contact Andres Sheikh at <a href="mailto:asheikh@rossstrategic.com">asheikh@rossstrategic.com</a>. </p>
+				
+			</div>
+
+			
+			
+			<h2>Alternative futures for Puget Sound</h2>
+			
+			<p>We’re starting the second phase of a scenario-planning effort to explore and understand the implications of different plausible futures for Puget Sound. The Puget Sound Future Scenarios Project (Future Scenarios)  will help the Puget Sound recovery community plan for the region’s future by considering multiple possibilities of what might happen. The project is not meant to predict the future but, rather, communicate the effects of current policy choices and prepare for uncertainties like climate change and population growth. </p>
+			
+			<p>Future Scenarios explores the interactions between driving forces (such as population growth, land use policy, climate change, and others) and looks at how our Puget Sound recovery goals (such as human wellbeing and orca populations) might respond to different driver trajectories.</p>
+			
+			<p>The overarching goal of the project is to help the Partnership and the Puget Sound recovery community do the following: </p>
+			
+			<ol class="bullet-size-fix">
+				<li>Explore the root causes and interactions that drive long-term Puget Sound system dynamics; </li>
+				<li>Think systematically when considering choices and tradeoffs in the face of limited resources in the future. </li>
+			</ol>
+			
+			<h2>What will the Future Scenarios project help us do?</h2>
+			<ul class="bullet-size-fix">
+				<li>Strengthen recovery strategies and enable quicker adaptive management by assessing the implications of a range of possible contexts;</li>
+				<li>Identify ways to improve planning and policy-setting; and</li>
+				<li>Strengthen the community of practice by bridging social and natural science disciplines.</li>
+			</ul>
+			
+			<img src="images/science-figures/future-scenario-phases.png" class="img-responsive" width=100% alt="Graphic that shows the timeline for phase two of the Future Scenarios project. The text in the timeline says the following: Future Scenarios phase one began in June 2021 and was completed in June 2022 with a final report. Phase two work can be broken down into five main steps: Refine project models to better reflect salmon-related outcomes across scenarios and connect the project to salmon recovery, July – September 2022. Develop the second version of the business-as-usual scenario to understand what could happen in Puget Sound on current trajectory, Sept. – Dec. 2022. Develop the second version of the four alternative scenarios to understand what could happen in Puget Sound if the recovery strategy changes or things don't go as planned, Oct. 2022 – Jan. 2023. Share scenario outcomes and analysis with broader recovery community to gauge consequences, Jan. – March 2023. Consider how scenarios can support decision makers and capture next steps for Future Scenarios in phase two report, April – June 2023"/><br>
+			
+			<h3>Future Scenarios phase one takeaways</h3>
+			
+			<p>In phase one of the project (March 2021- June 2022), Future Scenarios identified four key drivers of change in Puget Sound: (1) population growth (2) climate change (3) governance (4) public perceptions and behaviors. These drivers helped build the first version of five different scenarios the Puget Sound region could experience in the future: </p>
+			
+			<ul class="bullet-size-fix">
+				<li>one business-as-usual (BAU) scenario that reflects the region’s current trajectories and </li>
+				<li>four alternative-to-BAU scenarios that reflect variations of those trajectories. </li>
+			</ul>
+			
+			<p>The initial scenarios and how they compare in terms of drivers are illustrated in the figure below:</p>
+			
+			
+					<img class="img-responsive floatleft" src="images/science-figures/ScenariosChart.jpg" alt="Chart that illustrates differences in business-as-usual scenarios and alternative scenarios"/><br>
+						
+			<p>Initial trends and outcomes from phase one include:</p>
+			
+			<ul class="bullet-size-fix">
+				<li>The rate of population growth seems to significantly impact scenario results, as much as or more than other factors. </li>
+				<li>Existing urban growth areas  in Puget Sound can accommodate high population growth, provided there is a willingness to increase densities. </li>
+			</ul>
+			
+			<h2>Future Scenarios phase two</h2>
+			
+			<p>Phase two of the project (July 2022 - June 2023) will:</p>
+			
+			<ul class="bullet-size-fix">
+				<li>Refine and expand the quantitative and qualitative models used to develop scenarios (with particular emphasis on salmon-related outcomes); </li>
+				<li>Develop a second version of the five scenarios; and </li>
+				<li>Consider ways Future Scenarios outcomes can support the recovery community in different contexts. </li>
+			</ul>
+			
+			<h3>Future Scenarios advisory group</h3>
+			
+			<p>Future Scenarios is supported by an advisory group that functions as an overall sounding board for the project.   This group is made up of representatives of the Puget Sound advisory boards and others in the Puget Sound recovery community. Advisory group members also serve as points of contact and ambassadors for the project. They help ensure the project is reflective of and responsive to real-world recovery challenges. Regular input from the Puget Sound advisory boards helps inform the project.</p>
+			
+			<h3>Want to learn more?</h3>
+			
+			<p>More in-depth information on Future Scenarios, including additional project background, FAQs, and the advisory group, can be found in <a href="https://pspwa.box.com/s/8a776uv94xxlb3cduo3a17zcdyye8hox" target="new">the project’s public Box note.</a></p>
+			
+			<p>If you would like to be involved in this project work, have data to share, or would like a briefing, please get in touch with one of the project leads:</p>
+			
+			<ul class="bullet-size-fix">
+				<li>Katherine Wyatt, <a href="mailto:katherine.wyatt@psp.wa.gov">katherine.wyatt@psp.wa.gov</a></li>
+				<li>Elizabeth McManus, <a href="mailto:emcmanus@rosstrategic.com">emcmanus@rossstrategic.com</a></li>
+			</ul>
+			
+			
+			<p class="last-update">Last updated: 11/2/23</p>
+			<!-- InstanceEndEditable --> </div>
+		
+		<div class="col-sm-2 padding-20-top padding-0-right"> <!-- InstanceBeginRepeat name="right_nav_repeat" --><!-- InstanceBeginRepeatEntry --> <!-- InstanceBeginEditable name="right_nav_title" -->
+			
+			<!-- InstanceEndEditable -->
+			<ul class="nav-rightside-custom">
+				<!-- InstanceBeginEditable name="right_nav_links" -->
+				
+				<!-- InstanceEndEditable -->
+			</ul>
+			<!-- InstanceEndRepeatEntry --><!-- InstanceBeginRepeatEntry --> <!-- InstanceBeginEditable name="right_nav_title" -->
+			
+			<!-- InstanceEndEditable -->
+			<ul class="nav-rightside-custom">
+				<!-- InstanceBeginEditable name="right_nav_links" -->
+				
+				<!-- InstanceEndEditable -->
+			</ul>
+			<!-- InstanceEndRepeatEntry --><!-- InstanceBeginRepeatEntry --> <!-- InstanceBeginEditable name="right_nav_title" -->
+			
+			<!-- InstanceEndEditable -->
+			<ul class="nav-rightside-custom">
+				<!-- InstanceBeginEditable name="right_nav_links" -->
+				
+				<!--		<li role="presentation"> <a href="#">State of the Watersheds</a></li>
+					<li role="presentation"> <a href="#">Health of the Salish Sea Ecosystem Report</a></li>--> 
+				<!-- InstanceEndEditable -->
+			</ul>
+			<!-- InstanceEndRepeatEntry --><!-- InstanceEndRepeat --> </div>
+	</div>
+	<!--END OF ROW --> 
+</div>
+<!--END OF CONTENT CONTAINER -->
+
+<?php include 'includes/footer-inc.html';?>
+
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) --> 
+<script src="js/jquery-1.11.2.min.js"></script> 
+
+<!-- Include all compiled plugins (below), or include individual files as needed --> 
+<script src="js/bootstrap.js"></script> 
+<!-- custom js --> 
+<script src="js/custom.js"></script> 
+<!-- Google Tracking  --> 
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-69373425-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
+</body>
+<!-- InstanceEnd --></html>
