cdf4d648f9577c0dd9ae472a4923a2d7744c1058
ianmc3 <ian.mccabe@psp.wa.gov>
2024-05-07 16:01:10 -0700

    Create sound-safe-infrastructure.php
    Created draft for placeholder

---
+++ sound-safe-infrastructure.php
@@ -0,0 +1,169 @@
+<!DOCTYPE html>
+<html lang="en"><!-- InstanceBegin template="/Templates/page-4-template.dwt" codeOutsideHTMLIsLocked="false" -->
+<head>
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-K5R1WLJ6VJ"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-K5R1WLJ6VJ');
+</script>
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<!-- InstanceBeginEditable name="doctitle" -->
+<title>Puget Sound Partnership Strategic Funding Team</title>
+<!-- InstanceEndEditable -->
+<!-- Bootstrap -->
+<link href="css/bootstrap.css" rel="stylesheet">
+<link href="css/custom.css" rel="stylesheet" type="text/css">
+<link href="css/custom-erika.css" rel="stylesheet" type="text/css">
+<!-- loads the wf-loading class right away to minimize FOUT -->
+<script>document.documentElement.className += ' wf-loading';</script>
+<!-- Font PRENTON TYPEKIT -->
+<script src="https://use.typekit.net/srt5jze.js"></script>
+<script>try{Typekit.load({ async: true });}catch(e){}</script>
+<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+<!--[if lt IE 9]>
+	<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+	<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+<!-- InstanceBeginEditable name="head" -->
+<script>
+/*this variable is used to set the proper nav to active. It should to the order the nav item is in the list*/
+navSelected = 1;
+	
+</script>
+<meta property="og:image" content="http://www.psp.wa.gov/images/inside_image_99_sm.jpg" />
+<!-- InstanceEndEditable -->
+<!-- InstanceParam name="OptionalRegion1" type="boolean" value="true" -->
+<!-- InstanceParam name="OptionalRegion2" type="boolean" value="false" -->
+<!-- InstanceParam name="OptionalRegion3" type="boolean" value="true" -->
+<!-- InstanceParam name="OptionalRegion4" type="boolean" value="false" -->
+<!-- InstanceParam name="OptionalRegion5" type="boolean" value="true" -->
+<!-- InstanceParam name="OptionalRegion6" type="boolean" value="true" -->
+<!-- InstanceParam name="OptionalRegion7" type="boolean" value="false" -->
+<!-- InstanceParam name="OptionalRegion8" type="boolean" value="true" -->
+</head>
+<body>
+<?php include 'includes/modal-inc.html';?>
+<!-- START IMAGE HEADER --> 
+ <!-- InstanceBeginEditable name="overviewphoto" -->
+<header class="overview-page-image-style overview-page-image118">
+</header>
+<!-- InstanceEndEditable -->  <!-- InstanceBeginEditable name="EditRegion11" -->
+<div class="row">
+	<div class="col-xs-12 orange-fill align-center overview-logo-banner"><img src="images/2016-AA-banner-logo-none-small.png" width="146" height="30" alt=""/></div>
+</div>
+<!-- InstanceEndEditable -->
+<div class="container-fluid page-content padding-50-bottom">
+	<div class="row">
+		<div class="col-sm-3 padding-20-top"></div>
+		<div class="col-sm-7 padding-20-top"> <!-- InstanceBeginEditable name="6col_header" -->
+			<h1>Sound safe infrastructure</h1>
+			<!-- InstanceEndEditable --></div>
+		<div class="col-sm-2 padding-20-top"></div>
+	</div>
+	
+	<div class="row"> 
+		<div class="col-sm-3">
+			<div class="nav-leftside-custom">
+				<ul class="nav nav-stacked nav-pills nav-leftside-custom padding-left-0 margin-10-top">
+					<!-- InstanceBeginEditable name="left_nav" -->
+					<?php include 'includes/ln-strategic-funding.html';?>
+					
+					<!-- InstanceEndEditable -->
+				</ul>
+			</div>
+		</div>
+		 
+		<div class="col-sm-7 padding-20-top content-column"> <!-- InstanceBeginEditable name="6col_content" -->
+	<!--
+		<div class="highlightbox">
+			<p>On August 4, 2022, the Environmental Protection Agency (EPA) approved the<a href="https://pspwa.box.com/shared/static/8zak4wiakdy94vc6104er8l3kn9bdxkw.pdf" target="new">2022-2026 Action Agenda adopted by the Leadership Council in June</a> as the Puget Sound National Estuary Program's (NEP) <a href="https://www.epa.gov/nep/comprehensive-conservation-and-management-plans" target="new">Comprehensive Conservation and Management Plan</a>. Learn more about the 2022-2026 Action Agenda below or visit the interactive <a href="https://actionagenda.pugetsoundinfo.wa.gov/2022-2026ActionAgenda" target="new">2022-2026 Action Agenda Explorer</a>.</p>
+		</div>-->
+			
+			<p>The Sound Safe Infrastructure initiative is our vision of how we integrate transportation and restoration,<br>
+			  and the benefits it will have for Puget Sound ecosystem recovery and the transportation sector. Through<br>
+			  Sound Safe Infrastructure we aim to build on and strengthen the alignment between transportation and<br>
+			  Puget Sound ecosystem and salmon recovery to fundamentally unite the restoration and transportation<br>
+		    sectors throughout partnerships, planning, and implementation of projects.</p>
+			<p>			  Collaboration is crucial to the success of restoration of the region’s salmon and shellfish populations, and<br>
+			  the jobs and natural industries they support. To uphold Tribal treaty fishing rights, successful cooperation<br>
+			  between transportation and restoration professionals is needed to ensure that habitat is protected and<br>
+			  restored, so that there are fish to harvest. This cooperation is also essential for achieving Puget Sound<br>
+			  ecosystem recovery goals and supports the Washington State Department of Transportation’s values of<br>
+		    economic vitality, preservation, safety, mobility, environment, and stewardship.</p>
+			<p>We invite partners to engage in this work and take on this shared initiative for increased partnerships<br>
+			  and to create a strong cross-sector collaborative environment for a world-class transportation system<br>
+		    alongside thriving ecological processes and salmon populations</p>
+            <h2><strong>Areas of work</strong></h2>
+            <p>1. Partnerships: Strengthen existing relationships between the transportation and restoration<br>
+              sector and develop new connections when available. Broaden the restoration community to include<br>
+              transportation, and emphasize common values and goals.<br>
+              2. Planning: Strengthen communication between the transportation and restoration sectors as<br>
+              well as explore opportunities to align priorities throughout Puget Sound’s many decision-making<br>
+              bodies.<br>
+              3. Projects: Connect current and future projects from both sectors to maximize mutual benefit,<br>
+              streamline processes, and invest efficiently.<br>
+              4. Funding: Jointly pursue funding for projects which sit at the nexus of restoration and<br>
+            transportation.</p>
+            <h2><strong>anticipated outcomes </strong></h2>
+            <p>1. Broad cross-sectoral adoption of the Sound Safe Infrastructure Initiative and understanding of<br>
+              the benefits it presents to the restoration and transportation communities.<br>
+              2. Establish and strengthen the system of collaboration for restoration and transportation sectors,<br>
+              rooted in shared values and goals.<br>
+              3. Increase communication and coordination of project planning and implementation to support<br>
+              efficient investments in ecosystem restoration, salmon recovery, and transportation networks.<br>
+              4. Create a funding landscape that is supportive of the restoration and transportation work needed<br>
+              for ecosystem and salmon recovery. Increase awareness of funding opportunities and reduce barriers<br>
+            to project application and implementation.</p>
+            <h2><strong>Contact</strong><strong>&nbsp;</strong></h2>
+            <p><strong>Bebbhin Gilbert</strong><u></u></p>
+			
+		<!-- LAST UPDATED -->
+		<p class="last-update">Last updated: 4/4/24</p>
+
+        <!-- InstanceEndEditable --> </div>
+		
+		<div class="col-sm-2 padding-20-top padding-0-right"> <!-- InstanceBeginRepeat name="right_nav_repeat" --><!-- InstanceBeginRepeatEntry --> <!-- InstanceBeginEditable name="right_nav_title" -->
+			<div class="right-nav-title margin-0-top ">SEE ALSO</div>
+			<!-- InstanceEndEditable -->
+			<ul class="nav-rightside-custom">
+				<!-- InstanceBeginEditable name="right_nav_links" -->
+				<li class="active" role="presentation"><a href="https://actionagenda.pugetsoundinfo.wa.gov/2022-2026ActionAgenda" target="_blank"><strong>Action Agenda Explorer</strong></a></li>
+				
+			
+				<!-- InstanceEndEditable -->
+			</ul>
+			<!-- InstanceEndRepeatEntry --><!-- InstanceEndRepeat --> </div>
+	</div>
+	<!--END OF ROW --> 
+</div>
+<!--END OF CONTENT CONTAINER -->
+
+<?php include 'includes/footer-inc.html';?>
+
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) --> 
+<script src="js/jquery-1.11.2.min.js"></script> 
+
+<!-- Include all compiled plugins (below), or include individual files as needed --> 
+<script src="js/bootstrap.js"></script> 
+<!-- custom js --> 
+<script src="js/custom.js"></script> 
+<!-- Google Tracking  --> 
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-69373425-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
+</body>
+<!-- InstanceEnd --></html>
