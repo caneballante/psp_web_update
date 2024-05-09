84520ec0602257b43d93cd2b01b77306848d822e
ianmc3 <ian.mccabe@psp.wa.gov>
2024-05-09 16:52:22 -0700

    Update PSAR-CCA.php

--- PSAR-CCA.php
+++ PSAR-CCA.php
@@ -107,11 +107,8 @@ navSelected = 3;
 				  ➢ <a href="https://pspwa.box.com/s/c4obilmp4wgi1hnz8s4zd01mjxcu4j1u"> List of 2025-2027 PSAR Large Capital Project applications submitted for first round review.</a>
 				  <br>
 			    ➢ <a href="https://wa-psp.maps.arcgis.com/apps/dashboards/0d6ef4eb07164273a68f32e78089dfae"> Map of 2025-2027 PSAR Large Capital proposed projects invited to submit final applications.</a></p>
-				<p class="margin-20-top"><img src="images/PSAR/PSARCCAmap2.png" width="825" height="638.5" alt="Map of 2025-2027 PSAR Large Capital proposed projects"/></p>
-				<iframe src=”https://wa-psp.maps.arcgis.com/apps/dashboards/0d6ef4eb07164273a68f32e78089dfae” width="900" height="820" alt="Map of 2025-2027 PSAR Large Capital proposed projects">
-</iframe>
-
-				<p class="margin-20-top">&nbsp;</p>
+			  <p class="margin-20-top"><img src="images/PSAR/PSARCCAmap2.png" width="825" height="638.5" alt="Map of 2025-2027 PSAR Large Capital proposed projects"/></p>
+			  <p class="margin-20-top">&nbsp;</p>
 		</div>
 			<hr />
 		<div class="row margin-10-top"><br>
