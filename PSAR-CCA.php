f5ab0bae87abba208761ba6ec9ad75ff91f1200c
ianmc3 <ian.mccabe@psp.wa.gov>
2024-05-09 16:18:27 -0700

    Update PSAR-CCA.php

--- PSAR-CCA.php
+++ PSAR-CCA.php
@@ -78,8 +78,9 @@ navSelected = 3;
 		</div>
 		 
 		<div class="col-sm-9 padding-20-top padding-40-right content-column mobile-padding-20-left"> <!-- InstanceBeginEditable name="9col_content" -->
-				<p> The Puget Sound Acquition and Restoration (PSAR) program is partially supported with funding from Washington’s <strong>Climate Commitment Act</strong>. The CCA supports Washington’s climate action efforts by putting cap-and-invest dollars to work reducing climate pollution, creating jobs, and improving public health. Information about the CCA is available at <a href="http://www.climate.wa.gov">www.climate.wa.gov</a>.<br>
-            	<img class="img-responsive floatright" src="images/CCA-primary-logo.png" width="309" height="156" alt=""/></p>
+			<img class="img-responsive floatright" src="images/CCA-primary-logo.png" width="309" height="156" alt=""/>	
+			<p> The Puget Sound Acquition and Restoration (PSAR) program is partially supported with funding from Washington’s <strong>Climate Commitment Act</strong>. The CCA supports Washington’s climate action efforts by putting cap-and-invest dollars to work reducing climate pollution, creating jobs, and improving public health. Information about the CCA is available at <a href="http://www.climate.wa.gov">www.climate.wa.gov</a>.<br>
+            	</p>
 			<div>
 			  <p class="margin-20-top">In 2021, Governor Jay Inslee signed the Climate Commitment Act (CCA). The CCA created a 
 				  program that requires the state’s largest polluters to reduce greenhouse gas pollution.				  Greenhouse gases, such as carbon dioxide and methane, trap the sun’s heat and are raising the 
