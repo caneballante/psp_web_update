4badaf5bda3819e7f1aa6c1f9d6fb4594bf575a2
ianmc3 <ian.mccabe@psp.wa.gov>
2024-07-01 13:26:09 -0700

    Create tribal-relations.php

---
+++ tribal-relations.php
@@ -0,0 +1,196 @@
+<!DOCTYPE html>
+<html lang="en"><!-- InstanceBegin template="/Templates/page-4-template.dwt" codeOutsideHTMLIsLocked="false" -->
+<head>
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-K5R1WLJ6VJ"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-K5R1WLJ6VJ');
+</script>
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<!-- InstanceBeginEditable name="doctitle" -->
+<title>Puget Sound Partnership - Government Relations</title>
+<!-- InstanceEndEditable -->
+<!-- Bootstrap -->
+<link href="css/bootstrap.css" rel="stylesheet">
+<link href="css/custom.css" rel="stylesheet" type="text/css">
+<link href="css/custom-erika.css" rel="stylesheet" type="text/css">
+<!-- loads the wf-loading class right away to minimize FOUT -->
+<script>document.documentElement.className += ' wf-loading';</script>
+<!-- Font PRENTON TYPEKIT -->
+<script src="https://use.typekit.net/srt5jze.js"></script>
+<script>try{Typekit.load({ async: true });}catch(e){}</script>
+<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+<!--[if lt IE 9]>
+	<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+	<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+<!-- InstanceBeginEditable name="head" -->
+<script>
+/*this variable is used to set the proper nav to active. It should to the order the nav item is in the list*/
+  	navSelected = 1;
+</script>
+<!-- InstanceEndEditable -->
+<!-- InstanceParam name="OptionalRegion1" type="boolean" value="true" -->
+<!-- InstanceParam name="OptionalRegion2" type="boolean" value="false" -->
+<!-- InstanceParam name="OptionalRegion3" type="boolean" value="true" -->
+<!-- InstanceParam name="OptionalRegion4" type="boolean" value="false" -->
+<!-- InstanceParam name="OptionalRegion5" type="boolean" value="true" -->
+<!-- InstanceParam name="OptionalRegion6" type="boolean" value="true" -->
+<!-- InstanceParam name="OptionalRegion7" type="boolean" value="false" -->
+<!-- InstanceParam name="OptionalRegion8" type="boolean" value="true" -->
+</head>
+<body>
+<?php include 'includes/modal-inc.html';?>
+<!-- START IMAGE HEADER --> 
+ <!-- InstanceBeginEditable name="overviewphoto" -->
+<header class="overview-page-image-style overview-page-image124">
+	<div class="overview-quote pull-right">
+		<h3>&nbsp;</h3>
+	</div>
+</header>
+<!-- InstanceEndEditable -->  <!-- InstanceBeginEditable name="EditRegion11" -->
+<div class="row">
+	<div class="col-sm-12 orange"></div>
+</div>
+<!-- InstanceEndEditable -->
+<div class="container-fluid page-content padding-50-bottom">
+	<div class="row">
+		<div class="col-sm-3 padding-20-top"></div>
+		<div class="col-sm-7 padding-20-top"> <!-- InstanceBeginEditable name="6col_header" -->
+			<h1>Tribal RELATIONS</h1>
+			<p>&nbsp;</p>
+			<!-- InstanceEndEditable --></div>
+		<div class="col-sm-2 padding-20-top"></div>
+	</div>
+	
+	<div class="row"> 
+		<div class="col-sm-3">
+			<div class="nav-leftside-custom">
+				<ul class="nav nav-stacked nav-pills nav-leftside-custom padding-left-0 margin-10-top">
+					<!-- InstanceBeginEditable name="left_nav" -->
+				<?php include 'includes/ln-gov.html';?>
+				<!-- InstanceEndEditable -->
+				</ul>
+			</div>
+		</div>
+		 
+		<div class="col-sm-7 padding-20-top content-column"> <!-- InstanceBeginEditable name="6col_content" -->
+			<div class="highlightbox margin-0-top margin-20-bottom">
+			<p>Federally recognized Tribes (Tribes)  retain their rights since time immemorial and are as such inherently sovereign.  Tribes have a unique trust relationship with the United States federal  government as a result of treaties, legislation, and Executive Orders. Their  status as sovereign nations entitles them to a direct government-to-government  relationship with the Federal Government, independent of the states or local  jurisdictions where these Tribes may reside. Tribal nations retain their  inherent rights and are, as such, sovereign and operate their own Tribal  governments to govern their Tribal lands, citizenry and reservation populations  through self-governance and self-determination. </p>
+			<p>			  Under eight treaties negotiated by Territorial Governor  Stevens on behalf of the United States, Tribes ceded 64 million acres of land  to the U.S. for non-Indian settlement and the subsequent establishment of  Washington State. Tribes retained about 6 million acres of reservation land and  specifically retained the right to take fish in their &ldquo;usual and accustomed&rdquo;  areas along with the privilege of hunting on &ldquo;open and unclaimed lands,&rdquo; among  other things.<br>
+			  In 1989, the 26 federally recognized tribes and  the State of Washington under Governor Booth Gardner signed the <a href="https://goia.wa.gov/relations/centennial-accord">Centennial  Accord</a>. This made Washington  State and the Tribes historically the first in the nation to establish a  relationship to strengthen tribal government-to-government relations on issues  of shared interests and to promote collaborative best management practices. Washington  State recognizes its Indigenous peoples&rsquo; inherent right to exercise their  language, cultural beliefs, protection of Tribal resources,  sense of place and territory through their existence and inhabitance of  Washington territory since time immemorial. </p>
+			<p> Washington State Cabinet Agencies maintain a  government-to-government relation with 29 federally recognized Tribes located  within Washington; there are also an additional six federally recognized tribes  are located outside of the state, but who have interests, traditional territory  or treaty rights within the state- who may be involved in consultation  processes.</p>
+			<p> As a Cabinet Agency with a regionally  bound directive, Puget Sound Partnership engages with Tribal governments, with  a focus on the 19 federally recognized Tribal nations within the Puget Sound  and Salish Sea. across Washington state to foster a strong and effective  government-to-government relationship. <br>
+			  The Partnership recognizes, respects and  honors Tribal nations as unique, distinct, sovereign people with inherent  rights and connections to the Puget Sound. The Partnership acknowledges its  government-to-government duties and will prioritize consultation with federally  recognized Tribal nations. </p>
+            <h2>Invitations for Consultation and Informational DTLLs</h2>
+            <p>The Partnership invites Tribes to engage in formal  consultation in the development of policies, agreements, and program  implementation that directly affects Tribes and Tribal people. The  Partnership&rsquo;s policy is to consult on a government-to-government basis with  Tribes when our actions and decisions may affect tribal treaty and sovereign  interests.</p>
+            <p> Our Dear Tribal Leader Letters (DTLL) serve as formal  written mechanisms to notify Tribal Leaders from all federally recognized  tribes of consultation activities and invites Tribal Leaders to events or  meetings to provide other critical information to tribes. Other elected Tribal  leaders and staff may be included in these invites. <br>
+              <a href="https://pspwa.box.com/s/ehwsoz1ug2icmnwkpvyavv89et8j52dj"><strong>Archived</strong></a><strong>  </strong><br>
+              <a href="https://pspwa.box.com/s/h0vc9kh8ggzwws2434jw3kq1n39bxntw"><strong>Active</strong></a><strong></strong></p>
+            <ul>
+              <li><strong>Option 1: Reserve for individual links</strong></li>
+              <li><strong>Option 2: When applicable, this statment: &ldquo;No  active consultation periods at this time&rdquo;</strong></li>
+            </ul>
+            <h2>Tribal Engagement </h2>
+            <p>Ongoing collaboration with Tribal nations and consortia is  central to our collective effort of Puget Sound recovery. Tribal nations lead,  maintain, and participate in forums that guide and inform salmon and Puget  Sound recovery and the development and implementation of the 2022-2026 Puget  Sound Action Agenda. </p>
+            <ul>
+              <li>Tribal representatives hold seats on  our three boards established in statute: </li>
+              <ul>
+                <li><strong>Leadership  Council</strong> (Lower Elwha Klallam Tribe,  Skokomish Indian Tribe) </li>
+                <li><strong>Ecosystem  Coordination Board</strong> (Jamestown S&rsquo;Klallam, Lummi Nation,  Nisqually Indian Tribe) </li>
+                <li><strong>Science  Panel</strong> (Northwest Indian Fisheries  Commission (NWIFC)). </li>
+              </ul>
+              <li>Tribes hold multiple seats on our  two advisory boards: </li>
+              <li><strong>Puget Sound Salmon Recovery Council</strong> <em>N</em><em>ote: Each federally recognized Tribe within the Puget  Sound region can appoint a primary and alternate delegate to this Council.</em></li>
+              <li><strong>Puget  Sound Ecosystem Monitoring Program</strong> Steering Committee. </li>
+            </ul>
+            <h2>Partnership Tribal Co-Management Council (PTCC)</h2>
+            <p>Co-chaired by the Partnership's Leadership Council Chair  and a Northwest Indian Fisheries Commissioner, PTCC is a collaborative space,  facilitating communications and coordination between the Partnership and  sovereign Tribal nations. PTCC promotes ongoing State-Tribal  government-to-government priority setting and information sharing and provides  a reliable format for early and often engagement. The Tribal Affairs Manager  and Northwest Indian Fisheries Commission staff provide support on agenda  development and meeting logistics. </p>
+            <p> The PTCC promotes, but does not replace, timely state-tribal  government-to-government communication and consultation between each individual  tribal government and the Partnership to develop common objectives and  priorities for the protection and restoration of the Puget Sound ecosystem. <br>
+              The PTCC ensures policy decisions receive the benefit of  tribal technical expertise, increases opportunities to resolve differences  amongst the governments in policies or approaches, and provides an opportunity  for other State agencies to join and participate to increase coordination and  cooperation. <br>
+              PTCC typically meets the first Thursday of every month  and is open to all Tribal elected officials and staff. For meeting information please contact the  Partnership&rsquo;s <a href="mailto:lea-anne.burke@pap.wa.gov">Tribal Affairs Manager</a>.</p>
+            <h2>Centennial Accord </h2>
+            <p>Our Centennial Accord Implementation plan is available at the  Governor&rsquo;s <a href="https://goia.wa.gov/relations/centennial-accord">Office of Indian Affairs</a> website. <br>
+              Agency Centennial  Accord highlights are also available at the Governor&rsquo;s Office of Indian Affairs <a href="https://goia.wa.gov/relations">State-Tribal Relations / Centennial Accord | GOIA (wa.gov)</a> webpage.</p>
+            <h2>Healthy Environment for All (HEAL) Act </h2>
+            <p>The objective of the HEAL Act is to reduce environmental and health  disparities throughout Washington State and improve the health of all  Washington State residents. The Washington State Legislature passed Senate Bill  5141, <a href="http://lawfilesext.leg.wa.gov/biennium/2021-22/Pdf/Bills/Session%20Laws/Senate/5141-S2.SL.pdf?q=20210624103236">HEAL Act</a>, to be effective on July 25, 2021. </p>
+            <p> Key priorities of the HEAL Act include reducing exposure to  environmental hazards within Tribal lands, ensuring Tribal sovereignty and  rights in environmental justice (EJ), and eliminating environmental and health  disparities in disadvantaged, vulnerable, and low-income populations.<br>
+              Read more about the <a href="https://psp.wa.gov/HEAL.php">Partnership&rsquo;s HEAL Act</a> program and implementation.<br>
+              HEAL Act Tribal Government Engagement  Guide (Link soon!!)<br>
+              HEAL Act Native American Community  Engagement Guide (Link soon!!)</p><h2>Map of reservations</h2>
+              <p><br>
+                <a href="https://gcc02.safelinks.protection.outlook.com/?url=https%3A%2F%2Fpspwa.box.com%2Fs%2Fne189qesovfa5319cqu2z1u17n5s7oz2&data=05%7C02%7Clea-anne.burke%40psp.wa.gov%7C69f7fece3e71425a346508dc6f9c0149%7C11d0e217264e400a8ba057dcc127d72d%7C0%7C0%7C638507963024255736%7CUnknown%7CTWFpbGZsb3d8eyJWIjoiMC4wLjAwMDAiLCJQIjoiV2luMzIiLCJBTiI6Ik1haWwiLCJXVCI6Mn0%3D%7C0%7C%7C%7C&sdata=Bhq%2BPFpcrF%2FXsg0jbJ03jXYtRaoM%2BF0zWZkILw%2Fush8%3D&reserved=0">MAP GOES HERE</a><a href="https://pspwa.box.com/s/qzq53cwupaq8tfcswj8lqgqginxd2vi2"></a>
+                </p>
+              </p>
+              <div>
+              <div>
+                <h2>Links and Resources</h2>
+                <ul>
+                  <li><a href="https://goia.wa.gov/">GOIA | Governor's Office of Indian Affairs  (GOIA) (wa.gov)</a></li>
+                  <li>Government-to-Government  Consultation <a href="https://app.leg.wa.gov/rcw/default.aspx?cite=43.376">Chapter 43.376 RCW: GOVERNMENT-TO-GOVERNMENT  RELATIONSHIP WITH INDIAN TRIBES (wa.gov)</a></li>
+                  <li>Healthy  Environment for All Act&nbsp; <a href="https://app.leg.wa.gov/RCW/default.aspx?cite=70A.02.100">RCW 70A.02.100: Tribal consultation. (wa.gov)</a></li>
+                  <li>Department  of Archaeology and Historic Preservation <a href="https://dahp.wa.gov/archaeology/tribal-consultation-information">Tribal Consultation Information | Washington  State Department of Archaeology &amp; Historic Preservation (DAHP)</a></li>
+                  <li>Washington  State Department of Fish and Wildlife <a href="https://wdfw.wa.gov/fishing/management/north-falcon">North of Falcon</a></li>
+                  <li><a href="https://www.washingtontribes.org/">Washington Tribes</a></li>
+                  <li><a href="https://nwifc.org/">Northwest Indian Fisheries Commission (NWIFC) |  Serving the Treaty Tribes in Western Washington</a></li>
+                  <li><a href="http://pnptc.org/">Point No Point Treaty Council – Port Gamble and  Jamestown S&rsquo;Klallam Tribes (pnptc.org)</a></li>
+                  <li><a href="http://skagitcoop.org/">Skagit River System Cooperative  (skagitcoop.org)</a></li>
+                  <li><a href="https://atnitribes.org/">Affiliated  Tribes of Northwest Indians – Dedicated to Promoting Tribal Self Determination  &amp; Sovereignty (atnitribes.org</a> </li>
+                </ul>
+              </div>
+              <div> </div>
+            </div>
+			</div>
+			<h2 class="margin-0-top">&nbsp;</h2>
+			<p class="last-update">Last updated: 7/1/24</p>
+			<!-- InstanceEndEditable --> </div>
+		
+		<div class="col-sm-2 padding-20-top padding-0-right"> <!-- InstanceBeginRepeat name="right_nav_repeat" --><!-- InstanceBeginRepeatEntry --> <!-- InstanceBeginEditable name="right_nav_title" -->
+			<div class="right-nav-title margin-0-top ">CONTACT</div>
+			<!-- InstanceEndEditable -->
+			<ul class="nav-rightside-custom">
+				<!-- InstanceBeginEditable name="right_nav_links" -->
+				<p class="right-nav-contact">For questions about Tribal Relations:<br />
+					<br>
+					<strong>Lea Anne Burke,</strong> <br>
+					Tribal Affairs Manage<br>
+					<a href="mailto:don.gourlie@psp.wa.gov">lea-anne.burke@psp.wa.gov</a><br>
+					360.682.7532<br><br>
+				<a href="https://app.smartsheet.com/b/form/ab94add956f34fbdaa9114193168738e" target="new">Click here</a> to provide feedback on the content and functionality of this page.</p>
+				<!-- InstanceEndEditable -->
+			</ul>
+			<!-- InstanceEndRepeatEntry --><!-- InstanceEndRepeat --> </div>
+	</div>
+	<!--END OF ROW --> 
+</div>
+<!--END OF CONTENT CONTAINER -->
+
+<?php include 'includes/footer-inc.html';?>
+
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) --> 
+<script src="js/jquery-1.11.2.min.js"></script> 
+
+<!-- Include all compiled plugins (below), or include individual files as needed --> 
+<script src="js/bootstrap.js"></script> 
+<!-- custom js --> 
+<script src="js/custom.js"></script> 
+<!-- Google Tracking  --> 
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-69373425-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
+</body>
+<!-- InstanceEnd --></html>
