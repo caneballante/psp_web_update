5796d2115df3e8017342f0a5f64c548406ab1719
jon bridgman <jonbridgman@gmail.com>
2024-07-01 12:58:29 -0700

    Remove old google tracking

--- googletagtest.php
+++ googletagtest.php
@@ -1,21 +1,29 @@
 <!DOCTYPE html>
-<html lang="en"><!-- InstanceBegin template="/Templates/page-4-template.dwt" codeOutsideHTMLIsLocked="false" -->
+<html lang="en">
 <head>
 <!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-K5R1WLJ6VJ"></script>
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-BGEZ5L3EJY"></script>
 <script>
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
 
-  gtag('config', 'G-K5R1WLJ6VJ');
+  gtag('config', 'G-BGEZ5L3EJY');
+</script>	
+	
+	<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-BGEZ5L3EJY"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-BGEZ5L3EJY');
 </script>
 <meta charset="UTF-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<!-- InstanceBeginEditable name="doctitle" -->
-<title>Puget Sound Partnership - Contact</title>
-<!-- InstanceEndEditable -->
+<title>Action Agenda update</title>
 <!-- Bootstrap -->
 <link href="css/bootstrap.css" rel="stylesheet">
 <link href="css/custom.css" rel="stylesheet" type="text/css">
@@ -31,34 +39,27 @@
 	<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
 	<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
-<!-- InstanceBeginEditable name="head" -->
 <script>
 /*this variable is used to set the proper nav to active. It should to the order the nav item is in the list*/
-  	navSelected = 7;
+navSelected = 5;
+	
 </script>
-<!-- InstanceEndEditable -->
-<!-- InstanceParam name="OptionalRegion1" type="boolean" value="true" -->
-<!-- InstanceParam name="OptionalRegion2" type="boolean" value="false" -->
-<!-- InstanceParam name="OptionalRegion3" type="boolean" value="true" -->
-<!-- InstanceParam name="OptionalRegion4" type="boolean" value="false" -->
-<!-- InstanceParam name="OptionalRegion5" type="boolean" value="true" -->
-<!-- InstanceParam name="OptionalRegion6" type="boolean" value="true" -->
-<!-- InstanceParam name="OptionalRegion7" type="boolean" value="false" -->
-<!-- InstanceParam name="OptionalRegion8" type="boolean" value="false" -->
+<meta property="og:image" content="http://www.psp.wa.gov/images/inside_image_99_sm.jpg" />
 </head>
 <body>
 <?php include 'includes/modal-inc.html';?>
 <!-- START IMAGE HEADER --> 
- <!-- InstanceBeginEditable name="overviewphoto" -->
-<header class="overview-page-image-style overview-page-image12"> </header>
-<!-- InstanceEndEditable -->  
+<header class="overview-page-image-style overview-page-image114">
+</header>
+<div class="row">
+	<div class="col-xs-12 orange-fill align-center overview-logo-banner"><img src="images/2016-AA-banner-logo-none-small.png" width="146" height="30" alt=""/></div>
+</div>
 <div class="container-fluid page-content padding-50-bottom">
 	<div class="row">
 		<div class="col-sm-3 padding-20-top"></div>
-		<div class="col-sm-7 padding-20-top"> <!-- InstanceBeginEditable name="6col_header" -->
-			<h1>SIGN UP FOR UPDATES</h1>
-			<p>&nbsp;</p>
-			<!-- InstanceEndEditable --></div>
+		<div class="col-sm-7 padding-20-top">
+<h1>Action Agenda Update</h1>
+			</div>
 		<div class="col-sm-2 padding-20-top"></div>
 	</div>
 	
@@ -66,20 +67,19 @@
 		<div class="col-sm-3">
 			<div class="nav-leftside-custom">
 				<ul class="nav nav-stacked nav-pills nav-leftside-custom padding-left-0 margin-10-top">
-					<!-- InstanceBeginEditable name="left_nav" -->
-	
-				<!-- InstanceEndEditable -->
-				</ul>
+</ul>
 			</div>
 		</div>
 		 
-		<div class="col-sm-7 padding-20-top content-column"> <!-- InstanceBeginEditable name="6col_content" -->
-			<?php include 'includes/gov-delivery-inc-opt2.html';?>
-			
-			
-			<!-- InstanceEndEditable --> </div>
+		<div class="col-sm-7 padding-20-top content-column">
+<p>This page has been retired. You can find information on the 2022-2026 Action Agenda <a href="https://www.psp.wa.gov/2022AAupdate.php">here</a>.</p> 
+        </div>
 		
-		<div class="col-sm-2 padding-20-top padding-0-right"> <!-- InstanceBeginRepeat name="right_nav_repeat" --><!-- InstanceEndRepeat --> </div>
+		<div class="col-sm-2 padding-20-top padding-0-right">
+<div class="right-nav-title margin-0-top "></div>
+			<ul class="nav-rightside-custom">
+			</ul>
+			</div>
 	</div>
 	<!--END OF ROW --> 
 </div>
@@ -97,4 +97,4 @@
 <!-- Google Tracking  --> 
 
 </body>
-<!-- InstanceEnd --></html>
+</html>
