7a93ba998d082b7c726fc564f5332ecfe1d2656b
ianmc3 <ian.mccabe@psp.wa.gov>
2024-07-30 14:40:07 -0700

    Create funding-barriers.php

---
+++ funding-barriers.php
@@ -0,0 +1,228 @@
+<!DOCTYPE html>
+<html lang="en"><!-- InstanceBegin template="/Templates/page-4-template.dwt" codeOutsideHTMLIsLocked="false" -->
+<head>
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-K5R1WLJ6VJ"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-K5R1WLJ6VJ');
+</script>
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<!-- InstanceBeginEditable name="doctitle" -->
+<title>Funding Barriers</title>
+<!-- InstanceEndEditable -->
+<!-- Bootstrap -->
+<link href="css/bootstrap.css" rel="stylesheet">
+<link href="css/custom.css" rel="stylesheet" type="text/css">
+<link href="css/custom-erika.css" rel="stylesheet" type="text/css">
+<!-- loads the wf-loading class right away to minimize FOUT -->
+<script>document.documentElement.className += ' wf-loading';</script>
+<!-- Font PRENTON TYPEKIT -->
+<script src="https://use.typekit.net/srt5jze.js"></script>
+<script>try{Typekit.load({ async: true });}catch(e){}</script>
+<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+<!--[if lt IE 9]>
+	<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+	<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+<!-- InstanceBeginEditable name="head" -->
+<script>
+/*this variable is used to set the proper nav to active. It should to the order the nav item is in the list*/
+navSelected = 4;
+	
+</script>
+<meta property="og:image" content="http://www.psp.wa.gov/images/inside_image_99_sm.jpg" />
+<!-- InstanceEndEditable -->
+<!-- InstanceParam name="OptionalRegion1" type="boolean" value="true" -->
+<!-- InstanceParam name="OptionalRegion2" type="boolean" value="false" -->
+<!-- InstanceParam name="OptionalRegion3" type="boolean" value="true" -->
+<!-- InstanceParam name="OptionalRegion4" type="boolean" value="false" -->
+<!-- InstanceParam name="OptionalRegion5" type="boolean" value="true" -->
+<!-- InstanceParam name="OptionalRegion6" type="boolean" value="true" -->
+<!-- InstanceParam name="OptionalRegion7" type="boolean" value="false" -->
+<!-- InstanceParam name="OptionalRegion8" type="boolean" value="true" -->
+</head>
+<body>
+<?php include 'includes/modal-inc.html';?>
+<!-- START IMAGE HEADER --> 
+ <!-- InstanceBeginEditable name="overviewphoto" -->
+<header class="overview-page-image-style overview-page-image118">
+</header>
+<!-- InstanceEndEditable -->  <!-- InstanceBeginEditable name="EditRegion11" -->
+<div class="row">
+	<div class="col-xs-12 orange-fill align-center overview-logo-banner"><img src="images/2016-AA-banner-logo-none-small.png" width="146" height="30" alt=""/></div>
+</div>
+<!-- InstanceEndEditable -->
+<div class="container-fluid page-content padding-50-bottom">
+	<div class="row">
+		<div class="col-sm-3 padding-20-top"></div>
+		<div class="col-sm-7 padding-20-top"> <!-- InstanceBeginEditable name="6col_header" -->
+			<h1>Funding Barriers</h1>
+			<!-- InstanceEndEditable --></div>
+		<div class="col-sm-2 padding-20-top"></div>
+	</div>
+	
+	<div class="row"> 
+		<div class="col-sm-3">
+			<div class="nav-leftside-custom">
+				<ul class="nav nav-stacked nav-pills nav-leftside-custom padding-left-0 margin-10-top">
+					<!-- InstanceBeginEditable name="left_nav" -->
+					<?php include 'includes/ln-strategic-funding.html';?>
+					
+					<!-- InstanceEndEditable -->
+				</ul>
+			</div>
+		</div>
+		 
+		<div class="col-sm-7 padding-20-top content-column"> <!-- InstanceBeginEditable name="6col_content" -->
+	<!--
+		<div class="highlightbox">
+			<p>On August 4, 2022, the Environmental Protection Agency (EPA) approved the<a href="https://pspwa.box.com/shared/static/8zak4wiakdy94vc6104er8l3kn9bdxkw.pdf" target="new">2022-2026 Action Agenda adopted by the Leadership Council in June</a> as the Puget Sound National Estuary Program's (NEP) <a href="https://www.epa.gov/nep/comprehensive-conservation-and-management-plans" target="new">Comprehensive Conservation and Management Plan</a>. Learn more about the 2022-2026 Action Agenda below or visit the interactive <a href="https://actionagenda.pugetsoundinfo.wa.gov/2022-2026ActionAgenda" target="new">2022-2026 Action Agenda Explorer</a>.</p>
+		</div>-->
+			
+		<p>Our vision is to address barriers Tribes and local partners have in acquiring funding, 
+			  creating a more efficient, equitable system that increases funding for salmon and 
+			  ecosystem recovery work. Through the information provided below, we aim to provide
+			  an update on these lines of work, including an overview of the funding barriers<br>
+		  framework and the Strategic Funding Team’s decision to begin addressing the issues of
+		  <strong>match requirements</strong> and <strong>multiple application demands</strong>. </p>
+			<p>Below please find a 12-minute video that summarizes this work here:</p>
+			<p>Website Funding Barriers Recording.mp4 | Powered by Box</p>
+			<p>We define a<strong> funding barrier</strong> as systemic roadblocks that hinder one’s ability to access and acquire funding needed to support projects and initiatives. These barriers can
+			  include things like complicated application processes or not enough capacity to look for
+			  available funds; however, for our purposes, they do not include solely a general lack of 
+			  funds, as funding needs or funding gaps are being tackled separately on the Strategic
+		    Funding Team.</p>
+		<p> To identify and organize funding barriers faced by Tribes and local partners in a way that is actionable, the Strategic Funding Team conducted an analysis of over 400
+			  documents leading to the creation of a funding barriers framework. Through this 
+			  framework, we organized funding barriers identified by Tribes and local partners into 
+			  four primary funding barriers including: 1) Misalignment of Scale, 2) Systemic 
+			  Application Constraints, 3) Failed Governance, and 4) Insufficient Organizational
+			  Capacity. As these are complex and dynamic challenges, the Strategic Funding Team
+			  identified enabling conditions that not only contribute to each of these barriers but also
+			  occur as a result of these barriers. Addressing any one of these primary funding barriers<br>
+		  as well as the enabling conditions will require a large amount of capacity, coordination,
+		  and systems change. With this in mind, as well as our team’s capacity, it was determined
+		  to begin addressing these barriers at the enabling conditions level. Below is a visual
+		  representation of how we understand these issues to be interacting with one another.
+		  This information will be summarized in our <strong>Funding Barriers Framework
+		    Document </strong>(expected for release Fall 2024).</p>
+		<p>IMAGE</p>
+		<p>The Strategic Funding Team weighed the pros and cons of addressing each enabling
+			  condition, reviewed identified solutions discussed within previous forums, and held
+			  internal discussions to make decisions of where to focus. Five criteria were also used to
+			  weigh these enabling conditions between one another. These criteria included whether
+	    addressing each enabling condition: </p>
+		<ol>
+		  <li>Helps to address other issues </li>
+		  <li>Contributes to
+		    EEJ goals</li>
+		  <li>Has momentum throughout the region that we can build on and learn
+		    from</li>
+		  <li>Can be addressed quickly, and 5) seems feasible given support (or lack thereof)
+		    for these issues already.</li>
+	    </ol>
+		<p> As a result of this selection process, the Strategic Funding Team will begin by addressing:		    </p>
+		<p>1) match requirements </p>
+		<p>2) multiple application demands over the next
+		    coming years. </p>
+		<p>Our team is dedicated to leading the implementation of strategies to
+			  address these enabling conditions, which we hope will create a more efficient and
+		    equitable funding application process. </p>
+		<h2><strong>CONNECTION TO MANAGEMENT BOARDS</strong></h2></p>
+			<p>Since September of 2020, the Leadership Council and Ecosystem Coordination Board
+			  have hosted local forums to provide Local Integrating Organizations (LIOs), Lead
+			  Entities (LEs), and other partners the opportunity to identify and help troubleshoot
+		    barriers that local partners are facing in implementing recovery plans and priorities. From 2022 to 2024, barriers related to grant funding have featured prominently in
+		    recent local forums with the Leadership Council and Ecosystem Coordination Board.</p>
+		<p> One of the consistent messages from LIOs during these forums is the large burden
+			  associated with current grant program design and administration that perpetuate
+			  inequities (between rural and urban areas) and continue to strain capacity of project
+			  sponsors (especially those of smaller organizations and Tribes, non-profits, and rural<br>
+			  communities). To address these burdens felt by project sponsors, LIOs have asked the
+			  Leadership Council to support increased regional coordination, regional allocations,
+			  streamlined grant processes, elimination of duplicative applications,
+	        reduced/eliminated match requirements, and exploration of integrated watershed-
+	        based portfolios. These same challenges have been voiced by Tribes and LEs as well.</p>
+			<p> This work aims to address these concerns and asks brought forth to the Management
+	        Boards by our partners. The Leadership Council and Puget Sound Partnership continue
+	        to address several of these barriers through two mechanisms: 1) by strengthening the
+	        ability of the Align Partnership to advance efforts that crosscut and streamline state
+	        grant programs; and 2) by establishing a Strategic Funding Team to address additional<br>
+	        funding-related barriers and increase capacity to secure funding.</p><h2><strong>ANTICIPATED NEXT STEPS &amp; OUTCOMES</strong></h2></p>
+		<p> Addressing any of the primary funding barriers or enabling conditions will require
+			  significant systems change. The landscape to apply for and acquire funding varies
+			  greatly as each funder’s system is different. That in and of itself is a barrier, but it is also
+			  an opportunity to learn what is and is not working. The Strategic Funding Team will be
+			  meeting with Tribes, LIOs, LEs, and state and federal agencies to review funding
+			  programs, identify points of collaboration, highlight case studies, document lessons
+			  learned, and build a justification for the kind of change needed to address the selected
+			  enabling conditions. This will be incorporated into action plans with specific strategies
+		    and tasks to address the selected enabling conditions.</p>
+			<p> We will be requesting support from local partners, Tribes, and funders as needed to
+			  develop these strategies to address match requirements and multiple application
+			  demands. The Strategic Funding Team will then brief the management boards on the
+			  strategies developed to specifically tackle<strong> match requirements</strong> and <strong>multiple application
+		    demands. </strong>The Strategic Funding Team will then brief the management boards on the
+		    strategies developed to specifically tackle match requirements and multiple application
+		    demands and seek feedback. </p><h2><strong>PARTNER ENGAGEMENT &amp; REQUEST FOR INPUT</strong></h2></p>
+			<p> We invite partners to engage in this work and share experiences with various funding
+		    barriers, which is needed to tackle these huge issues together. </p><h2><strong>FOR TRIBES AND LOCAL PARTNERS</strong></h2></p>
+			<p> If you have experienced or are experiencing barriers to accessing, receiving, and
+		    implementing funding, please let us know by filling out this quick form <a href="https://app.smartsheet.com/b/form/3e88c1292c5d468596221957fcb1fe57" target="new"><strong>here</strong></a>. </p><h2><strong>FOR INTERNAL PSP STAFF, FUNDERS, AND OTHER INTERESTED
+			PARTIES</strong></h2></p>
+			<p> If you hear of funding barriers that our partners are facing, please let us know by filling
+		    out this quick form <a href="https://app.smartsheet.com/b/form/eb5e60e65da845e797215c35fc90d745" target="new"><strong>here</strong></a>. </p><h2><strong>FOR FUNDERS</strong></h2></p>
+			<p> If you would like a summary of funding barriers identified through our previous work
+			  and encountered by partners as they specifically relate to your funding program, please
+			  submit a request <a href="https://app.smartsheet.com/b/form/62a069e9e72a443e90a8d25721f1c039" target="new"><strong>here</strong></a>.</p>
+			<h2><strong>Contact</strong><strong>&nbsp;</strong></h2>
+            <p><strong>Jessica Reynolds</strong><br>
+              Funding Barrier Strategist<br>
+              360.870.6165 | <a href="mailto:jessica.reynolds@psp.wa.gov" target="new">jessica.reynolds@psp.wa.gov</a><br>
+            </p>
+        <p><br>
+        </p>
+        <p></p>
+			
+		<!-- LAST UPDATED -->
+		<p class="last-update">Last updated: 7/30/24</p>
+
+        <!-- InstanceEndEditable --> </div>
+		
+		<div class="col-sm-2 padding-20-top padding-0-right"> <!-- InstanceBeginRepeat name="right_nav_repeat" --><!-- InstanceBeginRepeatEntry --> <!-- InstanceBeginEditable name="right_nav_title" -->
+			<div class="right-nav-title margin-0-top ">SEE ALSO</div>
+			<!-- InstanceEndEditable -->
+			<ul class="nav-rightside-custom">
+				<!-- InstanceBeginEditable name="right_nav_links" -->
+				<li class="active" role="presentation"><a href="https://actionagenda.pugetsoundinfo.wa.gov/2022-2026ActionAgenda" target="_blank"><strong>Action Agenda Explorer</strong></a></li>
+				
+			
+				<!-- InstanceEndEditable -->
+			</ul>
+			<!-- InstanceEndRepeatEntry --><!-- InstanceEndRepeat --> </div>
+	</div>
+	<!--END OF ROW --> 
+</div>
+<!--END OF CONTENT CONTAINER -->
+
+<?php include 'includes/footer-inc.html';?>
+
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) --> 
+<script src="js/jquery-1.11.2.min.js"></script> 
+
+<!-- Include all compiled plugins (below), or include individual files as needed --> 
+<script src="js/bootstrap.js"></script> 
+<!-- custom js --> 
+<script src="js/custom.js"></script> 
+<!-- Google Tracking  -->
+<!-- InstanceBeginEditable name="EditRegion12" -->EditRegion12<!-- InstanceEndEditable --> 
+
+</body>
+<!-- InstanceEnd --></html>
