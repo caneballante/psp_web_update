b5cda6ebc9e270e99b4e3db0dc4d6df1aaed6179
ianmc3 <ian.mccabe@psp.wa.gov>
2024-08-05 11:44:04 -0700

    Update funding-barriers.php

--- funding-barriers.php
+++ funding-barriers.php
@@ -86,128 +86,35 @@ navSelected = 4;
 			<p>On August 4, 2022, the Environmental Protection Agency (EPA) approved the<a href="https://pspwa.box.com/shared/static/8zak4wiakdy94vc6104er8l3kn9bdxkw.pdf" target="new">2022-2026 Action Agenda adopted by the Leadership Council in June</a> as the Puget Sound National Estuary Program's (NEP) <a href="https://www.epa.gov/nep/comprehensive-conservation-and-management-plans" target="new">Comprehensive Conservation and Management Plan</a>. Learn more about the 2022-2026 Action Agenda below or visit the interactive <a href="https://actionagenda.pugetsoundinfo.wa.gov/2022-2026ActionAgenda" target="new">2022-2026 Action Agenda Explorer</a>.</p>
 		</div>-->
 			
-		<p>Our vision is to address barriers Tribes and local partners have in acquiring funding, 
-			  creating a more efficient, equitable system that increases funding for salmon and 
-			  ecosystem recovery work. Through the information provided below, we aim to provide
-			  an update on these lines of work, including an overview of the funding barriers framework and the Strategic Funding Team’s decision to begin addressing the issues of
-		  <strong>match requirements</strong> and <strong>multiple application demands</strong>. </p>
-			<p>Below please find a 12-minute video that summarizes this work here:</p>
+		<p><em>Our  vision is to address barriers Tribes and local partners have in acquiring  funding, creating a more efficient and fair system that increases funding for  salmon and ecosystem recovery work</em>.</p>
+		<h2>Overview</h2>
+		<p>Since  September of 2020, the Leadership Council and Ecosystem Coordination Board have  hosted local forums to provide Local Integrating Organizations (LIOs), Lead  Entities (LEs), and other partners the opportunity to identify and help  troubleshoot barriers that local partners are facing in implementing recovery  plans and priorities. From 2022 to 2024, barriers related to grant funding have  featured prominently in recent local forums with the Leadership Council and  Ecosystem Coordination Board. </p>
+			<p>Please find a 12-minute video that summarizes this work below:</p>
 			<p><div style="padding:56.25% 0 0 0;position:relative;"><iframe src="https://player.vimeo.com/video/993016997?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" frameborder="0" allow="autoplay; fullscreen; picture-in-picture; clipboard-write" style="position:absolute;top:0;left:0;width:100%;height:100%;" title="Addressing Funding Barriers"></iframe></div><script src="https://player.vimeo.com/api/player.js"></script></p>
 			<p>&nbsp;</p>
 			<h2>What is a funding barrier?</h2></p>
-			<p>We define a<strong> funding barrier</strong> as systemic roadblocks that hinder one’s ability to access and acquire funding needed to support projects and initiatives. These barriers can
-			  include things like complicated application processes or not enough capacity to look for
-			  available funds; however, for our purposes, they do not include solely a general lack of 
-			  funds, as funding needs or funding gaps are being tackled separately on the Strategic
-		    Funding Team.</p>
-		<p> To identify and organize funding barriers faced by Tribes and local partners in a way that is actionable, the Strategic Funding Team conducted an analysis of over 400
-			  documents leading to the creation of a funding barriers framework. Through this 
-			  framework, we organized funding barriers identified by Tribes and local partners into 
-		    four primary funding barriers including: </p>
-		<ol>
-		  <li>Misalignment of Scale.</li>
-		  <li> Systemic 
-		    Application Constraints.</li>
-		  <li>Failed Governance.</li>
-		  <li> Insufficient Organizational
-		    Capacity. </li>
-	    </ol>
-		<p>As these are complex and dynamic challenges, the Strategic Funding Team
-			  identified enabling conditions that not only contribute to each of these barriers but also
-			  occur as a result of these barriers. Addressing any one of these primary funding barriers
-		  as well as the enabling conditions will require a large amount of capacity, coordination,
-		  and systems change. With this in mind, as well as our team’s capacity, it was determined
-		  to begin addressing these barriers at the enabling conditions level. Below is a visual
-		  representation of how we understand these issues to be interacting with one another.
-		  This information will be summarized in our <strong>Funding Barriers Framework
-		    Document </strong>(expected for release Fall 2024).</p>
+			<p>We  define a <strong>funding barrier</strong> as a roadblock that prevents someone from  accessing and acquiring funding needed to support projects and initiatives. These  barriers can include things like complicated application processes or not  enough capacity to look for available funds. This does not include a general  lack of funds, which we consider <em>funding needs</em> or <em>funding gaps</em> and are being tackled separately by the Strategic Funding Team.</p>
+			<p>To  identify and organize funding barriers faced by Tribes and local partners, the  Strategic Funding Team conducted an analysis of over 400 documents, including  those from the local forums, leading to the creation of a funding barriers  framework. The framework is organized into four primary funding barriers and  the conditions that not only contribute to each of these barriers but also  occur because of them. Below is an image of how we understand these issues to  be interacting with one another. This information will be summarized in our <strong><u>Funding  Barriers Framework Document</u></strong> (expected for release Fall 2024). </p>
 		<p>&nbsp;</p>
 		<p><img src="images/Four-Primary-Funding-Barriers.png" width="1246" height="769" alt="" class="img-responsive img-rounded"/></p>
 		<p>&nbsp;</p>
-		<p>The Strategic Funding Team weighed the pros and cons of addressing each enabling
-			  condition, reviewed identified solutions discussed within previous forums, and held
-			  internal discussions to make decisions of where to focus. Five criteria were also used to
-			  weigh these enabling conditions between one another. These criteria included whether
-	    addressing each enabling condition: </p>
-		<ol>
-		  <li>Helps to address other issues. </li>
-		  <li>Contributes to EEJ goals.</li>
-		  <li>Has momentum throughout the region that we can build on and learn from.</li>
-		  <li>Can be addressed quickly.</li>
-		  <li>Seems feasible given support (or lack thereof)
-		    for these issues already.</li>
-	    </ol>
-		<p> As a result of this selection process, the Strategic Funding Team will begin by addressing:		    </p>
-		<ol>
-		  <li>Match requirements. </li>
-		  <li>Multiple application demands over the next
-		    coming years. </li>
-	    </ol>
-		<p>Our team is dedicated to leading the implementation of strategies to
-			  address these enabling conditions, which we hope will create a more efficient and
-		    equitable funding application process. </p>
-		<h2><strong>CONNECTION TO MANAGEMENT BOARDS</strong></h2></p>
-			<p>Since September of 2020, the Leadership Council and Ecosystem Coordination Board
-			  have hosted local forums to provide Local Integrating Organizations (LIOs), Lead
-			  Entities (LEs), and other partners the opportunity to identify and help troubleshoot
-		    barriers that local partners are facing in implementing recovery plans and priorities. From 2022 to 2024, barriers related to grant funding have featured prominently in
-		    recent local forums with the Leadership Council and Ecosystem Coordination Board.</p>
-		<p> One of the consistent messages from LIOs during these forums is the large burden
-			  associated with current grant program design and administration that perpetuate
-			  inequities (between rural and urban areas) and continue to strain capacity of project
-			  sponsors (especially those of smaller organizations and Tribes, non-profits, and rural communities). To address these burdens felt by project sponsors, LIOs have asked the
-			  Leadership Council to support increased regional coordination, regional allocations,
-			  streamlined grant processes, elimination of duplicative applications,
-	        reduced/eliminated match requirements, and exploration of integrated watershed-
-	        based portfolios. These same challenges have been voiced by Tribes and LEs as well.</p>
-			<p> This work aims to address these concerns and asks brought forth to the Management
-	        Boards by our partners. The Leadership Council and Puget Sound Partnership continue
-	        to address several of these barriers through two mechanisms: </p>
-			<ol>
-			  <li>By strengthening the
-			    ability of the Align Partnership to advance efforts that crosscut and streamline state
-			    grant programs.</li>
-			  <li>By establishing a Strategic Funding Team to address additional
-			    funding-related barriers and increase capacity to secure funding.</li>
-	    </ol>
-			<h2><strong>ANTICIPATED NEXT STEPS &amp; OUTCOMES</strong></h2></p>
-		<p> Addressing any of the primary funding barriers or enabling conditions will require
-			  significant systems change. The landscape to apply for and acquire funding varies
-			  greatly as each funder’s system is different. That in and of itself is a barrier, but it is also
-			  an opportunity to learn what is and is not working. The Strategic Funding Team will be
-			  meeting with Tribes, LIOs, LEs, and state and federal agencies to review funding
-			  programs, identify points of collaboration, highlight case studies, document lessons
-			  learned, and build a justification for the kind of change needed to address the selected
-			  enabling conditions. This will be incorporated into action plans with specific strategies
-		    and tasks to address the selected enabling conditions.</p>
-			<p> We will be requesting support from local partners, Tribes, and funders as needed to
-			  develop these strategies to address match requirements and multiple application
-			  demands. The Strategic Funding Team will then brief the management boards on the
-			  strategies developed to specifically tackle<strong> match requirements</strong> and <strong>multiple application
-		    demands. </strong>The Strategic Funding Team will then brief the management boards on the
-		    strategies developed to specifically tackle match requirements and multiple application
-		    demands and seek feedback. </p>
-	<p>
-	<h2>Partner engagement and Request for input		</h2>
-	<p> We invite partners to engage in this work and share experiences with various funding
-		    barriers, which is needed to tackle these huge issues together. </p>
+		<p>Over  the next few years, the Strategic Funding Team will begin addressing these barriers,  starting at the enabling condition level. </p>
+		<p>After  a selection process, the Strategic Funding Team will begin by addressing 1) <strong>match  requirements </strong>and 2) <strong>multiple application demands</strong>. Our team is  dedicated to leading the implementation of strategies to address these enabling  conditions, which we hope will create a more efficient and fair funding  application process.		    </p>
+		<h2>Partner engagement and Request for input		</h2>
+	<p>We  invite partners to engage in this work and share experiences with various  funding barriers, which is needed to tackle these huge issues together. </p>
 	<h3><strong>FOR TRIBES AND LOCAL PARTNERS</strong></h3></p>
-			<p> If you have experienced or are experiencing barriers to accessing, receiving, and
-		    implementing funding, please let us know by filling out this quick form <a href="https://app.smartsheet.com/b/form/3e88c1292c5d468596221957fcb1fe57" target="new"><strong>here</strong></a>. </p>
+			<p>The  Strategic Funding Team wants to continue to learn if you have experienced or  are experiencing barriers to accessing, receiving, and implementing funding. Please  let us know by filling out this quick form<a href="https://app.smartsheet.com/b/form/3e88c1292c5d468596221957fcb1fe57" target="new"><strong>here</strong></a>. </p>
 <h3><strong>FOR INTERNAL PSP STAFF, FUNDERS, AND OTHER INTERESTED
 			PARTIES</strong></h3></p>
-			<p> If you hear of funding barriers that our partners are facing, please let us know by filling
-		    out this quick form <a href="https://app.smartsheet.com/b/form/eb5e60e65da845e797215c35fc90d745" target="new"><strong>here</strong></a>. </p><h3><strong>FOR FUNDERS</strong></h3></p>
-			<p> If you would like a summary of funding barriers identified through our previous work
-			  and encountered by partners as they specifically relate to your funding program, please
-			  submit a request <a href="https://app.smartsheet.com/b/form/62a069e9e72a443e90a8d25721f1c039" target="new"><strong>here</strong></a>.<br>
+			<p>If  you hear of funding barriers that our partners are facing, please let us know  by filling out this quick form<a href="https://app.smartsheet.com/b/form/eb5e60e65da845e797215c35fc90d745" target="new"><strong>here</strong></a>. </p><h3><strong>FOR FUNDERS</strong></h3></p>
+			<p>If  you would like a summary of funding barriers identified through our previous  work and encountered by partners as they specifically relate to your funding  program, please submit a request<a href="https://app.smartsheet.com/b/form/62a069e9e72a443e90a8d25721f1c039" target="new"><strong>here</strong></a>.<br>
             </p>
         <p><br>
         </p>
         <p></p>
 			
 		<!-- LAST UPDATED -->
-		<p class="last-update">Last updated: 7/30/24</p>
+		<p class="last-update">Last updated: 8/5/24</p>
 
         <!-- InstanceEndEditable --> </div>
 		
