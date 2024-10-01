90f311ee33df9cacde344bfd47fa80c2664672ce
jon bridgman <jonbridgman@gmail.com>
2024-09-30 17:14:42 -0700

    Technical Assistance page
    Started but needs to be redone.

---
+++ technical-assistance.php
@@ -0,0 +1,202 @@
+<!DOCTYPE html>
+<html lang="en"><!-- InstanceBegin template="/Templates/page-4-template.dwt" codeOutsideHTMLIsLocked="false" -->
+<head>
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-K5R1WLJ6VJ"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-K5R1WLJ6VJ');
+</script>
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<!-- InstanceBeginEditable name="doctitle" -->
+<title>Puget Sound Partnership Funding Barriers</title>
+<!-- InstanceEndEditable -->
+<!-- Bootstrap -->
+<link href="css/bootstrap.css" rel="stylesheet">
+<link href="css/custom.css" rel="stylesheet" type="text/css">
+<link href="css/custom-erika.css" rel="stylesheet" type="text/css">
+<!-- loads the wf-loading class right away to minimize FOUT -->
+<script>document.documentElement.className += ' wf-loading';</script>
+<!-- Font PRENTON TYPEKIT -->
+<script src="https://use.typekit.net/srt5jze.js"></script>
+<script>try{Typekit.load({ async: true });}catch(e){}</script>
+<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+<!--[if lt IE 9]>
+	<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+	<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+<!-- InstanceBeginEditable name="head" -->
+<script>
+/*this variable is used to set the proper nav to active. It should to the order the nav item is in the list*/
+navSelected = 5;
+	
+</script>
+<meta property="og:image" content="http://www.psp.wa.gov/images/inside_image_99_sm.jpg" />
+<!-- InstanceEndEditable -->
+<!-- InstanceParam name="OptionalRegion1" type="boolean" value="true" -->
+<!-- InstanceParam name="OptionalRegion2" type="boolean" value="false" -->
+<!-- InstanceParam name="OptionalRegion3" type="boolean" value="true" -->
+<!-- InstanceParam name="OptionalRegion4" type="boolean" value="false" -->
+<!-- InstanceParam name="OptionalRegion5" type="boolean" value="true" -->
+<!-- InstanceParam name="OptionalRegion6" type="boolean" value="true" -->
+<!-- InstanceParam name="OptionalRegion7" type="boolean" value="false" -->
+<!-- InstanceParam name="OptionalRegion8" type="boolean" value="true" -->
+</head>
+<body>
+<?php include 'includes/modal-inc.html';?>
+<!-- START IMAGE HEADER --> 
+ <!-- InstanceBeginEditable name="overviewphoto" -->
+<header class="overview-page-image-style overview-page-image118">
+</header>
+<!-- InstanceEndEditable -->  <!-- InstanceBeginEditable name="EditRegion11" -->
+<div class="row">
+	<div class="col-xs-12 orange-fill align-center overview-logo-banner"><img src="images/2016-AA-banner-logo-none-small.png" width="146" height="30" alt=""/></div>
+</div>
+<!-- InstanceEndEditable -->
+<div class="container-fluid page-content padding-50-bottom">
+	<div class="row">
+		<div class="col-sm-3 padding-20-top"></div>
+		<div class="col-sm-7 padding-20-top"> <!-- InstanceBeginEditable name="6col_header" -->
+			<h1>Technical Assistance and Grant Writing</h1>
+			<!-- InstanceEndEditable --></div>
+		<div class="col-sm-2 padding-20-top"></div>
+	</div>
+	
+	<div class="row"> 
+		<div class="col-sm-3">
+			<div class="nav-leftside-custom">
+				<ul class="nav nav-stacked nav-pills nav-leftside-custom padding-left-0 margin-10-top">
+					<!-- InstanceBeginEditable name="left_nav" -->
+					<?php include 'includes/ln-strategic-funding.html';?>
+					
+					<!-- InstanceEndEditable -->
+				</ul>
+			</div>
+		</div>
+		 
+		<div class="col-sm-7 padding-20-top content-column"> <!-- InstanceBeginEditable name="6col_content" -->
+	<!--
+		<div class="highlightbox">
+			<p>On August 4, 2022, the Environmental Protection Agency (EPA) approved the<a href="https://pspwa.box.com/shared/static/8zak4wiakdy94vc6104er8l3kn9bdxkw.pdf" target="new">2022-2026 Action Agenda adopted by the Leadership Council in June</a> as the Puget Sound National Estuary Program's (NEP) <a href="https://www.epa.gov/nep/comprehensive-conservation-and-management-plans" target="new">Comprehensive Conservation and Management Plan</a>. Learn more about the 2022-2026 Action Agenda below or visit the interactive <a href="https://actionagenda.pugetsoundinfo.wa.gov/2022-2026ActionAgenda" target="new">2022-2026 Action Agenda Explorer</a>.</p>
+		</div>-->
+			
+		<p>The Strategic Funding Team assists partners in responding to  rapid funding availabilities that support the implementation of the&nbsp;<a href="https://gcc02.safelinks.protection.outlook.com/?url=https%3A%2F%2Fpsp.wa.gov%2F2022AAupdate.php&data=05%7C02%7Cjennifer.grimm%40psp.wa.gov%7Cb89a15cb177a42af77ba08dc96d08865%7C11d0e217264e400a8ba057dcc127d72d%7C0%7C0%7C638551069583933493%7CUnknown%7CTWFpbGZsb3d8eyJWIjoiMC4wLjAwMDAiLCJQIjoiV2luMzIiLCJBTiI6Ik1haWwiLCJXVCI6Mn0%3D%7C0%7C%7C%7C&sdata=RgU6mqTi4v4LybhCNWEmZDpRA5%2F3KhrRAfF9AjavD6U%3D&reserved=0">Action  Agenda</a>&nbsp;and&nbsp;<a href="https://gcc02.safelinks.protection.outlook.com/?url=https%3A%2F%2Fpsp.wa.gov%2Fsalmon-recovery-overview.php&data=05%7C02%7Cjennifer.grimm%40psp.wa.gov%7Cb89a15cb177a42af77ba08dc96d08865%7C11d0e217264e400a8ba057dcc127d72d%7C0%7C0%7C638551069583943886%7CUnknown%7CTWFpbGZsb3d8eyJWIjoiMC4wLjAwMDAiLCJQIjoiV2luMzIiLCJBTiI6Ik1haWwiLCJXVCI6Mn0%3D%7C0%7C%7C%7C&sdata=PQd9SFAKrgKzNE%2BoRyjJkwiiz5W8HQdbqJLCgJoSyeA%3D&reserved=0">Salmon  Recovery Plan</a>, ourcommunity&rsquo;s shared recovery plans for  Puget Sound.&nbsp;&nbsp; </p>
+<p>Specifically, the Grant Writer and Strategist can provide the  Tribes, LIOs, LEs, and local partners with grant writing technical assistance  and the capacity to prepare their grant applications for submission. They can  help develop grant proposals to federal, state, and private funding agencies. The  overall goal is to increase the number of successful submissions for local and  regional recovery projects. This technical assistance is free for partners and  the partnership does not take any portion of the award if successful. </p>
+<h2>Specific Grant writing Technical Assistance that we can  provide:</h2>
+<ul class="bullet-size-fix">
+	<li>Writing  and Writing Review</li>
+	<ul>
+		<li>Review drafts of applications or in collaboration with the project sponsor, create drafts on their behalf.  </li>
+	</ul>
+	<li>Opportunity or Notice of Funding Review</li>
+	<ul>
+		<li>Review a funding opportunity and break it down into manageable parts.</li>
+	</ul>
+	<li>Grant Application Management</li>
+	<ul>
+		<li>Provide management of the application process. Managing the progress and completion of sections in collaboration with the project sponsor. </li>
+	</ul>
+	<li>Grant Strategy Development</li>
+	<ul>
+		<li>If you are interested in brainstorming a large project or opportunity or have a planned project that you cannot match funding to. </li>
+		</ul>
+			</ul>
+	<p>Some things we cannot provide: We cannot write a grant  application in a silo, it will take collaboration and work from the partner. We cannot physically submit an application for a partner, and in general Puget Sound Partnership cannot be a lead applicant or a fiscal agent on any grant we are assisting on. We also cannot directly grant write on an HSIL or regional EPA opportunities, nor provide post award assistance. <br>
+		The Grant Writer and Strategist is one full-time position on  the Strategic Funding Team. Therefore, Puget Sound Partnership is being  proactive in thinking about their capacity and have created a decision matrix  to able to equitably distribute their time. The decision matrix will consider Puget Sound ecosystem and salmon recovery priorities, as well as equity and  environmental justice considerations.</p>
+	<h2>Grant Writing </h2>
+<p>For direct grant writing to be successful for there are some things on the partner side that can make this technical  assistance smooth as possible. Before reaching out for assistance please make  sure you have the following prepared. The Strategic Funding Team can only  support projects that align with the Puget Sound Partnership&rsquo;s collective effort to restore and protect the Puget Sound,  therefore requests must support the Action Agenda, Salmon Recovery  Plan, and Science Work Plan.</p>
+	<p><u>Project/Program Information</u></p>
+	<ul type="disc">
+		<li>Identified project concept, problem statement, project sponsor, collaborators, and project scope. </li>
+		<li>Technical requirements of the funding opportunity have either been completed or have a planned completion. This may include design, engineering, modeling, etc. The Strategic Funding Team cannot complete these technical requirements. </li>
+	</ul>
+	<p><u>Funding  Opportunity</u></p>
+	<ul type="disc">
+		<li>A grant       opportunity or notice of funding has been identified, reviewed, and it&rsquo;s       been determined that the entity and project are eligible. </li>
+	</ul>
+	<p>**<em>If  you have a project or concept that you have been unable to identify a funding  opportunity that fits, or if you have a project and/or funding need and want to  strategize local partnerships, please reach out to our Strategic Funding Coordinator </em><em>who can properly assist. It does not mean you cannot receive  assistance; it just means you are not at the writing stage yet!</em></p>
+	<p><u>Capacity  &amp; Readiness</u></p>
+	<ul type="disc">
+		<li>Assessed       internal capacity and if awarded a grant you have the capacity to not only execute the project, but capacity to complete financial management, compliance, reporting, and manage your relationship with your grant       management officer and/or program officer. <em>Puget Sound Partnership will  not be able to provide any post award support through this team. </em></li>
+		<li>Considered and       assured there are no barriers that would prevent successful execution of the project if the funding the Strategic Funding Team assisted on, was awarded. <em>If a barrier(s) exist please reach out to our Funding       Barriers Strategist </em><em>for support in how to address it prior to pursuing funding. </em></li>
+		<li>Partners       should ensure that they are eligible for the funding they are seeking  assistance for. The eligibility could include a current SAM account and UEI for federal opportunities. </li>
+	</ul>
+	<p><u>Financial  Information</u></p>
+	<ul type="disc">
+		<li>Draft or complete budget prepared. <em>The Strategic Funding Team may be able to give general feedback and advice on formatting, but specific numbers been to come from the project sponsor.</em></li>
+		<li>Partners should have their organization&rsquo;s financial documents in order and ready to include in any application. </li>
+	</ul>
+	<p>After partners have considered this list and are ready to  request assistance, please do so by completing this brief intake form: <strong>LINK </strong><em>If you are  unsure about some aspects to this list, please still complete the intake form  and describe your uncertainty and the Strategic Funding Team will assist.</em><strong> </strong></p>
+	<h2>Grant Strategy</h2>
+	<ul>
+		<ul>
+			<li>Grant Matching – Opportunity Driven</li>
+			<ul>
+				<li>When an opportunity opens that aligns with known projects or  partner funding needs and interests, the Strategic Funding Coordinator or the  Grant Writer and Strategist will share out the opportunity with the partner and  suggest that it may be an option for them. </li>
+			</ul>
+			<li>Grant Strategy – Project Driven</li>
+			<ul>
+				<li>A deeper dive into a large project where a partner seeks a  grant strategy and potential timelines for funding. </li>
+			</ul>
+		</ul>
+	</ul>
+	<p>Grant strategy thinks about the bigger picture of your  project and outcomes. The project you are seeking a strategy for should be  vetted through community process, have partners identified, have activities and  outcomes already determined. A request for grant strategy will begin with an  in-depth consultation so we may learn about your project, its history and  rationale, and begin to review any materials that have been created. We will  focus in on the overall goal of the project and intention and strategize how  those lines up with known funding sources. This may mean piecing together  different funding sources for phases and looking out over multiple years. We  will look at what could be potential successful funding pathways for your  project. We will look at what has historically been successful for the  opportunities we identify, to gauge how your project aligns with past  successful applicants as well as the specific funding opportunity scoring  criteria. <br>
+		Of course, we are not the funders. And while we might create  a grant strategy with options that seem promising, we have no guarantee that  you will be awarded those specific funding opportunities. We will of course do  our best to assist you to be in the best possible shape for your applications  with our suite of technical assistance. </p>
+	<p>Grant Strategy Steps:</p>
+	<ul>
+		<li>Internal Partner Readiness </li>
+		<li>Consultation </li>
+		<li>Strategy Development</li>
+		<li>Post Discussion   </li>
+	</ul>
+	<p><strong>Post Assistance</strong><br>
+		To record and monitor the effectiveness of this technical  assistance, we ask that after assistance the partner keeps us informed on  confirming submittal, and award if successful. Other data on impact and success  will be explored on a case-by-case basis. We will reach out for this  information. </p>
+<div>
+	<div> </div>
+	<div> </div>
+</div>
+</ul>
+
+		<!-- LAST UPDATED -->
+		<p class="last-update">Last updated: 9/30/24</p>
+
+        <!-- InstanceEndEditable --> </div>
+		
+		<div class="col-sm-2 padding-20-top padding-0-right"> <!-- InstanceBeginRepeat name="right_nav_repeat" --><!-- InstanceBeginRepeatEntry --> <!-- InstanceBeginEditable name="right_nav_title" --><strong>CONTACT</strong>
+                <div class="right-nav-title margin-0-top "></div>
+			<!-- InstanceEndEditable -->
+			<ul class="nav-rightside-custom">
+				<!-- InstanceBeginEditable name="right_nav_links" -->
+				<p class="right-nav-contact">For questions about funding barriers:<br />
+					<br>
+					<strong>Michelle Deckard</strong> <br>
+					Grant Writer and Strategist<br>
+					<a href="mailto:jessica.reynolds@psp.wa.gov">michelle.deckard@psp.wa.gov</a><br>
+					360.819.0342 <br><br><!-- InstanceEndEditable -->
+			</ul>
+			<!-- InstanceEndRepeatEntry --><!-- InstanceEndRepeat --> </div>
+	</div>
+	<!--END OF ROW --> 
+</div>
+<!--END OF CONTENT CONTAINER -->
+
+<?php include 'includes/footer-inc.html';?>
+
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) --> 
+<script src="js/jquery-1.11.2.min.js"></script> 
+
+<!-- Include all compiled plugins (below), or include individual files as needed --> 
+<script src="js/bootstrap.js"></script> 
+<!-- custom js --> 
+<script src="js/custom.js"></script> 
+<!-- Google Tracking  -->
+<!-- InstanceBeginEditable name="EditRegion12" -->EditRegion12<!-- InstanceEndEditable --> 
+
+</body>
+<!-- InstanceEnd --></html>
