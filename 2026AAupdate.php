60ff4c4c9ba93b67c9c721fe0274e1f49a6481bc
ianmc3 <ian.mccabe@psp.wa.gov>
2024-11-21 16:43:42 -0800

    Created AA 2026 update page

--- 2026AAupdate.php
+++ 2026AAupdate.php
@@ -64,7 +64,7 @@ navSelected = 1;
 	<div class="row">
 		<div class="col-sm-3 padding-20-top"></div>
 		<div class="col-sm-7 padding-20-top"> <!-- InstanceBeginEditable name="6col_header" -->
-			<h1>2026-2030 Action Agenda</h1>
+			<h1>2026-2030 Action Agenda update</h1>
 			<!-- InstanceEndEditable --></div>
 		<div class="col-sm-2 padding-20-top"></div>
 	</div>
@@ -87,55 +87,54 @@ navSelected = 1;
 			<p>On August 4, 2022, the Environmental Protection Agency (EPA) approved the<a href="https://pspwa.box.com/shared/static/8zak4wiakdy94vc6104er8l3kn9bdxkw.pdf" target="new">2022-2026 Action Agenda adopted by the Leadership Council in June</a> as the Puget Sound National Estuary Program's (NEP) <a href="https://www.epa.gov/nep/comprehensive-conservation-and-management-plans" target="new">Comprehensive Conservation and Management Plan</a>. Learn more about the 2022-2026 Action Agenda below or visit the interactive <a href="https://actionagenda.pugetsoundinfo.wa.gov/2022-2026ActionAgenda" target="new">2022-2026 Action Agenda Explorer</a>.</p>
 		</div>-->
 		<div>
-		<h2 class="margin-0-top">What is the Action Agenda? </h2>
-			<img class="floatright" src="images/2022-AA-cover.jpg" width="288" height="370" alt=""/>
-		<p>COMING SOON - The 2026-2030 Action Agenda charts the course for Puget Sound recovery as our community's shared plan for advancing protection and restoration efforts across the region.</p>
-		
-			<p>Learn about the Action Agenda:</p>
-			<ul class="bullet-size-fix">
-			<li><a href="https://makingwaves.psp.wa.gov/index.php/2022/10/28/introduction-action-agenda/" target="new">Watch our Action Agenda video in the Q4 2022 Making Waves issue</a></li>
-			<li><a href="https://pspwa.box.com/shared/static/30dh5a9bmlhankz516d7vm443dgqm70t.pdf">Read the Action Agenda brochure</a></li>
-			<li><a href="https://pspwa.box.com/shared/static/7lwvn8letqiyt342o1y00ltq02mnk5v9.pdf">Lea el folleto de la Agenda de Acción en español</a></li>
-			<li><a href="https://pspwa.box.com/shared/static/ri8a5rhkpkm5oej7jx3cx0i7c2qjhbuj.pdf">Basahin ang brosyur ng Action Agenda sa Tagalog</a></li>
-			<li><a href="https://pspwa.box.com/shared/static/tk022vggf87myu23slwdwxyzaloo8z3p.pdf">Đọc tập quảng cáo v&#7873; Chương Trình Hành Động b&#7857;ng Ti&#7871;ng Việt</a></li>
-			<li><a href="https://pspwa.box.com/shared/static/moe8onysucqeayu2aghh582r8ap527n6.pdf">Ознакомьтесь с брошюрой Action Agenda ("План действий") на русском языке</a></li>
-			<li><a href="https://pspwa.box.com/shared/static/j3iolontqjx1m4gpja78dprg3isvubmw.pdf">請閱讀繁體中文版行動議程手冊</a></li>
-			<li><a href="https://pspwa.box.com/shared/static/zqzl3t0y68imthqclpuy4iavphulkbr2.pdf">请阅读繁体中文版行动议程手册</a></li>
-			</ul><br>
-			<p>2022-2026 Action Agenda, Executive Summary, and related information:</p>
-			<ul class="bullet-size-fix">
-				<li><a href="https://pspwa.box.com/shared/static/7svj5x4xtrj6duyccqgo2y0ryekk9zjr.pdf">Download the 2022-2026 Action Agenda Executive Summary</a> </li>
-				<li><a href="https://pspwa.box.com/shared/static/8zak4wiakdy94vc6104er8l3kn9bdxkw.pdf">Download the full 2022-2026 Action Agenda PDF</a></li>
-				<li><a href="https://pspwa.box.com/shared/static/mejeolwllwccjj3rmc8kiduia604vxos.pdf">Lea la Agenda de Acción 2022-2026 en español</a></li>
-				<li><a href="https://pspwa.box.com/shared/static/adopaw136pastn603ojfzc046k9iza5b.xlsx">Download an Excel table of 2022-2026 Action Agenda strategies, actions, key opportunities, and implementation considerations</a></li>
-			</ul>
-			</div>
-		<div class="clearfix"></div>
-	
-		
-		<div class="container-fluid blue-outline-5px padding-20-all margin-20-top">
-			<div class="row ">
-				
-				<div class="col-sm-6">
-					<h4 class="margin-0-top">VISIT THE ONLINE ACTION AGENDA EXPLORER</h4>
-					<p>Visit the <a href="https://actionagenda.pugetsoundinfo.wa.gov/2022-2026ActionAgenda">2022-2026 Action Agenda Explorer</a> – the online companion tool to the Action Agenda. The Explorer allows for dynamic searchability of content relating to each one of the 31 strategies in the Action Agenda. Content will be updated regularly through 2026.</p>
-				</div>	
-				<div class="col-sm-6">
-					<img class="img-responsive floatright" src="images/AA-explorer-with-photo.jpg" alt=""/>	
-				</div>
-			</div>
+		<h2 class="margin-0-top">We are updating the Puget Sound Action Agenda </h2>
+		<p>The Action Agenda is our community&rsquo;s  shared plan for advancing Puget Sound recovery. The 2026-2030 Action Agenda  update will advance progress towards recovery of the Puget Sound&rsquo;s ecosystem and human wellbeing <a href="https://vitalsigns.pugetsoundinfo.wa.gov/" target="new">Vital Signs</a> by building on  the foundation of the<a href="https://www.psp.wa.gov/2022AAupdate.php" target="new">2022-2026  Action Agenda</a>.</p>
+		<p>The 2026-2030 Action Agenda will describe approaches that include   ecosystem health, human wellbeing, climate change, and  environmental justice. The plan will have Strategies and Actions that guide  implementation, including commitments from Puget Sound recovery partners. It  will also include more indicators and targets designed to help us assess the  effectiveness of our collective efforts and hold us all accountable to making  meaningful progress. </p>
+        <p>The goals of the update are to: </p>
+        <ul>
+          <li>Strengthen implementation, accountability,  alignment and tracking of the Action Agenda.   </li>
+          <li>Elevate and further integrate human  wellbeing, climate change, and environmental justice.</li>
+          <li>Elevate and honor Tribal sovereignty and treaty  rights.</li>
+          <li> Expand participation and  meaningful engagement of diverse audiences, including vulnerable  populations (VP) and overburdened communities (OBC)</li>
+          <li>Update the online Action Agenda to improve  accessibility and usability. </li>
+        </ul>
+        <div>
+          <div> </div>
+          <div> </div>
+          <div> </div>
+          <div> </div>
+          <div> </div>
+          <div> </div>
+        </div>
+<h2>Approach and timeline </h2>
+		  <img class="floatright" src="images/management-cycle-1.png" alt="An image of the simplified adaptive management cycle. Four arrows make up a circle to show that each part of the cycle interacts with the others. Each arrow says Evaluate and Learn, Adjust, Plan, and Do."/>
+<p>The   2026-2030 Action Agenda update process follows an adaptive management approach.  Over the course of 2025, the Partnership will work with recovery partners to <strong>evaluate  and learn</strong> from successes and challenges to implementing the 2022-2026  Action Agenda (<strong>plan</strong>) and <strong>adjust</strong> the 2026-2030 Action Agenda as  needed. The 2026-2030 Action Agenda is scheduled to be approved in summer 2026.<strong></strong></p>
+<div>
+  <div> </div>
+  <div></div>
+</div>
 		</div>
-		<div class="clearfix"></div>
-
-			
-		<h2>Why do we need an Action Agenda? </h2>
 		
-		<p>&nbsp;</p>
-			
-			
-			
-		<!-- LAST UPDATED -->
-		<p class="last-update">Last updated: 10/24/24</p>
+		<h2>Timeline</h2>
+		<p>GRAPHIC COMING SOON</p>
+		<p><strong>2025</strong> <strong></strong><br>
+        January-March 2025: Update 2026-2030 Action Agenda  content.</p>
+        <p>April-June 2025: Compile, develop initial draft 2026-2030  Action Agenda.&nbsp;</p>
+        <p>July-September 2025: Review first 2026-2030 Action Agenda  draft, Tribal consultation, confirm commitments/targets.&nbsp;</p>
+        <p>October-December 2025: Revise  and prepare the 2026-2030 Action Agenda for public comment.<br>
+          <strong> </strong><br>
+          <strong>2026 </strong><br>
+          January-March 2026: Public comment on 2026-2030 Action  Agenda, Tribal consultation.&nbsp;</p>
+        <p>April-August 2026: Finalize,  coordinate Boards review, and launch the 2026-2030 Action Agenda.</p>
+        <div>
+          <div> </div>
+          <div> </div>
+          <div> </div>
+          <div> </div>
+        </div>
+
+        <!-- LAST UPDATED -->
+		<p class="last-update">Last updated: 11/21/24</p>
 
         <!-- InstanceEndEditable --> </div>
 		
