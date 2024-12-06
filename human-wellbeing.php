5a1ece966dda19b06e7279a10bb750d676a492cc
ianmc3 <ian.mccabe@psp.wa.gov>
2024-12-06 13:57:55 -0800

    Create human-wellbeing.php

---
+++ human-wellbeing.php
@@ -0,0 +1,159 @@
+<!DOCTYPE html>
+<html lang="en"><!-- InstanceBegin template="/Templates/page-4-template.dwt" codeOutsideHTMLIsLocked="false" -->
+<head>
+
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-BGEZ5L3EJY"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-BGEZ5L3EJY');
+</script>
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<!-- InstanceBeginEditable name="doctitle" -->
+<title>Puget Sound Partnership Strategic Funding Team</title>
+<!-- InstanceEndEditable -->
+<!-- Bootstrap -->
+<link href="css/bootstrap.css" rel="stylesheet">
+<link href="css/custom.css" rel="stylesheet" type="text/css">
+<link href="css/custom-erika.css" rel="stylesheet" type="text/css">
+<!-- loads the wf-loading class right away to minimize FOUT -->
+<script>document.documentElement.className += ' wf-loading';</script>
+<!-- Font PRENTON TYPEKIT -->
+<script src="https://use.typekit.net/srt5jze.js"></script>
+<script>try{Typekit.load({ async: true });}catch(e){}</script>
+<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+<!--[if lt IE 9]>
+	<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+	<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+<!-- InstanceBeginEditable name="head" -->
+<script>
+/*this variable is used to set the proper nav to active. It should to the order the nav item is in the list*/
+navSelected = 1;
+	
+</script>
+<meta property="og:image" content="http://www.psp.wa.gov/images/inside_image_99_sm.jpg" />
+<!-- InstanceEndEditable -->
+<!-- InstanceParam name="OptionalRegion1" type="boolean" value="true" -->
+<!-- InstanceParam name="OptionalRegion2" type="boolean" value="false" -->
+<!-- InstanceParam name="OptionalRegion3" type="boolean" value="true" -->
+<!-- InstanceParam name="OptionalRegion4" type="boolean" value="false" -->
+<!-- InstanceParam name="OptionalRegion5" type="boolean" value="true" -->
+<!-- InstanceParam name="OptionalRegion6" type="boolean" value="true" -->
+<!-- InstanceParam name="OptionalRegion7" type="boolean" value="false" -->
+<!-- InstanceParam name="OptionalRegion8" type="boolean" value="true" -->
+</head>
+<body>
+<?php include 'includes/modal-inc.html';?>
+<!-- START IMAGE HEADER --> 
+ <!-- InstanceBeginEditable name="overviewphoto" -->
+<header class="overview-page-image-style overview-page-image120">
+</header>
+<!-- InstanceEndEditable -->  <!-- InstanceBeginEditable name="EditRegion11" -->
+<div class="row">
+	<div class="col-xs-12 orange-fill align-center overview-logo-banner"><img src="images/2016-AA-banner-logo-none-small.png" width="146" height="30" alt=""/></div>
+</div>
+<!-- InstanceEndEditable -->
+<div class="container-fluid page-content padding-50-bottom">
+	<div class="row">
+		<div class="col-sm-3 padding-20-top"></div>
+		<div class="col-sm-7 padding-20-top"> <!-- InstanceBeginEditable name="6col_header" -->
+			<h1>human wellbeing</h1>
+			<!-- InstanceEndEditable --></div>
+		<div class="col-sm-2 padding-20-top"></div>
+	</div>
+	
+	<div class="row"> 
+		<div class="col-sm-3">
+			<div class="nav-leftside-custom">
+				<ul class="nav nav-stacked nav-pills nav-leftside-custom padding-left-0 margin-10-top">
+					<!-- InstanceBeginEditable name="left_nav" -->
+					<?php include 'includes/ln-strategic-funding.html';?>
+					
+					<!-- InstanceEndEditable -->
+				</ul>
+			</div>
+		</div>
+		 
+		<div class="col-sm-7 padding-20-top content-column"> <!-- InstanceBeginEditable name="6col_content" -->
+	<!--
+		<div class="highlightbox">
+			<p>On August 4, 2022, the Environmental Protection Agency (EPA) approved the<a href="https://pspwa.box.com/shared/static/8zak4wiakdy94vc6104er8l3kn9bdxkw.pdf" target="new">2022-2026 Action Agenda adopted by the Leadership Council in June</a> as the Puget Sound National Estuary Program's (NEP) <a href="https://www.epa.gov/nep/comprehensive-conservation-and-management-plans" target="new">Comprehensive Conservation and Management Plan</a>. Learn more about the 2022-2026 Action Agenda below or visit the interactive <a href="https://actionagenda.pugetsoundinfo.wa.gov/2022-2026ActionAgenda" target="new">2022-2026 Action Agenda Explorer</a>.</p>
+		</div>-->
+			
+			<p>People  are an important part of Puget Sound. If we want to help restore and conserve  Puget Sound&rsquo;s ecosystem, we must do so while also caring for ourselves.  Our Human Wellbeing work aims to help everyone  live a fulfilling life, including physically, emotionally, socially, and  economically.</p>
+			<div class="highlightbox margin-0-top margin-20-bottom">
+			<p><i>People are part of nature and the wellbeing of one depends on the wellbeing  of the other.</i>			</p>
+			</div>
+			<h2>What is Human Wellbeing?</h2>
+			<p>Human  Wellbeing is a good quality of life that is supported by a healthy and  functional Puget Sound that is not threatened by the changes in our ecosystem.</p>
+		<p>Two  of our six recovery goals in Washington State Statute (RCW 90.71.300) address  human wellbeing:</p>
+			<ul class="bullet-size-fix">
+			<li>A  healthy human population that is supported by a healthy Puget Sound and is not  threatened by changes in the ecosystem.</li>
+				<li>A  quality of human life that is sustained by a functioning Puget Sound ecosystem.
+				  </p>
+		</li></ul>
+		<div class="highlightbox margin-10-left">
+		  <p>You  can read more about how we <a href="https://www.psp.wa.gov/evaluating-human-wellbeing.php" target="new">evaluate Human Wellbeing  here</a>.</p>
+		  <h2>Human Wellbeing and Environmental Justice</h2>
+		  <p>Environmental Justice is an important part of Human  Wellbeing. It is applied across all six of our ecosystem recovery goals by  applying <a href="https://psp.wa.gov/HEAL-environmental-justice-assessments.php">our  Environmental Justice Principles</a>, especially those that help communities  involved in recovery efforts.  </p>
+		  <p>You can find more information about  Environmental Justice within our Human Wellbeing work: </p>
+		  <ul class="bullet-size-fix">
+		    <li><a href="https://psp.wa.gov/HEAL.php">Learn about Washington&rsquo;s HEAL Act</a>.</li>
+		    <li><a href="https://psp.wa.gov/HEAL-environmental-justice-assessments.php">Learn more  about the Partnership&rsquo;s Equity &amp; Environmental Justice Program</a>.
+		      </p>
+	        </li>
+	      </ul>
+	<h2>Human Wellbeing across our work</h2>
+	<p>We  focus on Human Wellbeing in three areas of our work. Select one of the work  area examples below to learn more.</p>
+	<ul class="bullet-size-fix">
+	  <li>Science</li>
+	  <li>Planning</li>
+	  <li>Monitoring</li>
+	  </ul>
+	<div class="highlightbox margin-0-top margin-20-bottom">
+	<p><i>The Equity &amp; Environmental Justice Program promotes accountability,  builds relationships, advances education, and advises across these three work  areas and the larger work of the Partnership.</i>		</p>
+	
+		</div>
+			
+		<!-- LAST UPDATED -->
+		<p class="last-update">Last updated: 12/6/24</p>
+
+        <!-- InstanceEndEditable --> </div>
+		
+		<div class="col-sm-2 padding-20-top padding-0-right"> <!-- InstanceBeginRepeat name="right_nav_repeat" --><!-- InstanceBeginRepeatEntry --> <!-- InstanceBeginEditable name="right_nav_title" -->
+			<div class="right-nav-title margin-0-top ">SEE ALSO</div>
+			<!-- InstanceEndEditable -->
+			<ul class="nav-rightside-custom">
+				<!-- InstanceBeginEditable name="right_nav_links" -->
+				<li class="active" role="presentation"><a href="https://actionagenda.pugetsoundinfo.wa.gov/2022-2026ActionAgenda" target="_blank"><strong>Action Agenda Explorer</strong></a></li>
+				
+			
+				<!-- InstanceEndEditable -->
+			</ul>
+			<!-- InstanceEndRepeatEntry --><!-- InstanceEndRepeat --> </div>
+	</div>
+	<!--END OF ROW --> 
+</div>
+<!--END OF CONTENT CONTAINER -->
+
+<?php include 'includes/footer-inc.html';?>
+
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) --> 
+<script src="js/jquery-1.11.2.min.js"></script> 
+
+<!-- Include all compiled plugins (below), or include individual files as needed --> 
+<script src="js/bootstrap.js"></script> 
+<!-- custom js --> 
+<script src="js/custom.js"></script> 
+<!-- Google Tracking  -->
+<!-- InstanceBeginEditable name="EditRegion12" -->EditRegion12<!-- InstanceEndEditable --> 
+
+</body>
+<!-- InstanceEnd --></html>
