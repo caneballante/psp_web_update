06313876d93b7a5db0455e359794a4da9d547032
ianmc3 <ian.mccabe@psp.wa.gov>
2024-12-10 13:14:30 -0800

    Create grantwriting.php

---
+++ grantwriting.php
@@ -0,0 +1,198 @@
+<!DOCTYPE html>
+<html lang="en"><!-- InstanceBegin template="/Templates/page-4-template.dwt" codeOutsideHTMLIsLocked="false" -->
+<head>
+
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-BGEZ5L3EJY"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-BGEZ5L3EJY');
+</script>
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<!-- InstanceBeginEditable name="doctitle" -->
+<title>Puget Sound Partnership Grant Writing Assistance</title>
+<!-- InstanceEndEditable -->
+<!-- Bootstrap -->
+<link href="css/bootstrap.css" rel="stylesheet">
+<link href="css/custom.css" rel="stylesheet" type="text/css">
+<link href="css/custom-erika.css" rel="stylesheet" type="text/css">
+<!-- loads the wf-loading class right away to minimize FOUT -->
+<script>document.documentElement.className += ' wf-loading';</script>
+<!-- Font PRENTON TYPEKIT -->
+<script src="https://use.typekit.net/srt5jze.js"></script>
+<script>try{Typekit.load({ async: true });}catch(e){}</script>
+<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+<!--[if lt IE 9]>
+	<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+	<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+<!-- InstanceBeginEditable name="head" -->
+<script>
+/*this variable is used to set the proper nav to active. It should to the order the nav item is in the list*/
+navSelected = 4;
+	
+</script>
+<meta property="og:image" content="http://www.psp.wa.gov/images/inside_image_99_sm.jpg" />
+<!-- InstanceEndEditable -->
+<!-- InstanceParam name="OptionalRegion1" type="boolean" value="true" -->
+<!-- InstanceParam name="OptionalRegion2" type="boolean" value="false" -->
+<!-- InstanceParam name="OptionalRegion3" type="boolean" value="true" -->
+<!-- InstanceParam name="OptionalRegion4" type="boolean" value="false" -->
+<!-- InstanceParam name="OptionalRegion5" type="boolean" value="true" -->
+<!-- InstanceParam name="OptionalRegion6" type="boolean" value="true" -->
+<!-- InstanceParam name="OptionalRegion7" type="boolean" value="false" -->
+<!-- InstanceParam name="OptionalRegion8" type="boolean" value="true" -->
+</head>
+<body>
+<?php include 'includes/modal-inc.html';?>
+<!-- START IMAGE HEADER --> 
+ <!-- InstanceBeginEditable name="overviewphoto" -->
+<header class="overview-page-image-style overview-page-image118">
+</header>
+<!-- InstanceEndEditable -->  <!-- InstanceBeginEditable name="EditRegion11" -->
+<div class="row">
+	<div class="col-xs-12 orange-fill align-center overview-logo-banner"><img src="images/2016-AA-banner-logo-none-small.png" width="146" height="30" alt=""/></div>
+</div>
+<!-- InstanceEndEditable -->
+<div class="container-fluid page-content padding-50-bottom">
+	<div class="row">
+		<div class="col-sm-3 padding-20-top"></div>
+		<div class="col-sm-7 padding-20-top"> <!-- InstanceBeginEditable name="6col_header" -->
+			<h1>grant writing Assistance</h1>
+			<!-- InstanceEndEditable --></div>
+		<div class="col-sm-2 padding-20-top"></div>
+	</div>
+	
+	<div class="row"> 
+		<div class="col-sm-3">
+			<div class="nav-leftside-custom">
+				<ul class="nav nav-stacked nav-pills nav-leftside-custom padding-left-0 margin-10-top">
+					<!-- InstanceBeginEditable name="left_nav" -->
+					<?php include 'includes/ln-strategic-funding.html';?>
+					
+					<!-- InstanceEndEditable -->
+				</ul>
+			</div>
+		</div>
+		 
+		<div class="col-sm-7 padding-20-top content-column"> <!-- InstanceBeginEditable name="6col_content" -->
+	<!--
+		<div class="highlightbox">
+			<p>On August 4, 2022, the Environmental Protection Agency (EPA) approved the<a href="https://pspwa.box.com/shared/static/8zak4wiakdy94vc6104er8l3kn9bdxkw.pdf" target="new">2022-2026 Action Agenda adopted by the Leadership Council in June</a> as the Puget Sound National Estuary Program's (NEP) <a href="https://www.epa.gov/nep/comprehensive-conservation-and-management-plans" target="new">Comprehensive Conservation and Management Plan</a>. Learn more about the 2022-2026 Action Agenda below or visit the interactive <a href="https://actionagenda.pugetsoundinfo.wa.gov/2022-2026ActionAgenda" target="new">2022-2026 Action Agenda Explorer</a>.</p>
+		</div>-->
+			
+			<p>Our Strategic Funding  Team (SFT) supports partners by helping them find available funding that  supports our <a href="https://gcc02.safelinks.protection.outlook.com/?url=https%3A%2F%2Fpsp.wa.gov%2F2022AAupdate.php&data=05%7C02%7Cjennifer.grimm%40psp.wa.gov%7Cb89a15cb177a42af77ba08dc96d08865%7C11d0e217264e400a8ba057dcc127d72d%7C0%7C0%7C638551069583933493%7CUnknown%7CTWFpbGZsb3d8eyJWIjoiMC4wLjAwMDAiLCJQIjoiV2luMzIiLCJBTiI6Ik1haWwiLCJXVCI6Mn0%3D%7C0%7C%7C%7C&sdata=RgU6mqTi4v4LybhCNWEmZDpRA5%2F3KhrRAfF9AjavD6U%3D&reserved=0">Action  Agenda</a>, <a href="https://gcc02.safelinks.protection.outlook.com/?url=https%3A%2F%2Fpsp.wa.gov%2Fsalmon-recovery-overview.php&data=05%7C02%7Cjennifer.grimm%40psp.wa.gov%7Cb89a15cb177a42af77ba08dc96d08865%7C11d0e217264e400a8ba057dcc127d72d%7C0%7C0%7C638551069583943886%7CUnknown%7CTWFpbGZsb3d8eyJWIjoiMC4wLjAwMDAiLCJQIjoiV2luMzIiLCJBTiI6Ik1haWwiLCJXVCI6Mn0%3D%7C0%7C%7C%7C&sdata=PQd9SFAKrgKzNE%2BoRyjJkwiiz5W8HQdbqJLCgJoSyeA%3D&reserved=0">Salmon  Recovery Plan</a>, or the <a href="https://psp.wa.gov/science-workplan.php">Science  Work Plan</a>. The  SFT also offers free grant writing technical support. On this page, you can  learn more about our grant writing support along with some tips and resources.</p>
+			<p>You can read more about the SFT at our <a href="https://www.psp.wa.gov/strategic-funding-program.php">Strategic Funding Program page</a>. </p>
+			<h2>What does a Grant Writer do?</h2>
+<p>A grant writer drafts or assists in the  drafting of a proposal that is used to request grant funding from an awarding  body.   </p>
+<p>Our Grant Writer and Strategist can provide  Tribes, Local Integration Organizations (LIOs), Lead Entities (LEs), and local  partners with grant writing technical assistance and the capacity to prepare  their grant applications for submission. They can help develop grant proposals  to federal, state, and private funding agencies. The overall goal is to increase  the number of successful submissions for Tribal, local, and regional recovery  projects. </p>
+<p>Grant writing support is free for Tribes and  partners, and we do not take any of the award if you&rsquo;re successful.</p>
+            <h2><strong>How do I request grant-writing assistance?</strong></h2>
+            <p>For technical  assistance and any questions, please contact our Grant Writer and Strategist, Michelle  Deckard at&nbsp;￼Michelle Deckard at <a href="mailto:michelle.deckard@psp.wa.gov">michelle.deckard@psp.wa.gov</a> or by following this <a href="https://app.smartsheet.com/b/form/d835519e3c454ef286dbbdcdf9ae008b">link.</a>&nbsp;<br>
+            </p>
+            <h2><strong>What specific grant writing help do we provide? </strong></h2>
+            <h3>What we can do:</h3>
+            <ul>
+              <li>Review and edit applications. </li>
+              <li>Draft grant applications.</li>
+              <li>Review opportunities or notices of funding.</li>
+              <li>Provide grant application management.</li>
+              <li>Provide grant strategy development.</li>
+            </ul>
+            <h3>What we cannot do:</h3>
+            <ul>
+              <li>Write a grant application by ourselves. This  requires collaboration from the partner.</li>
+              <li>Submit a grant for a partner. </li>
+              <li>Be a lead applicant or fiscal agent on any  grant we are helping with.</li>
+              <li>Grant write on SIL or regional EPA funding  opportunities.</li>
+              <li>Provide post award help </li>
+            </ul>
+            <h2>What is the Grant-Writing Assistance Process?</h2>
+            <h3>Getting Started</h3>
+            <p>First, you can request assistance by using our <a href="https://app.smartsheet.com/b/form/d835519e3c454ef286dbbdcdf9ae008b">intake  form</a>. To help us provide a more efficient  experience, please try to include as much information as possible.            </p>
+            <h3>Review</h3>
+            <p>We  will review your application within 48 hours. During this time, we will decide  if the request is eligible, and if we can help. After the review we will  contact you to set up a first meeting.            </p>
+            <h3>First meeting</h3>
+            <p>During  the first meeting we will discuss a high-level project description with you and  explain how we can help and what the process may look like. We may also need  follow up meetings depending on the type of help we are providing and the  project status.            </p>
+            <h3>Grant writing help</h3>
+            <p>Our  Grant Writer and Strategist will provide you with the requested help using the  format of your choosing. We recommend using Tracked Changes in Word Documents  for reviewing and editing. Our Grant Writer will work with you to make sure  deadlines are met and that applications are complete and competitive.            </p>
+            <h3>Post application</h3>
+<p>After  you have submitted the grant application, our team will contact you with a  feedback form to record the outcome of the application we helped with. This  helps us be able to provide any further help if needed.        </p>
+<h2><strong>How you can help us</strong></h2>
+            <p>To provide you with a better grant writing  experience, there are a few things you can help us with. Before contacting us,  please make sure you have prepared the below information. </p>
+            <p><strong>Note</strong>: We can only  support projects that align with our <a href="https://gcc02.safelinks.protection.outlook.com/?url=https%3A%2F%2Fpsp.wa.gov%2F2022AAupdate.php&data=05%7C02%7Cjennifer.grimm%40psp.wa.gov%7Cb89a15cb177a42af77ba08dc96d08865%7C11d0e217264e400a8ba057dcc127d72d%7C0%7C0%7C638551069583933493%7CUnknown%7CTWFpbGZsb3d8eyJWIjoiMC4wLjAwMDAiLCJQIjoiV2luMzIiLCJBTiI6Ik1haWwiLCJXVCI6Mn0%3D%7C0%7C%7C%7C&sdata=RgU6mqTi4v4LybhCNWEmZDpRA5%2F3KhrRAfF9AjavD6U%3D&reserved=0">Action  Agenda</a>, <a href="https://gcc02.safelinks.protection.outlook.com/?url=https%3A%2F%2Fpsp.wa.gov%2Fsalmon-recovery-overview.php&data=05%7C02%7Cjennifer.grimm%40psp.wa.gov%7Cb89a15cb177a42af77ba08dc96d08865%7C11d0e217264e400a8ba057dcc127d72d%7C0%7C0%7C638551069583943886%7CUnknown%7CTWFpbGZsb3d8eyJWIjoiMC4wLjAwMDAiLCJQIjoiV2luMzIiLCJBTiI6Ik1haWwiLCJXVCI6Mn0%3D%7C0%7C%7C%7C&sdata=PQd9SFAKrgKzNE%2BoRyjJkwiiz5W8HQdbqJLCgJoSyeA%3D&reserved=0">Salmon  Recovery Plan</a>, or the <a href="https://psp.wa.gov/science-workplan.php">Science Work Plan</a>.            </p>
+            <h3>Project/Program  Information</h3>
+        <p>Please have a project concept, problem  statement, project sponsor, collaborators, and project scope. </p>
+            <p>We will also need to know if the technical requirements of the  funding opportunity have either been completed or have a planned completion.  This may include design, engineering, or modeling. We cannot complete any  technical requirements for you.            </p>
+            <h3>Funding  Opportunity</h3>
+        <p>A grant opportunity or notice of funding has been reviewed and  it has been determined that the project sponsor and project are eligible. </p>
+            <p><strong>Note</strong>: <em>If you have a  project or concept that you have been unable to identify a funding opportunity  that fits, or if you have a project and/or funding need and want support in  identifying possible local partnerships, please contact our </em><em>Strategic Funding Coordinator</em><em> for help. It does not mean you cannot receive  assistance; it just means you are not at the writing stage yet.</em>            </p>
+            <h3>Capacity  &amp; Readiness</h3>
+            <p>Before beginning the grant application process, assess your  organization&rsquo;s ability complete the project, along with financial management,  compliance, reporting, and that you can manage your relationship with your  grant management officer and/or program officer. <em>Puget Sound Partnership  will not be able to provide any post award support through this team. </em></p>
+        <p>Make sure there are no barriers that would prevent the success of  the project if the funding was awarded. If there is a barrier, please contact  to our <a href="mailto:Reynolds,%20Jessica%20(PSP)%20%3cjessica.reynolds@psp.wa.gov%3e">Funding Barriers Strategist</a> for help before applying for funding. Read  more about funding barriers <a href="https://www.psp.wa.gov/funding-barriers.php">here</a>.</p>
+            <p>You should also make sure that you are ready to receive the  funding. This could include a current SAM account and UEI for federal  opportunities.            </p>
+            <h3>Financial  Information</h3>
+            <p>Please prepare a draft or a completed budget. We may be able to  give general feedback and advice on formatting, but specific numbers need to  come from the project sponsor. You should also have your organization&rsquo;s  financial documents in order and ready to include in any application. </p>
+            <p>After you have considered these items and are  ready to request assistance, please apply by using our <a href="https://app.smartsheet.com/b/form/d835519e3c454ef286dbbdcdf9ae008b">intake  form</a>. If you are unsure  about any of the above, please still complete the intake form and describe your  uncertainty and the Strategic Funding Team will help.<strong></strong></p>
+            <h2>Developing a Funding Strategy</h2>
+        <h3>Grant Matching –  Opportunity Driven</h3>
+            <p>When an opportunity opens that aligns with any existing projects or partner  funding needs and interests, the Strategic Funding Coordinator or the Grant  Writer and Strategist will share the opportunity  with you and suggest that it may be an  option for them.            </p>
+            <h3>Funding Strategy –  Project Driven</h3>
+            <p>A deeper dive into a large project where a partner seeks support in  developing a roadmap of funding opportunities that align with a project&rsquo;s  timeframe, funding needs, and additional criteria. </p>
+            <p>Before  starting a funding strategy, your project should  be assessed through community processes, have partners named, and have activities and outcomes  already determined. A request to develop a funding strategy will begin with an  in-depth consultation so we can learn  about your project, its history, timelines, and begin to review any materials  that have been created. We will use that information to find existing funding  sources that align with the project&rsquo;s needs and that the project would likely  be competitive for. This may mean piecing together different types of funding  for phases and looking out over multiple years. Together we will select the  funding opportunities that seem best fitted to pursue to fully fund the  project.</p>
+            <p>We are not the funders and while we might create a funding strategy with  options that seem promising, we have no guarantee that you will be awarded  those specific funding opportunities. We will do our best to help you to be in the best possible shape for your  applications with our technical assistance. </p>
+        <h3>Funding Strategy Steps:</h3>
+            <ul>
+              <li>Internal  Partner Readiness. </li>
+              <li>Consultation. </li>
+              <li>Strategy  Development. </li>
+              <li>Post  Discussion.            </li>
+            </ul>
+            <h2>Post Assistance</h2>
+            <p>To track and improve the effectiveness of our technical assistance, we ask that you keep us informed  by confirming you have submitted an  application and if it is successful. Other data on impact and success will  be explored on a case-by-case basis. We will contact you for this information. </p>
+            <h3>&nbsp;</h3>
+            <h2>Contact</h2>
+<p>For more information or if you have any questions  about your inquiry, please contact our Grant Writer and Strategist, Michelle  Deckard at&nbsp;<a href="mailto:michelle.deckard@psp.wa.gov">michelle.deckard@psp.wa.gov</a>. or follow this <a href="https://app.smartsheet.com/b/form/d835519e3c454ef286dbbdcdf9ae008b">link</a>.</P>
+<p>&nbsp;</p>
+			
+		<!-- LAST UPDATED -->
+		<p class="last-update">Last updated: 12/10/24</p>
+
+        <!-- InstanceEndEditable --> </div>
+		
+		<div class="col-sm-2 padding-20-top padding-0-right"> <!-- InstanceBeginRepeat name="right_nav_repeat" --><!-- InstanceBeginRepeatEntry --> <!-- InstanceBeginEditable name="right_nav_title" -->
+			<div class="right-nav-title margin-0-top ">SEE ALSO</div>
+			<!-- InstanceEndEditable -->
+			<ul class="nav-rightside-custom">
+				<!-- InstanceBeginEditable name="right_nav_links" -->
+				<li class="active" role="presentation"><a href="https://actionagenda.pugetsoundinfo.wa.gov/2022-2026ActionAgenda" target="_blank"><strong>Action Agenda Explorer</strong></a></li>
+				
+			
+				<!-- InstanceEndEditable -->
+			</ul>
+			<!-- InstanceEndRepeatEntry --><!-- InstanceEndRepeat --> </div>
+	</div>
+	<!--END OF ROW --> 
+</div>
+<!--END OF CONTENT CONTAINER -->
+
+<?php include 'includes/footer-inc.html';?>
+
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) --> 
+<script src="js/jquery-1.11.2.min.js"></script> 
+
+<!-- Include all compiled plugins (below), or include individual files as needed --> 
+<script src="js/bootstrap.js"></script> 
+<!-- custom js --> 
+<script src="js/custom.js"></script> 
+<!-- Google Tracking  -->
+<!-- InstanceBeginEditable name="EditRegion12" -->EditRegion12<!-- InstanceEndEditable --> 
+
+</body>
+<!-- InstanceEnd --></html>
