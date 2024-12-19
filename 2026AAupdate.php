0677fdd2d55a1e3a3fc459483bfd411b8dc2125d
jon bridgman <jonbridgman@gmail.com>
2024-12-18 18:30:17 -0800

    aa and leg updates

--- 2026AAupdate.php
+++ 2026AAupdate.php
@@ -64,7 +64,7 @@ navSelected = 1;
 	<div class="row">
 		<div class="col-sm-3 padding-20-top"></div>
 		<div class="col-sm-7 padding-20-top"> <!-- InstanceBeginEditable name="6col_header" -->
-			<h1>2026-2030 Action Agenda update</h1>
+			<h1>2026-30 Action Agenda update</h1>
 			<!-- InstanceEndEditable --></div>
 		<div class="col-sm-2 padding-20-top"></div>
 	</div>
@@ -82,58 +82,115 @@ navSelected = 1;
 		</div>
 		 
 		<div class="col-sm-7 padding-20-top content-column"> <!-- InstanceBeginEditable name="6col_content" -->
-	<!--
-		<div class="highlightbox">
-			<p>On August 4, 2022, the Environmental Protection Agency (EPA) approved the<a href="https://pspwa.box.com/shared/static/8zak4wiakdy94vc6104er8l3kn9bdxkw.pdf" target="new">2022-2026 Action Agenda adopted by the Leadership Council in June</a> as the Puget Sound National Estuary Program's (NEP) <a href="https://www.epa.gov/nep/comprehensive-conservation-and-management-plans" target="new">Comprehensive Conservation and Management Plan</a>. Learn more about the 2022-2026 Action Agenda below or visit the interactive <a href="https://actionagenda.pugetsoundinfo.wa.gov/2022-2026ActionAgenda" target="new">2022-2026 Action Agenda Explorer</a>.</p>
-		</div>-->
-		<div>
+
 		<h2 class="margin-0-top">We are updating the Puget Sound Action Agenda </h2>
-		<p>The Action Agenda is our community&rsquo;s  shared plan for advancing Puget Sound recovery. The 2026-2030 Action Agenda  update will advance progress towards recovery of the Puget Sound&rsquo;s ecosystem and human wellbeing <a href="https://vitalsigns.pugetsoundinfo.wa.gov/" target="new">Vital Signs</a> by building on  the foundation of the<a href="https://www.psp.wa.gov/2022AAupdate.php" target="new">2022-2026  Action Agenda</a>.</p>
-		<p>The 2026-2030 Action Agenda will describe approaches that include   ecosystem health, human wellbeing, climate change, and  environmental justice. The plan will have Strategies and Actions that guide  implementation, including commitments from Puget Sound recovery partners. It  will also include more indicators and targets designed to help us assess the  effectiveness of our collective efforts and hold us all accountable to making  meaningful progress. </p>
-        <p>The goals of the update are to: </p>
-        <ul>
+		<p>The Action Agenda is our community&rsquo;s  shared plan for advancing Puget Sound recovery. The 2026-30 Action Agenda  update will advance progress towards recovery of the Puget Sound&rsquo;s ecosystem and human wellbeing <a href="https://vitalsigns.pugetsoundinfo.wa.gov/" target="new">Vital Signs</a> by building on  the foundation of the<a href="https://www.psp.wa.gov/2022AAupdate.php" target="new">2022-26  Action Agenda</a>.</p>
+		<h2>Our Vision for the 2026-30 Action Agenda </h2>	
+		<p>The Action Agenda should articulate what priority actions and policy changes need to occur over the next four years to accelerate Puget Sound recovery. The 2026-30 Action Agenda update will improve our ability to implement and monitor recovery efforts, and will elevate human wellbeing, climate change, and environmental justice. Updates to the Strategies and Actions will allow PSP, PSP’s Boards, and partners to identify commitments, investments, or geographically specific needs to drive implementation of recovery work. The updated plan will also have more indicators and targets to help us assess the effectiveness of our collective efforts and hold ourselves accountable to making meaningful progress. </p>
+      
+		<h2>What will this update achieve? </h2>	
+		<p>The Action Agenda update process follows an adaptive management approach, that will allow us to evaluate and learn what the recovery community did or did not achieve, so we can adjust the Action Agenda (our plan) to improve recovery efforts (do). </p>
+			
+		<div class="floatright margin-30-left">  
+			<img src="images/adaptive_management_cycle.gif" alt="An image of the simplified adaptive management cycle. Four arrows make up a circle to show that each part of the cycle interacts with the others. Each arrow says Evaluate and Learn, Adjust, Plan, and Do." width="289" height="285"/>
+			<p class="caption align-center">The simplified adaptive management cycle</p>
+		</div>
+			
+		<p>The 2026-30 Action Agenda update will: </p>
+        <ul class="bullet-size-fix">
           <li>Strengthen implementation, accountability,  alignment and tracking of the Action Agenda.   </li>
-          <li>Elevate and further integrate human  wellbeing, climate change, and environmental justice.</li>
+          <li>Elevate and further human wellbeing, climate change, and environmental justice.</li>
           <li>Elevate and honor Tribal sovereignty and treaty  rights.</li>
-          <li> Expand participation and  meaningful engagement of diverse audiences, including vulnerable  populations (VP) and overburdened communities (OBC)</li>
+          <li>Promote meaningful engagement of diverse audiences, including vulnerable populations (VP) and overburdened communities (OBC).</li>
           <li>Update the online Action Agenda to improve  accessibility and usability. </li>
         </ul>
-        <div>
-          <div> </div>
-          <div> </div>
-          <div> </div>
-          <div> </div>
-          <div> </div>
-          <div> </div>
-        </div>
-<h2>Approach and timeline </h2>
-		  <img class="floatright" src="images/management-cycle-1.png" alt="An image of the simplified adaptive management cycle. Four arrows make up a circle to show that each part of the cycle interacts with the others. Each arrow says Evaluate and Learn, Adjust, Plan, and Do."/>
-<p>The   2026-2030 Action Agenda update process follows an adaptive management approach.  Over the course of 2025, the Partnership will work with recovery partners to <strong>evaluate  and learn</strong> from successes and challenges to implementing the 2022-2026  Action Agenda (<strong>plan</strong>) and <strong>adjust</strong> the 2026-2030 Action Agenda as  needed. The 2026-2030 Action Agenda is scheduled to be approved in summer 2026.<strong></strong></p>
-<div>
-  <div> </div>
-  <div></div>
-</div>
-		</div>
+       
+		<h2>Approach and timeline </h2>
 		
-		<h3>Timeline		</h3>
-		<p><strong>2025</strong> <strong></strong><br>
-        January-March 2025: Update 2026-2030 Action Agenda  content.</p>
-        <p>April-June 2025: Compile, develop initial draft 2026-2030  Action Agenda.&nbsp;</p>
-        <p>July-September 2025: Review first 2026-2030 Action Agenda  draft, Tribal consultation, confirm commitments/targets.&nbsp;</p>
-        <p>October-December 2025: Revise  and prepare the 2026-2030 Action Agenda for public comment.<br>
-          <strong> </strong><br>
-          <strong>2026 </strong><br>
-          January-March 2026: Public comment on 2026-2030 Action  Agenda, Tribal consultation.&nbsp;</p>
-        <p>April-August 2026: Finalize,  coordinate Boards review, and launch the 2026-2030 Action Agenda.</p>
-        <div>
-          <div> </div>
-          <div> </div>
-          <div> </div>
-          <div> </div>
-        </div>
+		<p>Over the course of 2025, the Partnership will work with recovery partners to evaluate and learn from successes and challenges to implementing the 2022-26 Action Agenda and revise the 2026-30 Action Agenda as needed. The 2026-30 Action Agenda is scheduled to be approved in summer 2026.</p>
 
+		<h3>Timeline</h3>
+			
+		
+		<table class="table-striped table-responsive">
+		<tbody>
+		<tr>
+			<td><p><strong>2025</strong></p></td>
+			<td>&nbsp;</td>
+		</tr>
+		<tr>
+			<td> <p>January-March 2025:</p></td>
+			<td>
+				<ul class="bullet-size-fix">
+					<li>Update 2026-30 Action Agenda content.</li>
+				</ul>
+			</td>
+		</tr>
+		<tr>
+			<td><p>April-June 2025:</p> </td>
+			<td>
+				<ul class="bullet-size-fix">
+					<li>Develop initial draft 2026-30 Action Agenda</li>
+				</ul>
+			</td>
+		</tr>
+		<tr>
+			<td><p>July-September 2025:</p> </td>
+			<td>
+				<ul class="bullet-size-fix">
+					<li>Review initial draft of the 2026-30 Action Agenda</li>
+					<li>Tribal consultation</li>
+				</ul>
+			</td>
+		</tr>
+		<tr>
+			<td><p>October-December 2025:</p> </td>
+			<td>
+				<ul class="bullet-size-fix">
+					<li>Confirm commitments and targets</li>
+					<li>Revise 2-26-30 Action Agenda</li>
+				</ul>
+			</td>
+		</tr>
+		<tr>
+			<td><p><strong>2026</strong></p> </td>
+			<td>
+			</td>
+		</tr>
+		<tr>
+			<td><p>January-March 2026:</p> </td>
+			<td>
+				<ul class="bullet-size-fix">
+					<li>Public comment on 2026-30 Action Agenda</li>
+					<li>Tribal consultation</li>
+				</ul>
+			</td>
+		</tr>
+		<tr>
+			<td><p>April-June 2026:</p> </td>
+			<td>
+				<ul class="bullet-size-fix">
+					<li>Coordinate Boards review</li>
+					<li>Present 2026-30 Action Agenda to Leadership Council for adoption</li>
+				</ul>
+			</td>
+		</tr>
+		<tr>
+			<td><p>July-September 2026:</p> </td>
+			<td>
+				<ul class="bullet-size-fix">
+					<li>Submit adopted Action Agenda to EPA for approval</li>
+				</ul>
+			</td>
+		</tr>
+	</tbody>
+	</table>
+
+	
+       
+    
         <!-- LAST UPDATED -->
-		<p class="last-update">Last updated: 11/21/24</p>
+		<p class="last-update">Last updated: 12/18/24</p>
 
         <!-- InstanceEndEditable --> </div>
 		
@@ -142,16 +199,9 @@ navSelected = 1;
 			<!-- InstanceEndEditable -->
 			<ul class="nav-rightside-custom">
 				<!-- InstanceBeginEditable name="right_nav_links" -->
-				<li class="active" role="presentation"><a href="https://actionagenda.pugetsoundinfo.wa.gov/2022-2026ActionAgenda" target="_blank"><strong>Action Agenda Explorer</strong></a></li>
-				<li class="active" role="presentation"><a href="https://pspwa.box.com/s/ck3b5pdx2vj4dmlfgqkynvnlafoet1k0" target="new">View our 2022-2026 Action Agenda overview presentation</a></li>
-				<li><a href="https://pspwa.box.com/shared/static/adopaw136pastn603ojfzc046k9iza5b.xlsx">Download an Excel table of 2022-2026 Action Agenda strategies, actions, key opportunities, and implementation considerations</a></li>
-				<li><a href="http://www.psp.wa.gov/implementation-strategies.php">Implementation Strategies</a></li>
-				<li><a href="https://pspwa.box.com/s/qwqatl3l3zi4x3ncchizy68auca9xcte">Tribal Habitat Priorities</a></li>
-				<li><a href="https://nwtreatytribes.org/habitatstrategy/" target="_blank"><em>gw∂dzadad &ldquo;Teaching of Our Ancestors&rdquo;, Tribal Habitat Strategy</em></a></li>
-				<li><a href="https://goia.wa.gov/relations/centennial-accord">Centennial Accord </a> </li>
-				<li><a href="https://www.psp.wa.gov/science-basis-of-recovery-overview.php">Science basis of recovery</a></li>
-				<li><a href="https://www.psp.wa.gov/salmon-recovery-overview.php">Salmon Recovery in Puget Sound</a></li>
-				<li><a href="https://psp.wa.gov/LIO-overview.php">Local Integrating Organizations </a>
+				<li><a href="https://pspwa.box.com/s/2u8djhjls0ys2b3uxdmdj3h8spq92s9i" target="_blank">2026-30 Action Agenda Factsheet</a></li>
+				<li><a href="https://pspwa.box.com/s/lmcobgdmya9a7dusrdelbrl7ak1w22w6" target="_blank">Evaluate and Learn Workshop</a></li>
+				
 			
 				<!-- InstanceEndEditable -->
 			</ul>
