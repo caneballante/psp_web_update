421a80f61e75054f2a57a28b3b1c36109b370645
ianmc3 <ian.mccabe@psp.wa.gov>
2025-02-05 14:40:48 -0800

    Update index.php

--- index.php
+++ index.php
@@ -95,6 +95,7 @@
 		<div class="col-sm-3 padding-20-top">
 			<h1>CHARTING THE COURSE TO RECOVERY</h1>
 			<div class="padding-5-bottom"><a href="2022AAupdate.php">2022-2026 Action Agenda</a></div>
+			<div class="padding-5-bottom"><a href="2026AAupdate.php"><span class="new-red">NEW! </span>2026-2030 Action Agenda Update</a></div>
 			<div class="padding-5-bottom"><a href="implementation-strategies.php">Implementation Strategies</a> </div>
 			<div class="padding-5-bottom"><a href="science-adaptive-management-framework.php">Results-based management</a></div>
 			<div class="padding-5-bottom"><a href="strategic-plan.php"><span class="new-red">NEW! </span>Strategic Plan 2024-2029</a></div>
