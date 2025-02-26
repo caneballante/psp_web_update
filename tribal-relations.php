45bb5d8a9062d72f816f332b2b8e6d43b662f8f1
ianmc3 <ian.mccabe@psp.wa.gov>
2025-02-26 10:52:58 -0800

    Update tribal-relations.php

--- tribal-relations.php
+++ tribal-relations.php
@@ -108,7 +108,7 @@
               <a href="https://pspwa.box.com/s/3q34inra9pi56askq35r7xn4mlkkqo3x">HEAL Act Tribal Government Engagement  Guide</a><br>
               <a href="https://pspwa.box.com/s/qjskpfh3ip1zoa0xnktws8z3ykplj5k4">HEAL Act Native American Community  Engagement Guide</a></p><h2>Map of reservations</h2>
               <p>A full screen version of the map can be viewed <a href="https://experience.arcgis.com/experience/98dc152906ec478f9256dcc83093c63d/">here</a>.</p> 
-				  <iframe src= "https://experience.arcgis.com/experience/98dc152906ec478f9256dcc83093c63d/" width="100%" height="100%" alt="Map of Tribal Reservations and Draft Treaty Ceded Areas of Puget Sound">
+				  <iframe src= "https://experience.arcgis.com/experience/98dc152906ec478f9256dcc83093c63d/" width="100%" height="1000" alt="Map of Tribal Reservations and Draft Treaty Ceded Areas of Puget Sound">
 </iframe>
                 <br>
               
