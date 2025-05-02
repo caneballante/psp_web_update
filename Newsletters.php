4a25927acdf8b51c972bc1364eeb1691917d1922
ianmc3 <ian.mccabe@psp.wa.gov>
2025-05-02 13:36:28 -0700

    More newsletter page updates

---
+++ Newsletters.php
@@ -0,0 +1,148 @@
+<!DOCTYPE html>
+<html lang="en"><!-- InstanceBegin template="/Templates/page-4-template.dwt" codeOutsideHTMLIsLocked="false" -->
+<head>
+
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-BGEZ5L3EJY"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-BGEZ5L3EJY');
+</script>
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<!-- InstanceBeginEditable name="doctitle" -->
+<title>Puget Sound Partnership Newsletters</title>
+<!-- InstanceEndEditable -->
+<!-- Bootstrap -->
+<link href="css/bootstrap.css" rel="stylesheet">
+<link href="css/custom.css" rel="stylesheet" type="text/css">
+<link href="css/custom-erika.css" rel="stylesheet" type="text/css">
+<!-- loads the wf-loading class right away to minimize FOUT -->
+<script>document.documentElement.className += ' wf-loading';</script>
+<!-- Font PRENTON TYPEKIT -->
+<script src="https://use.typekit.net/srt5jze.js"></script>
+<script>try{Typekit.load({ async: true });}catch(e){}</script>
+<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+<!--[if lt IE 9]>
+	<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+	<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+<!-- InstanceBeginEditable name="head" -->
+<script>
+/*this variable is used to set the proper nav to active. It should to the order the nav item is in the list*/
+  	navSelected = 13;
+</script>
+<!-- InstanceEndEditable -->
+<!-- InstanceParam name="OptionalRegion1" type="boolean" value="true" -->
+<!-- InstanceParam name="OptionalRegion2" type="boolean" value="false" -->
+<!-- InstanceParam name="OptionalRegion3" type="boolean" value="true" -->
+<!-- InstanceParam name="OptionalRegion4" type="boolean" value="false" -->
+<!-- InstanceParam name="OptionalRegion5" type="boolean" value="true" -->
+<!-- InstanceParam name="OptionalRegion6" type="boolean" value="true" -->
+<!-- InstanceParam name="OptionalRegion7" type="boolean" value="false" -->
+<!-- InstanceParam name="OptionalRegion8" type="boolean" value="false" -->
+</head>
+<body>
+<?php include 'includes/modal-inc.html';?>
+<!-- START IMAGE HEADER --> 
+ <!-- InstanceBeginEditable name="overviewphoto" -->
+<header class="overview-page-image-style overview-page-image34"> </header>
+<!-- InstanceEndEditable -->  
+<div class="container-fluid page-content padding-50-bottom">
+	<div class="row">
+		<div class="col-sm-3 padding-20-top"></div>
+		<div class="col-sm-7 padding-20-top"> <!-- InstanceBeginEditable name="6col_header" -->
+			<h1>Puget sound partnership newsletters</h1>
+			<p>&nbsp;</p>
+			<!--ANCHOR LINK above target--> 
+			<a name="executive_team"></a> <!-- InstanceEndEditable --></div>
+		<div class="col-sm-2 padding-20-top"></div>
+	</div>
+	
+	<div class="row"> 
+		<div class="col-sm-3">
+			<div class="nav-leftside-custom">
+				<ul class="nav nav-stacked nav-pills nav-leftside-custom padding-left-0 margin-10-top">
+					<!-- InstanceBeginEditable name="left_nav" -->
+				<?php include 'includes/ln-psp-overview.html';?>
+				<!-- InstanceEndEditable -->
+				</ul>
+			</div>
+		</div>
+		 
+		<div class="col-sm-7 padding-20-top content-column"> <!-- InstanceBeginEditable name="6col_content" --> 
+			<!--EXECUTIVE TEAM-->
+		 <h1 class="margin-0-top">keep up to date with the puget sound partnership</h1>
+		 <p>You can keep up to date with news from the Puget Sound Partnership, as well as upcoming opportunities to join events and workshops, by subscribing to our newsletters. Your email address will only be used to send you the newsletter of your choosing.</p>
+		<p>
+	    <?php include 'includes/newsletters-inc.html';?>
+			</p>
+		 <p>After you have entered your email address and gone through the security check, you'll be presented with a list of our newsletters. Select the box next to the <strong>subscription topic(s)</strong> you want to subscribe to (see image below) and then select <strong>Submit</strong>.</p>
+		 <p><img src="images/newsletterselection1.png" width="302" height="196" alt=""/></p>
+		 <p>You can also manage your subscriptions and subscriber account at <a href="https://public.govdelivery.com/accounts/WAPSP/subscriber/edit?preferences=true#tab1">this page.</a></p>
+		 <p>Here are the newsletters for you to choose from. </p>
+		 <h2>Agency updates</h2>
+		 <p>Legislative updates</p>
+		 <li>Legislative news related to the Puget Sound Partnership work and <a href="https://www.psp.wa.gov/state-government-relations.php">state goverment relations</a>, such as bills that are in play doing the legislative session</li>	
+			
+		 <p>Action Agenda bulletins</p>
+			<li>Updates and news involving our <a href="https://www.psp.wa.gov/2022AAupdate.php">Action Agenda</a>, the community's shared plan for advancing Puget Sound recovery. </li>
+		 <p>News releases</p>
+			<li>General news and updates about the Puget Sound Partnership</li>
+		 <p>RFP/RFQQ announcements</p>
+		 <p>Puget Sound Day on the hill		 </p>
+			<li>Updates and news regarding the annual <a href="https://psdoth.org/">Puget Sound Day on the Hill event</a></li>
+		 <h2>Science and evaluation</h2>
+		 <p>State of the Sound</p>
+			<li>News and announcements involving the <a href="https://www.psp.wa.gov/sos">State of the Sound</a>, Puget Sound Partnership's report on the health and recovery work within Puget Sound.</li>
+		 <p>Puget Sound Vital Signs</p>
+			<li>Updates regarding<a href="https://vitalsigns.pugetsoundinfo.wa.gov">Puget Sound Vital Signs</a>, the measures of ecosystem health and progress toward Puget Sound recovery goals.</li>
+	    <p>Whidbey Basin Cumulative Effects Evaluation</p>
+			<li>Updates about the cumulative effects evaulation of salmon recovery efforts in the Whidbey Basin.</li>
+		 <h2>Ecosystem and salmon recovery		 </h2>
+		 <p>Watershed announcements</p>
+			<li>The email list of watershed leads and interested parties.</li>
+	    <p>Salmon recovery announcements</p>
+			<li>Announcements from the Puget Sound Partnership and partners related to salmon recovery efforts.</li>
+		 <h2>Making Waves</h2>
+		 <p>Making Waves</p>
+			<li>Receive updates about new stories from our online <a href="https://makingwaves.psp.wa.gov/">Making Waves magazine</a>, told from the people protecting and restoring Puget Sound.</li>
+						
+	<p class="last-update">Last updated: 05/02/25</p>
+
+	  <!-- InstanceEndEditable --> </div>
+		
+		<div class="col-sm-2 padding-20-top padding-0-right"> <!-- InstanceBeginRepeat name="right_nav_repeat" --><!-- InstanceBeginRepeatEntry --> <!-- InstanceBeginEditable name="right_nav_title" -->
+			<!-- <div class="right-nav-title margin-20-top">SEE ALSO</div> -->
+			<!-- InstanceEndEditable -->
+			<ul class="nav-rightside-custom">
+				<!-- InstanceBeginEditable name="right_nav_links" -->
+				<!--<li role="presentation">Organizational Chart</li> -->
+				
+				<!-- InstanceEndEditable -->
+			</ul>
+			<!-- InstanceEndRepeatEntry --><!-- InstanceEndRepeat --> </div>
+	</div>
+	<!--END OF ROW --> 
+</div>
+<!--END OF CONTENT CONTAINER -->
+
+<?php include 'includes/footer-inc.html';?>
+
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) --> 
+<script src="js/jquery-1.11.2.min.js"></script> 
+
+<!-- Include all compiled plugins (below), or include individual files as needed --> 
+<script src="js/bootstrap.js"></script> 
+<!-- custom js --> 
+<script src="js/custom.js"></script> 
+<!-- Google Tracking  -->
+<!-- InstanceBeginEditable name="EditRegion12" -->EditRegion12<!-- InstanceEndEditable --> 
+
+</body>
+<!-- InstanceEnd --></html>
