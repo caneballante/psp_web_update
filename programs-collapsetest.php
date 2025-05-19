a819156320f7321e5ed8ed4119e8f20c7a212326
ianmc3 <ian.mccabe@psp.wa.gov>
2025-05-19 13:06:45 -0700

    Create programs-collapsetest.php

---
+++ programs-collapsetest.php
@@ -0,0 +1,285 @@
+<!DOCTYPE html>
+<html lang="en"><!-- InstanceBegin template="/Templates/page-4-template.dwt" codeOutsideHTMLIsLocked="false" -->
+<head>
+
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-BGEZ5L3EJY"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-BGEZ5L3EJY');
+</script>
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<!-- InstanceBeginEditable name="doctitle" -->
+<title>Puget Sound Partnership - Program Descriptions</title>
+<!-- InstanceEndEditable -->
+<!-- Bootstrap -->
+<link href="css/bootstrap.css" rel="stylesheet">
+<link href="css/custom.css" rel="stylesheet" type="text/css">
+<link href="css/custom-erika.css" rel="stylesheet" type="text/css">
+<!-- loads the wf-loading class right away to minimize FOUT -->
+<script>document.documentElement.className += ' wf-loading';</script>
+<!-- Font PRENTON TYPEKIT -->
+<script src="https://use.typekit.net/srt5jze.js"></script>
+<script>try{Typekit.load({ async: true });}catch(e){}</script>
+<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+<!--[if lt IE 9]>
+	<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+	<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+<!-- InstanceBeginEditable name="head" -->
+<script>
+/*this variable is used to set the proper nav to active. It should to the order the nav item is in the list*/
+  	navSelected = 7;
+</script>
+<!-- InstanceEndEditable -->
+<!-- InstanceParam name="OptionalRegion1" type="boolean" value="true" -->
+<!-- InstanceParam name="OptionalRegion2" type="boolean" value="false" -->
+<!-- InstanceParam name="OptionalRegion3" type="boolean" value="true" -->
+<!-- InstanceParam name="OptionalRegion4" type="boolean" value="false" -->
+<!-- InstanceParam name="OptionalRegion5" type="boolean" value="true" -->
+<!-- InstanceParam name="OptionalRegion6" type="boolean" value="true" -->
+<!-- InstanceParam name="OptionalRegion7" type="boolean" value="false" -->
+<!-- InstanceParam name="OptionalRegion8" type="boolean" value="true" -->
+</head>
+<body>
+<?php include 'includes/modal-inc.html';?>
+<!-- START IMAGE HEADER --> 
+ <!-- InstanceBeginEditable name="overviewphoto" -->
+<header class="overview-page-image-style overview-page-image34"> </header>
+<!-- InstanceEndEditable -->  <!-- InstanceBeginEditable name="EditRegion11" -->
+<div class="row">
+	<div class="col-sm-12 orange"></div>
+</div>
+<!-- InstanceEndEditable -->
+<div class="container-fluid page-content padding-50-bottom">
+	<div class="row">
+		<div class="col-sm-3 padding-20-top"></div>
+		<div class="col-sm-7 padding-20-top"> <!-- InstanceBeginEditable name="6col_header" -->
+			<h1>program descriptions</h1>
+			<p>&nbsp;</p>
+			<!-- InstanceEndEditable --></div>
+		<div class="col-sm-2 padding-20-top"></div>
+	</div>
+	
+	<div class="row"> 
+		<div class="col-sm-3">
+			<div class="nav-leftside-custom">
+				<ul class="nav nav-stacked nav-pills nav-leftside-custom padding-left-0 margin-10-top">
+					<!-- InstanceBeginEditable name="left_nav" -->
+				<?php include 'includes/ln-psp-overview.html';?>
+				<!-- InstanceEndEditable -->
+				</ul>
+			</div>
+		</div>
+		 
+		<div class="col-sm-7 padding-20-top content-column"> <!-- InstanceBeginEditable name="6col_content" -->
+		<h2 class="margin-0-top">Executive Management Team</h2>
+			<p>The Executive Management Team is responsible for the strategic leadership and management oversight of the Puget Sound Partnership. The Executive Management Team includes: </p>
+			<p>- The Executive Director<br>
+			  - 
+		    Executive Assistant<br>
+		    - Deputy Director<br>
+		    - 
+		    Special Assistant to the Director<br>
+		    - 
+		    Legislative Policy Director<br>
+		    - Special Assistant for Federal Affairs<br>
+		    - 
+		    Equity and Environmental Justice Manager<br>
+		    - 
+		    Chief Operating Officer. </p>
+			
+			<h2>EXTERNAL OPERATIONS</h2>
+			<p>The External Operations Division consists of the Boards, Communications, Communications, Ecosystem Recovery, Implementation, Salmon Recovery, and Science and Evaluation programs. </p>
+			<p>Select the header of each division to read expan and read more about them.</p>
+		<div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
+  <div class="panel panel-default">
+    <div class="panel-heading" role="tab" id="headingOne">
+      <h4 class="panel-title">
+        <a role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+          ADAPTIVE SYSTEMS AND ACCOUNTABILITY PROGRAM
+        </a>
+      </h4>
+    </div>
+    <div id="collapseOne" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="headingOne">
+      <div class="panel-body"><span class="padding-20-left">
+      <p>The Adaptive Systems and Accountability Program leads the development, management, and implementation of adaptive management processes and information management systems that support the recovery and long-term protection of the Puget Sound ecosystem. Using the <a href="http://cmp-openstandards.org/">Open Standards for the Practice of Conservation</a> as a guiding framework, the team works closely with Partnership staff and partners to ensure decisions about Puget Sound recovery are informed by the best available science, consider the diverse perspectives from around the region, and incorporate learning from past and current efforts</p></span></div>
+    </div>
+  </div>
+  <div class="panel panel-default">
+    <div class="panel-heading" role="tab" id="headingTwo">
+     	<div class="flexme">
+			<h4 class="panel-title">
+				<a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+			  Boards
+				</a>
+		  	</h4>
+   		</div>
+	</div>
+    <div id="collapseTwo" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingTwo">
+      <div class="panel-body">
+        <p>The Boards Program is responsible for supporting the Leadership Council, Ecosystem Coordination Board, and Science Panel, which in turn guide the agency in its charge to accelerate Puget Sound recovery. The Boards Program also supports the Puget Sound Salmon Recovery Council, which advises the Leadership Council on matters related to salmon recovery. This program coordinates and focuses the efforts of recovery partners on each of the agency’s boards to align actions, remove barriers, build partnerships, mobilize funding, and drive innovation to advance the recovery effort.</p>
+      </div>
+    </div>
+  </div>
+  <div class="panel panel-default">
+    <div class="panel-heading" role="tab" id="headingThree">
+      <h4 class="panel-title">
+        <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+          Communications
+        </a>
+      </h4>
+    </div>
+    <div id="collapseThree" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingThree">
+      <div class="panel-body">
+       <p>The Communications Team fosters the networks and relationships through the which the Puget Sound recovery community can come together to create and deliver effective, decision-critical communications. The Communications Team engages partners in Puget Sound recovery to build trust, reinforce relationships, and foster commitments, enabling key decision-makers to have the tools, information, and support needed to make policy and funding decisions that support Puget Sound recovery.</p>	
+      </div>
+    </div>
+  </div>
+			<div class="panel panel-default">
+    <div class="panel-heading" role="tab" id="headingThree">
+      <h4 class="panel-title">
+        <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseFour" aria-expanded="false" aria-controls="collapsezFour">
+          Local Integrating Organizations
+        </a>
+      </h4>
+    </div>
+    <div id="collapseFour" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingFour">
+      <div class="panel-body">
+       <p>The Local Integrating Organization (LIO) Program  is responsible for coordinating and supporting the 10 LIOs of Puget Sound. LIOs  are watershed-scale networks of ecosystem recovery practitioners,  decision-makers, NGOs, Tribes, and staff from local governments and agencies,  and they play a key role in identifying and coordinating local strategies and  actions that contribute to ecosystem recovery. The LIO Program manages the LIO  capacity contracts, facilitates regular group meetings with LIO Coordinators and  various partners, coordinates programmatic initiatives, and serves as liaison  and regional representative at LIO committee meetings. The LIO Program supports  LIOs in developing LIO Action Plans on Action Agenda topics, maintaining the  longer-term Ecosystem Recovery Plans, and hosting Ecosystem Coordination Board  and Leadership Council meetings as part of the LIO Local Forums, generally  working to facilitate and amplify local-regional connections and collaboration.</p>	
+      </div>
+    </div>
+  </div>
+			<div class="panel panel-default">
+    <div class="panel-heading" role="tab" id="headingThree">
+      <h4 class="panel-title">
+        <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseFive" aria-expanded="false" aria-controls="collapseFive">
+          Planning
+        </a>
+      </h4>
+    </div>
+    <div id="collapseFive" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingFive">
+      <div class="panel-body">
+       <p>The Planning Team leads the planning activities of the agency, particularly the collaborative development, adoption, and implementation of the <a href="https://www.psp.wa.gov/2022AAupdate.php">Action Agenda</a>, as well as the development and execution of <a href="http://www.psp.wa.gov/implementation-strategies.php">Implementation Strategies</a>.  The Planning Team works collaboratively to incorporate the work of many partners to identify and describe regional strategies and specific actions needed to protect and restore Puget Sound. The Team also works to ensure that the Action Agenda brings efficiency and coordination to a complex system, guides effective investment in Puget Sound recovery, and meets both our federal National Estuary Program and state-mandated requirements.</p>	
+      </div>
+    </div>
+  </div>
+			<div class="panel panel-default">
+    <div class="panel-heading" role="tab" id="headingThree">
+      <h4 class="panel-title">
+        <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseSix" aria-expanded="false" aria-controls="collapseSix">
+          Science & Evaluation
+        </a>
+      </h4>
+    </div>
+    <div id="collapseSix" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingSix">
+      <div class="panel-body">
+       <p>The Science & Evaluation Program is responsible for coordinating and implementing a shared, science-based system of measurement and monitoring that allows assessment of collective actions. This team’s work informs updates to the Puget Sound Action Agenda by tracking the status of ecosystem recovery indicators and assessing the effectiveness of recovery efforts. Science & Evaluation staff report ecosystem recovery progress through the Vital Signs website, as well as the biennial State of the Sound report. The Science & Evaluation Program also provides support to the Science Panel, including development of the Quadrennial Science Work Plan, and provides support for the Puget Sound Ecosystem Monitoring Program (PSEMP).	</p>
+      </div>
+    </div>
+  </div>
+			<div class="panel panel-default">
+    <div class="panel-heading" role="tab" id="headingThree">
+      <h4 class="panel-title">
+        <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseSeven" aria-expanded="false" aria-controls="collapseSeven">
+          Implementation
+        </a>
+      </h4>
+    </div>
+    <div id="collapseSeven" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingSeven">
+      <div class="panel-body">
+       <p>The Implementation Program is responsible for leading the shared regional plan for Puget Sound recovery and achieving the goals of the Puget Sound Vital Signs. This team supports the agency to advance recovery actions by breaking down barriers, broadening partnerships, addressing challenges, and building opportunities to accelerate implementation of Puget Sound recovery plans. The Implementation Program plays a leadership role in how the agency strategically addresses salmon and ecosystem recovery challenges as well as environmental health and human wellbeing disparities across the Puget Sound region. The Implementation Program establishes and holds the vision of the agency’s environmental justice work, leads initiatives to incorporate environmental justice into agency program work, and ensures the agency’s compliance with the Healthy Environment for All (HEAL) Act and related Executive Orders.</p>	
+      </div>
+    </div>
+  </div>
+			<div class="panel panel-default">
+    <div class="panel-heading" role="tab" id="headingThree">
+      <h4 class="panel-title">
+        <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseEight" aria-expanded="false" aria-controls="collapseEight">
+          Salmon Recovery
+        </a>
+      </h4>
+    </div>
+    <div id="collapseEight" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingThree">
+      <div class="panel-body">
+       <p>Honoring our commitment to Treaty rights, the Salmon Recovery Program works with Tribes and our partners collaboratively to protect critical salmon habitat and restore ecosystems as we strive to meet our collective recovery goals. The Salmon Recovery Program leads implementation of the regional salmon recovery plan and develops updates, coordinates strategic actions, finds and removes funding and policy barriers, shares information that connects science to action, centers environmental justice in salmon recovery, and mobilizesfunding for restoration, acquisition, research, assessment work, and more. Driven by science and evolving knowledge, the Program adapts approaches as we strive for healthy, harvestable salmon runs.</p>	
+      </div>
+    </div>
+  </div>
+			<div class="panel panel-default">
+    <div class="panel-heading" role="tab" id="headingThree">
+      <h4 class="panel-title">
+        <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseNine" aria-expanded="false" aria-controls="collapseNine">
+          Strategic funding
+        </a>
+      </h4>
+    </div>
+    <div id="collapseNine" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingThree">
+      <div class="panel-body">
+       <p>The Strategic Funding Program is responsible for  the effective implementation and accountability for the agency&rsquo;s agreement with  the Environmental Protection Agency under the Bipartisan Infrastructure Law.  The team leads the efforts of removing barriers to funding for local and  regional recovery projects and provides technical and policy support for  acquiring and managing grants and loans for partners.</p>	
+      </div>
+    </div>
+  </div>
+</div>
+<h2>INTERNAL OPERATIONS</h2>
+			<p> The Internal Operations Division supports the core functions of the agency by providing Fiscal, Human Resources, Administrative, and IT services. Additionally, it is the central point for all Environmental Protection Agency (EPA) program management. </p>
+			<div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
+  <div class="panel panel-default">
+    <div class="panel-heading" role="tab" id="headingTen">
+      <h4 class="panel-title">
+        <a role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseTen" aria-expanded="true" aria-controls="collapseTen">
+          Fiscal
+        </a>
+      </h4>
+    </div>
+    <div id="collapseTen" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="headingOne">
+      <div class="panel-body"><span class="padding-20-left"><p>The Fiscal Services Program provides leadership and support for agency priorities and programs through quality budgeting, contracting, grant management, purchasing, and accounting services. The program performs agency-wide financial activities, and ensures financial compliance with state and federal laws/regulations and reporting requirements, as well as ensures operational effectiveness and financial stability of the agency. Fiscal Services monitors the day-to-day fiscal operations of the agency, ensuring funds are effectively requested and used in alignment with the agency's mission and goals.</p></span></div>
+    </div>
+  </div>
+  <div class="panel panel-default">
+    <div class="panel-heading" role="tab" id="headingEleven">
+     	<div class="flexme">
+			<h4 class="panel-title">
+				<a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseEleven" aria-expanded="false" aria-controls="collapseEleven">
+			  Environmental Protection Agency (EPA)
+				</a>
+		  	</h4>
+   		</div>
+	</div>
+    <div id="collapseEleven" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingTwo">
+      <div class="panel-body">
+        <p>The Puget Sound Partnership is part of the National Estuary Program (NEP) administered by the U.S. Environmental Protection Agency (EPA). The Partnership and EPA have cooperative agreements that support our base responsibilities related to the Puget Sound NEP and implementation of the Puget Sound Action Agenda. The Partnership’s EPA Program Manager is responsible for compliance with the requirements in the cooperative agreements. The Program Manager is the lead for developing cooperative agreements, negotiating amendments to the agreements, complying with EPA reporting requirements, coordinating activities with other NEPs in EPA Region 10 and nationally as needed, as well as providing subcontract oversight, coordinating with other state agencies and entities that receive NEP funds, and other related activities.</p>
+      </div>
+    </div>
+  </div>
+
+			
+			<p class="last-update">Last updated: 05/16/25</p>
+			<!-- InstanceEndEditable --> </div>
+		
+		<div class="col-sm-2 padding-20-top padding-0-right"> <!-- InstanceBeginRepeat name="right_nav_repeat" --><!-- InstanceEndRepeat --> </div>
+	</div>
+	<!--END OF ROW --> 
+</div>
+<!--END OF CONTENT CONTAINER -->
+
+<?php include 'includes/footer-inc.html';?>
+
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) --> 
+<script src="js/jquery-1.11.2.min.js"></script> 
+
+<!-- Include all compiled plugins (below), or include individual files as needed --> 
+<script src="js/bootstrap.js"></script> 
+<!-- custom js --> 
+<script src="js/custom.js"></script> 
+<!-- Google Tracking  -->
+<!-- InstanceBeginEditable name="EditRegion12" -->EditRegion12<!-- InstanceEndEditable --> 
+
+</body>
+<!-- InstanceEnd --></html>
