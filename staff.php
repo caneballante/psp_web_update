fcbeafe5baf1e978a8c54a9514acf9dc97887179
ianmc3 <ian.mccabe@psp.wa.gov>
2025-07-31 11:29:03 -0700

    Update staff.php

--- staff.php
+++ staff.php
@@ -152,9 +152,9 @@
 						<h5>Michelle Deckard</h5>
 						<p>Grant Writer and Strategist<br>
 							360.819.0342 | <a href="mailto:michelle.deckard@psp.wa.gov">michelle.deckard@psp.wa.gov</a></p>
-						<h5>Bebhinn Dechery</h5>
+						<h5>VACANT</h5>
 						<p>Restoration and Transportation Funding Strategist<br>
-							360.870.1467 | <a href="mailto:bebhinn.dechery@psp.wa.gov">bebhinn.dechery@psp.wa.gov</a></p>
+							</p>
 		<h5>VACANT</h5>
 						<p>Strategic Funding Coordinator<br>
 						<h5>Heeju jung</h5>
@@ -370,7 +370,7 @@
 						<p>IT Specialist<br />
 							360.819.7498 | <a href="mailto:Anthony.ouren@psp.wa.gov">Anthony.ouren@psp.wa.gov</a> </p>
 						
-	<p class="last-update">Last updated: 07/30/25</p>
+	<p class="last-update">Last updated: 07/31/25</p>
 
 		<!-- InstanceEndEditable --> </div>
 		
