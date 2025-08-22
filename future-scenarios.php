03ff54530207508063f0f707f073edadca694467
ianmc3 <ian.mccabe@psp.wa.gov>
2025-08-22 12:39:49 -0700

    Update future-scenarios.php

--- future-scenarios.php
+++ future-scenarios.php
@@ -161,8 +161,8 @@
 			<ul class="nav-rightside-custom">
 				<!-- InstanceBeginEditable name="right_nav_links" -->
 				<li class="active" role="presentation"> <a href="/workshops.php">WORKSHOPS</a></li>
-				<li role="presentation"> <a href="https://pspwa.box.com/s/qzcsdqj5vdxnb1j58fc1i1meepr5jms2">EXPLORING BOLD LAND USE IDEAS IN PUGET SOUND WORKSHOP (2024)</a></li>
-				<li role="presentation"> <a href="https://pspwa.box.com/s/jirh8hdfbbsen7iifi2ktcy2wdhb4tux">BOLD LAND USE PLANNING WORKSHOP (2023)</a></li>
+				<li role="presentation"> <a href="https://pspwa.box.com/s/qzcsdqj5vdxnb1j58fc1i1meepr5jms2" target="new">EXPLORING BOLD LAND USE IDEAS IN PUGET SOUND WORKSHOP (2024)</a></li>
+				<li role="presentation"> <a href="https://pspwa.box.com/s/o7pj56ivao3paimyxheaumf9sym41lif" target="new">BOLD LAND USE PLANNING WORKSHOP (2023)</a></li>
 				<!-- InstanceEndEditable -->
 			</ul>
 			<!-- InstanceEndRepeatEntry --><!-- InstanceEndRepeat --> </div>
