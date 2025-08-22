552cc7c3b9ad8d6d2871e7e0cbdbb8b69733d83d
ianmc3 <ian.mccabe@psp.wa.gov>
2025-08-22 12:18:48 -0700

    Create workshops.php

---
+++ workshops.php
@@ -0,0 +1,161 @@
+<!DOCTYPE html>
+<html lang="en"><!-- InstanceBegin template="/Templates/page-4-template.dwt" codeOutsideHTMLIsLocked="false" -->
+<head>
+
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-BGEZ5L3EJY"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-BGEZ5L3EJY');
+</script>
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<!-- InstanceBeginEditable name="doctitle" -->
+<title>Puget Sound Partnership - Workshops</title>
+<!-- InstanceEndEditable -->
+<!-- Bootstrap -->
+<link href="css/bootstrap.css" rel="stylesheet">
+<link href="css/custom.css" rel="stylesheet" type="text/css">
+<link href="css/custom-erika.css" rel="stylesheet" type="text/css">
+<!-- loads the wf-loading class right away to minimize FOUT -->
+<script>document.documentElement.className += ' wf-loading';</script>
+<!-- Font PRENTON TYPEKIT -->
+<script src="https://use.typekit.net/srt5jze.js"></script>
+<script>try{Typekit.load({ async: true });}catch(e){}</script>
+<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+<!--[if lt IE 9]>
+	<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+	<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+<!-- InstanceBeginEditable name="head" -->
+<script>
+/*this variable is used to set the proper nav to active. It should to the order the nav item is in the list*/
+  	navSelected = 6;
+</script>
+<!-- InstanceEndEditable -->
+<!-- InstanceParam name="OptionalRegion1" type="boolean" value="true" -->
+<!-- InstanceParam name="OptionalRegion2" type="boolean" value="false" -->
+<!-- InstanceParam name="OptionalRegion3" type="boolean" value="true" -->
+<!-- InstanceParam name="OptionalRegion4" type="boolean" value="false" -->
+<!-- InstanceParam name="OptionalRegion5" type="boolean" value="true" -->
+<!-- InstanceParam name="OptionalRegion6" type="boolean" value="true" -->
+<!-- InstanceParam name="OptionalRegion7" type="boolean" value="false" -->
+<!-- InstanceParam name="OptionalRegion8" type="boolean" value="false" -->
+</head>
+<body>
+<?php include 'includes/modal-inc.html';?>
+<!-- START IMAGE HEADER --> 
+ <!-- InstanceBeginEditable name="overviewphoto" -->
+<header class="overview-page-image-style overview-page-image25"> </header>
+<!-- InstanceEndEditable -->  
+<div class="container-fluid page-content padding-50-bottom">
+	<div class="row">
+		<div class="col-sm-3 padding-20-top"></div>
+		<div class="col-sm-7 padding-20-top"> <!-- InstanceBeginEditable name="6col_header" -->
+			<h1>workshops			</h1>
+		<!-- InstanceEndEditable --></div>
+		<div class="col-sm-2 padding-20-top"></div>
+	</div>
+	
+	<div class="row"> 
+		<div class="col-sm-3">
+			<div class="nav-leftside-custom">
+				<ul class="nav nav-stacked nav-pills nav-leftside-custom padding-left-0 margin-10-top">
+					<!-- InstanceBeginEditable name="left_nav" -->
+				
+				<!-- InstanceEndEditable -->
+				</ul>
+			</div>
+		</div>
+		 
+		<div class="col-sm-7 padding-20-top content-column"> <!-- InstanceBeginEditable name="6col_content" -->
+		<div class="highlightbox margin-0-top margin-20-bottom">
+			
+			<h2>Past workshops</h2>
+			<p><h2 class="margin-0-top"><strong>Exploring Bold Land Use Ideas in Puget Sound  WORKSHOP</strong></h2>
+			
+			<p><br>
+			  On June 18, 2024, the  Future Scenarios project hosted a <a href="https://pspwa.box.com/s/qzcsdqj5vdxnb1j58fc1i1meepr5jms2" target="new">morning workshop</a> with the invitation to: <br>
+              <strong> - Learn about a new alternative scenario that incorporates bold land  use ideas</strong>. Leaders in Puget Sound recovery joined together to consider a future  that encompasses five of the bold ideas you helped generate in the November  2023 Bold Land Use Planning in Puget Sound workshop. <br>
+              <strong>- Explore each bold land use idea.</strong> A discussion was held to consider the assumptions that  underpin the bold ideas and their associated tradeoffs to identify specific  implications, risks, and opportunities, as well as ways to navigate and offset  them. <br>
+              <strong>- Contribute to a working list of concrete next steps.</strong> Using  insights from the scenario analysis, we helped to identify actions that could advance  some or all the bold ideas in both the near and long terms.<br>
+              <br>
+              This workshop  built on the work of the<a href="https://gcc02.safelinks.protection.outlook.com/?url=https%3A%2F%2Fpspwa.box.com%2Fs%2F21soyjonl5xcpbg07xvtvg6ztjs8do7f&data=05%7C02%7Ckatherine.wyatt%40psp.wa.gov%7Cb751ac52be7342f566cc08dc83ecc79e%7C11d0e217264e400a8ba057dcc127d72d%7C0%7C0%7C638530300562853263%7CUnknown%7CTWFpbGZsb3d8eyJWIjoiMC4wLjAwMDAiLCJQIjoiV2luMzIiLCJBTiI6Ik1haWwiLCJXVCI6Mn0%3D%7C0%7C%7C%7C&sdata=xuukOQenh5kQNVsdyKeX67WTLtZrJk1fr3MoQ5ePKFw%3D&reserved=0">November  2023 workshop</a>, which generated bold ideas that  address various development, human wellbeing, equity, and ecosystem restoration  implications derived from three alternative scenarios. Since then, the Future  Scenarios Project Team has worked closely with Strategic Initiative Leads and  other partners to create another alternative scenario that illustrates a subset  of bold land use ideas.  The June 2024 workshop  explored a new hybrid scenario that included five bold ideas: </p>
+		  <ol class="bullet-size-fix">
+			  <li><strong>Integrate  growth management.</strong> Adopt  growth management frameworks that encourage cohesion and planning to achieve  multiple benefits at the watershed scale working across jurisdictions.&nbsp;&nbsp;</li>
+			  <li><strong>Concentrate  growth in already developed areas.</strong> Adopt  policies and incentives that funnel growth into already developed urban areas  and emphasize redevelopment; limit rural growth and ensure rural growth that  does occur is near urban boundaries.&nbsp;&nbsp;</li>
+			  <li><strong>Redefine  critical areas.</strong> Protect  remaining salmon habitat through enhanced and more consistent critical area  definitions and implementation across Puget Sound and invest in restoration of  habitat important to salmon including barrier removal, riparian habitat,  floodplains, and estuaries. Use climate impacts as a catalyst to expand  restoration efforts in impacted areas.</li>
+			  <li><strong>Protect  and expand working lands.</strong> Adopt  policies that increase the viability of working agriculture and working  forestry. Fully value working lands and their services to incentivize farm and  forest land preservation.</li>
+			  <li><strong>Emphasize  innovative stormwater management</strong><strong>.</strong> Implement a variety of stormwater policies that emphasize  multiple benefits at the watershed scale such as use of regional facilities,  stormwater parks, and stormwater transfers.</li>
+		  </ol>
+			<p>If you&rsquo;d like to   learn more, please explore the workshop <a href="https://pspwa.box.com/s/0txxppe3lheqyhelxdpupjbh0v0kofzg" target="new">agenda</a> and <a href="https://pspwa.box.com/s/qzcsdqj5vdxnb1j58fc1i1meepr5jms2" target="new">materials</a><a href="mailto:katherine.wyatt@psp.wa.gov" target="new"></a>.</div>
+			
+						<div class="highlightbox margin-0-top margin-20-bottom">
+
+			</p>
+			<h2>BOLD LAND USE  PLanning WORKSHOP, 2023</h2><br>
+		  <p>The Bold Land Use Planning Workshop on November 7, 2023,  brought together over sixty regional land use and growth management experts,  local and state agency planners and coordinators, and stormwater and salmon  managers as members of the Puget Sound recovery community to generate bold  ideas that could help shepherd the <a href="https://pspwa.box.com/s/lcjugfkkijtffulmmyak0670jj4z6imf">Action Agenda&rsquo;s  Smart Growth strategy</a> into the future. (Links: <a href="https://pspwa.box.com/s/wqudt2vkenh1kkyiwww8muhwf6g5a562">Workshop Agenda</a>, <a href="https://pspwa.box.com/s/jirh8hdfbbsen7iifi2ktcy2wdhb4tux">Workshop  Presentation</a>, <a href="https://pspwa.box.com/s/o7pj56ivao3paimyxheaumf9sym41lif">Additional  Workshop Materials</a>, <a href="https://pspwa.box.com/s/21soyjonl5xcpbg07xvtvg6ztjs8do7f">Workshop  Synopsis</a>)<br></p>
+			  <p>Together, the group examined alternative future scenarios  and <strong>identified over seventy future implications</strong> for development, human  wellbeing and equity, and ecosystem restoration and conservation.  They <strong>generated twenty-seven individual  bold ideas</strong> that could address those implications.<br>
+		  Three Alternative Future  Scenarios informed that workshop.   Scenarios are not intended to be predictive, but rather illuminate a  range of possible conditions to assess our strategies and spur creative  thinking.  The scenarios were as follows: </p>
+<h3>Alternative 1 – Salmon Forward:</h3> 
+			<p>Explores a future where both government cohesion and willingness to act and human perceptions & behaviors are positive toward Puget Sound. This creates a mutually reinforcing virtuous cycle that unlocks large scale changes in how big cities are developed to minimize their impacts and maximize their livability in a way that calls people in to dense urban living and maximizes protection and restoration of land outside growth centers in existing urban areas. Land use is tightly managed, and protection of critical areas is high. <a href="https://pspwa.box.com/s/5w0i44z9toz52ay5kpder8urzwsuiknv">Read more</a>.</p>
+			
+			<h3>Alternative 2 – Networked Growth:</h3> <p>Government cohesion and willingness to act remain positive toward Puget Sound but human perceptions and behaviors are not. This alternative explores a future where people are less willing to live in dense urban centers and instead distribute themselves more evenly across the Puget Sound region and are relatively unengaged in considering the impacts of dispersed growth. Government responds to this by developing new planning models that ensure small and medium sized cities can make the infrastructure investments needed to manage new growth and land use continues to be tightly managed. <a href="https://pspwa.box.com/s/uuhaahsb4g8zqacbhg9eshmfa54lymas">Read more</a>.</p>  
+			
+		  <h3>Alternative 3 – Stewardship and Rural Living:</h3> <p>Explores a future where government cohesion and willingness to act is low but where human perceptions and behaviors are very aligned and positive toward Puget Sound. In this future, people distribute themselves more evenly across the Puget Sound region and prefer rural living to urban centers. Cities grow less rapidly and are less dense. Non- and quasi-government actors play a strong role in supporting individuals in implementing best management practices and taking other individual actions to slow Puget Sound decline and implement neighborhood-scale restorations. <a href="https://pspwa.box.com/s/xspjum0qy94973s5fisrxbft2t9r988k">Read more</a></p></div>
+			
+			
+			<p class="last-update">Last updated: 08/22/25</p>
+			<!-- InstanceEndEditable --> </div>
+		
+		<div class="col-sm-2 padding-20-top padding-0-right"> <!-- InstanceBeginRepeat name="right_nav_repeat" --><!-- InstanceBeginRepeatEntry --> <!-- InstanceBeginEditable name="right_nav_title" -->
+			
+			<!-- InstanceEndEditable -->
+			<ul class="nav-rightside-custom">
+				<!-- InstanceBeginEditable name="right_nav_links" -->
+				
+				<!-- InstanceEndEditable -->
+			</ul>
+			<!-- InstanceEndRepeatEntry --><!-- InstanceBeginRepeatEntry --> <!-- InstanceBeginEditable name="right_nav_title" -->
+			
+			<!-- InstanceEndEditable -->
+			<ul class="nav-rightside-custom">
+				<!-- InstanceBeginEditable name="right_nav_links" -->
+				
+				<!-- InstanceEndEditable -->
+			</ul>
+			<!-- InstanceEndRepeatEntry --><!-- InstanceBeginRepeatEntry --> <!-- InstanceBeginEditable name="right_nav_title" -->
+			
+			<!-- InstanceEndEditable -->
+			<ul class="nav-rightside-custom">
+				<!-- InstanceBeginEditable name="right_nav_links" -->
+				
+				<!--		<li role="presentation"> <a href="#">State of the Watersheds</a></li>
+					<li role="presentation"> <a href="#">Health of the Salish Sea Ecosystem Report</a></li>--> 
+				<!-- InstanceEndEditable -->
+			</ul>
+			<!-- InstanceEndRepeatEntry --><!-- InstanceEndRepeat --> </div>
+	</div>
+	<!--END OF ROW --> 
+</div>
+<!--END OF CONTENT CONTAINER -->
+
+<?php include 'includes/footer-inc.html';?>
+
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) --> 
+<script src="js/jquery-1.11.2.min.js"></script> 
+
+<!-- Include all compiled plugins (below), or include individual files as needed --> 
+<script src="js/bootstrap.js"></script> 
+<!-- custom js --> 
+<script src="js/custom.js"></script> 
+<!-- Google Tracking  -->
+<!-- InstanceBeginEditable name="EditRegion12" -->EditRegion12<!-- InstanceEndEditable --> 
+
+</body>
+<!-- InstanceEnd --></html>
