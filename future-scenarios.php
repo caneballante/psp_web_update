47b30dee51e93febc47a19145fb9854eff772fd4
ianmc3 <ian.mccabe@psp.wa.gov>
2025-08-22 12:27:09 -0700

    Update future-scenarios.php

--- future-scenarios.php
+++ future-scenarios.php
@@ -35,7 +35,7 @@
 <!-- InstanceBeginEditable name="head" -->
 <script>
 /*this variable is used to set the proper nav to active. It should to the order the nav item is in the list*/
-  	navSelected = 6;
+  	navSelected = 1;
 </script>
 <!-- InstanceEndEditable -->
 <!-- InstanceParam name="OptionalRegion1" type="boolean" value="true" -->
@@ -68,7 +68,7 @@
 			<div class="nav-leftside-custom">
 				<ul class="nav nav-stacked nav-pills nav-leftside-custom padding-left-0 margin-10-top">
 					<!-- InstanceBeginEditable name="left_nav" -->
-				
+				<?php include 'includes/ln-future-scenarios';?>
 				<!-- InstanceEndEditable -->
 				</ul>
 			</div>
