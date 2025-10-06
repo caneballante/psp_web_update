d9a60ea5d77dd1dac6b473993a02afde98a5b1d8
ianmc3 <ian.mccabe@psp.wa.gov>
2025-10-06 16:49:20 -0700

    Page updates

--- accessibility.php
+++ accessibility.php
@@ -58,9 +58,8 @@
 	<div class="row">
 		<div class="col-sm-3 padding-20-top"></div>
 		<div class="col-sm-7 padding-20-top"> <!-- InstanceBeginEditable name="6col_header" -->
-			<h1>ACCESSIBILITY POLICY</h1>
-			<p>&nbsp;</p>
-			<!-- InstanceEndEditable --></div>
+			<h1>ACCESSIBILITY POLICY			</h1>
+		<!-- InstanceEndEditable --></div>
 		<div class="col-sm-2 padding-20-top"></div>
 	</div>
 	
@@ -77,9 +76,9 @@
 		 
 		<div class="col-sm-7 padding-20-top content-column"> <!-- InstanceBeginEditable name="6col_content" -->
 <h2 class="margin-0-top">OUR COMMITMENT TO ACCESSIBILITY</h2>
-			<p>The Puget Sound Partnership (PSP) recognizes the importance of using technology to provide information and services to carry out our mission. As such, PSP is committed to ensuring our Web sites, documents, and application systems are accessible to the public and Washington State employees, including individuals with disabilities.</p>
+			<p>The Puget Sound Partnership recognizes the importance of using technology to provide information and services to carry out our mission. As such, we are committed to ensuring our Web sites, documents, and application systems are accessible to the public and Washington State employees, including individuals with disabilities.</p>
 			<h2>GUIDELINES AND STANDARDS</h2>
-			<p>PSP strives to meet the Washington State Office of the Chief Information Officer (OCIO)<a href="https://ocio.wa.gov/policy/accessibility" target="_blank"> Policy 188 Accessibility</a>, <a href="http://ocio.wa.gov/policy/minimum-accessibility-standard" target="_blank">188.10 Minimum Accessibility Standards, </a>and the <a href="http://www.w3.org/TR/WCAG20/" target="_blank">Web Content Accessibility Guidelines (WCAG) 2.0</a>, Level AA compliance, including the guidelines associated with these principles:</p>
+			<p>We strive to meet the Washington State Office of the Chief Information Officer (OCIO)<a href="https://ocio.wa.gov/policy/accessibility" target="_blank"> Policy 188 Accessibility</a>, <a href="http://ocio.wa.gov/policy/minimum-accessibility-standard" target="_blank">188.10 Minimum Accessibility Standards, </a>and the <a href="http://www.w3.org/TR/WCAG20/" target="_blank">Web Content Accessibility Guidelines (WCAG) 2.0</a>, Level AA compliance, including the guidelines associated with these principles:</p>
 			<ol class="bullet-size-fix">
 				<li><strong>Perceivable:</strong> Information and user interface components must be presentable to users in ways they can perceive.</li>
 				<li><strong>Operable:</strong> User interface components and navigation must be operable.</li>
@@ -87,11 +86,11 @@
 				<li><strong>Robust:</strong> Content must be robust enough that it can be interpreted reliably by a wide variety of user agents, including assistive technologies.</li>
 			</ol>
 			<h2>Need assistance with our Web sites or want to report an accessibility issue?</h2>
-			<p>Please contact the PSP Accessibility Coordinator with information on Web sites, documents, or application systems that are not fully accessible to you, along with the type of accommodation that you need. The Accessibility Coordinator will review your accessibility request, contact the responsible agency staff, determine how to best meet your accessibility request, and respond to you within five (5) business days with the access or information that you need, and the plan to improve accessibility in the future. </p>
+			<p>Please contact our Accessibility Coordinator with information on websites, documents, or application systems that are not fully accessible to you, along with the type of accommodation that you need. The Accessibility Coordinator will review your accessibility request, contact the responsible agency staff, determine how to best meet your accessibility request, and respond to you within five (5) business days with the access or information that you need, and the plan to improve accessibility in the future. </p>
 			<h3>Contact Us</h3>
-			<p><a href="mailto:brent.barnes@psp.wa.gov">Accessibility Coordinator</a>,<br>
-			 360.339.5861</p>
-			
+			<p><a href="mailto:jennifer.carlson@psp.wa.gov">Accessibility Coordinator</a>,<br>
+		 360.819.0530</p>
+			<p class="last-update">Last updated: 10/06/25</p>
 			<!-- InstanceEndEditable --> </div>
 		
 		<div class="col-sm-2 padding-20-top padding-0-right"> <!-- InstanceBeginRepeat name="right_nav_repeat" --><!-- InstanceEndRepeat --> </div>
