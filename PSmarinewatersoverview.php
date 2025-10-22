3a263400e649886d9397343c968e3a9e0762c48f
ianmc3 <ian.mccabe@psp.wa.gov>
2025-10-22 12:07:15 -0700

    Update PSmarinewatersoverview.php

--- PSmarinewatersoverview.php
+++ PSmarinewatersoverview.php
@@ -76,8 +76,8 @@
 		<div class="col-sm-7 padding-20-top content-column"> <!-- InstanceBeginEditable name="6col_content" -->
 	
 <span class="floatright padding-20-left no-icon">
-	<p><a href="https://pspwa.box.com/s/7ye4t366v5uzil8zybrqtyx11qd32779"><img src="images/psmarinewaterscover24.png"  alt="Cover image from the 2024 Puget Sound Marine Waters Overview, showing Puget Sound water framed a rocky beach and green hills" width="400" height="309"/></a><br>
-			<a href="https://pspwa.box.com/s/7ye4t366v5uzil8zybrqtyx11qd32779">Click here to download the <br>
+	<p><a href="https://pspwa.box.com/s/7ye4t366v5uzil8zybrqtyx11qd32779" target="new"><img src="images/psmarinewaterscover24.png"  alt="Cover image from the 2024 Puget Sound Marine Waters Overview, showing Puget Sound water framed a rocky beach and green hills" width="400" height="309"/></a><br>
+			<a href="https://pspwa.box.com/s/7ye4t366v5uzil8zybrqtyx11qd32779" target="new">Select here to download the <br>
 			Puget Sound Marine Waters 2024 Overview</a></p></span>
 			
 <p>This Marine Waters Overview provides a perspective of 2024 Puget Sound marine water quality and conditions and associated biota from comprehensive monitoring and observing programs.<br>
