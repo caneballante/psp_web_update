16c110f672957c79f3d8a2397a32eedc9528b12d
ianmc3 <ian.mccabe@psp.wa.gov>
2025-10-30 10:19:42 -0700

    More salmon updates

--- salmon-recovery-overview.php
+++ salmon-recovery-overview.php
@@ -61,7 +61,7 @@
 	<div class="row">
 		<div class="col-sm-3 padding-20-top"></div>
 		<div class="col-sm-7 padding-20-top"> <!-- InstanceBeginEditable name="6col_header" -->
-			<h1>Salmon Recovery IN PUGET SOUND	-	Overview	</h1>
+			<h1>Salmon Recovery IN PUGET SOUND		Overview	</h1>
 		<!-- InstanceEndEditable --></div>
 		<div class="col-sm-2 padding-20-top"></div>
 	</div>
