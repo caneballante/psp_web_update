07cf95032f363adce492fd7b13a8db24abda48e5
ianmc3 <ian.mccabe@psp.wa.gov>
2026-01-05 15:11:38 -0800

    Update legislative_updates.php

--- legislative_updates.php
+++ legislative_updates.php
@@ -81,7 +81,7 @@
 		<div class="col-sm-7 padding-20-top content-column"> <!-- InstanceBeginEditable name="6col_content" -->
 			<h2 class="margin-0-top">2026 Legislative Session</h2>
 			<p><a href="https://content.govdelivery.com/accounts/WAPSP/bulletins/4014866" target="new">General session info</a></p>
-			<p><a href="https://pspwa.box.com/s/zbe76vto5pm7kbj11kfhxeqggoitrlfw" target="new">2025 Puget Sound Legislative Priorities</a></p><br>
+			<p><a href="https://pspwa.box.com/s/zbe76vto5pm7kbj11kfhxeqggoitrlfw" target="new">2026 Puget Sound Legislative Priorities</a></p><br>
 		<div>
 		  <IFRAME WIDTH=700 HEIGHT=500 FRAMEBORDER=0 SRC=https://app.smartsheet.com/b/publish?EQBCT=08e4eadc90534836b4ba1440cfd101a9></IFRAME>
 			</div>	
