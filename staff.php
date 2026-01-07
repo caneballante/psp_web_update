b2d665ad98b3a3d989c191ea269a95a571b1e210
ianmc3 <ian.mccabe@psp.wa.gov>
2026-01-07 15:38:51 -0800

    Update staff.php

--- staff.php
+++ staff.php
@@ -147,8 +147,7 @@
 			
 						<h5>Ali carter</h5>
 						<p>Funding Barrier Strategist</p>
-						<p>360.791.0966  | <a href="mailto:alexandra.carter@psp.wa.gov">alexandra.carter@psp.wa.gov</a><br>
-	    </p>
+						<p>360.791.0966  | <a href="mailto:alexandra.carter@psp.wa.gov">alexandra.carter@psp.wa.gov</a></p>
 						<h5>Michelle Deckard</h5>
 						<p>Grant Writer and Strategist<br>
 							360.819.0342 | <a href="mailto:michelle.deckard@psp.wa.gov">michelle.deckard@psp.wa.gov</a></p>
