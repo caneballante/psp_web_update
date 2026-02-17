778907ddc6174da4f7ac366945059965fc7ce18b
jon bridgman <jonbridgman@gmail.com>
2026-02-17 14:32:26 -0800

    Update 2026AAupdate.php

--- 2026AAupdate.php
+++ 2026AAupdate.php
@@ -208,7 +208,7 @@ navSelected = 1;
        
     
         <!-- LAST UPDATED -->
-		<p class="last-update">Last updated: 01/05/26</p>
+		<p class="last-update">Last updated: 02/17/26</p>
 
         <!-- InstanceEndEditable --> </div>
 		
