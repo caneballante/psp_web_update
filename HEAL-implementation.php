da20169118f2f131045956aba9c0223eab8e7f8c
ianmc3 <ian.mccabe@psp.wa.gov>
2026-02-17 17:01:41 -0800

    Update HEAL-implementation.php

--- HEAL-implementation.php
+++ HEAL-implementation.php
@@ -87,6 +87,7 @@ navSelected = 3;
 		    plan commits us to centering equity and environmental justice in our work.</p>
 <p>The Partnership published its <a href="https://pspwa.box.com/s/ywh3fvh26piwbajhi2ap8ezus3s43kh8" target="new">Healthy Environment for All (HEAL) Implementation Plan in 2023</a>. In 2024, we updated the graphics and   made small edits to address formatting and spelling errors only. <a href="https://pspwa.box.com/s/ywh3fvh26piwbajhi2ap8ezus3s43kh8">The HEAL Implementation Plan</a> describes how we will evaluate our progress toward this Strategic Plan goal. We look forward to reporting on our progress and publishing an updated plan in 2025.</p>
 <p>See below for reporting details to the Office of  Financial Management, the Governor&rsquo;s Office, and the Environmental Justice  Council:</p>
+<p>.</p>
 <p><a href="https://pspwa.box.com/s/ywh3fvh26piwbajhi2ap8ezus3s43kh8" target="new">Download a copy of the draft Puget Sound Partnership HEAL Implementation Plan</a></p>
 
 
