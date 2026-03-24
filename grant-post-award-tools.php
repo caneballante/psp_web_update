9205d2f7dcbdc5ad8bcb90c2b47fb283244f2035
ianmc3 <ian.mccabe@psp.wa.gov>
2026-03-24 15:27:32 -0700

    Create grant-post-award-tools.php

---
+++ grant-post-award-tools.php
@@ -0,0 +1,207 @@
+<!DOCTYPE html>
+<html lang="en"><!-- InstanceBegin template="/Templates/page-4-template.dwt" codeOutsideHTMLIsLocked="false" -->
+<head>
+
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-BGEZ5L3EJY"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-BGEZ5L3EJY');
+</script>
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<!-- InstanceBeginEditable name="doctitle" -->
+<title>Puget Sound Partnership Technical Assistance and Grant Writing</title>
+<!-- InstanceEndEditable -->
+<!-- Bootstrap -->
+<link href="css/bootstrap.css" rel="stylesheet">
+<link href="css/custom.css" rel="stylesheet" type="text/css">
+<link href="css/custom-erika.css" rel="stylesheet" type="text/css">
+<!-- loads the wf-loading class right away to minimize FOUT -->
+<script>document.documentElement.className += ' wf-loading';</script>
+<!-- Font PRENTON TYPEKIT -->
+<script src="https://use.typekit.net/srt5jze.js"></script>
+<script>try{Typekit.load({ async: true });}catch(e){}</script>
+<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+<!--[if lt IE 9]>
+	<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+	<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+<!-- InstanceBeginEditable name="head" -->
+<script>
+/*this variable is used to set the proper nav to active. It should to the order the nav item is in the list*/
+navSelected = 5;
+	
+</script>
+<meta property="og:image" content="http://www.psp.wa.gov/images/inside_image_99_sm.jpg" />
+<!-- InstanceEndEditable -->
+<!-- InstanceParam name="OptionalRegion1" type="boolean" value="true" -->
+<!-- InstanceParam name="OptionalRegion2" type="boolean" value="false" -->
+<!-- InstanceParam name="OptionalRegion3" type="boolean" value="true" -->
+<!-- InstanceParam name="OptionalRegion4" type="boolean" value="false" -->
+<!-- InstanceParam name="OptionalRegion5" type="boolean" value="true" -->
+<!-- InstanceParam name="OptionalRegion6" type="boolean" value="true" -->
+<!-- InstanceParam name="OptionalRegion7" type="boolean" value="false" -->
+<!-- InstanceParam name="OptionalRegion8" type="boolean" value="true" -->
+</head>
+<body>
+<?php include 'includes/modal-inc.html';?>
+<!-- START IMAGE HEADER --> 
+ <!-- InstanceBeginEditable name="overviewphoto" -->
+<header class="overview-page-image-style overview-page-image118">
+</header>
+<!-- InstanceEndEditable -->  <!-- InstanceBeginEditable name="EditRegion11" -->
+<div class="row">
+	<div class="col-xs-12 orange-fill align-center overview-logo-banner"><img src="images/2016-AA-banner-logo-none-small.png" width="146" height="30" alt=""/></div>
+</div>
+<!-- InstanceEndEditable -->
+<div class="container-fluid page-content padding-50-bottom">
+	<div class="row">
+		<div class="col-sm-3 padding-20-top"></div>
+		<div class="col-sm-7 padding-20-top"> <!-- InstanceBeginEditable name="6col_header" -->
+			<h1>Grant Writing: Post Award Toolkit</h1>
+			<!-- InstanceEndEditable --></div>
+		<div class="col-sm-2 padding-20-top"></div>
+	</div>
+	
+	<div class="row"> 
+		<div class="col-sm-3">
+			<div class="nav-leftside-custom">
+				<ul class="nav nav-stacked nav-pills nav-leftside-custom padding-left-0 margin-10-top">
+					<!-- InstanceBeginEditable name="left_nav" -->
+					<?php include 'includes/ln-strategic-funding.html';?>
+					
+					<!-- InstanceEndEditable -->
+				</ul>
+			</div>
+		</div>
+		 
+		<div class="col-sm-7 padding-20-top content-column"> <!-- InstanceBeginEditable name="6col_content" -->
+	<!--
+		<div class="highlightbox">
+			<p>On August 4, 2022, the Environmental Protection Agency (EPA) approved the<a href="https://pspwa.box.com/shared/static/8zak4wiakdy94vc6104er8l3kn9bdxkw.pdf" target="new">2022-2026 Action Agenda adopted by the Leadership Council in June</a> as the Puget Sound National Estuary Program's (NEP) <a href="https://www.epa.gov/nep/comprehensive-conservation-and-management-plans" target="new">Comprehensive Conservation and Management Plan</a>. Learn more about the 2022-2026 Action Agenda below or visit the interactive <a href="https://actionagenda.pugetsoundinfo.wa.gov/2022-2026ActionAgenda" target="new">2022-2026 Action Agenda Explorer</a>.</p>
+		</div>-->
+			
+			<h2>Post Award Toolkit – Getting Ahead Before Award</h2>
+            <p>After your application has been submitted, it may take time before  you hear more from the funder. In the meantime, if you think you are likely to  be successful and awarded the funds, you can start preparing now for successful  grant management. </p>
+            <p>This page will provide you with helpful tips and resources  if your application is successful. However, every funder has different requirements  and processes that must be followed and may differ from the information on this  page. If you are awarded, please consult your funder for their specifications  and follow their directions before anything on this page. </p>
+            <h2>When you receive notice that you have been awarded funding</h2>
+            <p>Once you receive notice that you have been awarded funding,  the first step is to begin conversations with your leadership, fiscal staff,  and partners to discuss the award, management, and timelines. If not already  identified, you must elect a staff member from the project sponsor and lead  applicant as the main contact and communicator from the grantee side. Once a  contract or agreement is in place, the grantee&rsquo;s main contact or project  manager should create a grant calendar.</p>
+            <h3>Grant Calendar</h3>
+            <p>The grant calendar should include all relevant dates for  reporting, evaluation, and any deadline or significant date for the duration of  the grant. </p>
+            <p>Ideally, the next step would be to have a kick-off meeting  with all partners. Here you should discuss the award and ask questions, such  as: </p>
+        <ul class="bullet-size-fix">
+              <li>Were there any differences in what was asked for  compared to what was awarded? </li>
+              <li>Are there any on-the-ground updates that would  have an impact on the project activities? </li>
+              <li>What does the reporting schedule look like? </li>
+            </ul>
+            <p>It&rsquo;s important that the grant calendar ensures the entire  team is aware of the significant dates, including who is responsible for completing  or contributing to any deliverable/performance metrics. As the project sponsor,  it will also be crucial to have your internal fiscal team aware of the calendar  and their roles as well.</p>
+            <h2>After you receive the award begin the project work </h2>
+            <p>Once you have received the award and started project work it  is crucial to track all time, expenses, and any impacts. If there is any  confusion in your guidance from the funder, make sure to contact them and receive  clarification before taking any actions that can&rsquo;t be undone. </p>
+            <h3>Consider using grant management software</h3>
+        <p>Depending on the capacity in your organization and the  number of grant opportunities you manage at one time, it may be wise to use grant  management software to help you:</p>
+            <ul class="bullet-size-fix">
+              <li><strong>Track staff time</strong> - Your organization  should have a process in place for staff to track their time spent on grant  activities. Whether this is through specific software or more informal  estimates. </li>
+              <li><strong>Track all expenses</strong> - All grant expenses, such  as staff salary, benefits, contracting, and direct purchases should be  accounted for, tracked, and documented according to the funder&rsquo;s requirements.  Different expenses will require different supporting documentation. </li>
+              <li><strong>Conduct regular audits and reconciliations - </strong>Regularly  check that staff time and expenses are progressing as scheduled. Identify and  discuss early any deviations from the schedule and involve the funder when  appropriate.            </li>
+            </ul>
+            <h2>Templates and Document Guidance </h2>
+            <p>This section provides some recommended templates to use  throughout your grant management. If you are awarded, please consult your  funder for their specifications and always follow the funder&rsquo;s directions above  this guidance. </p>
+            <h3>Reporting </h3>
+            <p>Your funder will have an outlined process for you to follow  and report on your grant progress. Different funders will have different  formats and timelines. Some will ask for information with every invoice and others  may ask only for a final report. It can vary greatly and should be discussed  early with your funder and grant team. </p>
+        <p>If the reporting guidance is open ended and you are unsure  what you should include, think about including:</p>
+            <ul type="disc" class="bullet-size-fix">
+              <li><strong>Percent       Spent Down</strong> – This is how much of the grant funds have you spent up to       that point. Alternatively, you could include a second metric to represent       how much money you have expended in that time frame alone. For example, you       may have 70% spent down at the end of this reporting period, but during       that period you only spent 10%. There is no right or wrong answer, it all       depends on what you planned out for your project. If you are concerned you       will not spend down at your anticipated rate or by the end of the grant       period, reach out to your funder.</li>
+              <li><strong>Milestones,       Accomplishments, and Progress</strong> – Did you complete a milestone or goal?       Did you work towards one? Relate the report back to the wording and       milestones you wrote into your proposal. Depending on the project or schedule,       you may not need to complete a milestone for each report, but you must       show the funder how you have been working to achieve them. </li>
+              <li><strong>Deviations</strong> – Has anything changed from your submitted work plan? If there are       significant changes, please discuss those with your funder before taking       any action. If there are small deviations within your plan these should be       noted. If there were any significant deviations that were discussed and       approved by your funder provide explanation on these. It never hurts to       have additional documentation</li>
+              <li><strong>Upcoming       Concerns</strong> – How are you looking ahead and problem solving? For example,       if you have low community turn out during your last outreach event, what       are you going to do differently next time? Or perhaps a permitting process       took longer than anticipated, so how will that impact the implementation       schedule? Small things will always happen but identifying them and problem       solving early in the process shows that you are adaptable and will help       keep the project on track. </li>
+              <li><strong>Attachments</strong> – What else can you include? Some funders will have separate submission       requirements for final deliverables but always feel free to include       attachments that show your work, progress, community participation, and       feedback with your reporting. This may not be applicable to every funder       and grant situation, but when able it can apply a personal touch that can       strengthen the grantee/funder relationship.            </li>
+            </ul>
+            <h3>Invoice Guidance</h3>
+            <p>Invoices should follow the grant terms and schedule. Invoices  that are not submitted on time can cause cash flow problems and may reduce your  ability to complete your agreed upon project outcomes and metrics. You should  also invoice clearly and consistently. Your funder may have an invoice template  for you to use, or you can use your own invoice template. Attach any required  reports or supporting documentation and then send your invoice as instructed. </p>
+        <h3>Extra invoice tips</h3>
+            <ul class="bullet-size-fix">
+              <li>The funder may ask you to include specific  additional things in your invoices. For example, they may ask for a summary  that describes the work that happened during that invoicing period, specific  staff names with hours spent on the grant during that invoicing period, and match  spend down. </li>
+              <li>It may be helpful to include the total amount  spent to date and what it is in a spend down percentage on each invoice&rsquo;s note  section for tracking purposes for you and the funder.            </li>
+            </ul>
+            <h3>Standard Form 270 for Federal opportunities</h3>
+            <p>Federal opportunities will require a Standard Form 270  (SF-270) for reimbursements or at times advances. The Natural Resources  Conservation Service has a detailed <a href="https://www.bing.com/ck/a?!&&p=3ae32c5c7a817d89b19ad6e7ec018a987d1fa8fd2db24fe5b77470ebffbeb1f4JmltdHM9MTc1ODE1MzYwMA&ptn=3&ver=2&hsh=4&fclid=132ba3c8-09e0-60b3-1e7e-b59b08b16170&psq=Standard+Form+270+(SF-270)&u=a1aHR0cHM6Ly93d3cubnJjcy51c2RhLmdvdi9zaXRlcy9kZWZhdWx0L2ZpbGVzLzIwMjQtMDcvYXBwZW5kaXgtYS1zZi0yNzAtaW5zdHJ1Y3Rpb25zLnBkZg">guide</a> to help you fill out the form that can be translated to other funders and  projects. </p>
+            <h3>Example invoice template</h3>
+            <p>Example  Invoice Template –  always follow your funder&rsquo;s directions on invoicing and reporting above this  guidance. </p>
+        <h2>Common Ecosystem Restoration Funder&rsquo;s Resources for grant management</h2>
+            <ul class="bullet-size-fix">
+              <li>WA Ecology </li>
+              <ul>
+                <li><a href="https://ecology.wa.gov/About-us/Payments-contracts-grants/Grants-loans/Grant-loan-guidance">Grant  &amp; loan guidance - Washington State Department of Ecology</a></li>
+              </ul>
+              <li>WA Recreation and Conservation Office (RCO)</li>
+              <ul>
+                <li><a href="https://rco.wa.gov/recreation-and-conservation-office-grants/apply-for-a-grant/prism/">PRISM  - RCO</a></li>
+                <li><a href="https://rco.wa.gov/recreation-and-conservation-office-grants/post-award-info/">Post  Award Information - Recreation and Conservation Office</a></li>
+              </ul>
+              <li>WA Department of Transportation (WSDOT)</li>
+              <ul>
+                <li><a href="https://wsdot.wa.gov/business-wsdot/grants/public-transportation-grants">Public  transportation grants | WSDOT</a></li>
+                <li><a href="https://wsdot.wa.gov/business-wsdot/grants/public-transportation-grants/manage-your-grant">Manage  your grant | WSDOT</a></li>
+              </ul>
+              <li>Environmental Protection Agency (EPA)</li>
+              <ul>
+                <li><a href="https://www.epa.gov/grants/epa-grants-management-training-applicants-and-recipients">EPA  Grants Management Training for Applicants and Recipients | US EPA</a></li>
+              </ul>
+              <li>US Department of Transportation</li>
+              <ul>
+                <li><a href="https://www.transportation.gov/RAISEgrants/performance">Performance  Measures | US Department of Transportation</a> – RAISE performance measures  guidance </li>
+              </ul>
+              <li>The Federal Emergency Management Agency (FEMA)          </li>
+              <ul>
+                <li><a href="https://www.fema.gov/grants/tools/technical-assistance">Grant Management  Technical Assistance</a> is a digital resource library to learn about federal  grant management. </li>
+              </ul>
+              <li>General</li>
+              <ul><li><a href="https://www.grants.gov/learn-grants/grant-reporting">Grant Reporting |  Grants.gov</a> – general grant reporting for federal grants info. </li>           </ul>
+            </ul>
+            <div>
+              <div> </div>
+        </div>
+            <h2>Contact</h2>
+<p>For more information or if you have any questions  about your inquiry, please contact our Grant Writer and Strategist, Michelle  Deckard at&nbsp;<a href="mailto:michelle.deckard@psp.wa.gov">michelle.deckard@psp.wa.gov</a>. or follow this <a href="https://app.smartsheet.com/b/form/d835519e3c454ef286dbbdcdf9ae008b">link</a>.</P>
+<p>&nbsp;</p>
+			
+		<!-- LAST UPDATED -->
+		<p class="last-update">Last updated: 03/24/26</p>
+
+        <!-- InstanceEndEditable --> </div>
+		
+		<div class="col-sm-2 padding-20-top padding-0-right"> <!-- InstanceBeginRepeat name="right_nav_repeat" --><!-- InstanceBeginRepeatEntry --> <!-- InstanceBeginEditable name="right_nav_title" -->
+			<div class="right-nav-title margin-0-top ">SEE ALSO</div>
+			<!-- InstanceEndEditable -->
+			<ul class="nav-rightside-custom">
+				<!-- InstanceBeginEditable name="right_nav_links" -->
+				<li class="active" role="presentation"><a href="https://actionagenda.pugetsoundinfo.wa.gov/2022-2026ActionAgenda" target="_blank"><strong>Action Agenda Explorer</strong></a></li>
+				
+			
+				<!-- InstanceEndEditable -->
+			</ul>
+			<!-- InstanceEndRepeatEntry --><!-- InstanceEndRepeat --> </div>
+	</div>
+	<!--END OF ROW --> 
+</div>
+<!--END OF CONTENT CONTAINER -->
+
+<?php include 'includes/footer-inc.html';?>
+
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) --> 
+<script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+<!-- Include all compiled plugins (below), or include individual files as needed --> 
+<script src="js/bootstrap.js"></script> 
+<!-- custom js --> 
+<script src="js/custom.js"></script> 
+<!-- Google Tracking  -->
+
+
+</body>
+<!-- InstanceEnd --></html>
