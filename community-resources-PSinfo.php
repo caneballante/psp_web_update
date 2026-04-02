ad85c89b604e9df47316ffcd2e41f3a99981f56c
ianmc3 <ian.mccabe@psp.wa.gov>
2026-04-02 11:47:09 -0700

    More community

---
+++ community-resources-PSinfo.php
@@ -0,0 +1,141 @@
+<!DOCTYPE html>
+<html lang="en"><!-- InstanceBegin template="/Templates/page-4-template.dwt" codeOutsideHTMLIsLocked="false" -->
+<head>
+
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-BGEZ5L3EJY"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-BGEZ5L3EJY');
+</script>
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<!-- InstanceBeginEditable name="doctitle" -->
+<title>Community Resources - Key Resources in Puget Sound Info</title>
+<!-- InstanceEndEditable -->
+<!-- Bootstrap -->
+<link href="css/bootstrap.css" rel="stylesheet">
+<link href="css/custom.css" rel="stylesheet" type="text/css">
+<link href="css/custom-erika.css" rel="stylesheet" type="text/css">
+<!-- loads the wf-loading class right away to minimize FOUT -->
+<script>document.documentElement.className += ' wf-loading';</script>
+<!-- Font PRENTON TYPEKIT -->
+<script src="https://use.typekit.net/srt5jze.js"></script>
+<script>try{Typekit.load({ async: true });}catch(e){}</script>
+<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+<!--[if lt IE 9]>
+	<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+	<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+<!-- InstanceBeginEditable name="head" -->
+<script>
+/*this variable is used to set the proper nav to active. It should to the order the nav item is in the list*/
+  	navSelected = 4;
+</script> 
+<!-- InstanceEndEditable -->
+<!-- InstanceParam name="OptionalRegion1" type="boolean" value="true" -->
+<!-- InstanceParam name="OptionalRegion2" type="boolean" value="false" -->
+<!-- InstanceParam name="OptionalRegion3" type="boolean" value="true" -->
+<!-- InstanceParam name="OptionalRegion4" type="boolean" value="false" -->
+<!-- InstanceParam name="OptionalRegion5" type="boolean" value="true" -->
+<!-- InstanceParam name="OptionalRegion6" type="boolean" value="true" -->
+<!-- InstanceParam name="OptionalRegion7" type="boolean" value="false" -->
+<!-- InstanceParam name="OptionalRegion8" type="boolean" value="false" -->
+</head>
+<body>
+<?php include 'includes/modal-inc.html';?>
+<!-- START IMAGE HEADER --> 
+ <!-- InstanceBeginEditable name="overviewphoto" -->
+<header class="overview-page-image-style overview-page-image120">
+	<div class="overview-quote pull-right">
+		<h3>&nbsp;</h3>
+	</div>
+</header>
+<!-- InstanceEndEditable -->  
+<div class="container-fluid page-content padding-50-bottom">
+	<div class="row">
+		<div class="col-sm-3 padding-20-top"></div>
+		<div class="col-sm-7 padding-20-top"> <!-- InstanceBeginEditable name="6col_header" -->
+			<h1> Community: Key resources in Puget  Sound Info</a></h1>
+		<!-- InstanceEndEditable --></div>
+		<div class="col-sm-2 padding-20-top"></div>
+	</div>
+	
+	<div class="row"> 
+		<div class="col-sm-3">
+			<div class="nav-leftside-custom">
+				<ul class="nav nav-stacked nav-pills nav-leftside-custom padding-left-0 margin-10-top">
+					<!-- InstanceBeginEditable name="left_nav" -->
+				<?php include 'includes/ln-community.html';?>	
+				<!-- InstanceEndEditable -->
+				</ul>
+			</div>
+		</div>
+		 
+		<div class="col-sm-7 padding-20-top content-column"> <!-- InstanceBeginEditable name="6col_content" -->
+		
+			
+			
+			
+       <p>On this page you'll find community engagement resources from our <a href="https://www.pugetsoundinfo.wa.gov/#goals">Puget  Sound Info site</a>.</p>
+            <h2>Healthy Water Quality Section</h2>
+        <ul class="bullet-size-fix">
+              <li><strong>Website</strong>: <a href="https://vitalsigns.pugetsoundinfo.wa.gov/VitalSignGoal/Detail/6">Healthy  Water Quality Section</a>: </li>
+              <li><strong>Description</strong>: Provides an overview of  water quality in Puget Sound, why it&rsquo;s an important topic, how it has changed  over the years, and what big issues of concern are.</li>
+              <ul class="bullet-size-fix">
+                <li><strong>Tip</strong>: At the bottom of the page, expand  any of the vital signs (Freshwater, Marine Water, or Toxics in Aquatic Life) to  find more specific information, data, and reports about how the Puget Sound is  doing on each vital sign indicator. For example, selecting the &ldquo;contaminants in  adult salmon&rdquo; indicator will provide data about contaminants in adult salmon.</li>
+                <li><strong>Tip</strong>: Selecting a vital sign header, such  as &ldquo;Freshwater&rdquo;, will provide background documents on the vital sign, and  information about related strategies that this work interconnects with.</li>
+              </ul>
+            </ul>
+            <h2>National Estuary Program activities list</h2>
+        <ul class="bullet-size-fix">
+              <li><strong>Website</strong>: <a href="https://nepatlas.pugetsoundinfo.wa.gov/Activity/Index">National Estuary  Program activities list</a></li>
+              <li><strong>Description</strong>: Use the search bar to look  up keywords and find events/opportunities/activities that you may want to get  involved in.</li>
+            </ul>
+        <h2>Ongoing Programs Portal Program list</h2>
+            <ul class="bullet-size-fix">
+              <li><strong>Website</strong>: <a href="https://www.pugetsoundinfo.wa.gov/OngoingProgram/Programs">Ongoing  Programs Portal program list</a></li>
+              <li><strong>Description</strong>: Use  the search bar to look up keywords, such as &ldquo;salmon&rdquo;, to find programs and work  that we are connected to and that are related to topics you are interested in.        </li>
+            </ul>
+        <h2>Contact us</h2>
+			
+        <p>Do you have a resource to suggest that we add to  this list? Do you have questions about any of these resources? Send us a note  at <a href="mailto:environmental.justice@psp.wa.gov">environmental.justice@psp.wa.gov</a>.</p>
+        <p><br>
+        </p>
+			<p class="last-update">Last updated: 03/05/26</p>
+		<!-- InstanceEndEditable --> </div>
+		
+		<div class="col-sm-2 padding-20-top padding-0-right"> <!-- InstanceBeginRepeat name="right_nav_repeat" --><!-- InstanceBeginRepeatEntry --> <!-- InstanceBeginEditable name="right_nav_title" -->
+			<div class="right-nav-title margin-0-top nonresponsive-object-hide-rightnav">CONTACT</div>
+			<!-- InstanceEndEditable -->
+			<ul class="nav-rightside-custom">
+				<!-- InstanceBeginEditable name="right_nav_links" -->
+				<div class="nonresponsive-object-hide-rightnav-li">
+				<li class="active" role="presentation"> <?php include 'includes/ln-community-engagement.html';?></li>
+				</div>
+				<!-- InstanceEndEditable -->
+			</ul>
+			<!-- InstanceEndRepeatEntry --><!-- InstanceEndRepeat --> </div>
+	</div>
+	<!--END OF ROW --> 
+</div>
+<!--END OF CONTENT CONTAINER -->
+
+<?php include 'includes/footer-inc.html';?>
+
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) --> 
+<script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+<!-- Include all compiled plugins (below), or include individual files as needed --> 
+<script src="js/bootstrap.js"></script> 
+<!-- custom js --> 
+<script src="js/custom.js"></script> 
+<!-- Google Tracking  -->
+
+
+</body>
+<!-- InstanceEnd --></html>
