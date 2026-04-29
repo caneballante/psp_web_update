8dd020522fc7238cd5b294315f1d9c2abefdb3cd
ianmc3 <ian.mccabe@psp.wa.gov>
2026-04-28 17:41:32 -0700

    Grant writing updates

--- grant-post-award-tools.php
+++ grant-post-award-tools.php
@@ -15,7 +15,7 @@
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <!-- InstanceBeginEditable name="doctitle" -->
-<title>Puget Sound Partnership Technical Assistance and Grant Writing</title>
+<title>Puget Sound Partnership Technical Assistance and Grant Writing - Post Award Toolkit</title>
 <!-- InstanceEndEditable -->
 <!-- Bootstrap -->
 <link href="css/bootstrap.css" rel="stylesheet">
@@ -64,7 +64,7 @@ navSelected = 5;
 	<div class="row">
 		<div class="col-sm-3 padding-20-top"></div>
 		<div class="col-sm-7 padding-20-top"> <!-- InstanceBeginEditable name="6col_header" -->
-			<h1>Grant Writing: Post Award Toolkit</h1>
+			<h1 id="section1">Grant Writing: Post Award Toolkit</h1>
 			<!-- InstanceEndEditable --></div>
 		<div class="col-sm-2 padding-20-top"></div>
 	</div>
@@ -87,10 +87,19 @@ navSelected = 5;
 			<p>On August 4, 2022, the Environmental Protection Agency (EPA) approved the<a href="https://pspwa.box.com/shared/static/8zak4wiakdy94vc6104er8l3kn9bdxkw.pdf" target="new">2022-2026 Action Agenda adopted by the Leadership Council in June</a> as the Puget Sound National Estuary Program's (NEP) <a href="https://www.epa.gov/nep/comprehensive-conservation-and-management-plans" target="new">Comprehensive Conservation and Management Plan</a>. Learn more about the 2022-2026 Action Agenda below or visit the interactive <a href="https://actionagenda.pugetsoundinfo.wa.gov/2022-2026ActionAgenda" target="new">2022-2026 Action Agenda Explorer</a>.</p>
 		</div>-->
 			
-			<h2>Post Award Toolkit – Getting Ahead Before Award</h2>
+		<p>On this Page:
+			<ul class="bullet-size-fix">
+			  <li><a href="#section2">Getting ahead before award</a></li>
+				<li><a href="#section3">When You Receive Notice that You Have Been Awarded Funding</a></li>
+				<li><a href="#section4">After You Receive The Award Begin The Project Work</a></li>
+				<li><a href="#section5">Templates and Document Guidance</a></li>
+			  <li><a href="#section6">Common Ecosystem Restoration Funder's Resources for Grant Management</a>		</li>
+			</ul>
+		<h2 id="section2">Post Award Toolkit – Getting Ahead Before Award</h2>
             <p>After your application has been submitted, it may take time before  you hear more from the funder. In the meantime, if you think you are likely to  be successful and awarded the funds, you can start preparing now for successful  grant management. </p>
             <p>This page will provide you with helpful tips and resources  if your application is successful. However, every funder has different requirements  and processes that must be followed and may differ from the information on this  page. If you are awarded, please consult your funder for their specifications  and follow their directions before anything on this page. </p>
-            <h2>When you receive notice that you have been awarded funding</h2>
+            <p><a href="#section1">Back to the top.</a></p>
+            <h2 id="section3">When you receive notice that you have been awarded funding</h2>
             <p>Once you receive notice that you have been awarded funding,  the first step is to begin conversations with your leadership, fiscal staff,  and partners to discuss the award, management, and timelines. If not already  identified, you must elect a staff member from the project sponsor and lead  applicant as the main contact and communicator from the grantee side. Once a  contract or agreement is in place, the grantee&rsquo;s main contact or project  manager should create a grant calendar.</p>
             <h3>Grant Calendar</h3>
             <p>The grant calendar should include all relevant dates for  reporting, evaluation, and any deadline or significant date for the duration of  the grant. </p>
@@ -101,7 +110,8 @@ navSelected = 5;
               <li>What does the reporting schedule look like? </li>
             </ul>
             <p>It&rsquo;s important that the grant calendar ensures the entire  team is aware of the significant dates, including who is responsible for completing  or contributing to any deliverable/performance metrics. As the project sponsor,  it will also be crucial to have your internal fiscal team aware of the calendar  and their roles as well.</p>
-            <h2>After you receive the award begin the project work </h2>
+            <p><a href="#section1">Back to the top.</a></p>
+            <h2 id="section4">After you receive the award begin the project work </h2>
             <p>Once you have received the award and started project work it  is crucial to track all time, expenses, and any impacts. If there is any  confusion in your guidance from the funder, make sure to contact them and receive  clarification before taking any actions that can&rsquo;t be undone. </p>
             <h3>Consider using grant management software</h3>
         <p>Depending on the capacity in your organization and the  number of grant opportunities you manage at one time, it may be wise to use grant  management software to help you:</p>
@@ -110,7 +120,8 @@ navSelected = 5;
               <li><strong>Track all expenses</strong> - All grant expenses, such  as staff salary, benefits, contracting, and direct purchases should be  accounted for, tracked, and documented according to the funder&rsquo;s requirements.  Different expenses will require different supporting documentation. </li>
               <li><strong>Conduct regular audits and reconciliations - </strong>Regularly  check that staff time and expenses are progressing as scheduled. Identify and  discuss early any deviations from the schedule and involve the funder when  appropriate.            </li>
             </ul>
-            <h2>Templates and Document Guidance </h2>
+            <p><a href="#section1">Back to the top.</a></p>
+            <h2 id="section5">Templates and Document Guidance </h2>
             <p>This section provides some recommended templates to use  throughout your grant management. If you are awarded, please consult your  funder for their specifications and always follow the funder&rsquo;s directions above  this guidance. </p>
             <h3>Reporting </h3>
             <p>Your funder will have an outlined process for you to follow  and report on your grant progress. Different funders will have different  formats and timelines. Some will ask for information with every invoice and others  may ask only for a final report. It can vary greatly and should be discussed  early with your funder and grant team. </p>
@@ -133,7 +144,8 @@ navSelected = 5;
             <p>Federal opportunities will require a Standard Form 270  (SF-270) for reimbursements or at times advances. The Natural Resources  Conservation Service has a detailed <a href="https://www.bing.com/ck/a?!&&p=3ae32c5c7a817d89b19ad6e7ec018a987d1fa8fd2db24fe5b77470ebffbeb1f4JmltdHM9MTc1ODE1MzYwMA&ptn=3&ver=2&hsh=4&fclid=132ba3c8-09e0-60b3-1e7e-b59b08b16170&psq=Standard+Form+270+(SF-270)&u=a1aHR0cHM6Ly93d3cubnJjcy51c2RhLmdvdi9zaXRlcy9kZWZhdWx0L2ZpbGVzLzIwMjQtMDcvYXBwZW5kaXgtYS1zZi0yNzAtaW5zdHJ1Y3Rpb25zLnBkZg">guide</a> to help you fill out the form that can be translated to other funders and  projects. </p>
             <h3>Example invoice template</h3>
             <p>Example  Invoice Template –  always follow your funder&rsquo;s directions on invoicing and reporting above this  guidance. </p>
-        <h2>Common Ecosystem Restoration Funder&rsquo;s Resources for grant management</h2>
+			<p><a href="#section1">Back to the top.</a></p>
+        <h2 id="section6">Common Ecosystem Restoration Funder&rsquo;s Resources for grant management</h2>
             <ul class="bullet-size-fix">
               <li>WA Ecology </li>
               <ul>
@@ -162,26 +174,27 @@ navSelected = 5;
                 <li><a href="https://www.fema.gov/grants/tools/technical-assistance">Grant Management  Technical Assistance</a> is a digital resource library to learn about federal  grant management. </li>
               </ul>
               <li>General</li>
-              <ul><li><a href="https://www.grants.gov/learn-grants/grant-reporting">Grant Reporting |  Grants.gov</a> – general grant reporting for federal grants info. </li>           </ul>
+              <ul><li><a href="https://www.grants.gov/learn-grants/grant-reporting">Grant Reporting |  Grants.gov</a> – general grant reporting for federal grants info. 
+            </li>
+              </ul>
             </ul>
+			<p><a href="#section1">Back to the top.</a></p>
             <div>
               <div> </div>
-        </div>
-            <h2>Contact</h2>
-<p>For more information or if you have any questions  about your inquiry, please contact our Grant Writer and Strategist, Michelle  Deckard at&nbsp;<a href="mailto:michelle.deckard@psp.wa.gov">michelle.deckard@psp.wa.gov</a>. or follow this <a href="https://app.smartsheet.com/b/form/d835519e3c454ef286dbbdcdf9ae008b">link</a>.</P>
-<p>&nbsp;</p>
-			
-		<!-- LAST UPDATED -->
-		<p class="last-update">Last updated: 03/24/26</p>
+
+        <!-- LAST UPDATED -->		</div>
+        <p class="last-update">Last updated: 03/24/26</p>
 
         <!-- InstanceEndEditable --> </div>
 		
-		<div class="col-sm-2 padding-20-top padding-0-right"> <!-- InstanceBeginRepeat name="right_nav_repeat" --><!-- InstanceBeginRepeatEntry --> <!-- InstanceBeginEditable name="right_nav_title" -->
-			<div class="right-nav-title margin-0-top ">SEE ALSO</div>
+		<div class="col-sm-2 padding-20-top padding-0-right"> <!-- InstanceBeginRepeat name="right_nav_repeat" --><!-- InstanceBeginRepeatEntry --> <!-- InstanceBeginEditable name="right_nav_title" --><strong>CONTACT</strong>
+		        <div class="right-nav-title margin-0-top "></div>
 			<!-- InstanceEndEditable -->
 			<ul class="nav-rightside-custom">
 				<!-- InstanceBeginEditable name="right_nav_links" -->
-				<li class="active" role="presentation"><a href="https://actionagenda.pugetsoundinfo.wa.gov/2022-2026ActionAgenda" target="_blank"><strong>Action Agenda Explorer</strong></a></li>
+				<div class="nonresponsive-object-hide-rightnav-li">
+				<li class="active" role="presentation"> <?php include 'includes/rn-grant_contact.html';?></li>
+				</div>
 				
 			
 				<!-- InstanceEndEditable -->
