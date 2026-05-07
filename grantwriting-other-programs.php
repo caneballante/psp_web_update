86257199ba8f7c0d31c8d5b4604e0af7a8d04d75
jon bridgman <jonbridgman@gmail.com>
2026-05-06 19:18:23 -0700

    removed orange bar from strategic funding pages

--- grantwriting-other-programs.php
+++ grantwriting-other-programs.php
@@ -56,9 +56,7 @@ subNavSelected = 3;
 <header class="overview-page-image-style overview-page-image118">
 </header>
 <!-- InstanceEndEditable -->  <!-- InstanceBeginEditable name="EditRegion11" -->
-<div class="row">
-	<div class="col-xs-12 orange-fill align-center overview-logo-banner"><img src="images/2016-AA-banner-logo-none-small.png" width="146" height="30" alt=""/></div>
-</div>
+
 <!-- InstanceEndEditable -->
 <div class="container-fluid page-content padding-50-bottom">
 	<div class="row">
@@ -156,11 +154,7 @@ subNavSelected = 3;
 			<!-- InstanceEndEditable -->
 			<ul class="nav-rightside-custom">
 				<!-- InstanceBeginEditable name="right_nav_links" -->
-				<div class="nonresponsive-object-hide-rightnav-li">
 				<li class="active" role="presentation"> <?php include 'includes/rn-grant_contact.html';?></li>
-				</div>
-				
-			
 				<!-- InstanceEndEditable -->
 			</ul>
 			<!-- InstanceEndRepeatEntry --><!-- InstanceEndRepeat --> </div>
