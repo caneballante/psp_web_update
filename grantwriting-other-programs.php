ff8088828a5d8d6c4ae1696a0858ee47ec0dea7c
jon bridgman <jonbridgman@gmail.com>
2026-05-07 15:36:30 -0700

    Update grantwriting-other-programs.php

--- grantwriting-other-programs.php
+++ grantwriting-other-programs.php
@@ -56,9 +56,7 @@ subNavSelected = 3;
 <header class="overview-page-image-style overview-page-image118">
 </header>
 <!-- InstanceEndEditable -->  <!-- InstanceBeginEditable name="EditRegion11" -->
-<div class="row">
-	<div class="col-xs-12 orange-fill align-center overview-logo-banner"><img src="images/2016-AA-banner-logo-none-small.png" width="146" height="30" alt=""/></div>
-</div>
+
 <!-- InstanceEndEditable -->
 <div class="container-fluid page-content padding-50-bottom">
 	<div class="row">
