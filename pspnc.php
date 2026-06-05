c16349f1b7b7c0e6e280121f3686b03c32007e66
jon bridgman <jonbridgman@gmail.com>
2026-06-05 16:40:28 -0700

    Style PSPNC calculator link

--- pspnc.php
+++ pspnc.php
@@ -98,14 +98,14 @@ Open the Nearshore Conservation Credits map in a new tab.
 		
 		<div class="col-sm-2 padding-20-top padding-0-right"> <!-- InstanceBeginRepeat name="right_nav_repeat" --><!-- InstanceBeginRepeatEntry --> <!-- InstanceBeginEditable name="right_nav_title" -->
 			
-			<button class="button margin-20-bottom rounded-corners "><p class="line-height-evenlesstight"><a href="https://www.fisheries.noaa.gov/west-coast/habitat-conservation/puget-sound-nearshore-habitat-conservation-calculator" target="_blank" style="color:white">Puget Sound Nearshore Habitat Conservation Calculator</a></p></button>
+			<a class="pspnc-calculator-button margin-20-bottom" href="https://www.fisheries.noaa.gov/west-coast/habitat-conservation/puget-sound-nearshore-habitat-conservation-calculator" target="_blank">Puget Sound Nearshore Habitat Conservation Calculator</a>
 
 			<div class="right-nav-title margin-20-top nonresponsive-object-hide-rightnav">CONTACT</div>
 			<!-- InstanceEndEditable -->
 			<ul class="nav-rightside-custom">
 				<!-- InstanceBeginEditable name="right_nav_links" -->
 				<li class="active" role="presentation"> <?php include 'includes/pspnc_contact.html';?></li>
-				<li class="active" role="presentation"><button class=button><a href="https://www.fisheries.noaa.gov/west-coast/habitat-conservation/puget-sound-nearshore-habitat-conservation-calculator" target="_blank" style="color:white">Puget Sound Nearshore Habitat Conservation Calculator</a></button></li>
+				<li class="active" role="presentation"><a class="pspnc-calculator-button" href="https://www.fisheries.noaa.gov/west-coast/habitat-conservation/puget-sound-nearshore-habitat-conservation-calculator" target="_blank">Puget Sound Nearshore Habitat Conservation Calculator</a></li>
 				<div class="right-nav-title margin-0-top nonresponsive-object-hide-rightnav">SEE ALSO</div>
 				<li class="active" role="presentation"><a href="https://vimeo.com/637105177/d4713aa7ee" target="new"><strong>A new conservation marketplace in the Puget Sound</strong></a></li>
 				<li class="active" role="presentation"><a href="https://www.noaa.gov/news-release/army-and-noaa-to-advance-endangered-species-act-consultations" target="new"><strong>Army and NOAA to advance Endangered Species Act consulations</strong></a></li>
