c943265cbc1a32db0c2365f562b2f114bd0ce4ad
ianmc3 <ian.mccabe@psp.wa.gov>
2026-06-25 11:43:48 -0700

    Future scenarios

---
+++ future-scenarios-NEW-TEST.php
@@ -0,0 +1,182 @@
+<!DOCTYPE html>
+<html lang="en"><!-- InstanceBegin template="/Templates/page-4-template.dwt" codeOutsideHTMLIsLocked="false" -->
+<head>
+
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-BGEZ5L3EJY"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-BGEZ5L3EJY');
+</script>
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<!-- InstanceBeginEditable name="doctitle" -->
+<title>Puget Sound Partnership - Future Scenarios Project</title>
+<!-- InstanceEndEditable -->
+<!-- Bootstrap -->
+<link href="css/bootstrap.css" rel="stylesheet">
+<link href="css/custom.css" rel="stylesheet" type="text/css">
+<link href="css/custom-erika.css" rel="stylesheet" type="text/css">
+<!-- loads the wf-loading class right away to minimize FOUT -->
+<script>document.documentElement.className += ' wf-loading';</script>
+<!-- Font PRENTON TYPEKIT -->
+<script src="https://use.typekit.net/srt5jze.js"></script>
+<script>try{Typekit.load({ async: true });}catch(e){}</script>
+<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+<!--[if lt IE 9]>
+	<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+	<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+<!-- InstanceBeginEditable name="head" -->
+<script>
+/*this variable is used to set the proper nav to active. It should to the order the nav item is in the list*/
+  	navSelected = 1;
+</script>
+<!-- InstanceEndEditable -->
+<!-- InstanceParam name="OptionalRegion1" type="boolean" value="true" -->
+<!-- InstanceParam name="OptionalRegion2" type="boolean" value="false" -->
+<!-- InstanceParam name="OptionalRegion3" type="boolean" value="true" -->
+<!-- InstanceParam name="OptionalRegion4" type="boolean" value="false" -->
+<!-- InstanceParam name="OptionalRegion5" type="boolean" value="true" -->
+<!-- InstanceParam name="OptionalRegion6" type="boolean" value="true" -->
+<!-- InstanceParam name="OptionalRegion7" type="boolean" value="false" -->
+<!-- InstanceParam name="OptionalRegion8" type="boolean" value="false" -->
+</head>
+<body>
+<?php include 'includes/modal-inc.html';?>
+<!-- START IMAGE HEADER --> 
+ <!-- InstanceBeginEditable name="overviewphoto" -->
+<header class="overview-page-image-style overview-page-image25"> </header>
+<!-- InstanceEndEditable -->  
+<div class="container-fluid page-content padding-50-bottom">
+	<div class="row">
+		<div class="col-sm-3 padding-20-top"></div>
+		<div class="col-sm-7 padding-20-top"> <!-- InstanceBeginEditable name="6col_header" -->
+			<h1>Puget Sound Future Scenarios</h1>
+		<!-- InstanceEndEditable --></div>
+		<div class="col-sm-2 padding-20-top"></div>
+	</div>
+	
+	<div class="row"> 
+		<div class="col-sm-3">
+			<div class="nav-leftside-custom">
+				<ul class="nav nav-stacked nav-pills nav-leftside-custom padding-left-0 margin-10-top">
+					<!-- InstanceBeginEditable name="left_nav" -->
+				<?php include "includes/ln-future-scenarios-NEW-TEST.html";?>
+				<!-- InstanceEndEditable -->
+				</ul>
+			</div>
+		</div>
+		 
+		<div class="col-sm-7 padding-20-top content-column"> <!-- InstanceBeginEditable name="6col_content" -->
+		
+		<p>The future is uncertain. The Puget Sound region&rsquo;s climate is  changing and its population is growing. These changes bring challenges  including housing availability and affordability, increased flooding, pressure  to convert working lands, and at-risk salmon and orca populations. The choices  we make now can guide us toward a more resilient future.</p>
+        <p>The Puget Sound Future  Scenarios project, or Future Scenarios, is an exploratory scenario project,  which means it supports open-ended, interdisciplinary, and future-focused thinking.  It offers a structured way to explore possible futures and consider creative  and strategic approaches to resilience. Future Scenarios examines the of  different potential futures and provides a framework for thinking about  possible futures and preparing for uncertainties.</p>
+        <p><strong><em>Click here </em></strong><em>to learn more about  Future Scenarios&rsquo; origins and background.</em></p>
+        <h2>What are the scenarios?</h2>
+		<p>The Future Scenarios  project explores what Puget Sound could look like in 2080 with one baseline  scenario based on current growth, development, and land use trends, along with four  alternative scenarios that change those trends in different ways. Each scenario  is centered around a narrative, or story, about how a Puget Sound resident involved  in Puget Sound recovery might experience the future. Modeling helps bring those  stories to life by showing what each future could look like.</p>
+		<p>Some assumptions about the future stay the same across all  the scenarios:</p>
+		<ul class="bullet-size-fix">
+ 		<li>Population growth rates are at the high end of  Office of Financial Management projections: Between 2020 and 2080, Future  Scenarios assumes Puget Sound's overall population will grow by approximately  110% to just under 11 million people.</li>
+  		<li>Climate change impacts are high: The models use  Representative Concentration Pathway (RCP) level 8.5, which assumes high levels  of greenhouse gas emissions that increase global warming.</li>
+  		<li>Investments in habitat conservation and  restoration are consistently high, based on recent annual investment in Puget  Sound recovery. Priorities for conservation and restoration are generally the  same across scenarios.</li>
+  		<li>Urban growth area (UGA) boundaries remain the  same as 2020.</li>
+</ul>
+<p>Other factors vary between scenarios, including:</p>
+<ul class="bullet-size-fix">
+  <li>The way new population is distributed across the  region and between urban and rural areas.</li>
+  <li>How much jurisdictions support density,  including upzoning,   redevelopment, and accessory dwelling units.</li>
+  <li>How strongly working lands are protected or  allowed to convert.</li>
+  <li>The extent to which people make environmentally conscious  choices and lifestyle decisions.</li>
+  <li>How jurisdictions coordinate planning and  restoration activities.</li>
+</ul>
+<p><strong><em>Click  here</em></strong><em>to  learn more about the five scenarios, read each scenario story, and see the  scenario results.</em><br>
+  <strong><em>Click  here</em></strong><em>to  learn more about the scenario development and modeling process.</em></p>
+<h2>What have the scenarios shown us so far? </h2>
+<p>Across all scenarios, we observed some potential changes  between now and 2080 that are not as expected:</p>
+<ul class="bullet-size-fix">
+  <li>Big cities throughout the region continue to  grow (especially in central Puget Sound counties)</li>
+  <li>Population growth continues in key habitat areas,  such as floodplains and riparian areas, both including inside and outside urban  areas</li>
+  <li>Housing prices increase and likely  disproportionately affect overburdened communities</li>
+  <li>Agricultural and forestry economic activity  declines and may also disproportionately affect overburdened communities</li>
+  <li>More people experience high-heat days and  nights, as well as impacts of sea level rise</li>
+</ul>
+<p>The scenarios also revealed some things that were not as  expected:</p>
+<ul class="bullet-size-fix">
+  <li>Existing UGAs have enough capacity to  accommodate about 95% of new population growth between now and 2080 if communities  support significant increases in density via upzoning</li>
+  <li>Conservation and restoration opportunities can happen  both inside and outside UGAs; removing barriers could create substantial opportunities  to achieve conservation and restoration goals.</li>
+  <li>Salmon population  outcomes are challenging but there are still opportunities to improve habitat  conditions by concentrating population growth in urban areas and investing in  conservation.</li>
+  </ul>
+<h2>What&rsquo;s next for Future Scenarios?</h2>
+<p>Across the Puget Sound recovery community, there is growing interest  in exploratory scenarios and the ways they can test recovery strategies, support  decision making, and inspire behavior change. There is a lot of excitement  about continuing Future Scenarios work and sharing what has been learned with  communities across the region.</p>
+<p>In Spring 2026, the  Project Team began the next phase of the project, which will center Puget Sound  recovery community experts, combine broad and targeted engagement strategies to  bring in new perspectives and voices, and continue to help make connections  between Puget Sound recovery plans, systems, work streams, and people.</p>
+
+
+<h2>Want to learn more or get involved?</h2></p>
+			<p>We would love to  connect with you!<strong> </strong>Please reach out to our Assistant Science Director and  Future Scenarios Lead, Laura Rivas at <a href="mailto:laura.rivas@psp.wa.gov">laura.rivas@psp.wa.gov</a>.</p>
+			<p>&nbsp;</p>
+			
+			
+			<p class="last-update">Current as of: 06/25/26</p>
+			<!-- InstanceEndEditable --> </div>
+		
+		<div class="col-sm-2 padding-20-top padding-0-right"> <!-- InstanceBeginRepeat name="right_nav_repeat" --><!-- InstanceBeginRepeatEntry --> <!-- InstanceBeginEditable name="right_nav_title" -->
+			
+			<!-- InstanceEndEditable -->
+			<ul class="nav-rightside-custom">
+				<!-- InstanceBeginEditable name="right_nav_links" -->
+				
+				<!-- InstanceEndEditable -->
+			</ul>
+			<!-- InstanceEndRepeatEntry --><!-- InstanceBeginRepeatEntry --> <!-- InstanceBeginEditable name="right_nav_title" -->
+			
+			<!-- InstanceEndEditable -->
+			<ul class="nav-rightside-custom">
+				<!-- InstanceBeginEditable name="right_nav_links" -->
+				
+				<!-- InstanceEndEditable -->
+			</ul>
+			<!-- InstanceEndRepeatEntry --><!-- InstanceBeginRepeatEntry --> <!-- InstanceBeginEditable name="right_nav_title" -->
+			
+			<!-- InstanceEndEditable -->
+			<ul class="nav-rightside-custom">
+				<!-- InstanceBeginEditable name="right_nav_links" -->
+				
+				<!--		<li role="presentation"> <a href="#">State of the Watersheds</a></li>
+					<li role="presentation"> <a href="#">Health of the Salish Sea Ecosystem Report</a></li>--> 
+				<!-- InstanceEndEditable -->
+			</ul>
+			<!-- InstanceEndRepeatEntry --><!-- InstanceBeginRepeatEntry --> <!-- InstanceBeginEditable name="right_nav_title" -->
+			<div class="right-nav-title margin-0-top ">SEE ALSO</div>
+			<!-- InstanceEndEditable -->
+			<ul class="nav-rightside-custom">
+				<!-- InstanceBeginEditable name="right_nav_links" -->
+				<li class="active" role="presentation"> <a href="/workshops.php">WORKSHOPS</a></li>
+				<li role="presentation"> <a href="https://pspwa.box.com/s/qzcsdqj5vdxnb1j58fc1i1meepr5jms2" target="new">EXPLORING BOLD LAND USE IDEAS IN PUGET SOUND WORKSHOP (2024)</a></li>
+				<li role="presentation"> <a href="https://pspwa.box.com/s/o7pj56ivao3paimyxheaumf9sym41lif" target="new">BOLD LAND USE PLANNING WORKSHOP (2023)</a></li>
+				<!-- InstanceEndEditable -->
+			</ul>
+			<!-- InstanceEndRepeatEntry --><!-- InstanceEndRepeat --> </div>
+	</div>
+	<!--END OF ROW --> 
+</div>
+<!--END OF CONTENT CONTAINER -->
+
+<?php include 'includes/footer-inc.html';?>
+
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) --> 
+<script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+<!-- Include all compiled plugins (below), or include individual files as needed --> 
+<script src="js/bootstrap.js"></script> 
+<!-- custom js --> 
+<script src="js/custom.js"></script> 
+<!-- Google Tracking  -->
+
+
+</body>
+<!-- InstanceEnd --></html>
---
+++ future-scenarios-origins.php
@@ -0,0 +1,194 @@
+<!DOCTYPE html>
+<html lang="en"><!-- InstanceBegin template="/Templates/page-4-template.dwt" codeOutsideHTMLIsLocked="false" -->
+<head>
+
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-BGEZ5L3EJY"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-BGEZ5L3EJY');
+</script>
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<!-- InstanceBeginEditable name="doctitle" -->
+<title>Puget Sound Partnership - Future Scenarios Project Origins and Background</title>
+<!-- InstanceEndEditable -->
+<!-- Bootstrap -->
+<link href="css/bootstrap.css" rel="stylesheet">
+<link href="css/custom.css" rel="stylesheet" type="text/css">
+<link href="css/custom-erika.css" rel="stylesheet" type="text/css">
+<!-- loads the wf-loading class right away to minimize FOUT -->
+<script>document.documentElement.className += ' wf-loading';</script>
+<!-- Font PRENTON TYPEKIT -->
+<script src="https://use.typekit.net/srt5jze.js"></script>
+<script>try{Typekit.load({ async: true });}catch(e){}</script>
+<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+<!--[if lt IE 9]>
+	<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+	<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+<!-- InstanceBeginEditable name="head" -->
+<script>
+/*this variable is used to set the proper nav to active. It should to the order the nav item is in the list*/
+  	navSelected = 2;
+</script>
+<!-- InstanceEndEditable -->
+<!-- InstanceParam name="OptionalRegion1" type="boolean" value="true" -->
+<!-- InstanceParam name="OptionalRegion2" type="boolean" value="false" -->
+<!-- InstanceParam name="OptionalRegion3" type="boolean" value="true" -->
+<!-- InstanceParam name="OptionalRegion4" type="boolean" value="false" -->
+<!-- InstanceParam name="OptionalRegion5" type="boolean" value="true" -->
+<!-- InstanceParam name="OptionalRegion6" type="boolean" value="true" -->
+<!-- InstanceParam name="OptionalRegion7" type="boolean" value="false" -->
+<!-- InstanceParam name="OptionalRegion8" type="boolean" value="false" -->
+</head>
+<body>
+<?php include 'includes/modal-inc.html';?>
+<!-- START IMAGE HEADER --> 
+ <!-- InstanceBeginEditable name="overviewphoto" -->
+<header class="overview-page-image-style overview-page-image25"> </header>
+<!-- InstanceEndEditable -->  
+<div class="container-fluid page-content padding-50-bottom">
+	<div class="row">
+		<div class="col-sm-3 padding-20-top"></div>
+		<div class="col-sm-7 padding-20-top"> <!-- InstanceBeginEditable name="6col_header" -->
+			<h1>Future Scenarios Project Origins and Background</h1>
+		<!-- InstanceEndEditable --></div>
+		<div class="col-sm-2 padding-20-top"></div>
+	</div>
+	
+	<div class="row"> 
+		<div class="col-sm-3">
+			<div class="nav-leftside-custom">
+				<ul class="nav nav-stacked nav-pills nav-leftside-custom padding-left-0 margin-10-top">
+					<!-- InstanceBeginEditable name="left_nav" -->
+				<?php include "includes/ln-future-scenarios-NEW-TEST.html";?>
+				<!-- InstanceEndEditable -->
+				</ul>
+			</div>
+		</div>
+		 
+		<div class="col-sm-7 padding-20-top content-column"> <!-- InstanceBeginEditable name="6col_content" -->
+		
+		<h2>Project Origins</h2>
+			<p>In 2018, our Leadership Council adopted <a href="https://pspwa.box.com/s/2d7y0i1qk5a4rce6jcv5s143c16oyy7p" target="new">Beyond 2020 Resolution</a> which described our goals for Puget Sound recovery and made a call to action. The  resolution challenged the Puget Sound recovery community to pursue ambitious solutions,  secure funding, seek supportive legislation, and stay accountable for  implementing all actions needed to make Puget Sound resilient.<br>
+              <em>We have made  progress, but our efforts have not been at a scale or pace sufficient to  restore Puget Sound to health…This is our moment to define what our future can  look like, and to fight to make it a reality…</em><br>
+              <em>Our vision includes a resilient  ecosystem…a thriving economy, sustainable farms and forests, and human  communities with high quality of life…Most importantly, our vision includes a  broad community of engaged citizens who commit to save Puget Sound.</em><br>
+              <em>[We issue this call to action to everyone  connected to Puget Sound:] Join us in protecting habitat, recovering iconic  species, and ensuring that our lands and waters sustain generations to come.&rdquo;</em><br>
+			Excerpted from PSP&rsquo;s 2018 Action Agenda and  the Leadership Council&rsquo;s Call to Action <br>
+			The Puget Sound Future Scenarios project (Future Scenarios) grew  from our Leadership Council&rsquo;s vision and call to action. Achieving the goals of  the Beyond 2020 Resolution means planning for a future in a region that is  changing in significant ways, some of which we understand and can predict, while  others are uncertain. <br>
+			In 2020-21, after consulting with the Science Panel, we launched  Future Scenarios to:</p>
+            <ul class="bullet-size-fix">
+              <li>Invite creative and innovative thinking about  Puget Sound recovery strategies.</li>
+              <li>Strengthen the interdisciplinary community of  recovery practice </li>
+              <li>Better understand and communicate potential consequences  and tradeoffs of current policy and management choices on future conditions.</li>
+              </ul>
+			<p>Over the years, Future  Scenarios has been implemented through multiple phases, each with different  purposes, outcomes, questions, and forms of engagement. Despite evolving over  time, the project stayed true to its original intent by exploring a range of potential  futures for Puget Sound recovery through character-driven stories that reflect  different approaches to growth and development management.</p>
+			<h2>Project Background</h2>
+<p>There have been four  main phases for the Future Scenarios effort. Activities and accomplishments for  each phase are listed below.</p>
+<h3>Future Scenarios Phase 1 | 2020-22</h3>
+<ul class="bullet-size-fix">
+  <li>Established interdisciplinary advisory group to advise  on the project and build on similar regional scenario planning efforts</li>
+  <li>Identified regional drivers of change, along  with initial project metrics and data sets to assess them</li>
+  <li>Developed exploratory scenario tools: Envision  and qualitative network model</li>
+  <li>Created an initial baseline scenario and a set  of alternative scenarios </li>
+</ul>
+<h3>Future Scenarios Phase 2 | 2022-23</h3>
+<ul class="bullet-size-fix">
+  <li>Updated the baseline scenario and created  initial versions of the project&rsquo;s current alternative scenarios: Salmon  Forward, Networked Growth, and Rural Stewardship</li>
+  <li>Refined and expanded modeling representation,  including a salmon favorability index model that combines habitat factors into  a single score to compare outcomes across scenarios</li>
+  <li>Held  the first of two science-policy workshops in November 2023: Bold Land Use  Planning in Puget Sound. The workshop brought together over 60 regional land  use and growth management experts, local and state agency planners and  coordinators, and stormwater and salmon experts. Participants identified over 70  potential future impacts related to development, human wellbeing and equity,  and ecosystem restoration and conservation. They also generated 27 bold ideas  that could address those impacts.</li>
+</ul>
+<p><strong><em>Click here</em></strong><em>to read the November 2023 Future Scenarios </em>Bold Land Use Planning in Puget Sound<em> workshop synopsis, including the bold ideas that could address future  development, human wellbeing and equity, and ecosystem restoration and  conservation challenges.</em></p>
+<h3>Future Scenarios Phase 3 | 2023-2024</h3>
+<ul class="bullet-size-fix">
+  <li>Created the Hybrid Scenario, a fourth  alternative scenario that explores several bold  ideas  from the November 2023 Bold Land Use Planning workshop. </li>
+  <li>Continued to refine and expand modeling  representation.</li>
+  <li>Hosted the second science-policy workshop in  June 2024: Exploring Bold Land Use Ideas in Puget Sound. The workshop brought  together over 50 recovery experts who considered what it would take to  implement ideas from the November workshop. They generated 31 &ldquo;seeds of bold  action&rdquo; that can accelerate Puget Sound recovery.</li>
+</ul>
+<p><strong><em>Click here</em></strong><em>to read the June 2024 </em>Exploring Bold Land Use Ideas in Puget Sound<em> workshop synopsis, including the &ldquo;seeds of bold action&rdquo; to accelerate Puget  Sound recovery.</em></p>
+<h3>Advancing Future Scenarios | 2024-2026</h3>
+<ul class="bullet-size-fix">
+  <li>Updated all four alternative scenario narratives   to improve cohesion and reflect small changes in assumptions.</li>
+  <li>Improved ability to simulate conservation and  restoration activities.</li>
+  <li>Improved representation of habitat important to  salmon and added representation of sea level rise.</li>
+  <li>Developed additional models to explore outcomes  of interest to the Puget Sound recovery community, including: </li>
+  <ul>
+    <li>Housing affordability index </li>
+    <li>Environmental justice</li>
+    <li>Working lands</li>
+    <li>Recreation and tourism</li>
+    <li>The restoration economy.</li>
+  </ul>
+  <li>Worked with Jefferson County to consider how  Future Scenarios findings align with its recent comprehensive plan&rsquo;s goals and  policies.</li>
+  <li>Continued to engage with  key partners, including our boards team, Strategic Initiative Leads, and the  Puget Sound Institute.</li>
+</ul>            
+            
+<h2>Want to learn more or get involved?</h2></p>
+			<p>We would love to  connect with you!<strong> </strong>Please reach out to our Assistant Science Director and  Future Scenarios Lead, Laura Rivas at <a href="mailto:laura.rivas@psp.wa.gov">laura.rivas@psp.wa.gov</a>.</p>
+			<p>&nbsp;</p>
+			
+			
+			<p class="last-update">Current as of: 06/25/26</p>
+			<!-- InstanceEndEditable --> </div>
+		
+		<div class="col-sm-2 padding-20-top padding-0-right"> <!-- InstanceBeginRepeat name="right_nav_repeat" --><!-- InstanceBeginRepeatEntry --> <!-- InstanceBeginEditable name="right_nav_title" -->
+			
+			<!-- InstanceEndEditable -->
+			<ul class="nav-rightside-custom">
+				<!-- InstanceBeginEditable name="right_nav_links" -->
+				
+				<!-- InstanceEndEditable -->
+			</ul>
+			<!-- InstanceEndRepeatEntry --><!-- InstanceBeginRepeatEntry --> <!-- InstanceBeginEditable name="right_nav_title" -->
+			
+			<!-- InstanceEndEditable -->
+			<ul class="nav-rightside-custom">
+				<!-- InstanceBeginEditable name="right_nav_links" -->
+				
+				<!-- InstanceEndEditable -->
+			</ul>
+			<!-- InstanceEndRepeatEntry --><!-- InstanceBeginRepeatEntry --> <!-- InstanceBeginEditable name="right_nav_title" -->
+			
+			<!-- InstanceEndEditable -->
+			<ul class="nav-rightside-custom">
+				<!-- InstanceBeginEditable name="right_nav_links" -->
+				
+				<!--		<li role="presentation"> <a href="#">State of the Watersheds</a></li>
+					<li role="presentation"> <a href="#">Health of the Salish Sea Ecosystem Report</a></li>--> 
+				<!-- InstanceEndEditable -->
+			</ul>
+			<!-- InstanceEndRepeatEntry --><!-- InstanceBeginRepeatEntry --> <!-- InstanceBeginEditable name="right_nav_title" -->
+			<div class="right-nav-title margin-0-top ">SEE ALSO</div>
+			<!-- InstanceEndEditable -->
+			<ul class="nav-rightside-custom">
+				<!-- InstanceBeginEditable name="right_nav_links" -->
+				<li class="active" role="presentation"> <a href="/workshops.php">WORKSHOPS</a></li>
+				<li role="presentation"> <a href="https://pspwa.box.com/s/qzcsdqj5vdxnb1j58fc1i1meepr5jms2" target="new">EXPLORING BOLD LAND USE IDEAS IN PUGET SOUND WORKSHOP (2024)</a></li>
+				<li role="presentation"> <a href="https://pspwa.box.com/s/o7pj56ivao3paimyxheaumf9sym41lif" target="new">BOLD LAND USE PLANNING WORKSHOP (2023)</a></li>
+				<!-- InstanceEndEditable -->
+			</ul>
+			<!-- InstanceEndRepeatEntry --><!-- InstanceEndRepeat --> </div>
+	</div>
+	<!--END OF ROW --> 
+</div>
+<!--END OF CONTENT CONTAINER -->
+
+<?php include 'includes/footer-inc.html';?>
+
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) --> 
+<script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+<!-- Include all compiled plugins (below), or include individual files as needed --> 
+<script src="js/bootstrap.js"></script> 
+<!-- custom js --> 
+<script src="js/custom.js"></script> 
+<!-- Google Tracking  -->
+
+
+</body>
+<!-- InstanceEnd --></html>
