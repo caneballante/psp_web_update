5c0dabd723986b6c9bcb6778d0e64c51171eb6b5
ianmc3 <ian.mccabe@psp.wa.gov>
2026-06-26 10:42:45 -0700

    PEAR

--- PEAR-impact.php
+++ PEAR-impact.php
@@ -57,7 +57,7 @@
 	<div class="row">
 		<div class="col-sm-3 padding-20-top"></div>
 		<div class="col-sm-7 padding-20-top"> <!-- InstanceBeginEditable name="6col_header" -->
-			<h1>Pro-Equity Anti-Racism (PEAR)</h1> 
+			<h1>Pro-Equity Anti-Racism (impact)</h1> 
 			<!-- InstanceEndEditable --></div>
 		<div class="col-sm-2 padding-20-top"></div>
 	</div>
@@ -77,13 +77,56 @@
 		<div class="col-sm-7 padding-20-top content-column"> <!-- InstanceBeginEditable name="6col_content" -->
 
        
-        <div class="highlightbox margin-0-top margin-20-bottom">
+        <h2 class="margin-0-top"><strong>Our Pro-Equity Anti-Racism impact Statement</strong></h2>
+        <p>On March 21, 2022, Governor Inslee signed Executive Order 22-04, Implementing the Washington State Pro-Equity Anti-Racism (PEAR) Plan  &amp; Playbook. The Executive Order  directs all Cabinet agencies to partner with the Office of Equity in a  commitment to being an anti-racist government system.</p>
+        <p>The Puget Sound Partnership is dedicated to  upholding this executive order in our application of environmental justice  principles in Puget Sound ecosystem recovery goals. We stand in unity for the  just treatment and meaningful involvement of all people in government  decision-making, especially   decisions that affect human health and the environment.</p>
+        <p>The Leadership Team is  committed to the development of its<a href="https://pspwa.box.com/s/ggerhxalpjxp3ubf8bqphqswokexl6rl" target="new"> Five-Year Well-being, Equity, and Belonging Action Plan</a>  for embedding equity and environmental justice into its operations,  policymaking, and funding decisions.</p>
+        <p>We look forward to  continuing this journey with each of you, our partners, and the Office of  Equity. Please don&rsquo;t hesitate to  reach out to our Impact  Team.<br>
+        </p>
+            
+        <h2>Our <strong>Impact </strong> team</h2>
+            <table class="table-striped">
+                
+                 <tr>
+                    <td width="164"><p>Mindy Roberts</p></td>
+                    <td width="499"><p>Executive Director</p></td>
+                </tr>
+				<tr>
+                    <td width="164"><p>Lea Anne Burke</p></td>
+                    <td width="499"><p>Director of Tribal Relations</p></td>
+                </tr>
+                <tr>
+                    <td><p>Denise Ross</p></td>
+                    <td><p>Workplace Culture and Development Manager</p></td>
+                </tr>
+				<tr>
+                    <td><p>Bryan Gordon</p></td>
+                    <td><p>Workplace Culture Specialist</p></td>
+                </tr>
+				<tr>
+                    <td><p>Signe Lindquist</p></td>
+                    <td><p>Community Engagement Coordinator</p></td>
+                </tr>
+                <tr>
+                    <td><p>Dave Herrera</p></td>
+                    <td><p>Community Member</p></td>
+                </tr>
+				 <tr>
+                    <td><p>Larry Epstein</p></td>
+                    <td><p>Deputy Director</p></td>
+                </tr>
+				<tr>
+                    <td><p>Shannon Sanders</p></td>
+                    <td><p>Grants, Contracts, and Compliance Manager</p></td>
+                </tr>
+            </table>
 			
-		  <p>The Pro-Equity Anti-Racism (PEAR) page has moved and is now called PEAR Impact.             
-			<p>Please see our <a href="/PEAR-impact.php" target="new">new PEAR Impact page.</a>
-	    </div>
+			<h2>Staff Presentation Video - 8/18/2022</h2>
+			<p><img src="images/PEAR.jpg" width="299" height="171" alt="A screenshot of the Microsoft Teams presentation showing images of the attendees and speakers."/><br>
+            </p>
+			<p> <a href="https://pspwa.box.com/s/hxfglg57ny33e9qfvc8u5malexh1vr9d" target="new">Open the video recording of our staff presentation from 8/18/2022 in a new tab.</a><br>
 		</p>
-			<p class="last-update">Last updated: 06/26/26</p>
+			<p class="last-update">Last updated: 06/17/26</p>
 		
 			<!-- InstanceEndEditable --> </div>
 		
