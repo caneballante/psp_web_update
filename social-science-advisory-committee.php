7c27992a70350d3bb081132ab3ed6409cb22211f
ianmc3 <ian.mccabe@psp.wa.gov>
2026-07-02 14:59:25 -0700

    Create social-science-advisory-committee.php

---
+++ social-science-advisory-committee.php
@@ -0,0 +1,179 @@
+œ<!DOCTYPE html>
+<html lang="en"><!-- InstanceBegin template="/Templates/page-4-template.dwt" codeOutsideHTMLIsLocked="false" -->
+<head>
+
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-BGEZ5L3EJY"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-BGEZ5L3EJY');
+</script>
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<!-- InstanceBeginEditable name="doctitle" -->
+<title>Puget Sound Partnership - SOCIAL SCIENCES ADVISORY COMMITTEE (SSAC)</title>
+<!-- InstanceEndEditable -->
+<!-- Bootstrap -->
+<link href="css/bootstrap.css" rel="stylesheet">
+<link href="css/custom.css" rel="stylesheet" type="text/css">
+<link href="css/custom-erika.css" rel="stylesheet" type="text/css">
+<!-- loads the wf-loading class right away to minimize FOUT -->
+<script>document.documentElement.className += ' wf-loading';</script>
+<!-- Font PRENTON TYPEKIT -->
+<script src="https://use.typekit.net/srt5jze.js"></script>
+<script>try{Typekit.load({ async: true });}catch(e){}</script>
+<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+<!--[if lt IE 9]>
+	<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+	<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+<!-- InstanceBeginEditable name="head" -->
+<script>
+/*this variable is used to set the proper nav to active. It should to the order the nav item is in the list*/
+  	navSelected = 8;
+</script>
+<!-- InstanceEndEditable -->
+<!-- InstanceParam name="OptionalRegion1" type="boolean" value="true" -->
+<!-- InstanceParam name="OptionalRegion2" type="boolean" value="false" -->
+<!-- InstanceParam name="OptionalRegion3" type="boolean" value="true" -->
+<!-- InstanceParam name="OptionalRegion4" type="boolean" value="false" -->
+<!-- InstanceParam name="OptionalRegion5" type="boolean" value="true" -->
+<!-- InstanceParam name="OptionalRegion6" type="boolean" value="true" -->
+<!-- InstanceParam name="OptionalRegion7" type="boolean" value="false" -->
+<!-- InstanceParam name="OptionalRegion8" type="boolean" value="false" -->
+</head>
+<body>
+<?php include 'includes/modal-inc.html';?>
+<!-- START IMAGE HEADER --> 
+ <!-- InstanceBeginEditable name="overviewphoto" -->
+<header class="overview-page-image-style overview-page-image17"> </header>
+<!-- InstanceEndEditable -->  
+<div class="container-fluid page-content padding-50-bottom">
+	<div class="row">
+		<div class="col-sm-3 padding-20-top"></div>
+		<div class="col-sm-7 padding-20-top"> <!-- InstanceBeginEditable name="6col_header" -->
+			<h1 class="grey">SOCIAL SCIENCES ADVISORY COMMITTEE (SSAC)</h1>
+		<!-- InstanceEndEditable --></div>
+		<div class="col-sm-2 padding-20-top"></div>
+	</div>
+	
+	<div class="row"> 
+		<div class="col-sm-3">
+			<div class="nav-leftside-custom">
+				<ul class="nav nav-stacked nav-pills nav-leftside-custom padding-left-0 margin-10-top">
+					<!-- InstanceBeginEditable name="left_nav" -->
+				<?php include 'includes/ln-science-basis.html';?>
+				<!-- InstanceEndEditable -->
+				</ul>
+			</div>
+		</div>
+		 
+		<div class="col-sm-7 padding-20-top content-column"> <!-- InstanceBeginEditable name="6col_content" -->
+			<h2 class="margin-0-top">Overview</h2>
+			<h3>VISION</h3>
+			<p>We integrate social science research and human  dimensions into our planning, decision-making, and adaptive management. This  helps create more complete and effective recovery strategies that strengthen  the resilience of Puget Sound ecosystems and communities. </p>
+			<h3>PURPOSE</h3>
+			<p>The Puget Sound Social Sciences Advisory Committee (SSAC)  was established in 2010 by our Science Panel. The SSAC brings together social  scientists from multiple disciplines to advise us, including our Boards and  recovery partners, and provides guidance on social sciences, salmon and  ecosystem recovery, equity, environmental justice. and support our other goals. </p>
+            <p>Committee members include social scientists from academic,  government, and non-profit institutions across the Puget Sound and Salish Sea  region and beyond. </p>
+        <p>The SSAC meets regularly to: <br>
+            </p>
+            <ul class="bullet-size-fix">
+              <li>Build regional capacity in the social sciences</li>
+              <li>Discuss advancements in social science research  and monitoring </li>
+              <li>Review our materials</li>
+              <li>Guide priority social science for the region </li>
+              <li>Foster shared learning and collaboration among  members&rsquo; networks. </li>
+              <li>The SSAC also helps inform the work of the Puget  Sound Institute (PSI) in support of shared recovery efforts.
+                <div>
+                  <div></div>
+                </div>
+              </li>
+            </ul>
+		<h2>Objectives</h2>
+			<p>The SSAC:<strong><br>
+			</strong></p>
+            <ul class="bullet-size-fix">
+              <li>Promotes best available social science to  support Puget Sound and Salish Sea recovery goals, strategies, and products.</li>
+              <li>Encourages ecosystem recovery strategies that reflect  the needs and aspirations of the people and communities of the Puget Sound  region. </li>
+              <li>Supports collaboration across social science  disciplines and between the natural and social sciences.</li>
+              <li>Provides input on our or PSI calls for new  research.</li>
+              <li>Contributes social science priorities to our  Science Work Plan via the 2024 Social Sciences for the Puget Sound (PS4)  report.</li>
+              <li>Provides a forum to share and critique applied  regional social science research projects and papers.</li>
+              <li>Identifies and encourage opportunities for  social science contributions to our Science Panel and other boards.</li>
+              <li>Supports strong social science contributions in  the Salish Sea Ecosystem Conference. 
+       		  </li>
+            </ul>
+			<h2>Expertise Represented on the SSAC</h2>
+			<ul class="bullet-size-fix">
+			  <li>Archaeology </li>
+			  <li>Behavior change</li>
+			  <li>Conflict studies</li>
+			  <li>Conservation education</li>
+			  <li>Cultural/ecology</li>
+			  <li>Environmental &amp; maritime anthropology</li>
+			  <li>Environmental economics </li>
+			  <li>Environmental governance</li>
+			  <li>Environmental justice</li>
+			  <li>Environmental law</li>
+			  <li>Environmental policy</li>
+			  <li>Environmental psychology </li>
+			  <li>Environmental sociology</li>
+			  <li>Indigenous social science</li>
+			  <li>Human dimensions</li>
+			  <li>Human geography</li>
+			  <li>Human wellbeing</li>
+			  <li>Marine affairs</li>
+			  <li>Political ecology</li>
+			  <li>Public health </li>
+	    </ul>
+			<h2>Resources</h2>
+            <p><a href="https://pspwa.box.com/shared/static/iak8x19qh0qhem1qhsvqhmgcnmdg9m2g.pdf" target="new"><strong>Social  Science for the Salish Sea (S4)</strong></a><strong></strong><br>
+              The Social Science for the Salish  Sea (S4) project began in response to our need for stronger social science to  inform regional ecosystem recovery strategies.</p>
+            <p>Through a partnership with the  University of Washington&rsquo;s EarthLab, we brought together researchers and  practitioners from both sides of the U.S. and Canada to develop an  action-oriented social science research agenda. The agenda supports practical  ecosystem recovery across the Salish Sea, including British Columbia&rsquo;s Georgia  Basin and Washington State&rsquo;s Puget Sound.</p>
+            <p><strong>Special Issue for Coastal  Management (Volume 54, Issue 05)</strong><br>
+        Description and link to come.</p>
+            <p><strong>Puget Sound Social Science  Research Agenda (PS3) </strong>[eventually link]<br>
+              The Puget Sound Social Science  Research Agenda (PS3) identifies priority social science needs to for Puget  Sound ecosystem recovery. </p>
+            <p>PS3 updates the 2019 Social Science for the  Salish Sea report (S4) and promotes the production and use of social sciences  in Puget Sound recovery decisions. The agenda is intended for planners,  researchers, and funders, and supports improved social science integration and  decision-making across Puget Sound recovery projects.</p>
+            <p>&nbsp;</p>
+        <p class="last-update">Last updated: 07/02/26</p>
+			
+		<!-- InstanceEndEditable --> </div>
+		
+		<div class="col-sm-2 padding-20-top padding-0-right"> <!-- InstanceBeginRepeat name="right_nav_repeat" --><!-- InstanceBeginRepeatEntry --> <!-- InstanceBeginEditable name="right_nav_title" -->
+			<div class="right-nav-title margin-0-top ">DOWNLOADS</div>
+			<!-- InstanceEndEditable -->
+			<ul class="nav-rightside-custom">
+				<!-- InstanceBeginEditable name="right_nav_links" -->
+				<li role="presentation"><a href="https://pspwa.box.com/shared/static/iak8x19qh0qhem1qhsvqhmgcnmdg9m2g.pdf">S4 social science research agenda</a></li>
+				<li role="presentation"> <a href="https://pspwa.box.com/s/knxdmd0st71w4f3z3ggk7nfk76oqbx6d">Analysis of Organizations Engaged in Puget Sound Ecosystem Recovery</a></li>
+				<li role="presentation"> <a href="https://pspwa.box.com/s/45tqlfh4w6qidabkzlr21jy3y82xv0po" target="new">Sound Behavior Index reports</a></li>
+				<li role="presentation"> <a href="https://pspwa.box.com/s/uybht4wqvv0q04t6nltnsp9uyo9j6r3e" target="new">Public opion surveys</a></li>
+				<li role="presentation"> <a href="https://pspwa.box.com/s/w1galqayttesd0rkh8uhwavl0dt1cp0c" target="new">Human Dimensions Protocol</a></li>
+				
+				<!-- InstanceEndEditable -->
+			</ul>
+			<!-- InstanceEndRepeatEntry --><!-- InstanceEndRepeat --> </div>
+	</div>
+	<!--END OF ROW --> 
+</div>
+<!--END OF CONTENT CONTAINER -->
+
+<?php include 'includes/footer-inc.html';?>
+
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) --> 
+<script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+<!-- Include all compiled plugins (below), or include individual files as needed --> 
+<script src="js/bootstrap.js"></script> 
+<!-- custom js --> 
+<script src="js/custom.js"></script> 
+<!-- Google Tracking  -->
+
+
+</body>
+<!-- InstanceEnd --></html>
