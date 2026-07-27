2204be41fd6f0d3d797825bdbb54495a65be9f6f
ianmc3 <ian.mccabe@psp.wa.gov>
2026-07-27 16:43:22 -0700

    salmon in schools page

---
+++ salmon-in-schools.php
@@ -0,0 +1,141 @@
+<!DOCTYPE html>
+<html lang="en"><!-- InstanceBegin template="/Templates/page-4-template.dwt" codeOutsideHTMLIsLocked="false" -->
+<head>
+
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-BGEZ5L3EJY"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-BGEZ5L3EJY');
+</script>
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<!-- InstanceBeginEditable name="doctitle" -->
+<title>Puget Sound Salmon Recovery - Salmon in Schools Program</title>
+<!-- InstanceEndEditable -->
+<!-- Bootstrap -->
+<link href="css/bootstrap.css" rel="stylesheet">
+<link href="css/custom.css" rel="stylesheet" type="text/css">
+<link href="css/custom-erika.css" rel="stylesheet" type="text/css">
+<!-- loads the wf-loading class right away to minimize FOUT -->
+<script>document.documentElement.className += ' wf-loading';</script>
+<!-- Font PRENTON TYPEKIT -->
+<script src="https://use.typekit.net/srt5jze.js"></script>
+<script>try{Typekit.load({ async: true });}catch(e){}</script>
+<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+<!--[if lt IE 9]>
+	<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+	<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+<!-- InstanceBeginEditable name="head" -->
+<script>
+/*this variable is used to set the proper nav to active. It should to the order the nav item is in the list*/
+  	navSelected = 2;
+</script> 
+<!-- InstanceEndEditable -->
+<!-- InstanceParam name="OptionalRegion1" type="boolean" value="true" -->
+<!-- InstanceParam name="OptionalRegion2" type="boolean" value="false" -->
+<!-- InstanceParam name="OptionalRegion3" type="boolean" value="true" -->
+<!-- InstanceParam name="OptionalRegion4" type="boolean" value="false" -->
+<!-- InstanceParam name="OptionalRegion5" type="boolean" value="true" -->
+<!-- InstanceParam name="OptionalRegion6" type="boolean" value="true" -->
+<!-- InstanceParam name="OptionalRegion7" type="boolean" value="false" -->
+<!-- InstanceParam name="OptionalRegion8" type="boolean" value="false" -->
+</head>
+<body>
+<?php include 'includes/modal-inc.html';?>
+<!-- START IMAGE HEADER --> 
+ <!-- InstanceBeginEditable name="overviewphoto" -->
+<header class="overview-page-image-style overview-page-image109">
+
+<div class="overview-quote pull-right"></div>
+
+ </header>
+<!-- InstanceEndEditable -->  
+<div class="container-fluid page-content padding-50-bottom">
+	<div class="row">
+		<div class="col-sm-3 padding-20-top"></div>
+		<div class="col-sm-7 padding-20-top"> <!-- InstanceBeginEditable name="6col_header" -->
+			<h1>new release: salmon in schools</h1>
+		<!-- InstanceEndEditable --></div>
+		<div class="col-sm-2 padding-20-top"></div>
+	</div>
+	
+	<div class="row"> 
+		<div class="col-sm-3">
+			<div class="nav-leftside-custom">
+				<ul class="nav nav-stacked nav-pills nav-leftside-custom padding-left-0 margin-10-top">
+					<!-- InstanceBeginEditable name="left_nav" -->
+			<?php include ;?>	
+			<!-- InstanceEndEditable -->
+				</ul>
+			</div>
+		</div>
+		 
+		<div class="col-sm-7 padding-20-top content-column"> <!-- InstanceBeginEditable name="6col_content" -->
+			
+			
+		
+			<p>A new video features kids describing the <a href="https://wasalmonintheschools.org/" target="new">Salmon in the  Schools program,</a> an iconic environmental education opportunity for elementary  school students in Washington.</p>
+        <p align="center"><img src="/images/salmoninschools1.png" width="547" height="348"></p>
+            <p>What happens when a child helps a salmon grow up? We  followed third and fourth graders at 
+              two Washington schools to find out.</p>
+            <p>The Salmon in the Schools program brings salmon eggs to  classrooms, where students watch 
+              them hatch, grow into tiny fry, and eventually make the  journey back to local streams. Along the way, they&rsquo;re learning about more than  the salmon life cycle. They&rsquo;re building a connection to a species that has  shaped the region and to the responsibility they&rsquo;ll one day inherit to care for  the lands and waters those salmon call home.</p>
+            <p>From January through April 2026, the students allowed us in  on what they were learning. They told us about the role of salmon in the  ecosystem. They described the time they picked the winning fish during the  Mariners salmon run. They admitted that some (but not all) of their parents are  good at cooking salmon. They played salmon-themed games, wrote stories, and  decided that Fred was the best name for a fish.</p>
+            <p>After spending time with these students, one thing became  clear: they are the best storytellers for the Salmon in the Schools program.  So, we handed them the microphone, squeezed ourselves into chairs built for  8-year-olds, and let them explain in their own words what it means to raise a  salmon.</p>
+            <p>At a time when the future of programs like Salmon in the  Schools is uncertain, the students&rsquo; voices are a reminder of what&rsquo;s really at  stake. Today&rsquo;s classroom salmon become returning adults in a few years. And  today&rsquo;s curious kids become stewards of the rivers, forests, and communities  that depend on them. Raising these salmon is raising a generation that cares
+              enough to help them recover.</p>
+            <p>We hope you&rsquo;ll enjoy hearing their stories.</p>
+        <iframe title="vimeo-player" src="https://player.vimeo.com/video/1211272586?h=81faa5bc7e" width="640" height="360" frameborder="0" referrerpolicy="strict-origin-when-cross-origin" allow="autoplay; fullscreen; picture-in-picture; clipboard-write; encrypted-media; web-share"   allowfullscreen></iframe>
+           <p><a href="https://wasalmonintheschools.org/" target="new">Learn  more about Salmon in the Schools</a> and <a href="https://wasalmonintheschools.org/contact-us/" target="new">how to get involved</a>.</p>
+        <h2>Attend a screening</h2>
+           <ul>
+             <li>Film festival 1</li>
+             <li>Film festibal 2</li>
+             <li>Non-public screening</li>
+             <li>Boards meeting?</li>
+           </ul>
+        <div>
+          <div> </div>
+        </div>
+<p class="last-update">Last updated: 07/27/25</p>
+		
+
+		<!-- InstanceEndEditable --> </div>
+		
+		<div class="col-sm-2 padding-20-top padding-0-right"> <!-- InstanceBeginRepeat name="right_nav_repeat" --><!-- InstanceBeginRepeatEntry --> <!-- InstanceBeginEditable name="right_nav_title" -->
+					<div class="right-nav-title"></div>
+				<!-- InstanceEndEditable -->
+			<ul class="nav-rightside-custom">
+				<!-- InstanceBeginEditable name="right_nav_links" -->
+				
+				
+				
+				
+			  <!-- InstanceEndEditable -->
+			</ul>
+			<!-- InstanceEndRepeatEntry --><!-- InstanceEndRepeat --> </div>
+	</div>
+	<!--END OF ROW --> 
+</div>
+<!--END OF CONTENT CONTAINER -->
+
+<?php include 'includes/footer-inc.html';?>
+
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) --> 
+<script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+<!-- Include all compiled plugins (below), or include individual files as needed --> 
+<script src="js/bootstrap.js"></script> 
+<!-- custom js --> 
+<script src="js/custom.js"></script> 
+<!-- Google Tracking  -->
+
+
+</body>
+<!-- InstanceEnd --></html>
