fcdab35b514d353e0f9080e11d44cb74e1c82325
ianmc3 <ian.mccabe@psp.wa.gov>
2026-09-11 14:44:14 -0700

    Contact

--- contact.php
+++ contact.php
@@ -58,9 +58,8 @@
 	<div class="row">
 		<div class="col-sm-3 padding-20-top"></div>
 		<div class="col-sm-7 padding-20-top"> <!-- InstanceBeginEditable name="6col_header" -->
-			<h1>contacting the Partnership</h1>
-			<p>&nbsp;</p>
-			<!-- InstanceEndEditable --></div>
+			<h1>contacting the Partnership			</h1>
+		<!-- InstanceEndEditable --></div>
 		<div class="col-sm-2 padding-20-top"></div>
 	</div>
 	
